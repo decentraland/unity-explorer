6e34156d55d4316d5c68a93e0cba2ad4126f71dc
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2023-04-26 19:20:59 -0300

    refactor: optimize UpdateScene function, reducing the allocation per call from ~16kb to ~4kb

--- Explorer/Assets/Scripts/SceneRuntime/SceneRuntime.cs
+++ Explorer/Assets/Scripts/SceneRuntime/SceneRuntime.cs
@@ -16,8 +16,15 @@ public class SceneRuntime
 
     private readonly ScriptObject sceneCode;
 
+    private readonly ScriptObject updateFunc;
+    private readonly ScriptObject startFunc;
+
+    // ResetableSource is an optimization to reduce 11kb of memory allocation per Update (reduces 15kb to 4kb per update)
+    private readonly UniTaskResolverResetable resetableSource;
+
     public SceneRuntime(string sourceCode)
     {
+        resetableSource  = new UniTaskResolverResetable();
         moduleLoader = new SceneModuleLoader();
         engine = V8EngineFactory.Create();
 
@@ -30,11 +37,26 @@ public class SceneRuntime
         engine.AddHostObject("UnityOpsApi", unityOpsApi);
         engine.Execute(Helpers.LoadJavaScriptSourceCode("Js/Init.js"));
 
+        // Setup unitask resolver
+        engine.AddHostObject("__resetableSource", resetableSource);
+
         // Load and Compile Js Modules
         moduleLoader.LoadAndCompileJsModules(engine);
 
-        // Load the Scene Code
-        sceneCode = engine.Evaluate(@"require('~scene.js')") as ScriptObject;
+        engine.Execute(@"
+            const __internalScene = require('~scene.js')
+            const __internalOnUpdate = async function (dt) {
+                try {
+                    await __internalScene.onUpdate(dt)
+                    __resetableSource.Completed()
+                } catch(e) {
+                    __resetableSource.Reject(e)
+                }
+            }
+        ");
+
+        updateFunc = (ScriptObject)engine.Evaluate("__internalOnUpdate");
+        startFunc = (ScriptObject)engine.Evaluate("__internalScene.onStart");
     }
 
     public void RegisterEngineApi(IEngineApi api)
@@ -44,12 +66,13 @@ public class SceneRuntime
 
     public UniTask StartScene()
     {
-        return sceneCode.InvokeMethod("onStart").ToTask().AsUniTask();
+        return startFunc.InvokeAsFunction().ToTask().AsUniTask();
     }
 
     public UniTask UpdateScene(float dt)
     {
-        // TODO: Improve performance .ToTask() (alloc 11kb each call)
-        return sceneCode.InvokeMethod("onUpdate", dt).ToTask().AsUniTask();
+        resetableSource.Reset();
+        updateFunc.InvokeAsFunction(dt);
+        return resetableSource.Task;
     }
 }
