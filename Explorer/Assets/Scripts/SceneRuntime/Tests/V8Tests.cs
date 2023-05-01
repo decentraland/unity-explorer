02c00b5aee0d65e37d1c25ef2dc1a27cc1b64e9b
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2023-05-01 10:17:11 -0300

    add fail test

--- Explorer/Assets/Scripts/SceneRuntime/Tests/V8Tests.cs
+++ Explorer/Assets/Scripts/SceneRuntime/Tests/V8Tests.cs
@@ -13,6 +13,12 @@ using UnityEngine.TestTools;
 public class V8Tests
 {
     [Test]
+    public void FailTest()
+    {
+        Assert.Fail();
+    }
+
+    [Test]
     public void CallInvokeAsFunction()
     {
         var engine = V8EngineFactory.Create();
