088cbd54af67ad3fc9ef12c96473ef3e582b732d
Mikhail Agapov <mikhail.agapov@decentraland.org>
2023-05-10 16:58:06 +0200

    Replace individual bytes reading with a full structure reading

--- Explorer/Assets/Scripts/CrdtEcsBridge/Components/Transform/SDKTransformSerializer.cs
+++ Explorer/Assets/Scripts/CrdtEcsBridge/Components/Transform/SDKTransformSerializer.cs
@@ -1,6 +1,7 @@
 using CRDT;
 using CrdtEcsBridge.Serialization;
 using System;
+using UnityEngine;
 using Utility;
 
 namespace CrdtEcsBridge.Components.Transform
@@ -10,9 +11,9 @@ namespace CrdtEcsBridge.Components.Transform
         public void DeserializeInto(SDKTransform instance, in ReadOnlySpan<byte> data)
         {
             var pointer = data;
-            instance.Position.Set(pointer.Read<float>(), pointer.Read<float>(), pointer.Read<float>());
-            instance.Rotation.Set(pointer.Read<float>(), pointer.Read<float>(), pointer.Read<float>(), pointer.Read<float>());
-            instance.Scale.Set(pointer.Read<float>(), pointer.Read<float>(), pointer.Read<float>());
+            instance.Position = pointer.Read<Vector3>();
+            instance.Rotation = pointer.Read<Quaternion>();
+            instance.Scale = pointer.Read<Vector3>();
             instance.ParentId = pointer.Read<CRDTEntity>();
         }
     }
