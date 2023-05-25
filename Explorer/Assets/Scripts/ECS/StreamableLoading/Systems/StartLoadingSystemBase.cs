38b2657c8f787d2a665f306981e36dfd21d4904f
Mikhail Agapov <118179774+mikhail-dcl@users.noreply.github.com>
2023-05-25 11:16:47 +0300

    feat: Add a generic mechanism for Web Requests in ECS (#72)

---
+++ Explorer/Assets/Scripts/ECS/StreamableLoading/Systems/StartLoadingSystemBase.cs
@@ -0,0 +1,84 @@
+﻿using Arch.Core;
+using ECS.Abstract;
+using ECS.StreamableLoading.Components.Common;
+using UnityEngine.Networking;
+
+namespace ECS.StreamableLoading.Systems
+{
+    public abstract class StartLoadingSystemBase<TIntention, TAsset> : BaseUnityLoopSystem where TIntention: struct, ILoadingIntention
+    {
+        private delegate UnityWebRequest CreateDelegate(in TIntention intention);
+
+        private readonly QueryDescription createWebRequest = new QueryDescription()
+                                                            .WithAll<TIntention>()
+                                                            .WithNone<LoadingRequest>();
+
+        private readonly QueryDescription repeatWebRequest = new QueryDescription()
+                                                            .WithAll<TIntention, LoadingRequest>()
+                                                            .WithNone<StreamableLoadingResult<TAsset>>();
+
+        private CreateQuery createQuery;
+        private RepeatQuery repeatQuery;
+
+        protected StartLoadingSystemBase(World world) : base(world)
+        {
+            CreateDelegate createCached = CreateWebRequest;
+
+            createQuery = new CreateQuery(world, createCached);
+            repeatQuery = new RepeatQuery(createCached);
+        }
+
+        protected abstract UnityWebRequest CreateWebRequest(in TIntention intention);
+
+        protected override void Update(float t)
+        {
+            World.InlineEntityQuery<CreateQuery, TIntention>(in createWebRequest, ref createQuery);
+            World.InlineQuery<RepeatQuery, TIntention, LoadingRequest>(in repeatWebRequest, ref repeatQuery);
+        }
+
+        private static void CreateWebRequest(in CreateDelegate createDelegate, ref TIntention loadingIntention, ref LoadingRequest loadingRequest)
+        {
+            loadingRequest.WebRequest = createDelegate(in loadingIntention);
+            loadingRequest.WebRequest.SetCommonParameters(loadingIntention.CommonArguments);
+            loadingRequest.WebRequest.SendWebRequest();
+        }
+
+        private readonly struct CreateQuery : IForEachWithEntity<TIntention>
+        {
+            private readonly World world;
+            private readonly CreateDelegate create;
+
+            public CreateQuery(World world, CreateDelegate create)
+            {
+                this.world = world;
+                this.create = create;
+            }
+
+            public void Update(in Entity entity, ref TIntention intention)
+            {
+                var component = new LoadingRequest();
+                CreateWebRequest(in create, ref intention, ref component);
+                world.Add(entity, component);
+            }
+        }
+
+        private readonly struct RepeatQuery : IForEach<TIntention, LoadingRequest>
+        {
+            private readonly CreateDelegate create;
+
+            public RepeatQuery(CreateDelegate create)
+            {
+                this.create = create;
+            }
+
+            public void Update(ref TIntention intention, ref LoadingRequest loadingRequest)
+            {
+                // it's a tradeoff to not delete the component
+                if (loadingRequest.WebRequest is { isDone: false })
+                    return;
+
+                CreateWebRequest(in create, ref intention, ref loadingRequest);
+            }
+        }
+    }
+}
---
+++ Explorer/Assets/Scripts/SceneRunner/Scene/ISceneContentProvider.cs
@@ -0,0 +1,11 @@
+﻿namespace SceneRunner.Scene
+{
+    public interface ISceneContentProvider
+    {
+        /// <summary>
+        ///     Translates URL encoded in SDK components into a path in the scene bundle
+        ///     from which an asset can be downloaded from
+        /// </summary>
+        bool TryGetContentUrl(string url, out string result);
+    }
+}
