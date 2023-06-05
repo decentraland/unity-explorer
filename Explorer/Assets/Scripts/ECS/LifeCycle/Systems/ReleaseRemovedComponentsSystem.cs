bc903e13416fb6aa339c41df85185252f5501646
Juan Molteni <juanignaciomolteni@gmail.com>
2023-06-05 11:31:15 -0300

    Added partial

--- Explorer/Assets/Scripts/ECS/LifeCycle/Systems/ReleaseRemovedComponentsSystem.cs
+++ Explorer/Assets/Scripts/ECS/LifeCycle/Systems/ReleaseRemovedComponentsSystem.cs
@@ -2,19 +2,21 @@ using Arch.Core;
 using Arch.SystemGroups;
 using ECS.Abstract;
 using ECS.Groups;
-using ECS.LifeCycle;
 using ECS.LifeCycle.Components;
 
-[UpdateInGroup(typeof(CleanUpGroup))]
-public class ReleaseRemovedComponentsSystem : BaseUnityLoopSystem, IFinalizeWorldSystem
+namespace ECS.LifeCycle.Systems
 {
-    public ReleaseRemovedComponentsSystem(World world) : base(world) { }
-
-    protected override void Update(float t) { }
-
-    public void FinalizeComponents(in Query query)
+    [UpdateInGroup(typeof(CleanUpGroup))]
+    public partial class ReleaseRemovedComponentsSystem : BaseUnityLoopSystem, IFinalizeWorldSystem
     {
-        World.Query(in new QueryDescription().WithAll<RemovedComponents>(),
-            (ref RemovedComponents removedComponents) => removedComponents.Dispose());
+        public ReleaseRemovedComponentsSystem(World world) : base(world) { }
+
+        protected override void Update(float t) { }
+
+        public void FinalizeComponents(in Query query)
+        {
+            World.Query(in new QueryDescription().WithAll<RemovedComponents>(),
+                (ref RemovedComponents removedComponents) => removedComponents.Dispose());
+        }
     }
 }
