5bdc6ac24e73ffb8d4a4f72e206b61658b8589dd
Juan Molteni <juanignaciomolteni@gmail.com>
2023-07-25 17:06:44 -0300

    Added CapFrameTimeBudget test

---
+++ Explorer/Assets/Scripts/SceneRunner/Tests/CapFrameTimeBudgetProviderShould.cs
@@ -0,0 +1,36 @@
+using Cysharp.Threading.Tasks;
+using ECS.Profiling;
+using ECS.StreamableLoading.DeferredLoading.BudgetProvider;
+using NUnit.Framework;
+using System.Collections;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace ECS.StreamableLoading.DeferredLoading.Tests
+{
+    [TestFixture]
+    public class CapFrameTimeBudgetProviderShould
+    {
+        [UnityTest]
+        public IEnumerator SpendBudget()
+        {
+            ProfilingProvider provider = new ProfilingProvider();
+
+            // Arrange
+            var budgetProvider = new FrameTimeCapBudgetProvider(0.5, provider);
+
+            yield return UniTask.Yield();
+
+            // Assert
+            Assert.AreEqual(true, budgetProvider.TrySpendBudget());
+
+
+            for(int i=0;i<1000;i++)
+                GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+            Assert.AreEqual(false, budgetProvider.TrySpendBudget());
+        }
+
+    }
+}
