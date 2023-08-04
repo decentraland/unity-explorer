fd3ae1d86f699efa0a86c87a8da7b7f0ba8a8b1c
Mateo Kuruk Miccino <mateomiccino@gmail.com>
2023-08-03 21:04:32 -0300

    feat: add modules mocks

---
+++ Explorer/Assets/StreamingAssets/Js/Modules/PortableExperiences.js
@@ -0,0 +1,19 @@
+module.exports.spawn = async function(message) {
+    console.log('JSMODULE: spawn')
+    return {};
+}
+
+module.exports.kill = async function(message) {
+    console.log('JSMODULE: kill')
+    return {};
+}
+
+module.exports.exit = async function(message) {
+    console.log('JSMODULE: exit')
+    return {};
+}
+
+module.exports.getPortableExperiencesLoaded = async function(message) {
+    console.log('JSMODULE: getPortableExperiencesLoaded')
+    return {};
+}
---
+++ Explorer/Assets/StreamingAssets/Js/Modules/Testing.js
@@ -0,0 +1,14 @@
+module.exports.logTestResult = async function(message) {
+    console.log('JSMODULE: logTestResult')
+    return {};
+}
+
+module.exports.plan = async function(message) {
+    console.log('JSMODULE: plan')
+    return {};
+}
+
+module.exports.setCameraTransform = async function(message) {
+    console.log('JSMODULE: setCameraTransform')
+    return {};
+}
