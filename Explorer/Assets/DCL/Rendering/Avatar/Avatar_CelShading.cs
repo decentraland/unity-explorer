a459d0c4d7b4988052ee1d635675f6a07c621856
Geoff Birch <geoff.birch@decentraland.org>
2023-08-22 15:53:41 +0100

    AvatarV2 - Initial Shader testing for batching and instancing

---
+++ Explorer/Assets/DCL/Rendering/Avatar/Avatar_CelShading.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Avatar_CelShading : MonoBehaviour
+{
+    private MaterialPropertyBlock _propertyBlock;
+
+    [SerializeField]
+    Color col_MatPropBlock = Color.red;
+    [SerializeField]
+    int nTextureArrayID = 0;
+
+    private void Awake()
+    {
+        // Make sure to initialize it.
+        // Usually it needs to be initialized only once.
+        _propertyBlock = new MaterialPropertyBlock();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void OnEnable()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetColor("_BaseColor", col_MatPropBlock);
+        SetTextureArrayID("_TextureArrayID", nTextureArrayID);
+    }
+
+    private void SetColor(string colorPropertyName, Color color)
+    {
+        this.GetComponent<Renderer>().GetPropertyBlock(_propertyBlock); // Get previously set values. They will reset otherwise
+        _propertyBlock.SetColor(colorPropertyName, color);
+        this.GetComponent<Renderer>().SetPropertyBlock(_propertyBlock);
+    }
+
+    private void SetTextureArrayID(string colorPropertyName, int _nTextureArrayID)
+    {
+        this.GetComponent<Renderer>().GetPropertyBlock(_propertyBlock); // Get previously set values. They will reset otherwise
+        _propertyBlock.SetInt(colorPropertyName, _nTextureArrayID);
+        this.GetComponent<Renderer>().SetPropertyBlock(_propertyBlock);
+    }
+}
