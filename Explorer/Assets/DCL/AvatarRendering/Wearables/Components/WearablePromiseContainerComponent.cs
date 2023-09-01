f76803ddad348e34cda1edea909a2b8a90796248
Juan Molteni <juanignaciomolteni@gmail.com>
2023-09-01 17:40:48 -0300

    Added TODO

--- Explorer/Assets/DCL/AvatarRendering/Wearables/Components/WearablePromiseContainerComponent.cs
+++ Explorer/Assets/DCL/AvatarRendering/Wearables/Components/WearablePromiseContainerComponent.cs
@@ -5,6 +5,7 @@ namespace DCL.AvatarRendering.Wearables.Components
 {
     public struct WearablePromiseContainerComponent
     {
+        //TODO: How can I make both intentions inherent from the same interface to avoid the duplication?
         public AssetPromise<WearableDTO[], GetWearableByParamIntention> WearableRequestPromise;
 
         public AssetPromise<WearableDTO[], GetWearableByPointersIntention> WearableByPointerRequestPromise;
