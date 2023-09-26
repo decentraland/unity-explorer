db1061c43db20d71f99a06ba2961c45277bd5fe1
Juan Molteni <juanignaciomolteni@gmail.com>
2023-09-26 12:02:26 -0300

    Changed to mecanim for a performance boost

--- Explorer/Assets/DCL/AvatarRendering/AvatarShape/DemoScripts/RandomAnimationPlayer.cs
+++ Explorer/Assets/DCL/AvatarRendering/AvatarShape/DemoScripts/RandomAnimationPlayer.cs
@@ -1,12 +1,19 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RandomAnimationPlayer : MonoBehaviour
 {
-    public List<AnimationClip> AnimationClips;
+    private AnimationClip[] clips;
+    private Animator animator;
 
     private void Start()
     {
-        GetComponent<Animation>().Play(AnimationClips[Random.Range(0, AnimationClips.Count)].name);
+        // Get the animator component
+        animator = GetComponent<Animator>();
+
+        // Get all available clips
+        clips = animator.runtimeAnimatorController.animationClips;
+
+        animator.Play(clips[Random.Range(0, clips.Length)].name);
     }
+
 }
