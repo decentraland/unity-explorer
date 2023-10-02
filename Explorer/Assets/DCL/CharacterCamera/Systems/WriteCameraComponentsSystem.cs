184c51bf4048516c25ee837269218ad63af12009
Mikhail Agapov <mikhail.agapov@decentraland.org>
2023-10-02 22:05:38 +0200

    Comment LWW Components propagation

--- Explorer/Assets/DCL/CharacterCamera/Systems/WriteCameraComponentsSystem.cs
+++ Explorer/Assets/DCL/CharacterCamera/Systems/WriteCameraComponentsSystem.cs
@@ -1,6 +1,5 @@
 ﻿using Arch.Core;
 using Arch.SystemGroups;
-using CrdtEcsBridge.Components;
 using CrdtEcsBridge.Components.Transform;
 using CrdtEcsBridge.ECSToCRDTWriter;
 using DCL.ECSComponents;
@@ -49,9 +48,11 @@ namespace DCL.CharacterCamera.Systems
             SHARED_TRANSFORM.Rotation = exposedCameraData.WorldRotation;
 
             // TODO Access to CRDT LWW components is not synchronized, bi-directional access causes concurrency exceptions
-            // ecsToCrdtWriter.PutMessage(SpecialEntititiesID.CAMERA_ENTITY, SHARED_TRANSFORM);
-            ecsToCrdtWriter.PutMessage(SpecialEntitiesID.CAMERA_ENTITY, SHARED_CAMERA_MODE);
-            ecsToCrdtWriter.PutMessage(SpecialEntitiesID.CAMERA_ENTITY, SHARED_POINTER_LOCK);
+            // TODO Poor performance, should jobified
+            // TODO Uncommenting causes random APPEND/PUT [silent] failures
+            // ecsToCrdtWriter.PutMessage(SpecialEntitiesID.CAMERA_ENTITY, SHARED_TRANSFORM);
+            //ecsToCrdtWriter.PutMessage(SpecialEntitiesID.CAMERA_ENTITY, SHARED_CAMERA_MODE);
+            //ecsToCrdtWriter.PutMessage(SpecialEntitiesID.CAMERA_ENTITY, SHARED_POINTER_LOCK);
         }
     }
 }
