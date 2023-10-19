a0c335fc9d4a596f282146cbdc8e5ddae8ff7c84
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2023-10-19 14:32:03 -0300

    fix: Remove [RequirePlayMode] tag (#148)

--- Explorer/Assets/DCL/AvatarRendering/Wearables/AssemblyInfo.cs
+++ Explorer/Assets/DCL/AvatarRendering/Wearables/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 ﻿using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo("DCL.Editor")]
+[assembly: InternalsVisibleTo("DCL.PlayMode.Tests")]
 [assembly: InternalsVisibleTo("DCL.Tests")]
+
--- Explorer/Assets/DCL/PluginSystem/AssemblyInfo.cs
+++ Explorer/Assets/DCL/PluginSystem/AssemblyInfo.cs
@@ -2,3 +2,5 @@
 
 [assembly: InternalsVisibleTo("DCL.Editor")]
 [assembly: InternalsVisibleTo("DCL.Tests")]
+[assembly: InternalsVisibleTo("DCL.PlayMode.Tests")]
+
