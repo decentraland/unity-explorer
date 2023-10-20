ecc7ff7e110a548751550aea8a83c25f93ef5f95
Nicolas Lorusso <nalorusso@gmail.com>
2023-10-20 17:17:41 -0300

    add category & scene tags

--- Explorer/Assets/Scripts/Diagnostics/ReportsHandling/Sentry/SentryReportHandler.cs
+++ Explorer/Assets/Scripts/Diagnostics/ReportsHandling/Sentry/SentryReportHandler.cs
@@ -12,13 +12,13 @@ namespace Diagnostics.ReportsHandling.Sentry
 
         internal override void LogInternal(LogType logType, ReportData category, Object context, object message)
         {
-            SentrySdk.CaptureMessage(message.ToString(), ToSentryLevel(in logType));
+            SentrySdk.CaptureMessage(message.ToString(), scope => AddReportData(scope, in category), ToSentryLevel(in logType));
         }
 
         internal override void LogFormatInternal(LogType logType, ReportData category, Object context, object message, params object[] args)
         {
             var format = string.Format(message.ToString(), args);
-            SentrySdk.CaptureMessage(format, ToSentryLevel(in logType));
+            SentrySdk.CaptureMessage(format, scope => AddReportData(scope, in category), ToSentryLevel(in logType));
         }
 
         internal override void LogExceptionInternal<T>(T ecsSystemException)
@@ -28,7 +28,23 @@ namespace Diagnostics.ReportsHandling.Sentry
 
         internal override void LogExceptionInternal(Exception exception, ReportData reportData, Object context)
         {
-            SentrySdk.CaptureException(exception);
+            SentrySdk.CaptureException(exception, scope => AddReportData(scope, in reportData));
+        }
+
+        private void AddReportData(Scope scope, in ReportData data)
+        {
+            AddCategoryTag(scope, in data);
+            AddSceneInfo(scope, in data);
+        }
+
+        private void AddCategoryTag(Scope scope, in ReportData data) =>
+            scope.SetTag("category", data.Category);
+
+        private void AddSceneInfo(Scope scope, in ReportData data)
+        {
+            if (data.SceneShortInfo.BaseParcel == Vector2Int.zero) return;
+            scope.SetTag("scene.base_parcel", data.SceneShortInfo.BaseParcel.ToString());
+            scope.SetTag("scene.name", data.SceneShortInfo.Name);
         }
 
         private SentryLevel ToSentryLevel(in LogType logType)
