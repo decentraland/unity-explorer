4e2f9e10839bb56a6d293788a52b418918a0e5d5
Mikhail Agapov <mikhail.agapov@decentraland.org>
2023-11-08 13:18:15 +0300

    Provide Budget via an extension

---
+++ Explorer/Assets/DCL/PerformanceBudgeting/BudgetProviderExtensions.cs
@@ -0,0 +1,19 @@
+﻿using DCL.PerformanceBudgeting.AcquiredBudget;
+
+namespace DCL.PerformanceBudgeting
+{
+    public static class BudgetProviderExtensions
+    {
+        public static bool TrySpendBudget(this IConcurrentBudgetProvider budgetProvider, out IAcquiredBudget acquiredBudget)
+        {
+            if (budgetProvider.TrySpendBudget())
+            {
+                acquiredBudget = AcquiredBudget.AcquiredBudget.Create(budgetProvider);
+                return true;
+            }
+
+            acquiredBudget = null;
+            return false;
+        }
+    }
+}
