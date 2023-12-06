8e1b741a412cae6d47cbe249dfb487436bd63adc
Nicolas Lorusso <nalorusso@gmail.com>
2023-12-06 16:38:38 -0300

    added verify note

--- Explorer/Assets/DCL/Web3Authentication/IWeb3Identity.cs
+++ Explorer/Assets/DCL/Web3Authentication/IWeb3Identity.cs
@@ -6,6 +6,12 @@ namespace DCL.Web3Authentication
 
         string Sign(string message);
 
+        /// <summary>
+        ///     Check the message has been signed by this identity's address
+        /// </summary>
+        /// <param name="message">The original non-signed message</param>
+        /// <param name="signature">The signed message generated from original message</param>
+        /// <returns></returns>
         bool Verify(string message, string signature);
     }
 }
