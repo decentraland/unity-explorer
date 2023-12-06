59ee3e5bf385739529321227256b8c1b73bb59b2
Patricio Guerra <kinerius@gmail.com>
2023-12-06 17:07:52 -0300

    Added basic Noise generation with a neat Editor

---
+++ Explorer/Assets/DCL/Landscape/Jobs/NoiseJob.cs
@@ -0,0 +1,53 @@
+﻿using DCL.Landscape.Config;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace DCL.Landscape.Jobs
+{
+    [BurstCompile(CompileSynchronously = true)]
+    public struct NoiseJob : IJobParallelFor
+    {
+        public NativeArray<float> Result;
+        [ReadOnly] public NativeArray<float2> OctaveOffsets;
+        [ReadOnly] public int Width;
+        [ReadOnly] public int Height;
+        [ReadOnly] public NoiseSettings NoiseSettings;
+        [ReadOnly] public float MaxHeight;
+
+        public void Execute(int index)
+        {
+            int x = index % Width;
+            int y = index / Width;
+
+            float halfWidth = Width / 2f;
+            float halfHeight = Height / 2f;
+
+            float amplitude = 1;
+            float frequency = 1;
+            float noiseHeight = 0;
+
+            for (var i = 0; i < NoiseSettings.octaves; i++)
+            {
+                float sampleX = (x - halfWidth + OctaveOffsets[i].x) / NoiseSettings.scale * frequency;
+                float sampleY = (y - halfHeight + OctaveOffsets[i].y) / NoiseSettings.scale * frequency;
+
+                float perlinValue = (Mathf.PerlinNoise(sampleX, sampleY) * 2) - 1;
+                noiseHeight += perlinValue * amplitude;
+
+                amplitude *= NoiseSettings.persistance;
+                frequency *= NoiseSettings.lacunarity;
+            }
+
+            Result[index] = NoiseSettings.invert ? -noiseHeight : noiseHeight;
+
+            if (NoiseSettings.normalize)
+            {
+                float normalizedHeight = (Result[index] + 1) / (MaxHeight / 0.9f);
+                Result[index] = Mathf.Clamp(normalizedHeight, 0, int.MaxValue);
+            }
+        }
+    }
+}
---
+++ Explorer/Assets/DCL/PluginSystem/Global/LandscapePlugin.cs
@@ -0,0 +1,39 @@
+﻿using Arch.SystemGroups;
+using Cysharp.Threading.Tasks;
+using DCL.AssetsProvision;
+using DCL.Landscape.Settings;
+using System.Threading;
+
+namespace DCL.PluginSystem.Global
+{
+    public class LandscapePlugin : IDCLGlobalPlugin<LandscapeSettings>
+    {
+        private readonly IAssetsProvisioner assetsProvisioner;
+        private ProvidedAsset<LandscapeData> landscapeData;
+
+        public LandscapePlugin(IAssetsProvisioner assetsProvisioner)
+        {
+            this.assetsProvisioner = assetsProvisioner;
+        }
+
+        public async UniTask InitializeAsync(LandscapeSettings settings, CancellationToken ct)
+        {
+            landscapeData = await assetsProvisioner.ProvideMainAssetAsync(settings.landscapeData, ct);
+        }
+
+        public void InjectToWorld(ref ArchSystemsWorldBuilder<Arch.Core.World> builder, in GlobalPluginArguments arguments)
+        {
+            // Do we need the actual world or we can use a parallel one?
+            Arch.Core.World world = builder.World;
+
+            // add required entities
+
+            // add reqiored systems
+        }
+
+        public void Dispose()
+        {
+            landscapeData.Dispose();
+        }
+    }
+}
