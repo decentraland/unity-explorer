100e65e1c685b5614065ea0a69b8df9d58129154
nickkhalow <nickhalow@gmail.com>
2023-12-11 19:00:14 +0200

    tests

--- Explorer/Assets/DCL/Billboard/Tests/BillboardTest.cs
+++ Explorer/Assets/DCL/Billboard/Tests/BillboardTest.cs
@@ -5,6 +5,7 @@ using DCL.ECSComponents;
 using ECS.Unity.Transforms.Components;
 using NUnit.Framework;
 using UnityEngine;
+using CameraType = DCL.ECSComponents.CameraType;
 
 namespace DCL.Billboard.Tests
 {
@@ -13,20 +14,87 @@ namespace DCL.Billboard.Tests
         [Test]
         public void NoRotation()
         {
+            (Transform transform, BillboardSystem system) = Construct(BillboardMode.BmNone);
+
+            var expected = transform.rotation;
+            system.Update(0);
+            Assert.AreEqual(expected, transform.rotation);
+        }
+
+        [Test]
+        public void XRotation()
+        {
+            (Transform transform, BillboardSystem system) = Construct(BillboardMode.BmX);
+
+            var expected = transform.rotation.eulerAngles;
+            system.Update(0);
+            var actual = transform.rotation.eulerAngles;
+            Assert.AreNotEqual(expected.x, actual.x);
+            Assert.AreEqual(expected.y, actual.y);
+            Assert.AreEqual(expected.z, actual.z);
+        }
+
+        [Test]
+        public void YRotation()
+        {
+            (Transform transform, BillboardSystem system) = Construct(BillboardMode.BmY);
+
+            var expected = transform.rotation.eulerAngles;
+            system.Update(0);
+            var actual = transform.rotation.eulerAngles;
+            Assert.AreNotEqual(expected.y, actual.y);
+            Assert.AreEqual(expected.x, actual.x);
+            Assert.AreEqual(expected.z, actual.z);
+        }
+
+        [Test]
+        public void ZRotation()
+        {
+            (Transform transform, BillboardSystem system) = Construct(BillboardMode.BmZ);
+
+            var expected = transform.rotation.eulerAngles;
+            system.Update(0);
+            var actual = transform.rotation.eulerAngles;
+            Assert.AreNotEqual(expected.z, actual.z);
+            Assert.AreEqual(expected.x, actual.x);
+            Assert.AreEqual(expected.y, actual.y);
+        }
+
+        [Test]
+        public void AllRotation()
+        {
+            (Transform transform, BillboardSystem system) = Construct(BillboardMode.BmAll);
+
+            var expected = transform.rotation.eulerAngles;
+            system.Update(0);
+            var actual = transform.rotation.eulerAngles;
+            Assert.AreNotEqual(expected.x, actual.x);
+            Assert.AreNotEqual(expected.y, actual.y);
+            Assert.AreNotEqual(expected.z, actual.z);
+        }
+
+        private static (Transform transform, BillboardSystem system) Construct(BillboardMode mode)
+        {
             var world = World.Create();
-            var system = new BillboardSystem(world, new IExposedCameraData.Random());
+
+            var system = new BillboardSystem(
+                world,
+                new IExposedCameraData.Fake(
+                    Vector3.one,
+                    Quaternion.Euler(Vector3.one),
+                    CameraType.CtFirstPerson,
+                    false
+                )
+            );
+
             var transform = new GameObject().transform;
 
             world.Create(
-                new PBBillboard { BillboardMode = BillboardMode.BmNone },
+                new PBBillboard { BillboardMode = mode },
                 new TransformComponent(transform)
             );
 
-            var expected = transform.rotation;
-
-            system.Update(0);
-
-            Assert.AreEqual(expected, transform.rotation);
+            return (transform, system);
         }
     }
 }
