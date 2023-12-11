1ee0045d0972bd9d75b935d2c8f889eff4c5c7ec
Mateo Miccino <mateomiccino@gmail.com>
2023-12-11 11:40:57 -0300

    feat: auth chain signing (#122)

--- Explorer/Assets/DCL/WebRequests/Analytics/Plugin/WebRequestsContainer.cs
+++ Explorer/Assets/DCL/WebRequests/Analytics/Plugin/WebRequestsContainer.cs
@@ -1,4 +1,6 @@
-﻿namespace DCL.WebRequests.Analytics
+﻿using DCL.Web3Authentication;
+
+namespace DCL.WebRequests.Analytics
 {
     public class WebRequestsContainer
     {
@@ -6,10 +8,10 @@
 
         public IWebRequestsAnalyticsContainer AnalyticsContainer { get; private set; }
 
-        public static WebRequestsContainer Create()
+        public static WebRequestsContainer Create(IWeb3Authenticator web3Authenticator)
         {
             var analyticsContainer = new WebRequestsAnalyticsContainer();
-            var webRequestController = new WebRequestController(analyticsContainer);
+            var webRequestController = new WebRequestController(analyticsContainer, web3Authenticator);
             return new WebRequestsContainer { WebRequestController = webRequestController, AnalyticsContainer = analyticsContainer };
         }
     }
