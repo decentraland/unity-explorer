fc134210cabeaa37c5345a77a99ebfc344c1ed2f
Vitaly Popuzin <popuzin@gmail.com>
2023-12-20 11:40:06 +0100

    added playmode asembly for AudioSources

---
+++ Explorer/Assets/DCL/SDKComponents/AudioSources/Tests/PlayMode/SDKAudioSourcesAutomatedTests.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine.TestTools;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using NUnit.Framework;
+using UnityEngine;
+
+namespace DCL.SDKComponents.AudioSources.Tests.PlayMode
+{
+    public class SDKAudioSourcesAutomatedTests
+    {
+        private string testScene = "Assets/Scenes/StaticSceneLoader.unity";
+
+        [UnityTest]
+        public IEnumerator VV()
+        {
+            // Load the scene asynchronously
+            AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(testScene, LoadSceneMode.Single);
+
+            // Wait until the scene is loaded
+            while (!asyncLoad.isDone)
+                yield return null;
+
+            yield return new WaitForSeconds(10000);
+
+            // Pass the test if this point is reached
+            Assert.IsTrue(true);
+        }
+    }
+}
