cf1bc2416094b657ce2c18f5adbb40df82d80d5e
Patricio Guerra <kinerius@gmail.com>
2023-12-21 14:40:51 -0300

    Added debug settings for Landscapes through another plugin

---
+++ Explorer/Assets/DCL/PluginSystem/Global/LandscapeDebugSettings.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace DCL.PluginSystem.Global
+{
+    public class LandscapeDebugSettings : IDCLPluginSettings
+    {
+        [field: Header(nameof(LandscapeSettings))] [field: Space]
+        [field: SerializeField]
+        internal StaticSettings.RealmPartitionSettingsRef realmPartitionSettings { get; }
+    }
+}
