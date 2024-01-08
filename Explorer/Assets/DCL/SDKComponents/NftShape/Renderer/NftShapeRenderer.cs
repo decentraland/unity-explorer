a20fce4629886031562bea3f7f88ec2cef8bb272
nickkhalow <nickhalow@gmail.com>
2024-01-08 11:16:21 +0200

    NftShapeRenderer base

---
+++ Explorer/Assets/DCL/SDKComponents/NftShape/Renderer/NftShapeRenderer.cs
@@ -0,0 +1,24 @@
+using DCL.ECSComponents;
+using System;
+
+namespace DCL.SDKComponents.NftShape.Renderer
+{
+    //TODO implement this class
+    public class NftShapeRenderer : INftShapeRenderer
+    {
+        public void Apply(PBNftShape nftShape)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Hide()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Show()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
