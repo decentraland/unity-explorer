c3bbd4a55b40db03ab24f0effa1ef871282559cf
nickkhalow <nickhalow@gmail.com>
2024-01-11 00:05:02 +0200

    NftShapePlugin RegisterReleasePoolableComponentSystem

--- Explorer/Assets/DCL/PluginSystem/World/NftShapePlugin.cs
+++ Explorer/Assets/DCL/PluginSystem/World/NftShapePlugin.cs
@@ -3,6 +3,8 @@ using Cysharp.Threading.Tasks;
 using DCL.Optimization.PerformanceBudgeting;
 using DCL.Optimization.Pools;
 using DCL.PluginSystem.World.Dependencies;
+using DCL.SDKComponents.NftShape.Component;
+using DCL.SDKComponents.NftShape.Renderer;
 using DCL.SDKComponents.NftShape.Renderer.Factory;
 using DCL.SDKComponents.NftShape.System;
 using ECS.LifeCycle;
@@ -15,16 +17,19 @@ namespace DCL.PluginSystem.World
     {
         private readonly INftShapeRendererFactory nftShapeRendererFactory;
         private readonly IPerformanceBudget instantiationFrameTimeBudgetProvider;
+        private readonly IComponentPoolsRegistry componentPoolsRegistry;
 
         public NftShapePlugin(IPerformanceBudget instantiationFrameTimeBudgetProvider, IComponentPoolsRegistry componentPoolsRegistry) : this(
             new PoolNftShapeRendererFactory(componentPoolsRegistry),
-            instantiationFrameTimeBudgetProvider
+            instantiationFrameTimeBudgetProvider,
+            componentPoolsRegistry
         ) { }
 
-        public NftShapePlugin(INftShapeRendererFactory nftShapeRendererFactory, IPerformanceBudget instantiationFrameTimeBudgetProvider)
+        public NftShapePlugin(INftShapeRendererFactory nftShapeRendererFactory, IPerformanceBudget instantiationFrameTimeBudgetProvider, IComponentPoolsRegistry componentPoolsRegistry)
         {
             this.nftShapeRendererFactory = nftShapeRendererFactory;
             this.instantiationFrameTimeBudgetProvider = instantiationFrameTimeBudgetProvider;
+            this.componentPoolsRegistry = componentPoolsRegistry;
         }
 
         public void Dispose()
@@ -35,8 +40,11 @@ namespace DCL.PluginSystem.World
         public UniTask Initialize(IPluginSettingsContainer container, CancellationToken ct) =>
             UniTask.CompletedTask;
 
-        public void InjectToWorld(ref ArchSystemsWorldBuilder<Arch.Core.World> builder, in ECSWorldInstanceSharedDependencies sharedDependencies, in PersistentEntities persistentEntities, List<IFinalizeWorldSystem> finalizeWorldSystems) =>
+        public void InjectToWorld(ref ArchSystemsWorldBuilder<Arch.Core.World> builder, in ECSWorldInstanceSharedDependencies sharedDependencies, in PersistentEntities persistentEntities, List<IFinalizeWorldSystem> finalizeWorldSystems)
+        {
             Inject(ref builder);
+            finalizeWorldSystems.RegisterReleasePoolableComponentSystem<INftShapeRenderer, NftShapeRendererComponent>(ref builder, componentPoolsRegistry);
+        }
 
         public void InjectToEmptySceneWorld(ref ArchSystemsWorldBuilder<Arch.Core.World> builder, in EmptyScenesWorldSharedDependencies dependencies) =>
             Inject(ref builder);
