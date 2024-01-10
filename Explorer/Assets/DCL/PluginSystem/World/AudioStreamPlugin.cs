46359cdb843442bffa344e41c329688154fcb44e
Vitaly Popuzin <popuzin@gmail.com>
2024-01-10 13:59:20 +0100

    properly returning MediaPlayer to the pool

--- Explorer/Assets/DCL/PluginSystem/World/AudioStreamPlugin.cs
+++ Explorer/Assets/DCL/PluginSystem/World/AudioStreamPlugin.cs
@@ -1,7 +1,9 @@
 ﻿using Arch.SystemGroups;
 using DCL.Optimization.Pools;
 using DCL.PluginSystem.World.Dependencies;
+using ECS.ComponentsPooling.Systems;
 using ECS.LifeCycle;
+using ECS.Unity.AudioStreams.Components;
 using ECS.Unity.AudioStreams.Systems;
 using RenderHeads.Media.AVProVideo;
 using System.Collections.Generic;
@@ -15,9 +17,16 @@ namespace DCL.PluginSystem.World
         public AudioStreamPlugin(ECSWorldSingletonSharedDependencies sharedDependencies)
         {
             componentPoolsRegistry = sharedDependencies.ComponentPoolsRegistry;
-            componentPoolsRegistry.AddGameObjectPool<MediaPlayer>(); // componentPoolsRegistry.AddGameObjectPool<MediaPlayer>(HandleCreation);
+            componentPoolsRegistry.AddGameObjectPool<MediaPlayer>(onRelease: OnRelease); // componentPoolsRegistry.AddGameObjectPool<MediaPlayer>(HandleCreation);
 
             // cacheCleaner.Register(componentPoolsRegistry.GetReferenceTypePool<AudioSource>());
+        }
+
+        private static void OnRelease(MediaPlayer mediaPlayer)
+        {
+            mediaPlayer.Stop();
+            mediaPlayer.CloseMedia();
+            mediaPlayer.Events.RemoveAllListeners();
         }
 
         // private static MediaPlayer HandleCreation()
@@ -31,6 +40,7 @@ namespace DCL.PluginSystem.World
         public void InjectToWorld(ref ArchSystemsWorldBuilder<Arch.Core.World> builder, in ECSWorldInstanceSharedDependencies sharedDependencies, in PersistentEntities persistentEntities, List<IFinalizeWorldSystem> finalizeWorldSystems)
         {
             InstantiateAudioStreamSystem.InjectToWorld(ref builder, componentPoolsRegistry);
+            finalizeWorldSystems.Add(ReleasePoolableComponentSystem<MediaPlayer, AudioStreamComponent>.InjectToWorld(ref builder, componentPoolsRegistry));
         }
 
         public void InjectToEmptySceneWorld(ref ArchSystemsWorldBuilder<Arch.Core.World> builder, in EmptyScenesWorldSharedDependencies dependencies) { }
--- Explorer/Assets/Scripts/ECS/Unity/AudioStreams/Components/AudioStreamComponent.cs
+++ Explorer/Assets/Scripts/ECS/Unity/AudioStreams/Components/AudioStreamComponent.cs
@@ -1,17 +1,30 @@
 ﻿using DCL.ECSComponents;
+using DCL.Optimization.Pools;
 using RenderHeads.Media.AVProVideo;
+using System;
 
 namespace ECS.Unity.AudioStreams.Components
 {
-    public struct AudioStreamComponent
+    public readonly struct AudioStreamComponent : IPoolableComponentProvider<MediaPlayer>
     {
         public readonly PBAudioStream PBAudioStream;
         public readonly MediaPlayer MediaPlayer;
+
+        public MediaPlayer PoolableComponent => MediaPlayer;
+
+        MediaPlayer IPoolableComponentProvider<MediaPlayer>.PoolableComponent => MediaPlayer;
+
+        Type IPoolableComponentProvider<MediaPlayer>.PoolableComponentType => typeof(MediaPlayer);
 
         public AudioStreamComponent(PBAudioStream pbAudioStream, MediaPlayer mediaPlayer)
         {
             PBAudioStream = pbAudioStream;
             MediaPlayer = mediaPlayer;
+        }
+
+        public void Dispose()
+        {
+            MediaPlayer.Stop();
         }
     }
 }
