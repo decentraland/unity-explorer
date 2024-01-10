91753e5d6f2f449107b82b5484f5600dd8cdf1ae
Nicolas Lorusso <nalorusso@gmail.com>
2024-01-10 11:54:43 -0300

    fixed jsonParams naming

--- Explorer/Assets/StreamingAssets/Js/Modules/EthereumController.js
+++ Explorer/Assets/StreamingAssets/Js/Modules/EthereumController.js
@@ -1,4 +1,4 @@
 module.exports.sendAsync = async function(message) {
-    const result = await UnityEthereumApi.SendAsync(message.id, message.method, message.params)
+    const result = await UnityEthereumApi.SendAsync(message.id, message.method, message.jsonParams)
     return result;
 }
