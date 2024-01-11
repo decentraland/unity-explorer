8a1cc98ec2c8cd9ec259b9abf4caa5eac5f9ca2a
Santi Andrade <sandrade@decentraland.org>
2024-01-11 14:26:46 +0100

    Apply feedback: Implement missing logic that removes a child from the old UITransform parent

--- Explorer/Assets/DCL/SDKComponents/SceneUI/Systems/UITransform/UITransformParentingSystem.cs
+++ Explorer/Assets/DCL/SDKComponents/SceneUI/Systems/UITransform/UITransformParentingSystem.cs
@@ -8,6 +8,7 @@ using DCL.ECSComponents;
 using DCL.SDKComponents.SceneUI.Components;
 using ECS.Abstract;
 using ECS.Groups;
+using ECS.LifeCycle.Components;
 using System.Collections.Generic;
 
 namespace DCL.SDKComponents.SceneUI.Systems.UITransform
@@ -29,7 +30,18 @@ namespace DCL.SDKComponents.SceneUI.Systems.UITransform
 
         protected override void Update(float t)
         {
+            OrphanChildrenOfDeletedEntityQuery(World);
             DoUITransformParentingQuery(World);
+        }
+
+        [Query]
+        [All(typeof(PBUiTransform), typeof(UITransformComponent), typeof(DeleteEntityIntention))]
+        private void OrphanChildrenOfDeletedEntity(ref UITransformComponent uiTransformComponentToBeDeleted)
+        {
+            foreach (EntityReference childEntity in uiTransformComponentToBeDeleted.Children)
+                SetNewChild(ref World.Get<UITransformComponent>(childEntity.Entity), childEntity, sceneRoot);
+
+            uiTransformComponentToBeDeleted.Children.Clear();
         }
 
         [Query]
@@ -39,8 +51,19 @@ namespace DCL.SDKComponents.SceneUI.Systems.UITransform
             if (!sdkModel.IsDirty)
                 return;
 
-            if (entitiesMap.TryGetValue(sdkModel.Parent, out Entity newParentEntity) && newParentEntity != sceneRoot)
-                SetNewChild(ref uiTransformComponent, World.Reference(entity), newParentEntity);
+            Entity parentReference = sceneRoot;
+
+            if (entitiesMap.TryGetValue(sdkModel.Parent, out Entity newParentEntity))
+            {
+                parentReference = newParentEntity;
+
+                //We have to remove the child from the old parent
+                if (uiTransformComponent.Parent != parentReference)
+                    RemoveFromParent(uiTransformComponent, World.Reference(entity));
+
+                if (parentReference != sceneRoot)
+                    SetNewChild(ref uiTransformComponent, World.Reference(entity), parentReference);
+            }
         }
 
         private void SetNewChild(ref UITransformComponent childComponent, EntityReference childEntityReference, Entity parentEntity)
@@ -56,8 +79,14 @@ namespace DCL.SDKComponents.SceneUI.Systems.UITransform
 
             UITransformComponent parentComponent = World.Get<UITransformComponent>(parentEntity);
             parentComponent.Transform.Add(childComponent.Transform);
-            parentComponent.Children.Add(childEntityReference);
             childComponent.Parent = World.Reference(parentEntity);
+            parentComponent.Children.Add(childEntityReference);
+        }
+
+        private void RemoveFromParent(UITransformComponent childComponent, EntityReference childEntityReference)
+        {
+            if (childComponent.Parent.IsAlive(World))
+                World.Get<UITransformComponent>(childComponent.Parent.Entity).Children.Remove(childEntityReference);
         }
     }
 }
