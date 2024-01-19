8595dbd2b0b052b66f69fd3986cba0aa91859b2f
Vitaly Popuzin <35366872+popuz@users.noreply.github.com>
2024-01-19 13:06:07 +0100

    Feat: sdk7 audio source url change (#260)
    - add support to clip url change for audioSource component
    - now AudioSource is played only if player is inside the scene (muted otherwise)
    
    # How to test
    - start experience and verify there is no any music playing from the start
    - go to Dance Floor and verify music is only played if when you inside the dance floor

--- Explorer/Assets/DCL/SDKComponents/AudioSources/Systems/UpdateAudioSourceSystem.cs
+++ Explorer/Assets/DCL/SDKComponents/AudioSources/Systems/UpdateAudioSourceSystem.cs
@@ -6,10 +6,15 @@ using DCL.ECSComponents;
 using DCL.Optimization.PerformanceBudgeting;
 using DCL.Optimization.Pools;
 using ECS.Abstract;
+using ECS.Prioritization.Components;
+using ECS.StreamableLoading.AudioClips;
+using ECS.StreamableLoading.Cache;
 using ECS.StreamableLoading.Common.Components;
 using ECS.Unity.Transforms.Components;
+using SceneRunner.Scene;
 using UnityEngine;
 using Utility;
+using Promise = ECS.StreamableLoading.Common.AssetPromise<UnityEngine.AudioClip, ECS.StreamableLoading.AudioClips.GetAudioClipIntention>;
 
 namespace DCL.SDKComponents.AudioSources
 {
@@ -21,11 +26,20 @@ namespace DCL.SDKComponents.AudioSources
         private readonly IPerformanceBudget frameTimeBudgetProvider;
         private readonly IPerformanceBudget memoryBudgetProvider;
         private readonly IComponentPool<AudioSource> audioSourcesPool;
+        private readonly World world;
+        private readonly ISceneData sceneData;
+        private readonly ISceneStateProvider sceneStateProvider;
+        private readonly IStreamableCache<AudioClip, GetAudioClipIntention> cache;
 
-        internal UpdateAudioSourceSystem(World world, IComponentPoolsRegistry poolsRegistry, IPerformanceBudget frameTimeBudgetProvider, IPerformanceBudget memoryBudgetProvider) : base(world)
+        internal UpdateAudioSourceSystem(World world, ISceneData sceneData, ISceneStateProvider sceneStateProvider, IStreamableCache<AudioClip, GetAudioClipIntention> cache, IComponentPoolsRegistry poolsRegistry, IPerformanceBudget frameTimeBudgetProvider,
+            IPerformanceBudget memoryBudgetProvider) : base(world)
         {
+            this.world = world;
+            this.sceneData = sceneData;
+            this.sceneStateProvider = sceneStateProvider;
             this.frameTimeBudgetProvider = frameTimeBudgetProvider;
             this.memoryBudgetProvider = memoryBudgetProvider;
+            this.cache = cache;
 
             audioSourcesPool = poolsRegistry.GetReferenceTypePool<AudioSource>();
         }
@@ -34,21 +48,6 @@ namespace DCL.SDKComponents.AudioSources
         {
             CreateAudioSourceQuery(World);
             UpdateAudioSourceQuery(World);
-
-            // TODO: Handle Volume updates - refer to ECSAudioSourceComponentHandler.cs in unity-renderer and check UpdateAudioSourceVolume() method and its usages
-        }
-
-        [Query]
-        [All(typeof(PBAudioSource), typeof(AudioSourceComponent))]
-        private void UpdateAudioSource(ref PBAudioSource sdkAudioSource, ref AudioSourceComponent audioSourceComponent)
-        {
-            if (sdkAudioSource.IsDirty && audioSourceComponent.Result != null)
-            {
-                audioSourceComponent.Result.ApplyPBAudioSource(sdkAudioSource);
-                sdkAudioSource.IsDirty = false;
-            }
-
-            // TODO: Handle clip url changes - refer to ECSAudioSourceComponentHandler.cs in unity-renderer
         }
 
         [Query]
@@ -59,15 +58,15 @@ namespace DCL.SDKComponents.AudioSources
                 || !audioSourceComponent.ClipPromise.TryConsume(World, out StreamableLoadingResult<AudioClip> promiseResult))
                 return;
 
-            if (audioSourceComponent.Result == null)
-                audioSourceComponent.Result ??= audioSourcesPool.Get();
+            if (!audioSourceComponent.AudioSourceAssigned)
+                audioSourceComponent.SetAudioSource(audioSourcesPool.Get());
 
-            audioSourceComponent.Result.FromPBAudioSource(promiseResult.Asset, audioSourceComponent.PBAudioSource);
+            audioSourceComponent.AudioSource.FromPBAudioSourceWithClip(audioSourceComponent.PBAudioSource, clip: promiseResult.Asset);
 
             // Reset isDirty as we just applied the PBAudioSource to the AudioSource
             sdkAudioSource.IsDirty = false;
 
-            Transform transform = audioSourceComponent.Result.transform;
+            Transform transform = audioSourceComponent.AudioSource.transform;
             transform.SetParent(entityTransform.Transform, false);
             transform.ResetLocalTRS();
 
@@ -75,6 +74,35 @@ namespace DCL.SDKComponents.AudioSources
 
             bool NoBudget() =>
                 !frameTimeBudgetProvider.TrySpendBudget() || !memoryBudgetProvider.TrySpendBudget();
+        }
+
+        [Query]
+        [All(typeof(PBAudioSource), typeof(AudioSourceComponent))]
+        private void UpdateAudioSource(ref PBAudioSource sdkComponent, ref AudioSourceComponent component, ref PartitionComponent partitionComponent)
+        {
+            if (component.AudioSourceAssigned)
+                component.AudioSource.volume = sceneStateProvider.IsCurrent ? sdkComponent.GetVolume() : 0;
+
+            HandleSDKChanges(sdkComponent, ref component, partitionComponent);
+        }
+
+        private void HandleSDKChanges(PBAudioSource sdkComponent, ref AudioSourceComponent component, PartitionComponent partitionComponent)
+        {
+            if (!sdkComponent.IsDirty) return;
+
+            if (component.AudioSourceAssigned)
+                component.AudioSource.ApplyPBAudioSource(sdkComponent);
+
+            if (component.AudioClipUrl != sdkComponent.AudioClipUrl)
+            {
+                component.CleanUp(world, cache, audioSourcesPool);
+                component.AudioClipUrl = sdkComponent.AudioClipUrl;
+
+                if (AudioUtils.TryCreateAudioClipPromise(world, sceneData, sdkComponent.AudioClipUrl, partitionComponent, out Promise? clipPromise))
+                    component.ClipPromise = clipPromise!.Value;
+            }
+
+            sdkComponent.IsDirty = false;
         }
     }
 }
