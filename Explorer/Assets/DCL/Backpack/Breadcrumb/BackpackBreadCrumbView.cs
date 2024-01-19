412198c6953b855a5c1caaa6131a66a3d7b6c4ed
davidejensen <davidejensen@live.it>
2024-01-19 14:18:40 +0100

    Minor breadcrumb fix

--- Explorer/Assets/DCL/Backpack/Breadcrumb/BackpackBreadCrumbView.cs
+++ Explorer/Assets/DCL/Backpack/Breadcrumb/BackpackBreadCrumbView.cs
@@ -6,6 +6,9 @@ namespace DCL.Backpack.Breadcrumb
     public class BackpackBreadCrumbView : MonoBehaviour
     {
         [field: SerializeField]
+        public GameObject AllButtonArrow { get; private set; }
+
+        [field: SerializeField]
         public NftSubCategoryFilterComponentView AllButton { get; private set; }
 
         [field: SerializeField]
