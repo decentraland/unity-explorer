98b187cb26e08c1d6b7d26fa0a47f0ce80a6b1e3
nickkhalow <nickhalow@gmail.com>
2024-01-19 16:34:15 +0400

    BasedUrnSource memory optimization

--- Explorer/Assets/Scripts/ECS/StreamableLoading/NftShapes/Urns/BasedUrnSource.cs
+++ Explorer/Assets/Scripts/ECS/StreamableLoading/NftShapes/Urns/BasedUrnSource.cs
@@ -16,6 +16,55 @@ namespace ECS.StreamableLoading.NftShapes.Urns
 
         public URLAddress UrlOrEmpty(string urn)
         {
+            ReadOnlySpan<char> CutBeforeColon(ref int endIndex, out bool success)
+            {
+                int atBeginning = endIndex;
+
+                for (; endIndex >= 0; endIndex--)
+                    if (urn[endIndex] is ':')
+                    {
+                        success = true;
+                        return urn.AsSpan().Slice(endIndex + 1, atBeginning - endIndex);
+                    }
+
+                success = false;
+                return new ReadOnlySpan<char>();
+            }
+
+            void LogError()
+            {
+                ReportHub.LogError(ReportCategory.NFT_SHAPE_WEB_REQUEST, $"Error parsing urn: {urn}");
+            }
+
+            int index = urn.Length - 1;
+            bool success;
+
+            ReadOnlySpan<char> id = CutBeforeColon(ref index, out success);
+
+            if (success == false)
+            {
+                LogError();
+                return URLAddress.EMPTY;
+            }
+
+            index--;
+            ReadOnlySpan<char> address = CutBeforeColon(ref index, out success);
+
+            if (success == false)
+            {
+                LogError();
+                return URLAddress.EMPTY;
+            }
+
+            return URLAddress.FromString(
+                baseUrl
+                   .Replace("{address}", new string(address)) //may be optimized further, or create custom ReplaceMethod that works with spans
+                   .Replace("{id}", new string(id))
+            );
+        }
+
+        public URLAddress UrlOrEmptyAlloc(string urn)
+        {
             //"https://opensea.decentraland.org/api/v2/chain/ethereum/contract/0x06012c8cf97bead5deae237070f9587f8e7a266d/nfts/1631847"
             //urn:decentraland:ethereum:erc721:0x06012c8cf97bead5deae237070f9587f8e7a266d:1631847
 
