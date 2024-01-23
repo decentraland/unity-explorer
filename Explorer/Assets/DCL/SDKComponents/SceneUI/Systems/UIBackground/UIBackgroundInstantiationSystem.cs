c9869bc3967fa1c01b89dc494496b5198d878bb0
Santi Andrade <sandrade@decentraland.org>
2024-01-23 14:46:03 +0100

    feat: UIBackground + UIInput SDK components + Fixes in UITransform sorting (#259)
    * Initial commit
    
    * Set clearComponent flag to true for UI_TEXT and UI_BACKGROUND
    
    * Small fix in UITransformSortingSystem
    
    * Request and load texture for UIBackground component
    
    * Implement ui background release system
    
    * Small fixes
    
    * Fix UITransform sorting
    
    * Fix UI component release systems
    
    * Test coverage
    
    * Update UIToolkit RootCanvas Settings.asset
    
    * feat: UIInput component (#269)
    
    * Implement UIInput SDK component
    
    * UIInput test coverage
    
    * [Apply feedback] Clean all unneeded query's tags
    
    * [Apply feedback] Integrate TextFieldPlaceholder in the pooling of TextField
    
    * [Apply feedback] Fix UIBackground's clean-up and reconfiguration flows
    
    * Fix UIInputInstantiationSystemShould tests

---
+++ Explorer/Assets/DCL/SDKComponents/SceneUI/Systems/UIBackground/UIBackgroundInstantiationSystem.cs
@@ -0,0 +1,156 @@
+﻿using Arch.Core;
+using Arch.System;
+using Arch.SystemGroups;
+using DCL.Diagnostics;
+using DCL.ECSComponents;
+using DCL.Optimization.PerformanceBudgeting;
+using DCL.Optimization.Pools;
+using DCL.SDKComponents.SceneUI.Classes;
+using DCL.SDKComponents.SceneUI.Components;
+using DCL.SDKComponents.SceneUI.Groups;
+using DCL.SDKComponents.SceneUI.Utils;
+using ECS.Abstract;
+using ECS.Prioritization.Components;
+using ECS.StreamableLoading;
+using ECS.StreamableLoading.Common.Components;
+using ECS.StreamableLoading.Textures;
+using ECS.Unity.Textures.Components;
+using ECS.Unity.Textures.Components.Defaults;
+using SceneRunner.Scene;
+using UnityEngine;
+using Promise = ECS.StreamableLoading.Common.AssetPromise<UnityEngine.Texture2D, ECS.StreamableLoading.Textures.GetTextureIntention>;
+
+namespace DCL.SDKComponents.SceneUI.Systems.UIBackground
+{
+    [UpdateInGroup(typeof(SceneUIComponentInstantiationGroup))]
+    [LogCategory(ReportCategory.SCENE_UI)]
+    public partial class UIBackgroundInstantiationSystem : BaseUnityLoopSystem
+    {
+        private const int ATTEMPTS_COUNT = 6;
+
+        private readonly IComponentPool<DCLImage> imagesPool;
+        private readonly ISceneData sceneData;
+        private readonly IPerformanceBudget frameTimeBudgetProvider;
+        private readonly IPerformanceBudget memoryBudgetProvider;
+
+        public UIBackgroundInstantiationSystem(
+            World world,
+            IComponentPoolsRegistry poolsRegistry,
+            ISceneData sceneData,
+            IPerformanceBudget frameTimeBudgetProvider,
+            IPerformanceBudget memoryBudgetProvider) : base(world)
+        {
+            imagesPool = poolsRegistry.GetReferenceTypePool<DCLImage>();
+            this.sceneData = sceneData;
+            this.frameTimeBudgetProvider = frameTimeBudgetProvider;
+            this.memoryBudgetProvider = memoryBudgetProvider;
+        }
+
+        protected override void Update(float t)
+        {
+            InstantiateUIBackgroundQuery(World);
+            UpdateUIBackgroundQuery(World);
+            LoadUIBackgroundTextureQuery(World);
+        }
+
+        [Query]
+        [All(typeof(PBUiBackground))]
+        [None(typeof(UIBackgroundComponent))]
+        private void InstantiateUIBackground(in Entity entity, ref UITransformComponent uiTransformComponent)
+        {
+            var image = imagesPool.Get();
+            image.Initialize(uiTransformComponent.Transform);
+            var uiBackgroundComponent = new UIBackgroundComponent();
+            uiBackgroundComponent.Image = image;
+            uiBackgroundComponent.Status = LifeCycle.LoadingNotStarted;
+            World.Add(entity, uiBackgroundComponent);
+        }
+
+        [Query]
+        private void UpdateUIBackground(ref PBUiBackground sdkModel, ref UIBackgroundComponent uiBackgroundComponent, ref PartitionComponent partitionComponent)
+        {
+            if (!sdkModel.IsDirty)
+                return;
+
+            // Create texture promise if needed
+            TextureComponent? backgroundTexture = sdkModel.Texture.CreateTextureComponent(sceneData);
+            TryCreateGetTexturePromise(in backgroundTexture, ref uiBackgroundComponent.TexturePromise, ref partitionComponent);
+            uiBackgroundComponent.Status = LifeCycle.LoadingInProgress;
+
+            if (uiBackgroundComponent.TexturePromise == null)
+            {
+                // Backgrounds without texture
+                UiElementUtils.SetupDCLImage(ref uiBackgroundComponent.Image, ref sdkModel);
+                uiBackgroundComponent.Status = LifeCycle.LoadingFinished;
+            }
+
+            sdkModel.IsDirty = false;
+        }
+
+        [Query]
+        private void LoadUIBackgroundTexture(ref PBUiBackground sdkModel, ref UIBackgroundComponent uiBackgroundComponent)
+        {
+            if (uiBackgroundComponent.Status != LifeCycle.LoadingInProgress ||
+                uiBackgroundComponent.TexturePromise == null ||
+                !frameTimeBudgetProvider.TrySpendBudget() ||
+                !memoryBudgetProvider.TrySpendBudget() ||
+                !uiBackgroundComponent.TexturePromise.Value.TryConsume(World, out StreamableLoadingResult<Texture2D> promiseResult))
+                return;
+
+            // Backgrounds with texture
+            UiElementUtils.SetupDCLImage(ref uiBackgroundComponent.Image, ref sdkModel, promiseResult.Asset);
+            uiBackgroundComponent.Status = LifeCycle.LoadingFinished;
+        }
+
+        private void TryCreateGetTexturePromise(in TextureComponent? textureComponent, ref Promise? promise, ref PartitionComponent partitionComponent)
+        {
+            if (textureComponent == null)
+            {
+                // If component is being reuse forget the previous promise
+                TryAddAbortIntention(World, ref promise);
+                return;
+            }
+
+            TextureComponent textureComponentValue = textureComponent.Value;
+
+            // If data inside promise has not changed just reuse the same promise
+            // as creating and waiting for a new one can be expensive
+            if (Equals(ref textureComponentValue, ref promise))
+                return;
+
+            // If component is being reused forget the previous promise
+            TryAddAbortIntention(World, ref promise);
+
+            promise = Promise.Create(World, new GetTextureIntention
+            {
+                CommonArguments = new CommonLoadingArguments(textureComponentValue.Src, attempts: ATTEMPTS_COUNT),
+                WrapMode = textureComponentValue.WrapMode,
+                FilterMode = textureComponentValue.FilterMode,
+            }, partitionComponent);
+        }
+
+        private static void TryAddAbortIntention(World world, ref Promise? promise)
+        {
+            if (promise == null)
+                return;
+
+            promise.Value.ForgetLoading(world);
+
+            // Nullify the entity reference
+            promise = null;
+        }
+
+        private static bool Equals(ref TextureComponent textureComponent, ref Promise? promise)
+        {
+            if (promise == null)
+                return false;
+
+            Promise promiseValue = promise.Value;
+            GetTextureIntention intention = promiseValue.LoadingIntention;
+
+            return textureComponent.Src == promiseValue.LoadingIntention.CommonArguments.URL &&
+                   textureComponent.WrapMode == intention.WrapMode &&
+                   textureComponent.FilterMode == intention.FilterMode;
+        }
+    }
+}
