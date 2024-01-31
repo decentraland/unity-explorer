3df812f875386637d168f705a3d02ab618a4fcd5
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2024-01-31 10:50:49 -0300

    Added UpdateVisualSceneStateSystem test

--- Explorer/Assets/DCL/LOD/Tests/EditMode/UpdateVisualSceneStateSystemShould.cs
+++ Explorer/Assets/DCL/LOD/Tests/EditMode/UpdateVisualSceneStateSystemShould.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using DCL.LOD.Systems;
 using ECS.TestSuite;
+using NUnit.Framework;
 using UnityEngine;
 
 public class UpdateVisualSceneStateSystemShould : UnitySystemTestBase<UpdateSceneLODInfoSystem>
 {
+    [SetUp]
+    public void Setup()
+    {
+    }
+
 }
