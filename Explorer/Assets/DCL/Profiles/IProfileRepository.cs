a135e17eac7d06a96f337d6197f889f543892242
Nicolas Lorusso <56365551+lorux0@users.noreply.github.com>
2024-02-06 11:23:53 -0300

    feat: ipfs hashing, entity publishing & profile saving (#293)

--- Explorer/Assets/DCL/Profiles/IProfileRepository.cs
+++ Explorer/Assets/DCL/Profiles/IProfileRepository.cs
@@ -5,6 +5,7 @@ namespace DCL.Profiles
 {
     public interface IProfileRepository
     {
+        UniTask SetAsync(Profile profile, CancellationToken ct);
         UniTask<Profile?> GetAsync(string id, int version, CancellationToken ct);
     }
 }
