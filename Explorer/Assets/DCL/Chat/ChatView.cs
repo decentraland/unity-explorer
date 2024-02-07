07ae929dd52cfdac8975734b27f72a93505fe392
davidejensen <davidejensen@live.it>
2024-02-07 16:26:10 +0100

    Added chat panel background on hover, fixed input box states

--- Explorer/Assets/DCL/Chat/ChatView.cs
+++ Explorer/Assets/DCL/Chat/ChatView.cs
@@ -1,11 +1,17 @@
 using MVC;
+using DG.Tweening;
+using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
 
 namespace DCL.Chat
 {
-    public class ChatView : ViewBase, IView
+    public class ChatView : ViewBase, IView, IPointerEnterHandler, IPointerExitHandler
     {
+        private const float BACKGROUND_FADE_TIME = 0.2f;
+
         [field: SerializeField]
         public Transform MessagesContainer { get; private set; }
 
@@ -14,5 +20,23 @@ namespace DCL.Chat
 
         [field: SerializeField]
         public CharacterCounterView CharacterCounter { get; private set; }
+
+        [field: SerializeField]
+        public CanvasGroup PanelBackgroundCanvasGroup { get; private set; }
+
+        private void Start()
+        {
+            PanelBackgroundCanvasGroup.alpha = 0;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            PanelBackgroundCanvasGroup.DOFade(1, BACKGROUND_FADE_TIME);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            PanelBackgroundCanvasGroup.DOFade(0, BACKGROUND_FADE_TIME);
+        }
     }
 }
