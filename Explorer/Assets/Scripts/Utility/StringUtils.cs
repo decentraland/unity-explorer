15a41cdc1f5f04d3e7f08b1740681cebcbe50fca
davidejensen <davidejensen@live.it>
2024-02-09 16:12:01 +0100

    Removed linq usage in random string util

--- Explorer/Assets/Scripts/Utility/StringUtils.cs
+++ Explorer/Assets/Scripts/Utility/StringUtils.cs
@@ -1,15 +1,19 @@
-using System.Linq;
 using UnityEngine;
 
 namespace Utility
 {
     public class StringUtils
     {
-        private static string chars = " ABCDEFGHIJ KLMNOPQRSTU VWXYZ0123456789 ";
+        private const string CHARS = "ABCDEFGHIJ KLMNOPQRSTU VWXYZ 0123456789 abcdefghij klmnopqrstu vwxyz";
+        private static string randomStringResult = "";
 
         public static string GenerateRandomString(int length)
         {
-            return new string(Enumerable.Repeat(chars, length).Select(s => s[Random.Range(0, s.Length)]).ToArray());
+            randomStringResult = "";
+            for (var i = 0; i < length; i++)
+                randomStringResult += CHARS[Random.Range(0, CHARS.Length)];
+
+            return randomStringResult;
         }
     }
 }
