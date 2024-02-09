167d45111a20515ed8161f4c8dfa3506f0e7bba8
Santi Andrade <sandrade@decentraland.org>
2024-02-09 16:10:59 +0100

    feat: UIPointerEvents implementation (#290)
    * Initial commit
    
    * Implementation
    
    * Update UnitySystemTestBase.cs
    
    * Update UIPointerEventsSystem.cs
    
    * Test coverage
    
    * Update UIPointerEventsSystemShould.cs
    
    * Apply feedback: Make UITransformComponent a poolable class
    
    * Apply feedback: Make UITransformComponent a poolable class (II)
    
    * Apply feedback: Make UITransformComponent a poolable class (III)
    
    * Apply feedback: Move all UITransformComponent methods into extensions
    
    * Apply feedback: Avoid to call AppendMessage inside the callbacks
    
    * Apply feedback: Handle PBPointerEvents removal
    
    * Update UIPointerEventsSystem.cs
    
    * Apply feedback: Remove redundant code
    
    * Apply feedback: Remove unneeded field "RegisteredPointerEvents"
    
    * Apply feedback: Make the pointer events subscriptions only for transforms with PBPointerEvents component
    
    * Small fix

--- Explorer/Assets/DCL/SDKComponents/SceneUI/Components/UITransformComponent.cs
+++ Explorer/Assets/DCL/SDKComponents/SceneUI/Components/UITransformComponent.cs
@@ -1,26 +1,39 @@
 ﻿using Arch.Core;
-using DCL.Optimization.Pools;
-using System;
+using DCL.ECSComponents;
+using DCL.SDKComponents.SceneUI.Utils;
 using System.Collections.Generic;
 using UnityEngine.Pool;
 using UnityEngine.UIElements;
 
 namespace DCL.SDKComponents.SceneUI.Components
 {
-    public struct UITransformComponent : IPoolableComponentProvider<VisualElement>
+    public class UITransformComponent
     {
         public VisualElement Transform;
         public EntityReference Parent;
         public HashSet<EntityReference> Children;
         public bool IsHidden;
         public int RightOf;
+        public PointerEventType? PointerEventTriggered;
 
-        VisualElement IPoolableComponentProvider<VisualElement>.PoolableComponent => Transform;
-        Type IPoolableComponentProvider<VisualElement>.PoolableComponentType => typeof(VisualElement);
+        internal EventCallback<PointerDownEvent> currentOnPointerDownCallback;
+        internal EventCallback<PointerUpEvent> currentOnPointerUpCallback;
+
+        public void Initialize(string componentName, Entity entity, ref PBUiTransform sdkModel)
+        {
+            Transform ??= new VisualElement();
+            Transform.name = UiElementUtils.BuildElementName(componentName, entity);
+            Parent = EntityReference.Null;
+            Children = HashSetPool<EntityReference>.Get();
+            IsHidden = false;
+            RightOf = sdkModel.RightOf;
+            PointerEventTriggered = null;
+        }
 
         public void Dispose()
         {
             HashSetPool<EntityReference>.Release(Children);
+            this.UnregisterPointerCallbacks();
         }
     }
 }
