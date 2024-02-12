3d7e762cba13df434808918eb63e884261d33d74
Vitaly Popuzin <35366872+popuz@users.noreply.github.com>
2024-02-12 14:19:46 +0100

    Feat: sdk7 video events (#288)
    - add support for sdk video events
    - removed duplication between Audio and Video streaming by merging it in one Media streaming component
    - refactored Media Streaming code base to bi more align with ECS approach
    
    # How to test
    check that Video Streaming is not broken (see [this pr](https://github.com/decentraland/unity-explorer/pull/275) for test steps)

---
+++ Explorer/Assets/DCL/SDKComponents/MediaStream/MediaPlayerExtensions.cs
@@ -0,0 +1,83 @@
+using DCL.ECSComponents;
+using DCL.Utilities.Extensions;
+using RenderHeads.Media.AVProVideo;
+using UnityEngine;
+
+namespace DCL.SDKComponents.MediaStream
+{
+    public static class MediaPlayerExtensions
+    {
+        public static MediaPlayer OpenMediaIfValid(this MediaPlayer mediaPlayer, string url, bool autoPlay)
+        {
+            if (url.IsValidUrl())
+                mediaPlayer.OpenMedia(MediaPathType.AbsolutePathOrURL, url, autoPlay);
+
+            return mediaPlayer;
+        }
+
+        public static void UpdateVolume(this MediaPlayer mediaPlayer, bool isCurrentScene, bool hasVolume, float volume)
+        {
+            if (!isCurrentScene)
+                mediaPlayer.AudioVolume = 0f;
+            else
+            {
+                if (!hasVolume)
+                    mediaPlayer.AudioVolume = MediaPlayerComponent.DEFAULT_VOLUME;
+                else if (!Mathf.Approximately(mediaPlayer.AudioVolume, volume))
+                    mediaPlayer.AudioVolume = volume;
+            }
+        }
+
+        public static void SetPlaybackProperties(this MediaPlayer mediaPlayer, PBVideoPlayer sdkVideoPlayer)
+        {
+            IMediaControl control = mediaPlayer.Control;
+
+            control.SetLooping(sdkVideoPlayer.HasLoop && sdkVideoPlayer.Loop); // default: false
+            control.SetPlaybackRate(sdkVideoPlayer.HasPlaybackRate ? sdkVideoPlayer.PlaybackRate : MediaPlayerComponent.DEFAULT_PLAYBACK_RATE);
+            control.SeekFast(sdkVideoPlayer.HasPosition ? sdkVideoPlayer.Position : MediaPlayerComponent.DEFAULT_POSITION);
+        }
+
+        public static void UpdatePlaybackProperties(this MediaPlayer mediaPlayer, PBVideoPlayer sdkVideoPlayer)
+        {
+            IMediaControl control = mediaPlayer.Control;
+
+            if (sdkVideoPlayer.HasLoop && sdkVideoPlayer.Loop != control.IsLooping())
+                control.SetLooping(sdkVideoPlayer.Loop);
+
+            if (sdkVideoPlayer.HasPlaybackRate && !Mathf.Approximately(control.GetPlaybackRate(), sdkVideoPlayer.PlaybackRate))
+                control.SetPlaybackRate(sdkVideoPlayer.PlaybackRate);
+
+            if (sdkVideoPlayer.HasPosition)
+                control.SeekFast(sdkVideoPlayer.Position);
+        }
+
+        public static MediaPlayer UpdatePlayback(this MediaPlayer mediaPlayer, bool hasPlaying, bool playing)
+        {
+            IMediaControl control = mediaPlayer.Control;
+
+            if (hasPlaying)
+            {
+                if (playing != control.IsPlaying())
+                {
+                    if (playing)
+                        control.Play();
+                    else
+                        control.Pause();
+                }
+            }
+            else if (control.IsPlaying())
+                control.Stop();
+
+            return mediaPlayer;
+        }
+
+        public static void CloseCurrentStream(this MediaPlayer mediaPlayer)
+        {
+            mediaPlayer.Stop();
+            mediaPlayer.CloseMedia();
+
+            if (mediaPlayer.Events.HasListeners())
+                mediaPlayer.Events.RemoveAllListeners();
+        }
+    }
+}
