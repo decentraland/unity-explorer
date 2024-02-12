c04796c078c201353083371daae25f770a37eb9e
nickkhalow <nickhalow@gmail.com>
2024-02-12 14:53:17 +0200

    message pipe

--- Explorer/Assets/DCL/Multiplayer/Connections/Messaging/ReceivedMessage.cs
+++ Explorer/Assets/DCL/Multiplayer/Connections/Messaging/ReceivedMessage.cs
@@ -1,24 +1,29 @@
+using Decentraland.Kernel.Comms.Rfc4;
 using Google.Protobuf;
 using LiveKit.Internal.FFIClients.Pools;
 using System;
 
 namespace DCL.Multiplayer.Connections.Messaging
 {
-    public readonly struct Message<T> : IDisposable where T: class, IMessage, new()
+    public readonly struct ReceivedMessage<T> : IDisposable where T: class, IMessage, new()
     {
         public readonly T Payload;
         public readonly string FromSid;
+        private readonly Packet packet;
         private readonly IMultiPool multiPool;
 
-        public Message(T payload, string fromSid, IMultiPool multiPool)
+        public ReceivedMessage(T payload, Packet packet, string fromSid, IMultiPool multiPool)
         {
             this.Payload = payload;
             this.FromSid = fromSid;
             this.multiPool = multiPool;
+            this.packet = packet;
         }
 
         public void Dispose()
         {
+            packet.ClearMessage();
+            multiPool.Release(packet);
             multiPool.Release(Payload);
         }
     }
