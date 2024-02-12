d736809cad8bde5e7d610fed020d4f2fe3b33e1a
Vitaly Popuzin <popuzin@gmail.com>
2024-02-12 16:35:16 +0100

    small refactoring to remove duplication

--- Explorer/Assets/DCL/Multiplayer/Movement/MessageBusMock/Receiver.cs
+++ Explorer/Assets/DCL/Multiplayer/Movement/MessageBusMock/Receiver.cs
@@ -1,21 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace DCL.Multiplayer.Movement.MessageBusMock
 {
+    public enum InterpolationType
+    {
+        Linear,
+        Hermite,
+    }
     public class Receiver : MonoBehaviour
     {
         private readonly Queue<MessageMock> receivedMessages = new ();
         [SerializeField] private MessageBus messageBus;
         [SerializeField] private float minDelta;
+        [SerializeField] private InterpolationType interpolationType;
 
         private bool isLerping;
         private Coroutine coroutine;
 
         private bool isFirst;
+        private Func<MessageMock, MessageMock, float, float, Vector3> interpolation;
 
-        private void Awake() =>
+        private void Awake()
+        {
             messageBus.MessageSent += newMessage =>
             {
                 if (isFirst)
@@ -27,6 +36,13 @@ namespace DCL.Multiplayer.Movement.MessageBusMock
                 receivedMessages.Enqueue(newMessage);
             };
 
+            interpolation = interpolationType switch
+                            {
+                                InterpolationType.Linear => LinearInterpolation,
+                                InterpolationType.Hermite => HermiteInterpolation,
+                            };
+        }
+
         private void Update()
         {
             if (receivedMessages.Count > 1 && !isLerping)
@@ -35,11 +51,11 @@ namespace DCL.Multiplayer.Movement.MessageBusMock
                 MessageMock endPoint = receivedMessages.Peek();
 
                 if (Vector3.Distance(startPoint.position, endPoint.position) > minDelta)
-                    StartCoroutine(MoveToHermite(startPoint, endPoint));
+                    StartCoroutine(MoveTo(startPoint, endPoint, interpolation));
             }
         }
 
-        private IEnumerator MoveToLinearly(MessageMock start, MessageMock end)
+        private IEnumerator MoveTo(MessageMock start, MessageMock end, Func<MessageMock, MessageMock, float, float, Vector3> interpolation)
         {
             isLerping = true;
 
@@ -49,40 +65,27 @@ namespace DCL.Multiplayer.Movement.MessageBusMock
             while (t < timeDif)
             {
                 t += UnityEngine.Time.deltaTime;
-                transform.position = Vector3.Lerp(start.position, end.position, t / timeDif);
+                transform.position = interpolation(start, end, t / timeDif, timeDif);
                 yield return null;
             }
 
             isLerping = false;
         }
 
-        private IEnumerator MoveToHermite(MessageMock start, MessageMock end)
-        {
-            isLerping = true;
+        private static Vector3 LinearInterpolation(MessageMock start, MessageMock end, float t, float _) =>
+            Vector3.Lerp(start.position, end.position, t);
 
-            var t = 0.0f;
-            float timeDif = end.timestamp - start.timestamp;
-
-            while (t < timeDif)
-            {
-                t += UnityEngine.Time.deltaTime;
-                transform.position = CubicHermiteSpline(start, end, t / timeDif, timeDif);
-                yield return null;
-            }
-
-            isLerping = false;
-        }
-
-        private static Vector3 CubicHermiteSpline(MessageMock start, MessageMock end, float t, float timeDif)
+        /// Cubic Hermite Spline version of Hermite interpolation
+        private static Vector3 HermiteInterpolation(MessageMock start, MessageMock end, float t, float timeDif)
         {
             float t2 = t * t;
             float t3 = t2 * t;
 
-            float h2 = (-2 * t3) + (3 * t2); // end position h_01
-            float h1 = -h2 + 1; // start position h_00
+            float h2 = (-2 * t3) + (3 * t2); // Hermite basis function h_01 (for end position)
+            float h1 = -h2 + 1; // Hermite basis function h_00 (for start position)
 
-            float h3 = t3 - (2 * t2) + t; // start velocity h_10
-            float h4 = t3 - t2; // end velocity h_11
+            float h3 = t3 - (2 * t2) + t; // Hermite basis function h_10 (for start velocity)
+            float h4 = t3 - t2; // Hermite basis function h_11 (for end velocity)
 
             return (h1 * start.position) + (h2 * end.position) + (start.velocity * (h3 * timeDif)) + (end.velocity * (h4 * timeDif));
         }
