bf7a4247ac6185e625036c4ff3a0ea9cd3b99447
Santi Andrade <sandrade@decentraland.org>
2024-02-13 13:51:27 +0100

    Base implementation

---
+++ Explorer/Assets/Scripts/CrdtEcsBridge/RestrictedActions/RestrictedActionsAPIImplementation.cs
@@ -0,0 +1,30 @@
+﻿using Cysharp.Threading.Tasks;
+using DCL.Browser;
+using SceneRuntime.Apis.Modules;
+
+namespace CrdtEcsBridge.RestrictedActions
+{
+    public class RestrictedActionsAPIImplementation : IRestrictedActionsAPI
+    {
+        private readonly IWebBrowser webBrowser;
+
+        public RestrictedActionsAPIImplementation(IWebBrowser webBrowser)
+        {
+            this.webBrowser = webBrowser;
+        }
+
+        public bool OpenExternalUrl(string url)
+        {
+            OpenUrlAsync(url).Forget();
+            return true;
+        }
+
+        private async UniTask OpenUrlAsync(string url)
+        {
+            await UniTask.SwitchToMainThread();
+            webBrowser.OpenUrl(url);
+        }
+
+        public void Dispose() { }
+    }
+}
