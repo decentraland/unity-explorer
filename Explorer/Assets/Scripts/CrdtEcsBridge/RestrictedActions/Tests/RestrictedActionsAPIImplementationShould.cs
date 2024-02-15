98d9f2c0a03763ed9e774bab7f490ac16b45cabe
Santi Andrade <sandrade@decentraland.org>
2024-02-15 16:12:22 +0100

    Test coverage for RestrictedActionsAPIImplementation

---
+++ Explorer/Assets/Scripts/CrdtEcsBridge/RestrictedActions/Tests/RestrictedActionsAPIImplementationShould.cs
@@ -0,0 +1,33 @@
+﻿using DCL.ExternalUrlPrompt;
+using MVC;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace CrdtEcsBridge.RestrictedActions.Tests
+{
+    public class RestrictedActionsAPIImplementationShould
+    {
+        private RestrictedActionsAPIImplementation restrictedActionsAPIImplementation;
+        private IMVCManager mvcManager;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mvcManager = Substitute.For<IMVCManager>();
+            restrictedActionsAPIImplementation = new RestrictedActionsAPIImplementation(mvcManager);
+        }
+
+        [Test]
+        public void OpenExternalUrl()
+        {
+            // Arrange
+            var testUrl = "www.test.com";
+
+            // Act
+            restrictedActionsAPIImplementation.OpenExternalUrl(testUrl);
+
+            // Assert
+            mvcManager.Received(1).ShowAsync(ExternalUrlPromptController.IssueCommand(new ExternalUrlPromptController.Params(testUrl)));
+        }
+    }
+}
