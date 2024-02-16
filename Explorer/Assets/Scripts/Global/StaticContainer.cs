a6093e94a552e7fa4ebb6802178c40df5141325a
Fran Colarich <56565104+fcolarich@users.noreply.github.com>
2024-02-16 16:15:59 +0100

    Fix merge issue

--- Explorer/Assets/Scripts/Global/StaticContainer.cs
+++ Explorer/Assets/Scripts/Global/StaticContainer.cs
@@ -187,8 +187,6 @@ namespace Global
                 new InteractionPlugin(sharedDependencies, profilingProvider, exposedGlobalDataContainer.GlobalInputEvents, componentsContainer.ComponentPoolsRegistry),
                 new SceneUIPlugin(sharedDependencies, addressablesProvisioner),
                 container.CharacterContainer.CreateWorldPlugin(),
-                new AudioStreamPlugin(sharedDependencies, container.CacheCleaner),
-                new VideoPlayerPlugin(sharedDependencies, container.CacheCleaner, videoTexturePool),
                 new TweenPlugin(componentsContainer.ComponentPoolsRegistry),
                 new MediaPlayerPlugin(sharedDependencies, container.CacheCleaner, videoTexturePool, sharedDependencies.FrameTimeBudget),
 
