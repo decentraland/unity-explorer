5c2fdbed54e77e2758508df5716b81db521e4351
nickkhalow <nickhalow@gmail.com>
2024-02-20 20:25:15 +0200

    UnixTime -> UnixTimeSeconds

--- Explorer/Assets/Scripts/Utility/Times/DateTimeExtensions.cs
+++ Explorer/Assets/Scripts/Utility/Times/DateTimeExtensions.cs
@@ -6,7 +6,7 @@ namespace Utility.Times
     {
         public static readonly DateTime UNIX_START = new (1970, 1, 1);
 
-        public static int UnixTime(this DateTime dateTime) =>
+        public static int UnixTimeSeconds(this DateTime dateTime) =>
             (int)dateTime.Subtract(UNIX_START).TotalSeconds;
 
         public static ulong UnixTimeAsMilliseconds(this DateTime dateTime) =>
