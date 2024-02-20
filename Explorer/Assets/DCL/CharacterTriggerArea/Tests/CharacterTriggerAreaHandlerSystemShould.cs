2a3c3a7e796fda1ca09cd1c6a848a4cf0540307e
Pravus <pravusjif@gmail.com>
2024-02-20 02:02:15 +0100

    added several tests

--- Explorer/Assets/DCL/CharacterTriggerArea/Tests/CharacterTriggerAreaHandlerSystemShould.cs
+++ Explorer/Assets/DCL/CharacterTriggerArea/Tests/CharacterTriggerAreaHandlerSystemShould.cs
@@ -1,6 +1,10 @@
 using Arch.Core;
+using Cysharp.Threading.Tasks;
 using DCL.AvatarRendering.AvatarShape.UnityInterface;
+using DCL.Character;
+using DCL.CharacterTriggerArea.Components;
 using DCL.CharacterTriggerArea.Systems;
+using DCL.ECSComponents;
 using DCL.Optimization.Pools;
 using ECS.Prioritization.Components;
 using ECS.TestSuite;
@@ -8,7 +12,9 @@ using ECS.Unity.Transforms.Components;
 using NSubstitute;
 using NUnit.Framework;
 using SceneRunner.Scene;
+using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 namespace DCL.CharacterTriggerArea.Tests
 {
@@ -19,14 +25,25 @@ namespace DCL.CharacterTriggerArea.Tests
         private ISceneStateProvider sceneStateProvider;
         private GameObject fakeMainPlayerGO;
         private GameObject fakeMainPlayerAvatarGO;
-        private GameObject fakeCharacterTriggerAreaGO;
+        private CharacterTriggerArea characterTriggerArea;
 
         [SetUp]
-        public void Setup()
+        public async void Setup()
         {
-            var fakeMainPlayerGO = new GameObject();
-            var fakeMainPlayerAvatarGO = new GameObject();
-            var fakeCharacterTriggerAreaGO = new GameObject();
+            entity = world.Create(PartitionComponent.TOP_PRIORITY);
+
+            entityTransformComponent = AddTransformToEntity(entity);
+            entityTransformComponent.SetTransform(Vector3.one * 30, Quaternion.identity, Vector3.one);
+            world.Set(entity, entityTransformComponent);
+
+            fakeMainPlayerGO = await Addressables.LoadAssetAsync<GameObject>("Character Object");
+            fakeMainPlayerGO = Object.Instantiate(fakeMainPlayerGO.GetComponent<CharacterObject>()).gameObject;
+            fakeMainPlayerGO.transform.position = Vector3.zero;
+
+            GameObject characterTriggerAreaBaseGO = await Addressables.LoadAssetAsync<GameObject>("CharacterTriggerArea");
+            characterTriggerArea = Object.Instantiate(characterTriggerAreaBaseGO.GetComponent<CharacterTriggerArea>());
+
+            fakeMainPlayerAvatarGO = new GameObject();
 
             var mainPlayerReferences = new MainPlayerReferences
             {
@@ -41,14 +58,11 @@ namespace DCL.CharacterTriggerArea.Tests
             IComponentPoolsRegistry poolsRegistry = Substitute.For<IComponentPoolsRegistry>();
             IComponentPool<CharacterTriggerArea> characterTriggerAreaPool = Substitute.For<IComponentPool<CharacterTriggerArea>>();
             poolsRegistry.GetReferenceTypePool<CharacterTriggerArea>().Returns(characterTriggerAreaPool);
-            characterTriggerAreaPool.Get().Returns(fakeCharacterTriggerAreaGO.AddComponent<CharacterTriggerArea>());
+            characterTriggerAreaPool.Get().Returns(characterTriggerArea);
 
             sceneStateProvider = Substitute.For<ISceneStateProvider>();
             sceneStateProvider.IsCurrent.Returns(true);
-            system = new CharacterTriggerAreaHandlerSystem(world, new ComponentPool<CharacterTriggerArea>(), mainPlayerReferences, sceneStateProvider);
-
-            entity = world.Create(PartitionComponent.TOP_PRIORITY);
-            entityTransformComponent = AddTransformToEntity(entity);
+            system = new CharacterTriggerAreaHandlerSystem(world, characterTriggerAreaPool, mainPlayerReferences, sceneStateProvider);
         }
 
         [TearDown]
@@ -57,39 +71,168 @@ namespace DCL.CharacterTriggerArea.Tests
             Object.DestroyImmediate(entityTransformComponent.Transform.gameObject);
             Object.DestroyImmediate(fakeMainPlayerGO);
             Object.DestroyImmediate(fakeMainPlayerAvatarGO);
-            Object.DestroyImmediate(fakeCharacterTriggerAreaGO);
+            Object.DestroyImmediate(characterTriggerArea.gameObject);
         }
 
         [Test]
-        public void SetupMonoBehaviourCorrectly() { }
+        public async Task SetupMonoBehaviourCorrectly()
+        {
+            // Workaround for Unity bug not awaiting async Setup correctly
+            await UniTask.WaitUntil(() => system != null);
+
+            var targetAreaSize = new Vector3(15, 6, 18);
+
+            Assert.AreNotEqual(characterTriggerArea.BoxCollider.size, targetAreaSize);
+
+            var pbComponent = new PBCameraModeArea();
+
+            var component = new CharacterTriggerAreaComponent
+            {
+                AreaSize = targetAreaSize,
+                IsDirty = true,
+            };
+
+            world.Add(entity, component, pbComponent);
+
+            system.Update(0);
+
+            Assert.AreEqual(targetAreaSize, characterTriggerArea.BoxCollider.size);
+        }
 
         [Test]
-        public void UpdateMonoBehaviourCorrectly() { }
+        public async Task UpdateMonoBehaviourCorrectly()
+        {
+            // Workaround for Unity bug not awaiting async Setup correctly
+            await UniTask.WaitUntil(() => system != null);
+
+            var targetAreaSize = new Vector3(15, 6, 18);
+
+            var pbComponent = new PBCameraModeArea();
+
+            var component = new CharacterTriggerAreaComponent
+            {
+                AreaSize = targetAreaSize,
+                IsDirty = true,
+            };
+
+            world.Add(entity, component, pbComponent);
+
+            system.Update(0);
+
+            Assert.AreEqual(targetAreaSize, characterTriggerArea.BoxCollider.size);
+
+            // update component values
+            targetAreaSize /= 3;
+            component.AreaSize = targetAreaSize;
+            component.IsDirty = true;
+            world.Set(entity, component);
+
+            Assert.AreNotEqual(targetAreaSize, characterTriggerArea.BoxCollider.size);
+
+            system.Update(0);
+
+            Assert.AreEqual(targetAreaSize, characterTriggerArea.BoxCollider.size);
+        }
 
         [Test]
-        public void TriggerEnterEventOnCharacterEnter() { }
+        public async Task UpdateTransformCorrectlyIgnoringScale()
+        {
+            // Workaround for Unity bug not awaiting async Setup correctly
+            await UniTask.WaitUntil(() => system != null);
+
+            var pbComponent = new PBCameraModeArea();
+
+            var component = new CharacterTriggerAreaComponent
+            {
+                AreaSize = Vector3.one,
+                IsDirty = true,
+            };
+
+            world.Add(entity, component, pbComponent);
+
+            system.Update(0);
+
+            Assert.AreEqual(entityTransformComponent.Transform.position, characterTriggerArea.transform.position);
+
+            entityTransformComponent.SetTransform(Vector3.one * 10, Quaternion.identity, Vector3.one);
+            world.Set(entity, entityTransformComponent);
+
+            system.Update(0);
+            Assert.AreEqual(entityTransformComponent.Transform.position, characterTriggerArea.transform.position);
+
+            entityTransformComponent.SetTransform(Vector3.one * 38, Quaternion.Euler(33, 65, 59), Vector3.one * 66);
+            world.Set(entity, entityTransformComponent);
+
+            system.Update(0);
+
+            Assert.AreEqual(entityTransformComponent.Transform.position, characterTriggerArea.transform.position);
+            Assert.AreEqual(entityTransformComponent.Transform.rotation, characterTriggerArea.transform.rotation);
+            Assert.AreNotEqual(entityTransformComponent.Transform.localScale, characterTriggerArea.BoxCollider.size);
+            Assert.AreEqual(Vector3.one, characterTriggerArea.BoxCollider.size);
+        }
 
         [Test]
-        public void TriggerExitEventOnCharacterExit() { }
+        public async Task TriggerEnterExitEventsCorrectly()
+        {
+            // Workaround for Unity bug not awaiting async Setup correctly
+            await UniTask.WaitUntil(() => system != null);
+
+            var enterTriggerCalled = false;
+            var exitTriggerCalled = false;
+
+            var pbComponent = new PBCameraModeArea();
+
+            var component = new CharacterTriggerAreaComponent
+            {
+                AreaSize = Vector3.one * 4,
+                OnEnteredTrigger = collider => enterTriggerCalled = true,
+                OnExitedTrigger = collider => exitTriggerCalled = true,
+                IsDirty = true,
+            };
+
+            world.Add(entity, component, pbComponent);
+
+            system.Update(0);
+
+            Assert.IsFalse(enterTriggerCalled);
+            Assert.IsFalse(exitTriggerCalled);
+
+            // Move character inside area
+            fakeMainPlayerGO.transform.position = entityTransformComponent.Transform.position;
+
+            await UniTask.Yield();
+
+            Assert.IsTrue(enterTriggerCalled);
+            Assert.IsFalse(exitTriggerCalled);
+            enterTriggerCalled = false;
+
+            // Move character outside area
+            fakeMainPlayerGO.transform.position = entityTransformComponent.Transform.position + (Vector3.one * 50);
+
+            await UniTask.Yield();
+
+            Assert.IsTrue(exitTriggerCalled);
+            Assert.IsFalse(enterTriggerCalled);
+        }
 
         [Test]
-        public void ResetTriggerAreaEffectOnSceneLeave() { }
+        public async Task ResetTriggerAreaEffectOnSceneLeave() { }
 
         [Test]
-        public void HandleComponentRemoveCorrectly() { }
+        public async Task HandleComponentRemoveCorrectly() { }
 
         [Test]
-        public void HandleEntityDestructionCorrectly() { }
+        public async Task HandleEntityDestructionCorrectly() { }
 
         [Test]
-        public void DisableTriggerColliderWhenMainPlayerIsOutOfScene() { }
+        public async Task DisableTriggerColliderWhenMainPlayerIsOutOfScene() { }
 
         [Test]
-        public void WaitUntilAvatarBaseIsConfiguredBeforeActing() { }
+        public async Task WaitUntilAvatarBaseIsConfiguredBeforeActing() { }
 
         [Test]
         [TestCase(true)]
         [TestCase(false)]
-        public void DiscriminateCharacterTypeCorrectly(bool onlyMainPlayer) { }
+        public async Task DiscriminateCharacterTypeCorrectly(bool onlyMainPlayer) { }
     }
 }
