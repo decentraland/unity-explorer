c1d256f374ff972c227e4cf0bb0657379cf4e6b0
Vitaly Popuzin <popuzin@gmail.com>
2024-02-21 12:50:17 +0100

    add damping to extrapolation

--- Explorer/Assets/DCL/Multiplayer/Movement/ECS/Components/ExtrapolationComponent.cs
+++ Explorer/Assets/DCL/Multiplayer/Movement/ECS/Components/ExtrapolationComponent.cs
@@ -1,14 +1,16 @@
 ﻿using DCL.Multiplayer.Movement.MessageBusMock;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 namespace DCL.Multiplayer.Movement.ECS
 {
     public struct ExtrapolationComponent
     {
         private const float MIN_SPEED = 0.01f;
+        private const float LINEAR_EXTRAPOLATION_TIME = 0.33f;
+        private const int DAMPED_EXTRAPOLATION_STEPS = 2;
 
         public readonly Transform Transform;
+        private readonly float maxDuration;
 
         public bool Enabled;
 
@@ -18,9 +20,11 @@ namespace DCL.Multiplayer.Movement.ECS
 
         public ExtrapolationComponent(Transform transform)
         {
-            this.Transform = transform;
+            Transform = transform;
 
             Start = null;
+            maxDuration = LINEAR_EXTRAPOLATION_TIME * DAMPED_EXTRAPOLATION_STEPS;
+
             Time = 0f;
             Velocity = Vector3.zero;
             Enabled = false;
@@ -29,6 +33,7 @@ namespace DCL.Multiplayer.Movement.ECS
         public void Update(float deltaTime)
         {
             Time += deltaTime;
+            Velocity = DampVelocity();
 
             if (Velocity.sqrMagnitude > MIN_SPEED)
                 Transform.position += Velocity * UnityEngine.Time.deltaTime;
@@ -41,6 +46,14 @@ namespace DCL.Multiplayer.Movement.ECS
             Time = 0f;
             Velocity = from.velocity;
             Enabled = true;
+        }
+
+        private Vector3 DampVelocity()
+        {
+            if (Time > LINEAR_EXTRAPOLATION_TIME && Time < maxDuration)
+                return Vector3.Lerp(Start.velocity, Vector3.zero, Time / maxDuration);
+
+            return Time >= maxDuration ? Vector3.zero : Velocity;
         }
     }
 }
