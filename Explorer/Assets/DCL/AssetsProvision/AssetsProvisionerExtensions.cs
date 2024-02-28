3e82951481ec4ce23d1ab1082def0d8b502a3003
nickkhalow <nickhalow@gmail.com>
2024-02-28 15:46:35 +0200

    CharacterContainer with error spawn

--- Explorer/Assets/DCL/AssetsProvision/AssetsProvisionerExtensions.cs
+++ Explorer/Assets/DCL/AssetsProvision/AssetsProvisionerExtensions.cs
@@ -2,6 +2,7 @@ using Cysharp.Threading.Tasks;
 using DCL.AssetsProvision.Provisions;
 using System;
 using System.Threading;
+using UnityEngine;
 using UnityEngine.AddressableAssets;
 using Object = UnityEngine.Object;
 
@@ -17,6 +18,20 @@ namespace DCL.AssetsProvision
         public static async UniTask<ProvidedAsset<T>> ProvideMainAssetAsync<T>(this IAssetsProvisioner assetsProvisioner, AssetReferenceT<T> assetReferenceT, CancellationToken ct, string error) where T: Object
         {
             try { return await assetsProvisioner.ProvideMainAssetAsync(assetReferenceT, ct); }
+            catch (Exception e) { throw new Exception($"Cannot provide main asset: {error}", e); }
+        }
+
+        public static async UniTask<ProvidedInstance<T>> ProvideInstanceAsync<T>(
+            this IAssetsProvisioner assetsProvisioner,
+            ComponentReference<T> componentReference,
+            Vector3 position,
+            Quaternion rotation,
+            Transform? parent = null,
+            CancellationToken ct = default,
+            string error = ""
+        ) where T: Object
+        {
+            try { return await assetsProvisioner.ProvideInstanceAsync(componentReference, position, rotation, parent, ct); }
             catch (Exception e) { throw new Exception($"Cannot provide main asset: {error}", e); }
         }
 
