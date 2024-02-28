f01413d00958abe32210c4083761d6b81a1ed65a
nickkhalow <nickhalow@gmail.com>
2024-02-28 20:39:45 +0200

    validating settings

--- Explorer/Assets/DCL/PluginSystem/Validatables/ValidatableExtensions.cs
+++ Explorer/Assets/DCL/PluginSystem/Validatables/ValidatableExtensions.cs
@@ -1,17 +1,50 @@
+using Cysharp.Threading.Tasks;
+using DCL.Diagnostics;
 using System;
 using UnityEngine.AddressableAssets;
+using Object = UnityEngine.Object;
 
 namespace DCL.Utilities.Addressables
 {
     public static class ValidatableExtensions
     {
-        public static void EnsureValid(this AssetReference assetReference, string name)
+        public static async UniTask<Exception?> EnsureValidWithExceptionAsync(this AssetReference? assetReference, string name)
         {
-            if (assetReference.EnsureValidWithException(name) is { } e)
-                throw e;
+            ReportHub.Log(ReportData.UNSPECIFIED, "Validate AssetReference: " + name);
+
+            if (assetReference == null)
+                return new Exception("AssetReference is null: " + name);
+
+            using var ensureInitialState = new EnsureInitialState(assetReference);
+
+            try
+            {
+                await assetReference.LoadAssetAsync<Object>().ToUniTask();
+                assetReference.ReleaseAsset();
+                return null;
+            }
+            catch (Exception e) { return new Exception("AssetReference is not valid: " + name, e); }
         }
 
-        public static Exception? EnsureValidWithException(this AssetReference assetReference, string name) =>
-            assetReference.IsValid() == false ? new Exception("AssetReference is not valid: " + name) : null;
+        private readonly struct EnsureInitialState : IDisposable
+        {
+            private readonly AssetReference reference;
+            private readonly bool isLoaded;
+
+            public EnsureInitialState(AssetReference reference) : this()
+            {
+                this.reference = reference;
+                isLoaded = reference.IsDone;
+
+                if (this.reference.IsDone)
+                    this.reference.ReleaseAsset();
+            }
+
+            public void Dispose()
+            {
+                if (isLoaded && reference.IsDone == false)
+                    reference.LoadAssetAsync<Object>().ToUniTask().Forget();
+            }
+        }
     }
 }
