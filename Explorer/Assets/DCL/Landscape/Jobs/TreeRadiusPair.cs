28afca7628566bdb3b435b1bcfac80cd3f9a98a3
Patricio Guerra <kinerius@gmail.com>
2024-03-11 14:12:36 -0300

    fix: tree overlap (#388)
    Co-authored-by: Ashley Canning <ashley.canning@decentraland.org>

---
+++ Explorer/Assets/DCL/Landscape/Jobs/TreeRadiusPair.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DCL.Landscape.Jobs
+{
+    [Serializable]
+    public struct TreeRadiusPair
+    {
+        public float radius;
+        public float secondaryRadius;
+    }
+}
