92756f4bb97b9164cd9bf2614a567ab8e366b74c
davidejensen <davidejensen@live.it>
2024-03-13 16:27:13 +0100

    Added emoji section toggles

---
+++ Explorer/Assets/DCL/EmojiPanel/EmojiSectionToggle.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DCL.Emoji
+{
+    public class EmojiSectionToggle : MonoBehaviour
+    {
+        [field: SerializeField]
+        public Toggle SectionToggle { get; private set; }
+
+        [field: SerializeField]
+        public Image SectionImage { get; private set; }
+
+        [field: SerializeField]
+        public Color SelectedColor { get; private set; }
+
+        [field: SerializeField]
+        public Color UnselectedColor { get; private set; }
+
+        public void Start() =>
+            SectionToggle.onValueChanged.AddListener(OnValueChanged);
+
+        private void OnValueChanged(bool isOn)
+        {
+            SectionImage.color = isOn ? SelectedColor : UnselectedColor;
+        }
+    }
+}
