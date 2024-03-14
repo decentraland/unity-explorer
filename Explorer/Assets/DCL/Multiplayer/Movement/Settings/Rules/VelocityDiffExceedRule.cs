c277f38df596974816e0a595c853ac952d397527
Vitaly Popuzin <35366872+popuz@users.noreply.github.com>
2024-03-14 20:30:31 +0100

    Feat: multiplayer movement (#364)
    Added client-side interpolation for remote players:
    
    - simple fixed-rate sending of player movement message
    - update protobuf with the new Movement message
    - inbox management for received messages with PrioirtyQueue
    - client-side interpolation, extrapolation and teleportation with settings (not exposed to debug window yet)
    - possibility to simulate network with the self-replica
    
    not finished (for upcoming branches) :
    - rules to playtest and optimize send rates
    - decoding of message
    - proper animation handling
    - splitting animation system from transform's
    
    # How to test
    You need 2 people to test 
    - just spawn in the same spot
    - run, jump, rotate - mostly should be smooth even with package lost
    - animations replicated (but not 100% identical)

---
+++ Explorer/Assets/DCL/Multiplayer/Movement/Settings/Rules/VelocityDiffExceedRule.cs
@@ -0,0 +1,24 @@
+﻿using DCL.CharacterMotion.Components;
+using UnityEngine;
+
+namespace DCL.Multiplayer.Movement.Settings
+{
+    public class VelocityDiffExceedRule : SendRuleBase
+    {
+        public override string Message => $"$\"<color={color}> VELOCITY DIFF </color>\"";
+
+        [Space]
+        public float VelocityChangeThreshold;
+
+        public override bool IsSendConditionMet(
+            in float t,
+            in FullMovementMessage lastFullMovementMessage,
+            in CharacterAnimationComponent playerAnimationComponent,
+            in StunComponent playerStunComponent,
+            in MovementInputComponent move,
+            in JumpInputComponent jump,
+            CharacterController playerCharacter,
+            IMultiplayerMovementSettings settings) =>
+            Vector3.SqrMagnitude(lastFullMovementMessage.velocity - playerCharacter.velocity) > VelocityChangeThreshold;
+    }
+}
