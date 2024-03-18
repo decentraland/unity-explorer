c07a21a2b72070a4def38b6b166db6e5121a07ed
Fran Colarich <56565104+fcolarich@users.noreply.github.com>
2024-03-18 15:11:55 +0100

    feat: SDK7 - SceneAPI - getSceneInfo (#404)
    * Implemented Scene API
    
    * Small fixes
    
    * Fixed error + added dispose

---
+++ Explorer/Assets/Scripts/CrdtEcsBridge/Engine/SceneApiImplementation.cs
@@ -0,0 +1,29 @@
+﻿using Cysharp.Threading.Tasks;
+using Newtonsoft.Json;
+using SceneRunner.Scene;
+using SceneRuntime.Apis.Modules;
+using System.Threading;
+
+namespace CrdtEcsBridge.Engine
+{
+    public class SceneApiImplementation : ISceneApi
+    {
+        private readonly ISceneData sceneData;
+
+        public SceneApiImplementation(ISceneData sceneData)
+        {
+            this.sceneData = sceneData;
+        }
+
+        public void Dispose() { }
+
+        public ISceneApi.GetSceneResponse GetSceneInfo() =>
+            new ()
+            {
+                baseUrl = sceneData.SceneContent.ContentBaseUrl.Value,
+                contents = sceneData.SceneEntityDefinition.content,
+                cid = sceneData.SceneEntityDefinition.id,
+                metadata = JsonConvert.SerializeObject(sceneData.SceneEntityDefinition.metadata),
+            };
+    }
+}
