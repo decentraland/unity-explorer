c4a9528de6497ef85abe6f82d0837365fc0141d3
nickkhalow <nickhalow@gmail.com>
2024-03-20 12:11:40 +0200

    MessageWrap broadcasting

--- Explorer/Assets/DCL/Multiplayer/Profiles/BroadcastProfiles/ProfileBroadcast.cs
+++ Explorer/Assets/DCL/Multiplayer/Profiles/BroadcastProfiles/ProfileBroadcast.cs
@@ -1,10 +1,7 @@
 using Cysharp.Threading.Tasks;
-using DCL.Multiplayer.Connections.Messaging;
 using DCL.Multiplayer.Connections.Messaging.Hubs;
 using DCL.Multiplayer.Connections.Messaging.Pipe;
-using DCL.Multiplayer.Connections.RoomHubs;
 using Decentraland.Kernel.Comms.Rfc4;
-using LiveKit.Rooms;
 using System.Threading;
 
 namespace DCL.Multiplayer.Profiles.BroadcastProfiles
@@ -13,27 +10,24 @@ namespace DCL.Multiplayer.Profiles.BroadcastProfiles
     {
         private const int CURRENT_PROFILE_VERSION = 0;
         private readonly IMessagePipesHub messagePipesHub;
-        private readonly IRoomHub roomHub;
         private readonly CancellationTokenSource cancellationTokenSource = new ();
 
-        public ProfileBroadcast(IMessagePipesHub messagePipesHub, IRoomHub roomHub)
+        public ProfileBroadcast(IMessagePipesHub messagePipesHub)
         {
             this.messagePipesHub = messagePipesHub;
-            this.roomHub = roomHub;
         }
 
         public UniTaskVoid NotifyRemotesAsync()
         {
-            SendTo(messagePipesHub.IslandPipe(), roomHub.IslandRoom());
-            SendTo(messagePipesHub.ScenePipe(), roomHub.SceneRoom());
+            SendTo(messagePipesHub.IslandPipe());
+            SendTo(messagePipesHub.ScenePipe());
             return new UniTaskVoid();
         }
 
-        private void SendTo(IMessagePipe messagePipe, IRoom room)
+        private void SendTo(IMessagePipe messagePipe)
         {
             var message = messagePipe.NewMessage<AnnounceProfileVersion>();
             message.Payload.ProfileVersion = CURRENT_PROFILE_VERSION;
-            message.AddRecipients(room);
             message.SendAndDisposeAsync(cancellationTokenSource.Token).Forget();
         }
 
