0f1fce2e7f84c3b44fc22c1ae47fba8c8057caf0
nickkhalow <nickhalow@gmail.com>
2024-03-29 10:11:46 +0200

    joi library

---
+++ Explorer/Assets/StreamingAssets/Js/EditorTests/Dev/src/index.js
@@ -0,0 +1,5 @@
+const joi = require('joi');
+
+export function Joi() {
+    return joi;
+}
---
+++ Explorer/Assets/StreamingAssets/Js/EditorTests/Dev/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'production',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'joi-validates.min.js',
+        library: 'Validates',
+        libraryTarget: 'umd',
+        umdNamedDefine: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    },
+    externals: {
+        lodash: {
+            commonjs: 'lodash',
+            commonjs2: 'lodash',
+            amd: 'lodash',
+            root: '_'
+        }
+    }
+};
