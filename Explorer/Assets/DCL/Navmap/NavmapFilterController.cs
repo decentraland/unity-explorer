ab614f15aacb2f61d1b51bc174cf4af355489480
davidejensen <davidejensen@live.it>
2024-04-02 14:59:11 +0200

    Added filter panel close when clicking outside

--- Explorer/Assets/DCL/Navmap/NavmapFilterController.cs
+++ Explorer/Assets/DCL/Navmap/NavmapFilterController.cs
@@ -10,20 +10,12 @@ namespace DCL.Navmap
         public NavmapFilterController(NavmapFilterView filterView)
         {
             this.filterView = filterView;
-            filterView.filterButton.onClick.AddListener(ToggleFilterContent);
             filterView.infoButton.onClick.AddListener(ToggleInfoContent);
         }
 
         private void ToggleInfoContent()
         {
             filterView.infoContent.SetActive(!filterView.infoContent.activeSelf);
-        }
-
-        private void ToggleFilterContent()
-        {
-            //Add animations
-            filterView.filterContentTransform.gameObject.SetActive(!filterView.filterContentTransform.gameObject.activeSelf);
-            filterView.infoContent.SetActive(false);
         }
     }
 }
--- Explorer/Assets/DCL/Navmap/NavmapFilterView.cs
+++ Explorer/Assets/DCL/Navmap/NavmapFilterView.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -5,8 +6,16 @@ namespace DCL.Navmap
 {
     public class NavmapFilterView : MonoBehaviour
     {
+        private const float ANIMATION_TIME = 0.2f;
+
+        [field: SerializeField]
+        public CanvasGroup CanvasGroup { get; private set; }
+
         [field: SerializeField]
         public RectTransform filterContentTransform;
+
+        [field: SerializeField]
+        public DeselectableUiElement FilterContentDeselectable { get; private set; }
 
         [field: SerializeField]
         public Button filterButton;
@@ -16,5 +25,26 @@ namespace DCL.Navmap
 
         [field: SerializeField]
         public GameObject infoContent;
+
+        private void Start()
+        {
+            filterButton.onClick.AddListener(OnSortDropdownClick);
+            FilterContentDeselectable.gameObject.SetActive(false);
+            FilterContentDeselectable.OnDeselectEvent += OnSortDropdownClick;
+        }
+
+        private void OnSortDropdownClick()
+        {
+            if (FilterContentDeselectable.gameObject.activeInHierarchy)
+            {
+                CanvasGroup.DOFade(0, ANIMATION_TIME).SetEase(Ease.InOutQuad).OnComplete(() => FilterContentDeselectable.gameObject.SetActive(false));
+            }
+            else
+            {
+                FilterContentDeselectable.gameObject.SetActive(true);
+                FilterContentDeselectable.SelectElement();
+                CanvasGroup.DOFade(1, ANIMATION_TIME).SetEase(Ease.InOutQuad);
+            }
+        }
     }
 }
