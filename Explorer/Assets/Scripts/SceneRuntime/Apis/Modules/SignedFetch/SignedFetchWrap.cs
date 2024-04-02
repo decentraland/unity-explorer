86b9b74516a2f9e832f1c3a426c2541d963d36ed
nickkhalow <nickhalow@gmail.com>
2024-04-02 16:17:27 +0400

    error catch SignedFetch

--- Explorer/Assets/Scripts/SceneRuntime/Apis/Modules/SignedFetch/SignedFetchWrap.cs
+++ Explorer/Assets/Scripts/SceneRuntime/Apis/Modules/SignedFetch/SignedFetchWrap.cs
@@ -1,8 +1,10 @@
+using Cysharp.Threading.Tasks;
 using DCL.WebRequests;
 using JetBrains.Annotations;
 using SceneRuntime.Apis.Modules.SignedFetch.Messages;
 using System;
 using System.Threading;
+using UnityEngine.Networking;
 
 namespace SceneRuntime.Apis.Modules.SignedFetch
 {
@@ -24,10 +26,21 @@ namespace SceneRuntime.Apis.Modules.SignedFetch
         }
 
         [UsedImplicitly]
-        public object SignedFetch(string url, string? jsonMetaData) =>
-            webController
-               .SignedFetchAsync(url, jsonMetaData ?? string.Empty, cancellationTokenSource.Token)
-               .ToPromise();
+        public object SignedFetch(string url, string? jsonMetaData)
+        {
+            async UniTask<GenericPostRequest> ExecuteAsync()
+            {
+                var result = await webController
+                   .SignedFetchAsync(url, jsonMetaData ?? string.Empty, cancellationTokenSource.Token);
+
+                if (result.UnityWebRequest.result is not UnityWebRequest.Result.Success)
+                    throw new Exception($"Failed to fetch {url} with status code {result.UnityWebRequest.responseCode}");
+
+                return result;
+            }
+
+            return ExecuteAsync().ToPromise();
+        }
 
         public void Dispose()
         {
