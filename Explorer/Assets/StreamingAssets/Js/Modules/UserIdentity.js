438f0f2d69863684a601e0b10bc6315c41a1cad9
nickkhalow <nickhalow@gmail.com>
2024-04-03 14:16:13 +0400

    fix getUserData

--- Explorer/Assets/StreamingAssets/Js/Modules/UserIdentity.js
+++ Explorer/Assets/StreamingAssets/Js/Modules/UserIdentity.js
@@ -1,31 +1,7 @@
 module.exports.getUserPublicKey = async function(message) {
-    return UnityUserIdentityApi.UserPublicKey();
+    return UnityUserIdentityApi.UserPublicKey()
 }
 
 module.exports.getUserData = async function(message) {
-    const result = await UnityUserIdentityApi.GetOwnUserData();
-    const data = result.data;
-    
-    if (!data) {
-        return {
-            data: undefined
-        };
-    }
-    
-    const avatar = data.avatar;
-    const wearables = Array(avatar.wearables.Count);
-    
-    for (let i = 0; i < avatar.wearables.Count; i++) {
-        wearables[i] = avatar.wearables[i];
-    }
-    
-    return {
-        data: {
-            ...data,
-            avatar: {
-                ...avatar,
-                wearables: wearables
-            }
-        }
-    };
+    return  UnityUserIdentityApi.GetOwnUserData()
 }
