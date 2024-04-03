b56505a35928d241f1f62606e5c867cab9b2efcd
nickkhalow <nickhalow@gmail.com>
2024-04-03 16:06:40 +0400

    restore getUserData unpacking

--- Explorer/Assets/StreamingAssets/Js/Modules/UserIdentity.js
+++ Explorer/Assets/StreamingAssets/Js/Modules/UserIdentity.js
@@ -3,5 +3,26 @@ module.exports.getUserPublicKey = async function(message) {
 }
 
 module.exports.getUserData = async function(message) {
-    return  UnityUserIdentityApi.GetOwnUserData()
+    const result = await UnityUserIdentityApi.GetOwnUserData();
+    if (result.data === undefined){
+        return result
+    }
+    
+    const data = result.data;
+    const avatar = data.avatar;
+    const wearables = Array(avatar.wearables.Count);
+    
+    for (let i = 0; i < avatar.wearables.Count; i++) {
+        wearables[i] = avatar.wearables[i];
+    }
+    
+    return {
+        data: {
+            ...data,
+            avatar: {
+                ...avatar,
+                wearables: wearables
+            }
+        }
+    };
 }
