cacc0eedab9e65ef637bd3c4dd601ee865dc25c3
Santi Andrade <sandrade@decentraland.org>
2024-04-08 15:41:59 +0200

    Define a SliderType for sliders

--- Explorer/Assets/DCL/Settings/ModuleViews/SettingsSliderModuleView.cs
+++ Explorer/Assets/DCL/Settings/ModuleViews/SettingsSliderModuleView.cs
@@ -1,20 +1,38 @@
 ﻿using DCL.UI;
 using System;
+using System.Globalization;
+using TMPro;
 using UnityEngine;
 
 namespace DCL.Settings.ModuleViews
 {
+    public enum SliderType
+    {
+        Numeric,
+        Percentage,
+        Time,
+    }
     public class SettingsSliderModuleView : SettingsModuleView<SettingsSliderModuleView.Config>
     {
+        private SliderType sliderType;
+
         [Serializable]
         public class Config : SettingsModuleViewConfiguration
         {
+            public SliderType sliderType;
             public float minValue;
             public float maxValue;
             public bool wholeNumbers;
         }
 
         [field: SerializeField] public SliderView SliderView { get; private set; }
+        [field: SerializeField] public TMP_Text SliderValueText { get; private set; }
+
+        private void Awake()
+        {
+            SliderView.Slider.onValueChanged.AddListener(OnValueChanged);
+            OnValueChanged(SliderView.Slider.value);
+        }
 
         protected override void Configure(Config configuration)
         {
@@ -22,6 +40,26 @@ namespace DCL.Settings.ModuleViews
             SliderView.Slider.minValue = configuration.minValue;
             SliderView.Slider.maxValue = configuration.maxValue;
             SliderView.Slider.wholeNumbers = configuration.wholeNumbers;
+            sliderType = configuration.sliderType;
+        }
+
+        private void OnValueChanged(float value)
+        {
+            switch (sliderType)
+            {
+                case SliderType.Numeric:
+                    SliderValueText.text = value.ToString(SliderView.Slider.wholeNumbers ? "0" : "0.00", CultureInfo.InvariantCulture);
+                    break;
+                case SliderType.Percentage:
+                    SliderValueText.text = $"{value.ToString(SliderView.Slider.wholeNumbers ? "0" : "0.00", CultureInfo.InvariantCulture)}%";
+                    break;
+                case SliderType.Time:
+                    value = Mathf.Clamp(value, 0, 23.998f);
+                    var hourSection = (int)value;
+                    var minuteSection = (int)((value - hourSection) * 60);
+                    SliderValueText.text = $"{hourSection:00}:{minuteSection:00}";
+                    break;
+            }
         }
     }
 }
