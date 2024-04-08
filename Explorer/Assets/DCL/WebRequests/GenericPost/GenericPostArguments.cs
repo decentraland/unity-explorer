f1e3cb311e3451767d90504160e4e8d42d8275a2
nickkhalow <nickhalow@gmail.com>
2024-04-08 19:02:03 +0400

    GenericPostArguments toString

--- Explorer/Assets/DCL/WebRequests/GenericPost/GenericPostArguments.cs
+++ Explorer/Assets/DCL/WebRequests/GenericPost/GenericPostArguments.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -45,5 +46,26 @@ namespace DCL.WebRequests
 
         public static GenericPostArguments CreateJson(string postData) =>
             new (postData, "application/json");
+
+        public override string ToString() =>
+            "GenericPostArguments:"
+            + $"\nMultipartFormSections: {MultipartFormSections}"
+            + $"\nWWWForm: {WebFormToString(WWWForm)}"
+            + $"\nPostData: {PostData}"
+            + $"\nContentType: {ContentType}";
+
+        private static readonly IReadOnlyDictionary<string, string> EMPTY_DICTIONARY = new Dictionary<string, string>();
+
+        private static string WebFormToString(WWWForm? wwwForm)
+        {
+            if (wwwForm == null) return "Empty web form";
+
+            var sb = new StringBuilder();
+
+            foreach ((string? key, string? value) in wwwForm.headers ?? EMPTY_DICTIONARY)
+                sb.Append($"{key} : {value} \t");
+
+            return sb.ToString();
+        }
     }
 }
