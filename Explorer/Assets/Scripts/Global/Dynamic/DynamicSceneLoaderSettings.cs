163642a1c9c23e558da847da8e16a9e2b9029486
Nicolas Lorusso <nalorusso@gmail.com>
2024-04-09 16:04:15 -0300

    changed authentication urls to org & zone depending on debug build

--- Explorer/Assets/Scripts/Global/Dynamic/DynamicSceneLoaderSettings.cs
+++ Explorer/Assets/Scripts/Global/Dynamic/DynamicSceneLoaderSettings.cs
@@ -14,7 +14,9 @@ namespace Global.Dynamic
         [field: SerializeField] public List<int2> StaticLoadPositions { get; private set; }
         [field: SerializeField] public List<string> Realms { get; private set; }
         [field: SerializeField] public string AuthWebSocketUrl { get; private set; }
+        [field: SerializeField] public string AuthWebSocketUrlDev { get; private set; }
         [field: SerializeField] public string AuthSignatureUrl { get; private set; }
+        [field: SerializeField] public string AuthSignatureUrlDev { get; private set; }
         [field: SerializeField] public List<string> Web3WhitelistMethods { get; private set; }
     }
 }
