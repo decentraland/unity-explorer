33ffa306fe743837f092a2915d01e8e0358abf23
Pravus <pravusjif@gmail.com>
2024-04-11 10:32:39 +0200

    initial handling of scene reserved entities for other players

--- Explorer/Assets/DCL/Multiplayer/SDK/Systems/PlayerComponentsHandlerSystem.cs
+++ Explorer/Assets/DCL/Multiplayer/SDK/Systems/PlayerComponentsHandlerSystem.cs
@@ -3,6 +3,7 @@ using Arch.System;
 using Arch.SystemGroups;
 using Arch.SystemGroups.DefaultSystemGroups;
 using CRDT;
+using CrdtEcsBridge.Components;
 using DCL.Character.Components;
 using DCL.Diagnostics;
 using DCL.Multiplayer.Profiles.Systems;
@@ -11,6 +12,7 @@ using DCL.Profiles;
 using ECS.Abstract;
 using ECS.SceneLifeCycle;
 using SceneRunner.Scene;
+using System.Collections.Generic;
 using UnityEngine;
 using Utility;
 
@@ -23,8 +25,8 @@ namespace DCL.Multiplayer.SDK.Systems
     {
         private readonly IScenesCache scenesCache;
 
-        // FOR TESTING ONLY
-        private int crdtEntityId = 32;
+        // TODO: Clear somehow disposed scenes...
+        private readonly IDictionary<Vector2Int, SceneReservedCRDTEntitiesData> scenesReservedEntities = new Dictionary<Vector2Int, SceneReservedCRDTEntitiesData>();
 
         public PlayerComponentsHandlerSystem(World world, IScenesCache scenesCache) : base(world)
         {
@@ -43,10 +45,21 @@ namespace DCL.Multiplayer.SDK.Systems
             if (!scenesCache.TryGetByParcel(ParcelMathHelper.FloorToParcel(characterTransform.Transform.position), out ISceneFacade sceneFacade))
                 return;
 
-            // External world access should be always synchronized (Global World calls into Scene World)
-            // using (sceneFacade.EcsExecutor.Sync.GetScope())
+            if (!scenesReservedEntities.TryGetValue(sceneFacade.Info.BaseParcel, out SceneReservedCRDTEntitiesData sceneReservedCRDTEntitiesData))
             {
-                var crdtEntityComponent = new CRDTEntity(crdtEntityId++);
+                sceneReservedCRDTEntitiesData = new SceneReservedCRDTEntitiesData(SpecialEntitiesID.OTHER_PLAYER_ENTITIES_TO - SpecialEntitiesID.OTHER_PLAYER_ENTITIES_FROM);
+                scenesReservedEntities.Add(sceneFacade.Info.BaseParcel, sceneReservedCRDTEntitiesData);
+            }
+
+            int crdtEntityId = sceneReservedCRDTEntitiesData.ReserveNextFreeEntity();
+
+            // All reserved entities are taken
+            if (crdtEntityId == -1) return;
+
+            // External world access should be always synchronized (Global World calls into Scene World)
+            using (sceneFacade.EcsExecutor.Sync.GetScope())
+            {
+                var crdtEntityComponent = new CRDTEntity(crdtEntityId);
                 Debug.Log($"PRAVS - UpdatePlayerIdentityDataQuery() - Entity: {entity.Id}; CRDTEntity: {crdtEntityComponent.Id}; Address: {profile.UserId}; scene parcel: {sceneFacade.Info.BaseParcel}");
 
                 World sceneWorld = sceneFacade.EcsExecutor.World;
@@ -62,6 +75,40 @@ namespace DCL.Multiplayer.SDK.Systems
 
                 // sceneWorld.Add(entity, playerIdentityData, crdtEntityComponent);
                 sceneWorld.Add(entity, playerIdentityData);
+            }
+        }
+
+        private struct SceneReservedCRDTEntitiesData
+        {
+            private readonly bool[] reservedEntities;
+
+            public SceneReservedCRDTEntitiesData(int entitiesAmount)
+            {
+                reservedEntities = new bool[entitiesAmount];
+            }
+
+            // TODO: Optimize
+            public int ReserveNextFreeEntity()
+            {
+                for (var i = 0; i < reservedEntities.Length; i++)
+                {
+                    if (!reservedEntities[i])
+                    {
+                        reservedEntities[i] = true;
+                        return SpecialEntitiesID.OTHER_PLAYER_ENTITIES_FROM + i;
+                    }
+                }
+
+                // All reserved entities are taken
+                return -1;
+            }
+
+            public void ClearReservedEntity(int entityId)
+            {
+                entityId = entityId - SpecialEntitiesID.OTHER_PLAYER_ENTITIES_FROM;
+                if (entityId >= reservedEntities.Length) return;
+
+                reservedEntities[entityId] = false;
             }
         }
     }
