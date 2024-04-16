8e733335a072d72231d286edff07574b8f6f4d42
Santi Andrade <sandrade@decentraland.org>
2024-04-16 17:10:32 +0200

    Apply feedback: Make the SettingsMenuConfiguration setters private

--- Explorer/Assets/DCL/Settings/Configuration/SettingsMenuConfiguration.cs
+++ Explorer/Assets/DCL/Settings/Configuration/SettingsMenuConfiguration.cs
@@ -7,26 +7,24 @@ namespace DCL.Settings.Configuration
     [CreateAssetMenu(menuName = "Settings Menu/Create Settings Menu Configuration", fileName = "Settings Menu Configuration", order = 0)]
     public class SettingsMenuConfiguration : ScriptableObject
     {
-        [field: SerializeField] public SettingsGroupView SettingsGroupPrefab { get; private set; }
-        [field: SerializeField] public SettingsSectionConfig GeneralSectionConfig { get; set; }
-        [field: SerializeField] public SettingsSectionConfig GraphicsSectionConfig { get; set; }
-        [field: SerializeField] public SettingsSectionConfig SoundSectionConfig { get; set; }
-        [field: SerializeField] public SettingsSectionConfig ControlsSectionConfig { get; set; }
+        [field: SerializeField] internal SettingsGroupView SettingsGroupPrefab { get; set; }
+        [field: SerializeField] internal SettingsSectionConfig GeneralSectionConfig { get; set; }
+        [field: SerializeField] internal SettingsSectionConfig GraphicsSectionConfig { get; set; }
+        [field: SerializeField] internal SettingsSectionConfig SoundSectionConfig { get; set; }
+        [field: SerializeField] internal SettingsSectionConfig ControlsSectionConfig { get; set; }
     }
 
     [Serializable]
     public class SettingsSectionConfig
     {
-        [field: SerializeField] public List<SettingsGroup> SettingsGroups { get; set; }
+        [field: SerializeField] internal List<SettingsGroup> SettingsGroups { get; set; }
     }
 
     [Serializable]
     public class SettingsGroup
     {
-        [field: SerializeField]
-        public string GroupTitle { get; private set; }
+        [field: SerializeField] internal string GroupTitle { get; set; }
 
-        [field: SerializeReference, SubclassSelector]
-        public List<SettingsModuleBindingBase> Modules { get; private set; }
+        [field: SerializeReference, SubclassSelector] internal List<SettingsModuleBindingBase> Modules { get; set; }
     }
 }
