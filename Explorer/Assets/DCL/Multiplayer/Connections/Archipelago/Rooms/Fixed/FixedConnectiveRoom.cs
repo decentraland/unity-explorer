d8179a13f0cf8e9a507a59a1e323a8dcfae8c92d
nickkhalow <nickhalow@gmail.com>
2024-04-16 11:53:58 +0400

    FixedConnectiveRoom impl

--- Explorer/Assets/DCL/Multiplayer/Connections/Archipelago/Rooms/Fixed/FixedConnectiveRoom.cs
+++ Explorer/Assets/DCL/Multiplayer/Connections/Archipelago/Rooms/Fixed/FixedConnectiveRoom.cs
@@ -6,6 +6,7 @@ using DCL.WebRequests;
 using LiveKit.Rooms;
 using System;
 using System.Threading;
+using UnityEngine;
 
 namespace DCL.Multiplayer.Connections.Archipelago.Rooms.Fixed
 {
@@ -50,23 +51,36 @@ namespace DCL.Multiplayer.Connections.Archipelago.Rooms.Fixed
         private async UniTask<string> ConnectionStringAsync(CancellationToken token)
         {
             string adapterUrl = await currentAdapterAddress.AdapterUrlAsync(token);
-            GenericPostRequest result = await webRequests.SignedFetchPostAsync(adapterUrl, "{}", token);
+            string metadata = FixedMetadata.Default.ToJson();
+            GenericPostRequest result = await webRequests.SignedFetchPostAsync(adapterUrl, metadata, token);
             AdapterResponse response = await result.CreateFromJson<AdapterResponse>(WRJsonParser.Unity);
-            string connectionString = response.adapter;
+            string connectionString = response.fixedAdapter;
             ReportHub.WithReport(ReportCategory.ARCHIPELAGO_REQUEST).Log($"String is: {connectionString}");
             return connectionString;
         }
 
+        [Serializable]
         private struct FixedMetadata
         {
+            public static FixedMetadata Default = new ()
+            {
+                intent = "dcl:explorer:comms-handshake",
+                signer = "dcl:explorer",
+                isGuest = false,
+            };
+
             public string intent;
             public string signer;
+            public bool isGuest;
+
+            public string ToJson() =>
+                JsonUtility.ToJson(this)!;
         }
 
         [Serializable]
         private struct AdapterResponse
         {
-            public string adapter;
+            public string fixedAdapter;
         }
     }
 }
