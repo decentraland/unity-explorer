c07034e330e3a5eec7b8aa0afb5d23a6061fcc61
nickkhalow <nickhalow@gmail.com>
2024-04-17 17:36:19 +0400

    additional logging

--- Explorer/Assets/DCL/ScenesDebug/ScenesConsistency/ScenesConsistencyDebug.cs
+++ Explorer/Assets/DCL/ScenesDebug/ScenesConsistency/ScenesConsistencyDebug.cs
@@ -1,4 +1,3 @@
-using CommunicationData.URLHelpers;
 using Cysharp.Threading.Tasks;
 using DCL.Diagnostics;
 using DCL.ScenesDebug.ScenesConsistency.ChatTeleports;
@@ -21,28 +20,39 @@ namespace DCL.ScenesDebug.ScenesConsistency
         [Header("Debug")]
         [SerializeField] private List<SceneEntity> entities;
 
-        private readonly IChatTeleport chatTeleport = new LogChatTeleport(
-            new ChatTeleport(),
-            ReportHub.WithReport(ReportCategory.SCENE_LOADING).Log
-        );
+        private IChatTeleport chatTeleport = null!;
 
         private void Start()
         {
+            chatTeleport = new LogChatTeleport(
+                new ChatTeleport(),
+                Log
+            );
+
             LaunchAsync().Forget();
+        }
+
+        private void Log(string message)
+        {
+            ReportHub.WithReport(ReportCategory.SCENE_LOADING).Log($"Debugging: {message}");
         }
 
         private async UniTaskVoid LaunchAsync()
         {
+            Log("Scene loading...!");
             await SceneManager.LoadSceneAsync(productionScene, LoadSceneMode.Additive)!.ToUniTask();
+            Log("Scene loaded!");
 
             entities = SceneEntities.FromText(scenesCoordinatesRaw)
                                     .Where(x => x.IsRunning() == false)
                                     .ToList();
+            Log("Entities parsed!");
 
             await chatTeleport.WaitReadyAsync();
 
             foreach (SceneEntity entity in entities)
             {
+                Log($"Executing entity: {entity.coordinate} {entity.status}");
                 chatTeleport.GoTo(entity.coordinate);
                 await UniTask.Delay(TimeSpan.FromSeconds(delayBetweenTeleports));
             }
