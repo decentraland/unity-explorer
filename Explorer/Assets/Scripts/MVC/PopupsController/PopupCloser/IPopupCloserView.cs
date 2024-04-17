3a135fc85c8a5a6bb025c847203b4ce4955b514d
nickkhalow <nickhalow@gmail.com>
2024-04-17 16:42:58 +0400

    ScenesConsistencyDebug

--- Explorer/Assets/Scripts/MVC/PopupsController/PopupCloser/IPopupCloserView.cs
+++ Explorer/Assets/Scripts/MVC/PopupsController/PopupCloser/IPopupCloserView.cs
@@ -1,3 +1,6 @@
+using Cysharp.Threading.Tasks;
+using System;
+using System.Threading;
 using UnityEngine.UI;
 
 namespace MVC.PopupsController.PopupCloser
@@ -5,5 +8,21 @@ namespace MVC.PopupsController.PopupCloser
     public interface IPopupCloserView : IView
     {
         public Button CloseButton { get; }
+
+        public class Fake : IPopupCloserView
+        {
+            public void SetDrawOrder(CanvasOrdering order)
+            {
+                throw new Exception("I'm fake!");
+            }
+
+            public UniTask ShowAsync(CancellationToken ct) =>
+                throw new Exception("I'm fake!");
+
+            public UniTask HideAsync(CancellationToken ct, bool isInstant = false) =>
+                throw new Exception("I'm fake!");
+
+            public Button CloseButton => throw new Exception("I'm fake!");
+        }
     }
 }
