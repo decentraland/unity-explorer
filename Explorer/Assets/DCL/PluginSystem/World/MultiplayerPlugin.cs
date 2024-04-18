48106c8b70d1a85937060c2d22c1ae07c745a851
Pravus <pravusjif@gmail.com>
2024-04-19 01:02:33 +0200

    forgotten file update

--- Explorer/Assets/DCL/PluginSystem/World/MultiplayerPlugin.cs
+++ Explorer/Assets/DCL/PluginSystem/World/MultiplayerPlugin.cs
@@ -30,8 +30,7 @@ namespace DCL.PluginSystem.World
         public void InjectToWorld(ref ArchSystemsWorldBuilder<Arch.Core.World> builder, in ECSWorldInstanceSharedDependencies sharedDependencies, in PersistentEntities persistentEntities, List<IFinalizeWorldSystem> finalizeWorldSystems, List<ISceneIsCurrentListener> sceneIsCurrentListeners)
         {
             // ResetDirtyFlagSystem<PB...>.InjectToWorld(ref builder);
-            var writePlayerIdentityDataSystem = WritePlayerIdentityDataSystem.InjectToWorld(ref builder, sharedDependencies.EcsToCRDTWriter, componentPoolsRegistry.GetReferenceTypePool<PBPlayerIdentityData>());
-            finalizeWorldSystems.Add(writePlayerIdentityDataSystem);
+            WritePlayerIdentityDataSystem.InjectToWorld(ref builder, sharedDependencies.EcsToCRDTWriter, componentPoolsRegistry.GetReferenceTypePool<PBPlayerIdentityData>());
         }
 
         public void InjectToEmptySceneWorld(ref ArchSystemsWorldBuilder<Arch.Core.World> builder, in EmptyScenesWorldSharedDependencies dependencies) { }
