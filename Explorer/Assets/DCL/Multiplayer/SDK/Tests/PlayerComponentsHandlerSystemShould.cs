da97b00f6dd2b6275b0a2ad42d0ef839c690084b
Pravus <pravusjif@gmail.com>
2024-04-22 16:32:48 +0200

    more test coverage

--- Explorer/Assets/DCL/Multiplayer/SDK/Tests/PlayerComponentsHandlerSystemShould.cs
+++ Explorer/Assets/DCL/Multiplayer/SDK/Tests/PlayerComponentsHandlerSystemShould.cs
@@ -1,5 +1,6 @@
 using Arch.Core;
 using CrdtEcsBridge.Components;
+using DCL.AvatarRendering.Emotes;
 using DCL.AvatarRendering.Wearables;
 using DCL.AvatarRendering.Wearables.Helpers;
 using DCL.Character;
@@ -132,34 +133,53 @@ namespace DCL.Multiplayer.SDK.Tests
         [Test]
         public void UpdatePlayerSDKDataWithEmoteEvents()
         {
-            // scene1Facade.SceneStateProvider.IsCurrent.Returns(true);
-            // scene2Facade.SceneStateProvider.IsCurrent.Returns(false);
-            // fakeCharacterUnityTransform.position = Vector3.one;
-            //
-            // var profile = new Profile(FAKE_USER_ID, "fake user", CreateTestAvatar());
-            // world.Add(entity, profile, new CharacterTransform(fakeCharacterUnityTransform));
-            //
-            // Assert.IsFalse(world.Has<PlayerSDKDataComponent>(entity));
-            //
-            // system.Update(0);
-            //
-            // Assert.IsTrue(world.TryGet(entity, out PlayerSDKDataComponent playerSDKDataComponent));
-            // Assert.AreEqual(FAKE_USER_ID, playerSDKDataComponent.Address);
-            // Assert.AreEqual(profile.Name, playerSDKDataComponent.Name);
-            // Assert.IsNotNull(playerSDKDataComponent.CRDTEntity);
-            // Assert.IsTrue(scene1World.TryGet(playerSDKDataComponent.SceneWorldEntity, out PlayerSDKDataComponent sceneplayerSDKDataComponent));
-            // Assert.AreEqual(playerSDKDataComponent, sceneplayerSDKDataComponent);
-            //
-            // world.TryGet(entity, out profile);
-            // profile.IsDirty = true;
-            // profile.Name = "NewName";
-            // world.Set(entity, profile);
-            //
-            // system.Update(0);
-            //
-            // Assert.IsTrue(world.TryGet(entity, out playerSDKDataComponent));
-            // Assert.IsTrue(world.TryGet(entity, out profile));
-            // Assert.AreEqual(profile.Name, playerSDKDataComponent.Name);
+            scene1Facade.SceneStateProvider.IsCurrent.Returns(true);
+            scene2Facade.SceneStateProvider.IsCurrent.Returns(false);
+            fakeCharacterUnityTransform.position = Vector3.one;
+
+            var profile = new Profile(FAKE_USER_ID, "fake user", CreateTestAvatar());
+            world.Add(entity, profile, new CharacterTransform(fakeCharacterUnityTransform));
+
+            Assert.IsFalse(world.Has<PlayerSDKDataComponent>(entity));
+
+            system.Update(0);
+
+            Assert.IsTrue(world.TryGet(entity, out PlayerSDKDataComponent playerSDKDataComponent));
+            Assert.AreEqual(FAKE_USER_ID, playerSDKDataComponent.Address);
+            Assert.AreEqual(profile.Name, playerSDKDataComponent.Name);
+            Assert.IsNotNull(playerSDKDataComponent.CRDTEntity);
+            Assert.IsTrue(scene1World.TryGet(playerSDKDataComponent.SceneWorldEntity, out PlayerSDKDataComponent scenePlayerSDKDataComponent));
+            Assert.AreEqual(playerSDKDataComponent, scenePlayerSDKDataComponent);
+
+            var emoteUrn1 = "thunder-kiss-65";
+            var emoteUrn2 = "thunder-kiss-66";
+
+            var emoteComponent = new CharacterEmoteComponent
+            {
+                EmoteUrn = emoteUrn1,
+                EmoteLoop = true,
+            };
+
+            Assert.AreNotEqual(emoteComponent.EmoteUrn, playerSDKDataComponent.PlayingEmote);
+            Assert.AreNotEqual(emoteComponent.EmoteLoop, playerSDKDataComponent.LoopingEmote);
+
+            world.Add(entity, emoteComponent);
+
+            system.Update(0);
+            Assert.IsTrue(world.TryGet(entity, out playerSDKDataComponent));
+            Assert.AreEqual(emoteComponent.EmoteUrn, playerSDKDataComponent.PlayingEmote);
+            Assert.AreEqual(emoteComponent.EmoteLoop, playerSDKDataComponent.LoopingEmote);
+
+            emoteComponent.EmoteUrn = emoteUrn2;
+            emoteComponent.EmoteLoop = false;
+
+            world.Set(entity, emoteComponent);
+
+            system.Update(0);
+            Assert.IsTrue(world.TryGet(entity, out playerSDKDataComponent));
+            Assert.IsTrue(playerSDKDataComponent.PreviousEmote.Equals(emoteUrn1));
+            Assert.AreEqual(emoteComponent.EmoteUrn, playerSDKDataComponent.PlayingEmote);
+            Assert.AreEqual(emoteComponent.EmoteLoop, playerSDKDataComponent.LoopingEmote);
         }
 
         [Test]
