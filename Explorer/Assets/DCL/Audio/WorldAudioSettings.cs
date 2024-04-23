bff663d6b2463960ba30338a7b573fc657441f99
Fran Colarich <56565104+fcolarich@users.noreply.github.com>
2024-04-23 21:42:36 +0200

    feat: landscape audio sfx (#645)
    * First implementation of Avatar Sounds
    
    * Tweaks to components and code
    
    * Added UIAudio Plugin and settings
    
    * Added singleton for UI audio
    
    * First changes to migrate from Enums to SO
    
    * Added Scriptable Objects instead of enums + window to view and edit them
    
    * Added sounds to many UI elements + created looping event for music
    
    * Added new sounds for EA 2024
    
    * Added audio from old renderer + added music to menus
    
    * Fixed issue with volume after fade out
    
    * Changes to handle pitch and to coalesce all types of audio configs
    
    * Changes, changes everywhere!
    
    * A couple of fixes for avatar audio settings
    
    * Changes to AvatarAudioSettings
    
    * Temp changes
    
    * Some improvements
    
    * Improvements overall but mostly refactored how audio configs and audio settings work
    
    * Moved files around
    
    * Fixed initialization background music
    
    * Small code adjustments
    
    * Extracted avatar audio prefab
    
    * Fixes after merge
    
    * further fixes after merge
    
    * Renamings
    
    * Removed Avatar code and left only UI + SDK code
    
    * MOre Ui elements
    
    * Fix for audiosource of SDKs not using spatial blend out of the box
    
    * Moved AudioListener from 1st person Camera to CharacterObject
    
    * First implementation tests
    
    * Added back login background music
    
    * Changes in implementation
    
    * Fixes for implementation
    
    * Overall Improvements
    
    * Small Change to frame check
    
    * Restored Animations
    
    * Update AvatarBase.prefab
    
    * Re-added basic sounds to emoji panel
    
    * Update ApplyAnimationMovementBlend.cs
    
    * Revert "Restored Animations"
    
    This reverts commit 501106b73a37633df0dfe67c5bdeb62ab1353631.
    
    * Removed avatar system
    
    * Restored Jog animation
    
    * Fixed  asmdefs and some animations
    
    * Removed pitch variation from hover sfx
    
    * Added audio atenuation when entering ExplorePanel
    
    * Removed some leftovers from failed system
    
    * Converted audio refs to proper properties
    
    * Moved scripts to preserve project structure
    
    * Changed UIAudioPlayback plugin to container
    
    * Cleanup
    
    * Added proper error reporting + filling dictionary when serializing SO
    
    * re-added websocket file
    
    * Fixed broken references after merge
    
    * Added proper logging to the avatar playback controller
    
    * Update Integration Tests World Container.asset
    
    * Update UpdateBoundariesCullingJob.cs
    
    * Fixes to container
    
    * Fix to settings dictionary conversion
    
    * Further tests
    
    * Added new WorldPlayback settings and bus
    
    * Added new calculation for audiosources
    
    * refactored logic
    
    * Fixes after merge + moved audio listener to camera
    
    * Added official audios
    
    * Extracted logic for landscape audio system
    
    * tweaks to settings
    
    * Added ocean landscape sounds
    
    * Small cleanup
    
    * Cleanup and fixes after CR
    
    * Missed call to the ocean update method :facepalm
    
    * Fixed reused jobHandle and other small issues
    
    * Update CalculateOceanAudioStatesJob.cs
    
    * Fixes after merge + modified ocean grid
    
    * Update Landscape_Ocean.asset
    
    * Restored Ocean prefab + changed how we calculate ocean sounds
    
    * Added new wood sounds and replaced it in config files

---
+++ Explorer/Assets/DCL/Audio/WorldAudioSettings.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DCL.Audio
+{
+    [CreateAssetMenu(fileName = "WorldAudioSettings", menuName = "SO/Audio/WorldAudioSettings")]
+    public class WorldAudioSettings : AudioCategorySettings, ISerializationCallbackReceiver
+    {
+        [SerializeField] private List<AudioClipTypeAndConfigKeyValuePair> audioClipConfigsList = new ();
+        private readonly Dictionary<WorldAudioClipType, AudioClipDayNightVariants> audioClipConfigs = new ();
+
+        public void OnBeforeSerialize() { }
+
+        public void OnAfterDeserialize()
+        {
+            audioClipConfigs.Clear();
+
+            foreach (AudioClipTypeAndConfigKeyValuePair clipConfig in audioClipConfigsList) { audioClipConfigs.Add(clipConfig.Key, clipConfig.Value); }
+        }
+
+        public AudioClipDayNightVariants GetAudioClipConfigForType(WorldAudioClipType type)
+        {
+            audioClipConfigs.TryGetValue(type, out AudioClipDayNightVariants clipConfig);
+            return clipConfig;
+        }
+
+        [Serializable]
+        public struct AudioClipDayNightVariants
+        {
+            public AudioClipConfig DayClip;
+            public AudioClipConfig NightClip;
+        }
+
+        [Serializable]
+        private struct AudioClipTypeAndConfigKeyValuePair
+        {
+            public WorldAudioClipType Key;
+            public AudioClipDayNightVariants Value;
+        }
+    }
+
+    public enum WorldAudioClipType
+    {
+        Landscape,
+        Ocean,
+    }
+}
