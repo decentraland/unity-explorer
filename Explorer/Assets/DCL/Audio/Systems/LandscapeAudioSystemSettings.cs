83b69eb2039e3597738479fe432de441b6e74600
Fran Colarich <56565104+fcolarich@users.noreply.github.com>
2024-04-23 03:13:11 +0200

    tweaks to settings

--- Explorer/Assets/DCL/Audio/Systems/LandscapeAudioSystemSettings.cs
+++ Explorer/Assets/DCL/Audio/Systems/LandscapeAudioSystemSettings.cs
@@ -16,14 +16,14 @@ namespace DCL.Audio.System
     public class LandscapeAudioSystemSettings : ScriptableObject, ILandscapeAudioSystemSettings
     {
         [SerializeField] private float distanceThreshold = 10f;
-        [SerializeField] private float mutingDistanceThreshold = 10f;
-        [SerializeField] private int cellsPerChunk = 9;
+        [SerializeField] private float mutingDistanceThreshold = 20f;
+        [SerializeField] private int rowsPerChunk = 3;
         [SerializeField] private int systemUpdateFrequency = 10;
         [SerializeField] private int audioSourcePositioningRetryAttempts = 3;
         [SerializeField] private int oceanDistanceThreshold = 150;
         public float ListeningDistanceThreshold => mutingDistanceThreshold;
         public float MutingDistanceThreshold => distanceThreshold;
-        public int RowsPerChunk => cellsPerChunk;
+        public int RowsPerChunk => rowsPerChunk;
         public int SystemUpdateFrequency => systemUpdateFrequency;
         public int AudioSourcePositioningRetryAttempts => audioSourcePositioningRetryAttempts;
         public int OceanDistanceThreshold => oceanDistanceThreshold;
