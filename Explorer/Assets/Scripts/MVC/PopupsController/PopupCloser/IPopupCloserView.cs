d276104a7a708073171c18ead732737f51227556
nickkhalow <nickhalow@gmail.com>
2024-04-23 13:37:06 +0400

    remove unused fake

--- Explorer/Assets/Scripts/MVC/PopupsController/PopupCloser/IPopupCloserView.cs
+++ Explorer/Assets/Scripts/MVC/PopupsController/PopupCloser/IPopupCloserView.cs
@@ -8,21 +8,5 @@ namespace MVC.PopupsController.PopupCloser
     public interface IPopupCloserView : IView
     {
         public Button CloseButton { get; }
-
-        public class Fake : IPopupCloserView
-        {
-            public void SetDrawOrder(CanvasOrdering order)
-            {
-                throw new Exception("I'm fake!");
-            }
-
-            public UniTask ShowAsync(CancellationToken ct) =>
-                throw new Exception("I'm fake!");
-
-            public UniTask HideAsync(CancellationToken ct, bool isInstant = false) =>
-                throw new Exception("I'm fake!");
-
-            public Button CloseButton => throw new Exception("I'm fake!");
-        }
     }
 }
