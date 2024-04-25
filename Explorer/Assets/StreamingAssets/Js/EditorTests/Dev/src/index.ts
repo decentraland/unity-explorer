0cfb0039b4bbebade2d57b15f3d2078ae56e6660
nickkhalow <nickhalow@gmail.com>
2024-04-25 12:18:56 +0400

    logsRegistrationWrap

--- Explorer/Assets/StreamingAssets/Js/EditorTests/Dev/src/index.ts
+++ Explorer/Assets/StreamingAssets/Js/EditorTests/Dev/src/index.ts
@@ -4,15 +4,17 @@ import { newCheckerRegistrationWrap } from './registrationWraps/checkerRegistrat
 import { Logger } from './loggers/logger'
 import { newTestsRegistrationWrap } from './registrationWraps/testsRegistrationWrap'
 import { newLazy } from './lazies/lazy'
+import { newLogsRegistrationWrap } from './registrationWraps/logsRegistrationWrap'
 
 const checkerRegistrationWrap = newLazy(newCheckerRegistrationWrap)
 const testsRegistrationWrap = newLazy(newTestsRegistrationWrap)
+const logsRegistrationWrap = newLazy(newLogsRegistrationWrap)
 
 //To be called from jsSide
 export function registerBundle(
     mutableBundle: any,
     logger: Logger
-) {
+): void {
     checkerRegistrationWrap.value().register(mutableBundle, logger)
 }
 
@@ -21,6 +23,13 @@ export function registerBundle(
 export function registerIntegrationTests(
     mutableBundle: any,
     logger: Logger
-) {
+): void {
     testsRegistrationWrap.value().register(mutableBundle, logger)
+}
+
+export function registerLogs(
+    mutableBundle: any,
+    logger: Logger
+): void {
+    logsRegistrationWrap.value().register(mutableBundle, logger)
 }
