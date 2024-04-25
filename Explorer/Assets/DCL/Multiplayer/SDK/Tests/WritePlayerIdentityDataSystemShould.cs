b1e259889bbd873c45f5957cb98be281cebaa0b1
Pravus <pravusjif@gmail.com>
2024-04-25 04:55:28 +0200

    adapted WritePlayerIdentityData system tests

--- Explorer/Assets/DCL/Multiplayer/SDK/Tests/WritePlayerIdentityDataSystemShould.cs
+++ Explorer/Assets/DCL/Multiplayer/SDK/Tests/WritePlayerIdentityDataSystemShould.cs
@@ -1,91 +1,87 @@
-// using Arch.Core;
-// using CRDT;
-// using CrdtEcsBridge.ECSToCRDTWriter;
-// using DCL.ECSComponents;
-// using DCL.Multiplayer.SDK.Components;
-// using DCL.Multiplayer.SDK.Systems;
-// using DCL.Optimization.Pools;
-// using ECS.LifeCycle.Components;
-// using ECS.TestSuite;
-// using NSubstitute;
-// using NUnit.Framework;
-// using System;
-// using WritePlayerIdentityDataSystem = DCL.Multiplayer.SDK.Systems.SceneWorld.WritePlayerIdentityDataSystem;
-//
-// namespace DCL.Multiplayer.SDK.Tests
-// {
-//     public class WritePlayerIdentityDataSystemShould : UnitySystemTestBase<WritePlayerIdentityDataSystem>
-//     {
-//         private Entity entity;
-//         private IECSToCRDTWriter ecsToCRDTWriter;
-//         private PlayerProfileDataComponent playerProfileData;
-//
-//         [SetUp]
-//         public void Setup()
-//         {
-//             ecsToCRDTWriter = Substitute.For<IECSToCRDTWriter>();
-//
-//             IComponentPool<PBPlayerIdentityData> componentPoolRegistry = Substitute.For<IComponentPool<PBPlayerIdentityData>>();
-//             var instantiatedPbComponent = new PBPlayerIdentityData();
-//             componentPoolRegistry.Get().Returns(instantiatedPbComponent);
-//             system = new WritePlayerIdentityDataSystem(world, ecsToCRDTWriter, componentPoolRegistry);
-//
-//             playerProfileData = new PlayerProfileDataComponent
-//             {
-//                 Address = "Y065SoThoT",
-//                 IsGuest = false,
-//                 CRDTEntity = 3,
-//             };
-//
-//             entity = world.Create(playerProfileData);
-//         }
-//
-//         [TearDown]
-//         public void TearDown()
-//         {
-//             world.Dispose();
-//         }
-//
-//         [Test]
-//         public void PropagatePlayerIdentityDataCorrectly()
-//         {
-//             Assert.IsFalse(world.Has<PBPlayerIdentityData>(entity));
-//             Assert.IsFalse(world.Has<CRDTEntity>(entity));
-//
-//             system.Update(0);
-//
-//             ecsToCRDTWriter.Received(1)
-//                            .PutMessage(
-//                                 Arg.Any<Action<PBPlayerIdentityData, PBPlayerIdentityData>>(),
-//                                 Arg.Is<CRDTEntity>(crdtEntity => crdtEntity.Id == playerProfileData.CRDTEntity.Id),
-//                                 Arg.Is<PBPlayerIdentityData>(comp =>
-//                                     comp.Address == playerProfileData.Address
-//                                     && comp.IsGuest == playerProfileData.IsGuest));
-//
-//             Assert.IsTrue(world.TryGet(entity, out PBPlayerIdentityData pbPlayerIdentityData));
-//             Assert.AreEqual(pbPlayerIdentityData.Address, playerProfileData.Address);
-//             Assert.AreEqual(pbPlayerIdentityData.IsGuest, playerProfileData.IsGuest);
-//             Assert.IsTrue(world.Has<CRDTEntity>(entity));
-//         }
-//
-//         [Test]
-//         public void HandleComponentRemovalCorrectly()
-//         {
-//             Assert.IsFalse(world.Has<PBPlayerIdentityData>(entity));
-//
-//             system.Update(0);
-//
-//             Assert.IsTrue(world.Has<PBPlayerIdentityData>(entity));
-//
-//             world.Remove<PlayerProfileDataComponent>(entity);
-//
-//             system.Update(0);
-//
-//             ecsToCRDTWriter.Received(1).DeleteMessage<PBPlayerIdentityData>(playerProfileData.CRDTEntity.Id);
-//             Assert.IsFalse(world.Has<PBPlayerIdentityData>(entity));
-//             Assert.IsFalse(world.Has<CRDTEntity>(entity));
-//             Assert.IsTrue(world.Has<DeleteEntityIntention>(entity));
-//         }
-//     }
-// }
+using Arch.Core;
+using CrdtEcsBridge.Components;
+using CrdtEcsBridge.ECSToCRDTWriter;
+using DCL.AvatarRendering.Wearables;
+using DCL.AvatarRendering.Wearables.Helpers;
+using DCL.ECSComponents;
+using DCL.Multiplayer.SDK.Components;
+using DCL.Profiles;
+using ECS.LifeCycle.Components;
+using ECS.TestSuite;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using WritePlayerIdentityDataSystem = DCL.Multiplayer.SDK.Systems.SceneWorld.WritePlayerIdentityDataSystem;
 
+namespace DCL.Multiplayer.SDK.Tests
+{
+    public class WritePlayerIdentityDataSystemShould : UnitySystemTestBase<WritePlayerIdentityDataSystem>
+    {
+        private const string FAKE_USER_ID = "Ia4Ia5Cth0ulhu2Ftaghn2";
+
+        private Entity entity;
+        private IECSToCRDTWriter ecsToCRDTWriter;
+        private Profile profile;
+        private PlayerCRDTEntity playerCRDTEntity;
+
+        private Avatar CreateTestAvatar() =>
+            new (BodyShape.MALE,
+                WearablesConstants.DefaultWearables.GetDefaultWearablesForBodyShape(BodyShape.MALE),
+                WearablesConstants.DefaultColors.GetRandomEyesColor(),
+                WearablesConstants.DefaultColors.GetRandomHairColor(),
+                WearablesConstants.DefaultColors.GetRandomSkinColor());
+
+        [SetUp]
+        public void Setup()
+        {
+            ecsToCRDTWriter = Substitute.For<IECSToCRDTWriter>();
+
+            system = new WritePlayerIdentityDataSystem(world, ecsToCRDTWriter);
+
+            profile = new Profile(FAKE_USER_ID, "fake user", CreateTestAvatar());
+
+            playerCRDTEntity = new PlayerCRDTEntity
+            {
+                IsDirty = true,
+                CRDTEntity = SpecialEntitiesID.OTHER_PLAYER_ENTITIES_FROM,
+            };
+
+            entity = world.Create(playerCRDTEntity);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            world.Dispose();
+        }
+
+        [Test]
+        public void DispatchPlayerIdentityDataUpdateCorrectly()
+        {
+            world.Add(entity, profile);
+
+            system.Update(0);
+
+            ecsToCRDTWriter.Received(1)
+                           .PutMessage(
+                                Arg.Any<Action<PBPlayerIdentityData, (string address, bool isGuest)>>(),
+                                playerCRDTEntity.CRDTEntity.Id,
+                                Arg.Is<(string address, bool isGuest)>(data =>
+                                    data.address == profile.UserId
+                                    && data.isGuest == !profile.HasConnectedWeb3));
+        }
+
+        [Test]
+        public void HandleComponentRemovalCorrectly()
+        {
+            world.Add(entity, profile);
+            system.Update(0);
+
+            world.Add<DeleteEntityIntention>(entity);
+
+            system.Update(0);
+
+            ecsToCRDTWriter.Received(1).DeleteMessage<PBPlayerIdentityData>(playerCRDTEntity.CRDTEntity.Id);
+        }
+    }
+}
