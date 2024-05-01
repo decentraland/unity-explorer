1af1b9ab1dd215426ff9938ff38edf5953a5a12d
Patricio Guerra <kinerius@gmail.com>
2024-05-01 17:44:41 -0300

    removed log

--- Explorer/Assets/Scripts/ECS/StreamableLoading/AudioClips/AudioClipsCache.cs
+++ Explorer/Assets/Scripts/ECS/StreamableLoading/AudioClips/AudioClipsCache.cs
@@ -39,8 +39,6 @@ namespace ECS.StreamableLoading.AudioClips
                 listedCache.Add((key, cache[key]));
             }
 
-            Debug.Log("Added audio clip to cache");
-
             ProfilingCounters.AudioClipsInCache.Value = cache.Count;
         }
 
