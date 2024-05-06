8de045075c558eab83eb79a88a6c4f96916183f3
Vitaly Popuzin <popuzin@gmail.com>
2024-05-06 13:17:28 +0200

    removed file

--- Explorer/Assets/DCL/SDKComponents/TextShape/TMPProSdkExtensions.cs
+++ Explorer/Assets/DCL/SDKComponents/TextShape/TMPProSdkExtensions.cs
@@ -1,14 +1,13 @@
-using DCL.ECSComponents;
+﻿using DCL.ECSComponents;
 using DCL.SDKComponents.TextShape.Fonts;
-using DCL.Utilities.Extensions;
 using ECS.Unity.ColorComponent;
 using System;
 using TMPro;
 using UnityEngine;
 
-namespace DCL.SDKComponents.TextShape.Renderer
+namespace DCL.SDKComponents.TextShape
 {
-    public class TMPTextShapeRenderer : ITextShapeRenderer
+    public static class TMPProSdkExtensions
     {
         private static readonly int ID_OUTLINE_COLOR = Shader.PropertyToID("_OutlineColor");
         private static readonly int ID_OUTLINE_WIDTH = Shader.PropertyToID("_OutlineWidth");
@@ -16,51 +15,23 @@ namespace DCL.SDKComponents.TextShape.Renderer
         private static readonly int ID_UNDERLAY_SOFTNESS = Shader.PropertyToID("_UnderlaySoftness");
         private static readonly int ID_UNDERLAY_OFFSET_Y = Shader.PropertyToID("_UnderlayOffsetY");
         private static readonly int ID_UNDERLAY_OFFSET_X = Shader.PropertyToID("_UnderlayOffsetX");
-        private readonly TMP_Text tmpText;
-        private readonly MeshRenderer meshRenderer;
-        private readonly MaterialPropertyBlock materialPropertyBlock;
-        private readonly RectTransform rectTransform;
-        private readonly IFontsStorage fontsStorage;
 
-        public TMPTextShapeRenderer(TMP_Text tmp, IFontsStorage fontsStorage) : this(
-            tmp,
-            tmp.GetComponent<MeshRenderer>().EnsureNotNull(),
-            new MaterialPropertyBlock(),
-            tmp.GetComponent<RectTransform>().EnsureNotNull(),
-            fontsStorage
-        ) { }
-
-        public TMPTextShapeRenderer(
-            TMP_Text tmpText,
-            MeshRenderer meshRenderer,
-            MaterialPropertyBlock materialPropertyBlock,
-            RectTransform rectTransform,
-            IFontsStorage fontsStorage
-        )
-        {
-            this.tmpText = tmpText;
-            this.meshRenderer = meshRenderer;
-            this.materialPropertyBlock = materialPropertyBlock;
-            this.rectTransform = rectTransform;
-            this.fontsStorage = fontsStorage;
-        }
-
-        public void Apply(PBTextShape textShape)
+         public static void Apply(this TextMeshPro tmpText, PBTextShape textShape, IFontsStorage fontsStorage, MaterialPropertyBlock materialPropertyBlock)
         {
             tmpText.font = fontsStorage.Font(textShape.Font) ?? tmpText.font;
 
             // NOTE: previously width and height weren't working (setting sizeDelta before anchors and offset result in sizeDelta being reset to 0,0)
-            rectTransform.anchorMin = Vector2.zero;
-            rectTransform.anchorMax = Vector2.one;
-            rectTransform.offsetMin = Vector2.zero;
-            rectTransform.offsetMax = Vector2.zero;
+            tmpText.rectTransform.anchorMin = Vector2.zero;
+            tmpText.rectTransform.anchorMax = Vector2.one;
+            tmpText.rectTransform.offsetMin = Vector2.zero;
+            tmpText.rectTransform.offsetMax = Vector2.zero;
 
             // to fix textWrapping and avoid backwards compatibility issues as result of the size being properly set (like text alignment) we only set it if textWrapping is enabled.
             float width = textShape.HasWidth ? textShape.Width : 1f;
             float height = textShape.HasHeight ? textShape.Height : 0.2f;
-            rectTransform.rect.Set(0, 0, width, height);
+            // rectTransform.rect.Set(0, 0, width, height);
 
-            rectTransform.sizeDelta = textShape.TextWrapping ? new Vector2(width, height) : Vector2.zero;
+            tmpText.rectTransform.sizeDelta = textShape.TextWrapping ? new Vector2(width, height) : Vector2.zero;
 
             tmpText.text = textShape.Text;
 
@@ -83,6 +54,14 @@ namespace DCL.SDKComponents.TextShape.Renderer
             tmpText.maxVisibleLines = textShape.HasLineCount && textShape.LineCount != 0 ? Mathf.Max(textShape.LineCount, 1) : int.MaxValue;
             tmpText.enableWordWrapping = textShape is { HasTextWrapping: true, TextWrapping: true } && !tmpText.enableAutoSizing;
 
+            tmpText.renderer.SetPropertyBlock(
+                materialPropertyBlock.Prepare(textShape));
+        }
+
+        private static MaterialPropertyBlock Prepare(this MaterialPropertyBlock materialPropertyBlock, PBTextShape textShape)
+        {
+            materialPropertyBlock.Clear();
+
             // TODO (Vit) : Disable outline when if (textShape.OutlineWidth <= 0)
             materialPropertyBlock.SetColor(ID_OUTLINE_COLOR, textShape.OutlineColor?.ToUnityColor() ?? Color.white);
             materialPropertyBlock.SetFloat(ID_OUTLINE_WIDTH, textShape.OutlineWidth);
@@ -93,17 +72,7 @@ namespace DCL.SDKComponents.TextShape.Renderer
             materialPropertyBlock.SetFloat(ID_UNDERLAY_OFFSET_X, textShape.ShadowOffsetX);
             materialPropertyBlock.SetFloat(ID_UNDERLAY_OFFSET_Y, textShape.ShadowOffsetY);
 
-            meshRenderer.SetPropertyBlock(materialPropertyBlock);
-        }
-
-        public void Show()
-        {
-            tmpText.enabled = true;
-        }
-
-        public void Hide()
-        {
-            tmpText.enabled = false;
+            return materialPropertyBlock;
         }
 
         private static TextAlignmentOptions TextAlignmentOptions(TextAlignMode mode) =>
@@ -120,8 +89,5 @@ namespace DCL.SDKComponents.TextShape.Renderer
                 TextAlignMode.TamBottomRight => TMPro.TextAlignmentOptions.BottomRight,
                 _ => throw new ArgumentOutOfRangeException(nameof(mode), mode, $"Mode {mode} is not supported"),
             };
-
-        public override string ToString() =>
-            $"{nameof(TMPTextShapeRenderer)} on {tmpText.gameObject.name}";
     }
 }
