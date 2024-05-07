6c8fba1988d258c468591524558350a81e8dd6bb
Àdam Carballo <adam@f10.dev>
2024-05-07 11:48:41 +0200

    Added test env variables, CloudBuild test

---
+++ Explorer/Assets/Editor/CloudBuild.cs
@@ -0,0 +1,27 @@
+﻿#if UNITY_CLOUD_BUILD
+using JetBrains.Annotations;
+using System;
+using UnityEngine;
+
+namespace Editor
+{
+    public static class CloudBuild
+    {
+        [UsedImplicitly]
+        public static void PreExport()
+        {
+            Debug.Log("PreExport");
+            string testEnvGit = Environment.GetEnvironmentVariable("TEST_ENV_GIT");
+            Debug.Log(testEnvGit);
+        }
+
+        [UsedImplicitly]
+        public static void PostExport()
+        {
+            Debug.Log("PostExport");
+            string testEnvGit = Environment.GetEnvironmentVariable("TEST_ENV_GIT");
+            Debug.Log(testEnvGit);
+        }
+    }
+}
+#endif
--- scripts/cloudbuild/build.py
+++ scripts/cloudbuild/build.py
@@ -4,7 +4,8 @@ import requests
 
 URL = f'https://build-api.cloud.unity3d.com/api/v1/orgs/{os.getenv('ORG_ID')}/projects/{os.getenv('PROJECT_ID')}'
 URL_TARGET = f'{URL}/buildtargets/{os.getenv('TARGET')}'
-URL_TARGET_BUILD = f'{URL_TARGET}/builds'
+URL_BUILD = f'{URL_TARGET}/builds'
+URL_ENVVARS = f'{URL_TARGET}/envvars'
 
 def create_headers(api_key):
     # Encoding API key in Base64 format
@@ -18,10 +19,21 @@ def create_headers(api_key):
 
 headers = create_headers(os.getenv('API_KEY'))
 
+# Set ENVs
+body = {
+    'TEST_ENV_GIT': 'workflowDefault'
+}
+response = requests.put(URL_ENVVARS, headers=headers, json=body)
+print("Response:", response.json())
+
+# Run Build
+body = {
+    'branch': os.getenv('BRANCH_NAME'),
+    # 'commit': '7d6423555eb96a1e7208adec2b8b7e2f74f1a18f'
+}
+response = requests.post(URL_BUILD, headers=headers, json=body)
+print("Response:", response.json())
+
 # Get build target
 # response = requests.get(URL_TARGET, headers=headers)
 # print("Response:", response.json())
-
-# Run Build
-response = requests.put(URL_TARGET_BUILD, headers=headers)
-print("Response:", response.json())
