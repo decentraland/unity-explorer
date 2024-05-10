8a48631758fb45110a36048c69e921d71b905920
Vitaly Popuzin <popuzin@gmail.com>
2024-05-10 18:26:02 +0200

    preparation for refactoring

--- Explorer/Assets/DCL/Landscape/SatelliteFloor.cs
+++ Explorer/Assets/DCL/Landscape/SatelliteFloor.cs
@@ -8,9 +8,12 @@ namespace DCL.Landscape
 {
     public class SatelliteFloor
     {
-        private const int PARCEL_SIZE = (int)ParcelMathHelper.PARCEL_SIZE;
+        private const string SATELLITE_FLOOR = "Satellite Floor";
 
+        private const int PARCEL_SIZE = (int)ParcelMathHelper.PARCEL_SIZE;
         private const int CHUNK_SIZE = 40;
+        private const int TEXTURE_SIZE = CHUNK_SIZE * PARCEL_SIZE;
+
         private const int GENESIS_HALF_PARCEL_WIDTH = 150;
         private const int SATELLITE_MAP_RESOLUTION = 8;
         private const float Z_FIGHT_THRESHOLD = 0.02f;
@@ -18,66 +21,60 @@ namespace DCL.Landscape
         private static readonly int BASE_MAP = Shader.PropertyToID("_BaseMap");
 
         private Renderer[] satelliteRenderers;
-
-        private Transform landscapeParentObject;
-        private MaterialPropertyBlock materialPropertyBlock;
         private LandscapeData landscapeData;
 
         private bool satelliteRenderersEnabled;
-        private bool Initialized;
+        private bool initialized;
 
         public void Initialize(LandscapeData config)
         {
             landscapeData = config;
-
-            landscapeParentObject = new GameObject("Satellite View").transform;
-            materialPropertyBlock = new MaterialPropertyBlock();
 
             satelliteRenderers = new Renderer[SATELLITE_MAP_RESOLUTION * SATELLITE_MAP_RESOLUTION];
         }
 
         public void Create(MapRendererTextureContainer textureContainer)
         {
-            int textureSize = CHUNK_SIZE * PARCEL_SIZE;
+            var materialPropertyBlock = new MaterialPropertyBlock();
+
             var genesisCityOffset = new Vector3(GENESIS_HALF_PARCEL_WIDTH * PARCEL_SIZE, 0, GENESIS_HALF_PARCEL_WIDTH * PARCEL_SIZE);
+            var landscapeRootObject = new GameObject(SATELLITE_FLOOR).transform;
 
-            // the map has some black weird margins, this is an approximation to fit the satellite view in place
+            // the map has some black weird margins, this is an approximation to fit the satellite floor in place
             var mapTextureMargins = new Vector3(-2 * PARCEL_SIZE, 0, -(20 * PARCEL_SIZE) + 50 - 1.7f);
-
-            var quadCenter = new Vector3(textureSize * 0.5f, 0, textureSize * 0.5f);
-            Vector3 zFightPrevention = Vector3.down * Z_FIGHT_THRESHOLD;
+            var quadCenter = new Vector3(TEXTURE_SIZE / 2f, 0, TEXTURE_SIZE / 2f);
 
             for (var x = 0; x < SATELLITE_MAP_RESOLUTION; x++)
             for (var y = 0; y < SATELLITE_MAP_RESOLUTION; y++)
             {
-                int posX = x * textureSize;
-                int posZ = y * textureSize;
+                Transform groundTile = Object.Instantiate(landscapeData.mapChunk,landscapeRootObject);
+                groundTile.name = $"{SATELLITE_FLOOR} {x},{y}";
 
-                Vector3 coord = new Vector3(posX, 0, posZ) - genesisCityOffset + quadCenter + mapTextureMargins + zFightPrevention;
+                // Position and rotation
+                int posX = x * TEXTURE_SIZE;
+                int posZ = y * TEXTURE_SIZE;
+                Vector3 position = new Vector3(posX, -Z_FIGHT_THRESHOLD, posZ) - genesisCityOffset + quadCenter + mapTextureMargins ;
+                groundTile.SetPositionAndRotation(position, Quaternion.Euler(90, 0, 0));
 
-                Transform groundTile = Object.Instantiate(landscapeData.mapChunk, landscapeParentObject, true);
-                groundTile.name = $"SatelliteView {x},{y}";
-                groundTile.SetPositionAndRotation(coord, Quaternion.Euler(90, 0, 0));
-
+                // Rendering
                 materialPropertyBlock.SetTexture(BASE_MAP, textureContainer.GetChunk(new Vector2Int(x, SATELLITE_MAP_RESOLUTION - 1 - y)));
-
                 Renderer satelliteRenderer = groundTile.GetComponent<Renderer>();
                 satelliteRenderer.SetPropertyBlock(materialPropertyBlock);
 
                 satelliteRenderers[x + (y * SATELLITE_MAP_RESOLUTION)] = satelliteRenderer;
             }
 
-            Initialized = true;
-            SwitchVisibilityAsync(landscapeData.showSatelliteView);
-        }
+            SwitchVisibilityAsync(landscapeData.showSatelliteView).Forget();
 
+            initialized = true;
+        }
 
         public async UniTask SwitchVisibilityAsync(bool isVisible)
         {
             if (satelliteRenderersEnabled == isVisible) return;
 
-            if (!Initialized)
-                await UniTask.WaitUntil(() => Initialized);
+            if (!initialized)
+                await UniTask.WaitUntil(() => initialized);
 
             satelliteRenderersEnabled = isVisible;
 
