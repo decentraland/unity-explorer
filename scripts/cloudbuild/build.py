c2b772cbb0b741ecc79e9cf3e4971b7156c63a0f
Àdam Carballo <adam@f10.dev>
2024-05-10 20:39:07 +0200

    Added target cloning, polling

--- scripts/cloudbuild/build.py
+++ scripts/cloudbuild/build.py
@@ -1,12 +1,18 @@
 import os
+import re
 import sys
 import base64
 import requests
 
 URL = f'https://build-api.cloud.unity3d.com/api/v1/orgs/{os.getenv('ORG_ID')}/projects/{os.getenv('PROJECT_ID')}'
-URL_TARGET = f'{URL}/buildtargets/{os.getenv('TARGET')}'
+URL_BUILD_TARGETS = f'{URL}/buildtargets'
+URL_TARGET = f'{URL_BUILD_TARGETS}/{os.getenv('TARGET')}'
 URL_BUILD = f'{URL_TARGET}/builds'
 URL_ENVVARS = f'{URL_TARGET}/envvars'
+URL_BUILD_ID = f'{URL_BUILD}/{build_id}'
+POLL_TIME = 30 # Seconds
+
+build_id = -1;
 
 def create_headers(api_key):
     # Encoding API key in Base64 format
@@ -19,6 +25,32 @@ def create_headers(api_key):
     }
 
 headers = create_headers(os.getenv('API_KEY'))
+
+def get_target_data():
+    response = requests.get(URL_TARGET, headers=headers)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Failed to get target data with status code:", response.status_code)
+        print("Response body:", response.text)
+        sys.exit(1)
+
+def clone_current_target():
+    body = get_target_data()
+    # Set target name based on branch
+    clone_target = f'{re.sub(r'^@T_', '', os.getenv('TARGET'))}-{re.sub('[^A-Za-z0-9]+', '', os.getenv('BRANCH_NAME'))}'
+    body['name'] = clone_target
+    body['settings']['scm']['branch'] = os.getenv('BRANCH_NAME')
+
+    response = requests.post(URL_BUILD_TARGETS, headers=headers, json=body)
+
+    if response.status_code == 201:
+        os.environ['TARGET'] = clone_target
+    else:
+        print('Target failed to start with status code:', response.status_code)
+        print('Response body:', response.text)
+        sys.exit(1)
 
 def get_param_env_variables():
     param_variables = {}
@@ -50,16 +82,67 @@ def run_build(branch):
     response = requests.post(URL_BUILD, headers=headers, json=body)
 
     if response.status_code == 202:
-        print("Build started successfully. Response:", response.json())
+        response_json = response.json()
+        print("Build started successfully. Response:", response_json)
+        build_id = int(response_json['build'])
     else:
         print("Build failed to start with status code:", response.status_code)
         print("Response body:", response.text)
         sys.exit(1)
 
+def poll_build():
+    if build_id == -1:
+        print('Error: No build ID known (-1)')
+        return
+
+    response = requests.get(URL_BUILD_ID, headers=headers)
+
+    if response.status_code != 200:
+        print(f'Failed to poll build with ID {build_id} with status code: {response.status_code}')
+        print("Response body:", response.text)
+        sys.exit(1)
+
+    response_json = response.json()
+    # { created , queued , sentToBuilder , started , restarted , success , failure , canceled , unknown }
+    status = response_json['buildStatus']
+    time = response_json['totalTimeInSeconds']
+
+    match status:
+        case 'created' | 'queued' | 'sentToBuilder' | 'started' | 'restarted':
+            print(f'Build status: {status} | Elapsed time: {time}')
+            return True
+        case 'success':
+            print(f'Build finished! | Elapsed time: {time}')
+            return False
+        case 'failure' | 'canceled' | 'unknown':
+            print(f'Build error! Last known status: "{status}" - Failing...')
+            sys.exit(1)
+            return False
+        case _:
+            print(f'Build status is not known: "{status}" - Failing...')
+            sys.exit(1)
+            return False
+
+# Entrypoint here ->
+
+# Clone current target
+# This will clone the current $TARGET and replace the value in $TARGET with it
+# Also sets the branch to $BRANCH_NAME
+clone_current_target()
+
 # Set ENVs (Parameters)
 # This must run immediately before starting a build
-# to avoid any race conditions with other concurrent builds
+# to avoid any race conditions with other concurrent builds*
+#
+# *Above warning mostly applies to shared targets, not clones
 set_parameters(get_param_env_variables())
 
 # Run Build
 run_build(os.getenv('BRANCH_NAME'))
+
+# Poll the build stats every {POLL_TIME}s
+while True:
+    if poll_build():
+        time.sleep(POLL_TIME)
+    else:
+        break
