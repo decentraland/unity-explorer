0f77686d291427cc8a5a047b94b90bb92898cd79
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2024-05-14 08:45:57 -0300

    Fixed test

--- Explorer/Assets/DCL/Roads/Tests/EditMode/UnloadRoadSystemShould.cs
+++ Explorer/Assets/DCL/Roads/Tests/EditMode/UnloadRoadSystemShould.cs
@@ -4,6 +4,7 @@ using DCL.Roads.Systems;
 using ECS.LifeCycle.Components;
 using ECS.SceneLifeCycle;
 using ECS.SceneLifeCycle.Components;
+using ECS.SceneLifeCycle.SceneDefinition;
 using ECS.TestSuite;
 using NSubstitute;
 using NUnit.Framework;
@@ -31,7 +32,7 @@ namespace DCL.Roads.Tests
             {
                 IsDirty = false, CurrentKey = "key", CurrentAsset = new GameObject().transform
             };
-            var entity = world.Create(roadInfo, new VisualSceneState(), new DeleteEntityIntention());
+            var entity = world.Create(roadInfo, new SceneDefinitionComponent(), new VisualSceneState(), new DeleteEntityIntention());
 
             // Act
             system.Update(0);
