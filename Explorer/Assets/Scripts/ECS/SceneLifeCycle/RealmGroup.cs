ad91198d5b8b81efe9c705ebaa132a1c09149587
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2024-05-17 19:30:20 -0300

    Revert "Dummycommit"
    This reverts commit 3c554a9055187ce6fe6d9c748029e6cffbd0b25d.

--- Explorer/Assets/Scripts/ECS/SceneLifeCycle/RealmGroup.cs
+++ Explorer/Assets/Scripts/ECS/SceneLifeCycle/RealmGroup.cs
@@ -6,7 +6,5 @@ namespace ECS.SceneLifeCycle
 {
     [UpdateInGroup(typeof(SimulationSystemGroup))]
     [LogCategory(ReportCategory.SCENE_LOADING)]
-    public partial class RealmGroup
-    {
-    }
+    public partial class RealmGroup { }
 }
