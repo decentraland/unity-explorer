bdd90ad90d36faa28e914b9467a00ab06c2935ec
nickkhalow <nickhalow@gmail.com>
2024-06-10 17:27:30 +0700

    remove WEB_REQUESTS category

--- Explorer/Assets/DCL/PerformanceAndDiagnostics/Diagnostics/ReportsHandling/ReportCategory.cs
+++ Explorer/Assets/DCL/PerformanceAndDiagnostics/Diagnostics/ReportsHandling/ReportCategory.cs
@@ -61,11 +61,6 @@
         public const string ECS = nameof(ECS);
 
         /// <summary>
-        ///     Global events for web requests
-        /// </summary>
-        public const string WEB_REQUESTS = nameof(WEB_REQUESTS);
-
-        /// <summary>
         ///     Unspecified web request
         /// </summary>
         public const string GENERIC_WEB_REQUEST = nameof(GENERIC_WEB_REQUEST);
