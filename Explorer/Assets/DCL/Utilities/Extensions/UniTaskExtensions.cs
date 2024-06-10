c1745de48246efa05c39a1109718841aa97de901
Mikhail Agapov <mikhail.agapov@decentraland.org>
2024-06-10 19:59:38 +0300

    Handle API exceptions in a safer contextual way
    * Introduce the generic way of suppressing exception from WRs and UniTasks

---
+++ Explorer/Assets/DCL/Utilities/Extensions/UniTaskExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Cysharp.Threading.Tasks;
+using DCL.Diagnostics;
+using System;
+using Utility;
+
+namespace DCL.Utilities.Extensions
+{
+    public static class UniTaskExtensions
+    {
+        public static UniTask<TResult?> SuppressAnyExceptionWithFallback<TResult>(this UniTask<TResult?> coreOp,
+            TResult fallbackValue, ReportData? reportData = null) =>
+            coreOp.SuppressExceptionWithFallback(fallbackValue, Extensions.SuppressExceptionWithFallback.Behaviour.SuppressAnyException, reportData);
+
+        public static async UniTask<TResult?> SuppressExceptionWithFallback<TResult>(this UniTask<TResult?> coreOp,
+            TResult fallbackValue,
+            SuppressExceptionWithFallback.Behaviour behaviour = Extensions.SuppressExceptionWithFallback.Behaviour.Default,
+            ReportData? reportData = null)
+        {
+            try { return await coreOp; }
+            catch (UnityWebRequestException e)
+            {
+                ReportException(e);
+                return fallbackValue;
+            }
+            catch (OperationCanceledException) when (EnumUtils.HasFlag(behaviour, Extensions.SuppressExceptionWithFallback.Behaviour.SuppressCancellation)) { return fallbackValue; }
+            catch (Exception e) when (EnumUtils.HasFlag(behaviour, Extensions.SuppressExceptionWithFallback.Behaviour.SuppressAnyException))
+            {
+                ReportException(e);
+                return fallbackValue;
+            }
+
+            void ReportException(Exception e)
+            {
+                if (reportData != null)
+                    ReportHub.LogException(e, reportData.Value);
+            }
+        }
+    }
+}
---
+++ Explorer/Assets/DCL/WebRequests/SuppressExceptionWithFallback.cs
@@ -0,0 +1,34 @@
+﻿using Cysharp.Threading.Tasks;
+using DCL.Diagnostics;
+using DCL.Utilities.Extensions;
+using System.Threading;
+
+namespace DCL.WebRequests
+{
+    /// <summary>
+    ///     Wraps <see cref="TCoreOp" /> in try...catch and returns the default value on exception
+    /// </summary>
+    public readonly struct SuppressExceptionWithFallback<TCoreOp, TWebRequest, TResult>
+        : IWebRequestOp<TWebRequest, TResult> where TWebRequest: struct, ITypedWebRequest
+                                              where TCoreOp: IWebRequestOp<TWebRequest, TResult>
+    {
+        private readonly TCoreOp coreOp;
+        private readonly TResult fallbackValue;
+        private readonly SuppressExceptionWithFallback.Behaviour behaviour;
+        private readonly ReportData? reportContext;
+
+        public SuppressExceptionWithFallback(TCoreOp coreOp,
+            TResult fallbackValue,
+            SuppressExceptionWithFallback.Behaviour behaviour = SuppressExceptionWithFallback.Behaviour.Default,
+            ReportData? reportContext = null)
+        {
+            this.coreOp = coreOp;
+            this.fallbackValue = fallbackValue;
+            this.behaviour = behaviour;
+            this.reportContext = reportContext;
+        }
+
+        public UniTask<TResult?> ExecuteAsync(TWebRequest webRequest, CancellationToken ct) =>
+            coreOp.ExecuteAsync(webRequest, ct).SuppressExceptionWithFallback(fallbackValue, behaviour, reportContext);
+    }
+}
