4546c48f7886f8631e4b7835b288e1caa1d0613e
Mikhail Agapov <118179774+mikhail-dcl@users.noreply.github.com>
2024-06-12 17:03:40 +0200

    Chore: Improve API exceptions handling (#1222)
    * Handle API exceptions in a safer contextual way
    
    * Introduce the generic way of suppressing exception from WRs and UniTasks
    
    * Fix Unit Tests

---
+++ Explorer/Assets/DCL/Utilities/Extensions/SuppressExceptionWithFallback.cs
@@ -0,0 +1,23 @@
+﻿using Cysharp.Threading.Tasks;
+using System;
+
+namespace DCL.Utilities.Extensions
+{
+    public struct SuppressExceptionWithFallback
+    {
+        [Flags]
+        public enum Behaviour
+        {
+            Default = 0,
+            /// <summary>
+            ///     It might be undesirable to ignore Cancellation as it's controlled from the consumer flow
+            /// </summary>
+            SuppressCancellation = 1 << 0,
+
+            /// <summary>
+            ///     By default suppresses <see cref="UnityWebRequestException" /> only
+            /// </summary>
+            SuppressAnyException = 1 << 1,
+        }
+    }
+}
