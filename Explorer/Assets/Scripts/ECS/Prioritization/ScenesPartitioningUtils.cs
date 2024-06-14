06a866345ae9aeca8501f2f8fa1eabecdd88e54e
Vitaly Popuzin <popuzin@gmail.com>
2024-06-14 13:51:09 +0200

    small renaming

--- Explorer/Assets/Scripts/ECS/Prioritization/ScenesPartitioningUtils.cs
+++ Explorer/Assets/Scripts/ECS/Prioritization/ScenesPartitioningUtils.cs
@@ -85,7 +85,7 @@ namespace ECS.Prioritization
 
             public void Execute(int index)
             {
-                ParcelCornersData corners = ParcelCorners[index];
+                ParcelCornersData cornersData = ParcelCorners[index];
                 PartitionData partition = partitions[index];
                 byte bucket = partition.Bucket;
                 bool isBehind = partition.IsBehind;
@@ -99,7 +99,7 @@ namespace ECS.Prioritization
 
                 float minSqrMagnitude = float.MaxValue;
 
-                for (var i = 0; i < corners.Corners.Length; i++)
+                for (var i = 0; i < cornersData.Corners.Length; i++)
                 {
                     void ProcessCorners(float3 corner, ref PartitionData partitionData, ref float3 position, ref float3 forward)
                     {
@@ -115,13 +115,13 @@ namespace ECS.Prioritization
                             partitionData.IsBehind = Vector3.Dot(forward, vectorToCamera) < 0;
                     }
 
-                    ParcelCorners corners1 = corners.Corners[i];
-                    ProcessCorners(corners1.minXZ, ref partition, ref CameraPosition, ref CameraForward);
-                    ProcessCorners(corners1.minXmaxZ, ref partition, ref CameraPosition, ref CameraForward);
-                    ProcessCorners(corners1.maxXminZ, ref partition, ref CameraPosition, ref CameraForward);
-                    ProcessCorners(corners1.maxXZ, ref partition, ref CameraPosition, ref CameraForward);
+                    ParcelCorners parcelCorners = cornersData.Corners[i];
+                    ProcessCorners(parcelCorners.minXZ, ref partition, ref CameraPosition, ref CameraForward);
+                    ProcessCorners(parcelCorners.minXmaxZ, ref partition, ref CameraPosition, ref CameraForward);
+                    ProcessCorners(parcelCorners.maxXminZ, ref partition, ref CameraPosition, ref CameraForward);
+                    ProcessCorners(parcelCorners.maxXZ, ref partition, ref CameraPosition, ref CameraForward);
 
-                    partition.IsPlayerInScene = PlayerPosition.IsInside(corners1);
+                    partition.IsPlayerInScene = PlayerPosition.IsInside(parcelCorners);
                 }
 
                 // Find the bucket
