c2ebaf9005377cefd701cef9a52fc2ebf5b5e293
Santi Andrade <sandrade@decentraland.org>
2024-06-25 11:46:06 +0200

    Update PassportCharacterPreviewController.cs

--- Explorer/Assets/DCL/Passport/PassportCharacterPreviewController.cs
+++ Explorer/Assets/DCL/Passport/PassportCharacterPreviewController.cs
@@ -9,7 +9,6 @@ namespace DCL.Passport
     public class PassportCharacterPreviewController : CharacterPreviewControllerBase
     {
         private readonly List<URN> shortenedWearables = new ();
-        private readonly HashSet<URN> shortenedEmotes = new ();
 
         public PassportCharacterPreviewController(CharacterPreviewView view, ICharacterPreviewFactory previewFactory, World world)
             : base(view, previewFactory, world, false) { }
@@ -17,22 +16,11 @@ namespace DCL.Passport
         public override void Initialize(Avatar avatar)
         {
             shortenedWearables.Clear();
-            shortenedEmotes.Clear();
 
             foreach (URN urn in avatar.Wearables)
                 shortenedWearables.Add(urn.Shorten());
 
-            foreach (URN urn in avatar.Emotes)
-            {
-                if (urn.IsNullOrEmpty()) continue;
-                URN shortenedUrn = urn.Shorten();
-
-                if (!shortenedEmotes.Contains(shortenedUrn))
-                    shortenedEmotes.Add(shortenedUrn);
-            }
-
             previewAvatarModel.Wearables = shortenedWearables;
-            previewAvatarModel.Emotes = shortenedEmotes;
 
             base.Initialize(avatar);
             base.PlayEmote("wave");
