14697983ad1024abf6c3652b8b7b06a6bd18c5f7
Geoff Birch <geoff.birch@decentraland.org>
2024-06-26 08:43:48 +0100

    Added MenuItem - Empty

---
+++ Explorer/Assets/DCL/Rendering/Menus/CompileSceneShader.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+namespace DCL.Rendering.Menus
+{
+    public static class CompileSceneShader
+    {
+        [MenuItem("Decentraland/Shaders/Compile \"Scene\" Shader Variants")]
+        public static void ExecuteMenuItem()
+        {
+
+        }
+    }
+}
