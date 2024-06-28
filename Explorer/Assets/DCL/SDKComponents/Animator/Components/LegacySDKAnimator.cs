1afe3b19cb0b8a8ab65f6c51579edc5e32fe6781
Nicolas Lorusso <nalorusso@gmail.com>
2024-06-28 13:51:10 -0300

    LegacySDKAnimator to struct

--- Explorer/Assets/DCL/SDKComponents/Animator/Components/LegacySDKAnimator.cs
+++ Explorer/Assets/DCL/SDKComponents/Animator/Components/LegacySDKAnimator.cs
@@ -3,5 +3,5 @@ namespace DCL.SDKComponents.Animator.Components
     /// <summary>
     /// Acts a flag so proper systems update the SDKAnimatorComponent
     /// </summary>
-    public enum LegacySDKAnimator { }
+    public struct LegacySDKAnimator { }
 }
