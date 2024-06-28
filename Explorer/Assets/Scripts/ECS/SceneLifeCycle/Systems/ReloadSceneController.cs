de8dec7ad364e235f6c15df0877daf37de0c64c2
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2024-06-28 09:02:20 -0300

    chore: reload scene through chat command (#1301)

--- Explorer/Assets/Scripts/ECS/SceneLifeCycle/Systems/ReloadSceneController.cs
+++ Explorer/Assets/Scripts/ECS/SceneLifeCycle/Systems/ReloadSceneController.cs
@@ -1,61 +1,69 @@
 ﻿using Arch.Core;
-using Arch.SystemGroups;
-using Arch.SystemGroups.Metadata;
 using Cysharp.Threading.Tasks;
 using DCL.Character.Components;
+using DCL.Chat;
+using DCL.Chat.History;
+using DCL.Chat.MessageBus;
 using DCL.DebugUtilities;
-using DCL.DebugUtilities.UIBindings;
-using DCL.Diagnostics;
-using ECS.Abstract;
 using ECS.LifeCycle.Components;
 using ECS.Prioritization.Components;
 using ECS.SceneLifeCycle.Components;
 using ECS.SceneLifeCycle.SceneDefinition;
 using ECS.StreamableLoading.Common;
-using SceneRunner.EmptyScene;
 using SceneRunner.Scene;
-using UnityEngine;
 using Utility;
 
 namespace ECS.SceneLifeCycle.Systems
 {
-    public class ReloadSceneDebugController 
+    public class ReloadSceneController
     {
-        private readonly Entity playerEntity;
-        private readonly IScenesCache scenesCache;
-        private readonly World world;
+        private Entity playerEntity;
+        private IScenesCache scenesCache;
+        private IDebugContainerBuilder debugBuilder;
+        private World world;
+        private IChatMessagesBus chatMessagesBus;
 
-        public ReloadSceneDebugController(World world, Entity playerEntity, IScenesCache scenesCache,
-            IDebugContainerBuilder debugBuilder)
+        public void Initialize(World world, Entity playerEntity, IScenesCache scenesCache, IDebugContainerBuilder debugBuilder)
         {
+            this.world = world;
             this.playerEntity = playerEntity;
             this.scenesCache = scenesCache;
-            this.world = world;
+            this.debugBuilder = debugBuilder;
 
             debugBuilder.AddWidget("Scene Reload")
-                .AddSingleButton("Reload Scene", ReloadScene);
+                .AddSingleButton("Reload Scene", () =>  chatMessagesBus.Send("/reload"));
         }
 
-        private void ReloadScene()
+        public void InitializeChatMessageBus(IChatMessagesBus containerChatMessagesBus)
+        {
+            chatMessagesBus = containerChatMessagesBus;
+        }
+
+        public async UniTask<bool> TryReloadSceneAsync()
         {
             var playerPos = world.Get<CharacterTransform>(playerEntity).Transform.position;
             var parcel = ParcelMathHelper.FloorToParcel(playerPos);
             if (scenesCache.TryGetByParcel(parcel, out var sceneInCache))
             {
+                var foundEntity = Entity.Null;
                 world.Query(in new QueryDescription().WithAll<ISceneFacade, SceneDefinitionComponent>(),
                     (Entity entity, ref ISceneFacade sceneFacade) =>
                     {
                         if (sceneFacade.Equals(sceneInCache))
-                            DisposeAndRestartAsync(entity, sceneFacade);
+                        {
+                            foundEntity = entity;
+                        }
                     });
+                if (foundEntity != Entity.Null)
+                {
+                    await DisposeAndRestartAsync(foundEntity, sceneInCache);
+                    return true;
+                }
             }
-            else
-            {
-                ReportHub.Log(ReportCategory.REALM, "You need to be in an SDK7 scene to reload it");
-            }
+            return false;
         }
 
-        private async UniTaskVoid DisposeAndRestartAsync(Entity entity, ISceneFacade currentScene)
+        private async UniTask DisposeAndRestartAsync(Entity entity, ISceneFacade currentScene)
         {
             //There is a lingering promise we need to remove, and add the DeleteEntityIntention to make the standard unload flow.
             world.Remove<AssetPromise<ISceneFacade, GetSceneFacadeIntention>>(entity);
