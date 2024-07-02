37d4f4d4138ff9f64ff99568d7fe644e967ed886
Vitaly Popuzin <popuzin@gmail.com>
2024-07-02 23:26:06 +0200

    add reactive property

---
+++ Explorer/Assets/DCL/Utilities/ReactiveProperties/IReactiveProperty.cs
@@ -0,0 +1,12 @@
+﻿namespace DCL.Utilities
+{
+    public interface IReadOnlyReactiveProperty<out T>
+    {
+        T Value { get; }
+    }
+
+    public interface IReactiveProperty<T> : IReadOnlyReactiveProperty<T>
+    {
+        new T Value { get; set; }
+    }
+}
---
+++ Explorer/Assets/DCL/Utilities/ReactiveProperties/ReactiveProperty.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace DCL.Utilities
+{
+    [Serializable]
+    public class ReactiveProperty<T> : IReactiveProperty<T>, IDisposable
+    {
+        private T latestValue;
+        private Action<T> valueChanged;
+
+        public T Value
+        {
+            get => latestValue;
+
+            set
+            {
+                latestValue = value;
+                valueChanged?.Invoke(value);
+            }
+        }
+
+        public ReactiveProperty(T initialValue)
+        {
+            latestValue = initialValue;
+        }
+
+        public void Dispose()
+        {
+            valueChanged = null;
+        }
+
+        public void Subscribe(Action<T> observer) =>
+            valueChanged += observer;
+
+        public void Unsubscribe(Action<T> observer) =>
+            valueChanged -= observer;
+
+        public static implicit operator T(ReactiveProperty<T> value) =>
+            value.Value;
+
+        public override string ToString() =>
+            latestValue!.ToString();
+    }
+}
