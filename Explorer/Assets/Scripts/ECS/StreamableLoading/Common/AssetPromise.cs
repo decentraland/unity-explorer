1b6eac75a5db2b522b140908d2ade1a5b3c2ed73
nickkhalow <nickhalow@gmail.com>
2024-07-05 13:23:15 +0300

    remove empty code block

--- Explorer/Assets/Scripts/ECS/StreamableLoading/Common/AssetPromise.cs
+++ Explorer/Assets/Scripts/ECS/StreamableLoading/Common/AssetPromise.cs
@@ -99,11 +99,6 @@ namespace ECS.StreamableLoading.Common
 
             result = default(StreamableLoadingResult<TAsset>);
 
-            if (!world.IsAlive(Entity))
-            {
-                {}
-            }
-
             if (world.TryGet(Entity, out result))
             {
                 Result = result;
