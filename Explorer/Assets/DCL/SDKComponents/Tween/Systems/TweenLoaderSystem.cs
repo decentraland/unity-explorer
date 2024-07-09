12db8f51c91b8c9e0001e0774ae1f7899e32e17d
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2024-07-09 12:57:15 -0300

    Cleanup

--- Explorer/Assets/DCL/SDKComponents/Tween/Systems/TweenLoaderSystem.cs
+++ Explorer/Assets/DCL/SDKComponents/Tween/Systems/TweenLoaderSystem.cs
@@ -40,9 +40,6 @@ namespace DCL.SDKComponents.Tween.Systems
         {
             if (pbTween.ModeCase == PBTween.ModeOneofCase.None) return;
 
-            var pbTweenCopy = pbTweenPool.Get();
-
-            // We have to keep a copy of the tween to compare for possible changes when PBTween is not correctly dirtyed by SDK scenes
             SDKTweenComponent sdkTweenComponent = new SDKTweenComponent
             {
                 IsDirty = true
