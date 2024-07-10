a11457913c0b0123c165037e7302ecbf8ddda6a3
Pravus <pravusjif@gmail.com>
2024-07-10 17:01:03 +0200

    Reimplemented websocket connection for hot reload using native ClientWebSocket

--- Explorer/Assets/Scripts/ECS/SceneLifeCycle/LocalSceneDevelopment/LocalSceneDevelopmentController.cs
+++ Explorer/Assets/Scripts/ECS/SceneLifeCycle/LocalSceneDevelopment/LocalSceneDevelopmentController.cs
@@ -1,21 +1,22 @@
 ﻿using Cysharp.Threading.Tasks;
 using ECS.SceneLifeCycle.Systems;
-using Newtonsoft.Json;
-using SocketIOClient;
-using SocketIOClient.Newtonsoft.Json;
-using SocketIOClient.Transport;
 using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
 using UnityEngine;
 
 namespace ECS.SceneLifeCycle.LocalSceneDevelopment
 {
+    // TODO: Refactor logs for ReportHandler with new tag...
     public class LocalSceneDevelopmentController
     {
         private const int TIMEOUT_SECONDS = 60;
+        private const string SCENE_CODE_UPDATE_MESSAGE = "update";
         private bool initialized = false;
         private ReloadSceneController reloadController;
-        private string localSceneServer;
-        private SocketIO? webSocket;
+        private string localSceneWebsocketServer;
+        private ClientWebSocket webSocket;
 
         public LocalSceneDevelopmentController(ReloadSceneController reloadController)
         {
@@ -27,83 +28,30 @@ namespace ECS.SceneLifeCycle.LocalSceneDevelopment
             if (initialized) return;
 
             initialized = true;
-            this.localSceneServer = localSceneServer.Replace("http", "ws");
-            Debug.Log("PRAVS - trying to connect to: " + this.localSceneServer);
+            localSceneWebsocketServer = localSceneServer.Replace("http", "ws");
+            Debug.Log("LocalSceneDevelopmentController - trying to connect to: " + localSceneWebsocketServer);
 
-            // Start websocket transport...
-            // try
-            // {
-                // await ConnectToServerAsync();
-            // }
-            // finally
-            // {
-            //     await DisconnectFromServerAsync();
-            //     await UniTask.SwitchToMainThread(ct);
-            // }
-
-            // ConnectToServerAsync().Forget();
-            UniTask.Void(ConnectToServerAsync);
+            ConnectToServerAsync().Forget();
         }
 
         private async UniTaskVoid ConnectToServerAsync()
         {
-            webSocket = InitializeWebSocket();
-            // await webSocket.ConnectAsync().AsUniTask().Timeout(TimeSpan.FromSeconds(TIMEOUT_SECONDS));
-            // Debug.Log("PRAVS - connected to local scene websocke server??? " + webSocket.Connected);
-            await webSocket.ConnectAsync().AsUniTask();
-        }
+            webSocket = new ClientWebSocket();
+            var uri = new Uri(localSceneWebsocketServer);
+            await webSocket.ConnectAsync(uri, CancellationToken.None).AsUniTask();
+            Debug.Log($"PRAVs - websocket connection state: {webSocket.State}");
 
-        private SocketIO InitializeWebSocket()
-        {
-            if (webSocket != null) return webSocket;
+            var receiveBuffer = new byte[1024];
 
-            var uri = new Uri(localSceneServer);
-
-            webSocket = new SocketIO(uri, new SocketIOOptions
+            while (webSocket.State == WebSocketState.Open)
             {
-                Path = null,
-                ConnectionTimeout = default,
-                Query = null,
-                Reconnection = true,
-                ReconnectionDelay = 0,
-                ReconnectionDelayMax = 0,
-                ReconnectionAttempts = 10,
-                RandomizationFactor = 0,
-                ExtraHeaders = null,
-                Transport = TransportProtocol.WebSocket,
-                EIO = (EngineIO)0,
-                AutoUpgrade = false,
-                Auth = null,
-                Proxy = null
-            });
+                var receiveResult = await webSocket.ReceiveAsync(new ArraySegment<byte>(receiveBuffer), CancellationToken.None);
+                var receivedMessage = Encoding.UTF8.GetString(receiveBuffer, 0, receiveResult.Count);
+                Debug.Log($"PRAVs - websocket connection received message: {receivedMessage}");
 
-            webSocket.JsonSerializer = new NewtonsoftJsonSerializer(new JsonSerializerSettings());
-
-            // webSocket.On("outcome", ProcessSignatureOutcomeMessage);
-            webSocket.On("open", (response) =>
-            {
-                Debug.Log("PRAVS - WS connection OPEN");
-            });
-            webSocket.On("error", (response) =>
-            {
-                Debug.Log("PRAVS - WS connection ERROR");
-            });
-            webSocket.On("connect", (response) =>
-            {
-                Debug.Log("PRAVS - WS connection CONNECT");
-            });
-            webSocket.On("message", (response) =>
-            {
-                Debug.Log("PRAVS - WS connection MESSAGE " + response.GetValue<string>());
-            });
-
-            return webSocket;
-        }
-
-        private async UniTask DisconnectFromServerAsync()
-        {
-            if (webSocket is { Connected: true })
-                await webSocket.DisconnectAsync();
+                if (receivedMessage == SCENE_CODE_UPDATE_MESSAGE)
+                    await reloadController.TryReloadSceneAsync();
+            }
         }
 
         public void Dispose()
