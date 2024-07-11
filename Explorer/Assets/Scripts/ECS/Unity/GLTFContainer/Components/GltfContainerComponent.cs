f9dbdbd514d54f97f86c3c4e3187c415f1f13750
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2024-07-11 11:01:57 -0300

    fix: Add hash to GLTFComponent (#1402)
    * Adding hash to GetGLTFComponent
    
    * removed setting hash and name manually
    
    * Added result on faulted entity
    
    * Simetric exception
    
    * correct exception handling

--- Explorer/Assets/Scripts/ECS/Unity/GLTFContainer/Components/GltfContainerComponent.cs
+++ Explorer/Assets/Scripts/ECS/Unity/GLTFContainer/Components/GltfContainerComponent.cs
@@ -1,12 +1,15 @@
+using System;
 using DCL.ECSComponents;
 using ECS.StreamableLoading.Common;
+using ECS.StreamableLoading.Common.Components;
 using ECS.Unity.GLTFContainer.Asset.Components;
 
 namespace ECS.Unity.GLTFContainer.Components
 {
     public struct GltfContainerComponent
     {
-        public string Source => Promise.LoadingIntention.Name;
+        public string Hash => Promise.LoadingIntention.Hash;
+        public string Name => Promise.LoadingIntention.Name;
 
         public ColliderLayer VisibleMeshesCollisionMask;
         public ColliderLayer InvisibleMeshesCollisionMask;
@@ -22,5 +25,21 @@ namespace ECS.Unity.GLTFContainer.Components
             Promise = promise;
             State = LoadingState.Unknown;
         }
+
+        public static GltfContainerComponent CreateFaulty(Exception exception)
+        {
+            GltfContainerComponent component = new GltfContainerComponent();
+            component.SetFaulty(exception);
+            return component;
+        }
+
+        public void SetFaulty(Exception exception)
+        {
+            State = LoadingState.FinishedWithError;
+            Promise = AssetPromise<GltfContainerAsset, GetGltfContainerAssetIntention>.CreateFinalized(
+                default,
+                new StreamableLoadingResult<GltfContainerAsset>(exception));
+        }
+
     }
 }
