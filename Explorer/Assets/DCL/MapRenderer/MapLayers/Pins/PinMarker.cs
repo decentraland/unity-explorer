d4cec9726b99e4563fbe9716b70eab4353b4d225
davidejensen <davidejensen@live.it>
2024-07-11 13:34:44 +0200

    Feat: add map pins layer (#1306)
    * Feat: add map pins layer
    
    * Added new map pin sdk component and loading logic
    
    * Added deletion of map pin components
    
    * Added full logic for map pins instantiation
    
    * Added support for map pin tooltip
    
    * Added floating panel for map pins
    
    * Add quest textures
    
    * Added hover animation and floating panel template
    
    * Minor improvements to map pins UIs
    
    * Minor improvements to map pin panel
    
    * Minor code cleanup
    
    * Applied code suggestions
    
    * Minor code improvements
    
    * Minor fix
    
    * Test fix
    
    * Applied code suggestions
    
    * Update textures
    
    * Merged in a single query position and texture resolving
    
    * Bridge MapPin Handling via a single combined delegate
    
    ---------
    
    Signed-off-by: davidejensen <davidejensen@live.it>
    Co-authored-by: Romina Marchetti <rma.marchetti@gmail.com>
    Co-authored-by: Mikhail Agapov <mikhail.agapov@decentraland.org>

---
+++ Explorer/Assets/DCL/MapRenderer/MapLayers/Pins/PinMarker.cs
@@ -0,0 +1,100 @@
+using DCL.MapRenderer.CommonBehavior;
+using DCL.MapRenderer.Culling;
+using DG.Tweening;
+using System;
+using UnityEngine;
+using UnityEngine.Pool;
+
+namespace DCL.MapRenderer.MapLayers.Pins
+{
+    internal class PinMarker : IPinMarker
+    {
+        internal const int MAX_TITLE_LENGTH = 29;
+
+        private readonly IMapCullingController cullingController;
+
+        private MapMarkerPoolableBehavior<PinMarkerObject> poolableBehavior;
+        private float currentBaseScale;
+        private float currentNewScale;
+
+        public Vector3 CurrentPosition => poolableBehavior.currentPosition;
+
+        public bool IsVisible => poolableBehavior.isVisible;
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public Vector2Int ParcelPosition { get; private set; }
+
+        public Vector2 Pivot => new (0.5f, 0.5f);
+
+        public PinMarker(IObjectPool<PinMarkerObject> objectsPool, IMapCullingController cullingController)
+        {
+            poolableBehavior = new MapMarkerPoolableBehavior<PinMarkerObject>(objectsPool);
+            this.cullingController = cullingController;
+        }
+
+        public void Dispose()
+        {
+            OnBecameInvisible();
+            cullingController.StopTracking(this);
+        }
+
+        public void SetPosition(Vector2 position, Vector2Int parcelPosition)
+        {
+            ParcelPosition = parcelPosition;
+            poolableBehavior.SetCurrentPosition(position);
+        }
+
+        public void AnimateIn()
+        {
+            poolableBehavior.instance.gameObject.transform.DOScaleX(poolableBehavior.instance.gameObject.transform.localScale.x * 1.5f, 0.5f).SetEase(Ease.OutBack);
+            poolableBehavior.instance.gameObject.transform.DOScaleY(poolableBehavior.instance.gameObject.transform.localScale.x * 1.5f, 0.5f).SetEase(Ease.OutBack);
+        }
+
+        public void AnimateOut()
+        {
+            poolableBehavior.instance.gameObject.transform.DOScaleX(currentNewScale, 0.5f).SetEase(Ease.OutBack);
+            poolableBehavior.instance.gameObject.transform.DOScaleY(currentNewScale, 0.5f).SetEase(Ease.OutBack);
+        }
+
+        public void SetData(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+
+        public void SetTexture(Texture2D texture)
+        {
+            poolableBehavior.instance.SetTexture(texture);
+        }
+
+        public void OnBecameVisible()
+        {
+            poolableBehavior.OnBecameVisible();
+
+            if(currentBaseScale != 0)
+                poolableBehavior.instance.SetScale(currentBaseScale, currentNewScale);
+        }
+
+        public void OnBecameInvisible()
+        {
+            poolableBehavior.OnBecameInvisible();
+        }
+
+        public void SetZoom(float baseScale, float baseZoom, float zoom)
+        {
+            currentBaseScale = baseScale;
+            currentNewScale = Math.Max(zoom / baseZoom * baseScale, baseScale);
+
+            if (poolableBehavior.instance != null)
+                poolableBehavior.instance.SetScale(currentBaseScale, currentNewScale);
+        }
+
+        public void ResetScale(float scale)
+        {
+            currentNewScale = scale;
+
+            if (poolableBehavior.instance != null)
+                poolableBehavior.instance.SetScale(scale, scale);
+        }
+    }
+}
