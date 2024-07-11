cce71da3584b08e04e45b266c40e441131b28bf0
Fran Colarich <56565104+fcolarich@users.noreply.github.com>
2024-07-11 23:44:28 +0200

    Added missing ens utils

---
+++ Explorer/Assets/DCL/Utilities/EnsUtils.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace DCL.Utilities
+{
+    public static class EnsUtils
+    {
+        private const string WORLD_URL = "https://worlds-content-server.decentraland.org/world/";
+
+        public static string ConvertEnsToWorldUrl(string ens) =>
+            WORLD_URL + ens.ToLower();
+
+        public static bool ValidateEns(string ens) =>
+            !string.IsNullOrEmpty(ens) && Regex.IsMatch(ens, @"^[a-zA-Z0-9.]+\.eth$");
+    }
+}
