6b42f559aeb50cd0d7210e4f1879bb7ab9333b02
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2024-07-14 08:12:47 -0300

    Eliminaed everything iside CloudBuild
    Signed-off-by: Juan Ignacio Molteni <juanignaciomolteni@gmail.com>

--- Explorer/Assets/Editor/CloudBuild.cs
+++ Explorer/Assets/Editor/CloudBuild.cs
@@ -18,23 +18,6 @@ namespace Editor
         public static void PreExport()
         {
             Debug.Log("~~ PreExport ~~");
-
-            // Get all environment variables
-            var environmentVariables = Environment.GetEnvironmentVariables();
-            Parameters = environmentVariables.Cast<DictionaryEntry>().ToDictionary(x => x.Key.ToString(), x => x.Value);
-
-            // E.g. access like:
-            // Debug.Log(Parameters["TEST_VALUE"] as string);
-
-            //Unity suggestion: 1793168
-            //This should ensure that the rosyln compiler has been ran and everything is generated as needed.
-            EditorApplication.ExecuteMenuItem("File/Save Project");
-
-            // Set version for this build
-            var buildVersion = Parameters["BUILD_VERSION"] as string;
-            PlayerSettings.bundleVersion = buildVersion;
-            PlayerSettings.macOS.buildNumber = buildVersion;
-            Debug.Log($"Build version set to: {buildVersion}");
         }
 
         [UsedImplicitly]
