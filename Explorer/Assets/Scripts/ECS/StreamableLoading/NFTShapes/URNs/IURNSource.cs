044e27de985fcb55f9aeba5440d5964dcb9f96ea
Nick Khalow <71646502+NickKhalow@users.noreply.github.com>
2024-07-18 22:27:07 +0700

    Fix: nft instantiating and appearing (#1408)
    * fix InstantiateNftShapeSystem
    
    * fix LoadCycleNftShapeSystem
    
    * fix World Plugins Container
    
    * fix urn parsing
    
    * update max nft size up to 8 mb
    
    * remove delete intention OrphanChildrenOfDeletedEntity
    
    * PBNftShape partitioning
    
    * debug panel feature: show all entities in the world
    
    * fix assets
    
    * fix parenting and pooling issues
    
    * fix sizing
    
    ---------
    
    Co-authored-by: Ashley Canning <ashley.canning@decentraland.org>

--- Explorer/Assets/Scripts/ECS/StreamableLoading/NFTShapes/URNs/IURNSource.cs
+++ Explorer/Assets/Scripts/ECS/StreamableLoading/NFTShapes/URNs/IURNSource.cs
@@ -4,15 +4,15 @@ namespace ECS.StreamableLoading.NFTShapes.URNs
 {
     public interface IURNSource
     {
+        const string BASE_URL = "https://opensea.decentraland.org/api/v2/chain/{chain}/contract/{address}/nfts/{id}";
+
         URLAddress UrlOrEmpty(URN urn);
 
         class Fake : IURNSource
         {
             private readonly URLAddress url;
 
-            public Fake(string url) : this(URLAddress.FromString(url))
-            {
-            }
+            public Fake(string url) : this(URLAddress.FromString(url)) { }
 
             public Fake(URLAddress url)
             {
