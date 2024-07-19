b5e15fc17077545f7c6ddf96ae108a3c110395d9
nickkhalow <nickhalow@gmail.com>
2024-07-19 18:42:14 +0300

    UIPointerEventsSystem nullables

--- Explorer/Assets/DCL/SDKComponents/SceneUI/Systems/UIPointerEvents/UIPointerEventsSystem.cs
+++ Explorer/Assets/DCL/SDKComponents/SceneUI/Systems/UIPointerEvents/UIPointerEventsSystem.cs
@@ -34,10 +34,10 @@ namespace DCL.SDKComponents.SceneUI.Systems.UIPointerEvents
 
         protected override void Update(float _)
         {
-            TriggerPointerEventsQuery(World);
+            TriggerPointerEventsQuery(World!);
 
-            HandleEntityDestructionQuery(World);
-            HandleUIPointerEventsRemovalQuery(World);
+            HandleEntityDestructionQuery(World!);
+            HandleUIPointerEventsRemovalQuery(World!);
         }
 
         [Query]
@@ -55,12 +55,12 @@ namespace DCL.SDKComponents.SceneUI.Systems.UIPointerEvents
                 return;
 
             // Check if the component has any pointer events associated
-            foreach (var pEvent in sdkModel.PointerEvents)
+            foreach (var pEvent in sdkModel.PointerEvents!)
             {
                 if (pEvent.EventType != uiTransformComponent.PointerEventTriggered)
                     continue;
 
-                AppendMessage(ref sdkEntity, pEvent.EventInfo.Button, pEvent.EventType);
+                AppendMessage(ref sdkEntity, pEvent.EventInfo!.Button, pEvent.EventType);
                 break;
             }
 
@@ -79,18 +79,22 @@ namespace DCL.SDKComponents.SceneUI.Systems.UIPointerEvents
 
         private void AppendMessage(ref CRDTEntity sdkEntity, InputAction button, PointerEventType eventType)
         {
-            ecsToCRDTWriter.AppendMessage<PBPointerEventsResult, (RaycastHit sdkHit, InputAction button, PointerEventType eventType, ISceneStateProvider sceneStateProvider)>(
+            ecsToCRDTWriter.AppendMessage<PBPointerEventsResult, (RaycastHit? sdkHit, InputAction button, PointerEventType eventType, ISceneStateProvider sceneStateProvider)>(
                 static (result, data) =>
                 {
                     result.Hit = data.sdkHit;
                     result.Button = data.button;
                     result.State = data.eventType;
-                    result.Timestamp = data.sceneStateProvider.TickNumber;
+                    result.Timestamp = data.sceneStateProvider!.TickNumber;
                     result.TickNumber = data.sceneStateProvider.TickNumber;
-                }, sdkEntity, (int)sceneStateProvider.TickNumber, (null, button, eventType, sceneStateProvider));
+                },
+                sdkEntity,
+                (int)sceneStateProvider.TickNumber,
+                (null, button, eventType, sceneStateProvider)
+            );
         }
 
-        private void RemovePointerEvents(ref UITransformComponent uiTransformComponent, ref PBUiTransform sdkModel)
+        private static void RemovePointerEvents(ref UITransformComponent uiTransformComponent, ref PBUiTransform sdkModel)
         {
             uiTransformComponent.Transform.pickingMode = sdkModel.PointerFilter == PointerFilterMode.PfmBlock ? PickingMode.Position : PickingMode.Ignore;
             uiTransformComponent.UnregisterPointerCallbacks();
