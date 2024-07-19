23a288e2496ee51025ab660385c6cf5ec82d377f
Nicolas Lorusso <nalorusso@gmail.com>
2024-07-19 11:26:19 -0300

    Revert "removed test"
    This reverts commit f0943536064edcf4b591786ae6591edbd2f492fd.

--- Explorer/Assets/Scripts/SceneRunner/Scene/Tests/SceneExceptionHandlerShould.cs
+++ Explorer/Assets/Scripts/SceneRunner/Scene/Tests/SceneExceptionHandlerShould.cs
@@ -1,6 +1,7 @@
 ﻿using Arch.System;
 using Arch.SystemGroups;
 using Arch.SystemGroups.DefaultSystemGroups;
+using CRDT.Protocol;
 using DCL.Diagnostics;
 using NSubstitute;
 using NUnit.Framework;
@@ -67,6 +68,21 @@ namespace SceneRunner.Scene.Tests
             }
 
             sceneStateProvider.DidNotReceive().State = Arg.Any<SceneState>();
+        }
+
+        [Test]
+        public void SuspendIfToleranceExceededWhenJavascriptException()
+        {
+            for (var i = 0; i < SceneExceptionsHandler.ECS_EXCEPTIONS_PER_MINUTE_TOLERANCE + 1; i++)
+                sceneExceptionsHandler.OnJavaScriptException(new Exception("TEST"));
+
+            sceneStateProvider.Received().State = SceneState.EcsError;
+
+            reportHandler.Mock.Received(1)
+                         .LogException(
+                              Arg.Is<SceneExecutionException>(e => e.InnerExceptions.Count == SceneExceptionsHandler.ECS_EXCEPTIONS_PER_MINUTE_TOLERANCE + 1));
+
+            sceneStateProvider.Received().State = SceneState.JavaScriptError;
         }
 
         [Test]
