a680ccc1116be92db8147a8446c43c2f762d4a39
Vitaly Popuzin <popuzin@gmail.com>
2024-07-22 16:31:38 +0200

    add locks

--- Explorer/Assets/Scripts/CrdtEcsBridge/UpdateGate/SystemsDirtyMarkerPriorityGate.cs
+++ Explorer/Assets/Scripts/CrdtEcsBridge/UpdateGate/SystemsDirtyMarkerPriorityGate.cs
@@ -6,31 +6,37 @@ namespace CrdtEcsBridge.UpdateGate
 {
     public class SystemsDirtyMarkerPriorityGate : ISystemsUpdateGate
     {
-        private HashSet<Type> opened;
+        private readonly HashSet<Type> opened = new ();
 
         public void Open<T>()
         {
-            opened.Add(typeof(T));
+            lock (opened) { opened.Add(typeof(T)); }
         }
 
         public void Close<T>()
         {
-            opened.Remove(typeof(T));
+            lock (opened) { opened.Remove(typeof(T)); }
         }
 
-        public bool IsOpen<T>() where T : IDirtyMarker =>
-            opened.Contains(typeof(T));
+        public bool IsOpen<T>() where T: IDirtyMarker
+        {
+            lock (opened) { return opened.Contains(typeof(T)); }
+        }
 
-        public bool IsClosed<T>() where T : IDirtyMarker =>
-            !IsOpen<T>();
+        public bool IsClosed<T>() where T: IDirtyMarker
+        {
+            lock (opened) { return !IsOpen<T>(); }
+        }
     }
 
     public interface ISystemsUpdateGate
     {
         public void Open<T>();
+
         public void Close<T>();
 
         bool IsOpen<T>() where T: IDirtyMarker;
+
         bool IsClosed<T>() where T: IDirtyMarker;
     }
 }
