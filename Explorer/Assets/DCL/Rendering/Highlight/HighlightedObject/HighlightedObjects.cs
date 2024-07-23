03ddf39a15542dc76d0c3b1dcc8b210124abbad1
nickkhalow <nickhalow@gmail.com>
2024-07-23 13:09:56 +0400

    optimise HighlightedObjects

--- Explorer/Assets/DCL/Rendering/Highlight/HighlightedObject/HighlightedObjects.cs
+++ Explorer/Assets/DCL/Rendering/Highlight/HighlightedObject/HighlightedObjects.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -15,27 +14,16 @@ namespace DCL.Rendering.Highlight.HighlightedObject
 
         public void Highlight(Renderer renderer, Color color, float thickness)
         {
-            if (!highLightRenderers.ContainsKey(renderer))
+            highLightRenderers[renderer] = new HighlightSettings
             {
-                highLightRenderers.Add(renderer, new HighlightSettings
-                {
-                    Color = color,
-                    Width = thickness,
-                });
-            }
-            else
-            {
-                HighlightSettings highlightSettings = highLightRenderers[renderer];
-                highlightSettings.Color = color;
-                highlightSettings.Width = thickness;
-                highLightRenderers[renderer] = highlightSettings;
-            }
+                Color = color,
+                Width = thickness,
+            };
         }
 
         public void Disparage(Renderer renderer)
         {
-            if (highLightRenderers.ContainsKey(renderer))
-                highLightRenderers.Remove(renderer);
+            highLightRenderers.Remove(renderer);
         }
 
         public void DisparageAll()
