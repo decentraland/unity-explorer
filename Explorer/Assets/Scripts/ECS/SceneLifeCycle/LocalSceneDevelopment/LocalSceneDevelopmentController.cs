9d1fa51ced9715857564bcf805b4c16074c73cb9
Nicolas Lorusso <56365551+lorux0@users.noreply.github.com>
2024-07-25 17:49:23 -0300

    fix: scene account switch (#1492)

--- Explorer/Assets/Scripts/ECS/SceneLifeCycle/LocalSceneDevelopment/LocalSceneDevelopmentController.cs
+++ Explorer/Assets/Scripts/ECS/SceneLifeCycle/LocalSceneDevelopment/LocalSceneDevelopmentController.cs
@@ -6,34 +6,37 @@ using Google.Protobuf;
 using System;
 using System.Net.WebSockets;
 using System.Threading;
+using Utility;
 
 namespace ECS.SceneLifeCycle.LocalSceneDevelopment
 {
     public class LocalSceneDevelopmentController
     {
-        private readonly ReloadSceneController reloadController;
+        private readonly IReloadScene reloadScene;
+        private readonly CancellationTokenSource connectToServerCancellationToken = new ();
         private ClientWebSocket? webSocket;
 
-        public LocalSceneDevelopmentController(ReloadSceneController reloadController, string localSceneServer)
+        public LocalSceneDevelopmentController(IReloadScene reloadScene, string localSceneServer)
         {
-            this.reloadController = reloadController;
+            this.reloadScene = reloadScene;
 
             ConnectToServerAsync(
                     localSceneServer.Contains("https") ? localSceneServer.Replace("https", "wss") : localSceneServer.Replace("http", "ws"),
                     new WsSceneMessage(),
-                    new byte[1024]
+                    new byte[1024],
+                    connectToServerCancellationToken.Token
                 )
                .Forget();
         }
 
-        private async UniTaskVoid ConnectToServerAsync(string localSceneWebsocketServer, WsSceneMessage wsSceneMessage, byte[] receiveBuffer)
+        private async UniTaskVoid ConnectToServerAsync(string localSceneWebsocketServer, WsSceneMessage wsSceneMessage, byte[] receiveBuffer, CancellationToken ct)
         {
             await UniTask.SwitchToThreadPool();
 
             ReportHub.Log(ReportCategory.SDK_LOCAL_SCENE_DEVELOPMENT, $"Trying to connect to: {localSceneWebsocketServer}");
 
             webSocket = new ClientWebSocket();
-            await webSocket.ConnectAsync(new Uri(localSceneWebsocketServer), CancellationToken.None);
+            await webSocket.ConnectAsync(new Uri(localSceneWebsocketServer), ct);
 
             ReportHub.Log(ReportCategory.SDK_LOCAL_SCENE_DEVELOPMENT, $"Websocket connection state: {webSocket.State}");
 
@@ -42,7 +45,7 @@ namespace ECS.SceneLifeCycle.LocalSceneDevelopment
                 // every iteration starts on the thread pool
                 await UniTask.SwitchToThreadPool();
 
-                WebSocketReceiveResult? receiveResult = await webSocket.ReceiveAsync(receiveBuffer, CancellationToken.None);
+                WebSocketReceiveResult? receiveResult = await webSocket.ReceiveAsync(receiveBuffer, ct);
 
                 if (receiveResult.MessageType == WebSocketMessageType.Binary)
                 {
@@ -52,12 +55,12 @@ namespace ECS.SceneLifeCycle.LocalSceneDevelopment
                     // TODO: Discriminate 'wsSceneMessage.MessageCase == WsSceneMessage.MessageOneofCase.UpdateModel' to only update GLTF models...
 
                     // Switch to the main thread because `TryReloadSceneAsync` requires that
-                    await UniTask.SwitchToMainThread();
-                    await reloadController.TryReloadSceneAsync();
+                    await UniTask.SwitchToMainThread(cancellationToken: ct);
+                    await reloadScene.TryReloadSceneAsync(ct);
                 }
                 else if (receiveResult.MessageType == WebSocketMessageType.Close)
                 {
-                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, null, CancellationToken.None);
+                    await webSocket.CloseAsync(WebSocketCloseStatus.NormalClosure, null, ct);
                     ReportHub.Log(ReportCategory.SDK_LOCAL_SCENE_DEVELOPMENT, $"Websocket connection closed.");
                 }
             }
@@ -71,6 +74,7 @@ namespace ECS.SceneLifeCycle.LocalSceneDevelopment
                 webSocket?.Dispose();
             }
             catch (ObjectDisposedException) { }
+            connectToServerCancellationToken.SafeCancelAndDispose();
         }
     }
 }
