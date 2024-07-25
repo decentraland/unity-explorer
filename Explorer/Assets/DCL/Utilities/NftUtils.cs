ca0728c59f7f372dff44c245f7775a787ed9b681
Santi Andrade <sandrade@decentraland.org>
2024-07-25 17:17:56 +0200

    fix: support for multiple nft chains

--- Explorer/Assets/DCL/Utilities/NftUtils.cs
+++ Explorer/Assets/DCL/Utilities/NftUtils.cs
@@ -5,14 +5,15 @@ namespace DCL.Utilities
 {
     public class NftUtils
     {
-        public static bool TryParseUrn(URN urn, out string contractAddress, out string tokenId)
+        public static bool TryParseUrn(URN urn, out string chain, out string contractAddress, out string tokenId)
         {
             const char SEPARATOR = ':';
             const string DCL_URN_ID = "urn:decentraland";
-            const string CHAIN_ETHEREUM = "ethereum";
+            const string COLLECTIONS_THIRDPARTY = "collections-thirdparty";
 
             contractAddress = string.Empty;
             tokenId = string.Empty;
+            chain = string.Empty;
 
             try
             {
@@ -23,27 +24,36 @@ namespace DCL.Utilities
                     return false;
                 urnSpan = urnSpan.Slice(DCL_URN_ID.Length + 1);
 
-                // TODO: allow 'matic' chain when Opensea implements its APIv2 "retrieve assets" endpoint
-                // (https://docs.opensea.io/v2.0/reference/api-overview) in the future
                 // 2: chain/network
-                var chainSpan = urnSpan.Slice(0, CHAIN_ETHEREUM.Length);
-                if (!chainSpan.Equals(CHAIN_ETHEREUM, StringComparison.Ordinal))
-                    return false;
+                var chainSpan = urnSpan.Slice(0, urnSpan.IndexOf(SEPARATOR));
                 urnSpan = urnSpan.Slice(chainSpan.Length + 1);
 
                 // 3: contract standard
                 var contractStandardSpan = urnSpan.Slice(0, urnSpan.IndexOf(SEPARATOR));
                 urnSpan = urnSpan.Slice(contractStandardSpan.Length + 1);
 
-                // 4: contract address
-                var contractAddressSpan = urnSpan.Slice(0, urnSpan.IndexOf(SEPARATOR));
-                urnSpan = urnSpan.Slice(contractAddressSpan.Length + 1);
+                // check if wearables is third-party
+                if (contractStandardSpan.ToString().Equals(COLLECTIONS_THIRDPARTY, StringComparison.Ordinal))
+                {
+                    // 4: contract address
+                    var contractAddressSpan = urnSpan;
+                    contractAddress = contractAddressSpan.ToString();
 
-                // 5: token id
-                var tokenIdSpan = urnSpan;
-                contractAddress = contractAddressSpan.ToString();
-                tokenId = tokenIdSpan.ToString();
+                    // NOTE: Third Party wearables do not have token id at the moment
+                }
+                else
+                {
+                    // 4: contract address
+                    var contractAddressSpan = urnSpan.Slice(0, urnSpan.IndexOf(SEPARATOR));
+                    urnSpan = urnSpan.Slice(contractAddressSpan.Length + 1);
 
+                    // 5: token id
+                    var tokenIdSpan = urnSpan;
+                    contractAddress = contractAddressSpan.ToString();
+                    tokenId = tokenIdSpan.ToString();
+                }
+
+                chain = chainSpan.ToString();
                 return true;
             }
             catch (Exception)
