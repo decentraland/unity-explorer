80668fa6c157c149df69929b83f83681e005de56
Vitaly Popuzin <35366872+popuz@users.noreply.github.com>
2024-07-25 09:18:36 +0200

    Fix: stutter avatar on moving platform (#1489)
    ## What does this PR change?
    fix #1294 
    fix #1029 
    fix #1068
    
    - disable gravity when player is on moving platform

---
+++ Explorer/Assets/DCL/Utilities/DCLPhysics.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace DCL.Utilities
+{
+    /// <summary>
+    /// Decorator over Unity's Physics with Debug gizmos info.
+    /// </summary>
+    public static class DCLPhysics
+    {
+        /// <summary>
+        /// Unity SphereCast with Debug gizmos.
+        /// </summary>
+        public static bool SphereCast(Ray ray, float radius, out RaycastHit hitInfo, float maxDistance, int layerMask, QueryTriggerInteraction queryTriggerInteraction)
+        {
+            bool hasHit = Physics.SphereCast(ray, radius, out hitInfo, maxDistance, layerMask, queryTriggerInteraction);
+            if (Application.isEditor) DebugUtils.DrawRaycast(ray.origin, maxDistance, hasHit, hitInfo, radius);
+            return hasHit;
+        }
+
+        /// <summary>
+        /// Unity Raycast with Debug gizmos.
+        /// </summary>
+        public static bool Raycast(Ray ray, out RaycastHit hitInfo, float maxDistance, int layerMask, QueryTriggerInteraction queryTriggerInteraction)
+        {
+            bool hasHit = Physics.Raycast(ray, out hitInfo, maxDistance, layerMask, queryTriggerInteraction);
+            if (Application.isEditor) DebugUtils.DrawRaycast(ray.origin, maxDistance, hasHit, hitInfo, 0.1f);
+            return hasHit;
+        }
+    }
+}
