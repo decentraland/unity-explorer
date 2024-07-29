1ba11b9537f29354c5cb1663b249592b8da99c4d
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2024-07-29 08:56:37 -0300

    fix: Get comms adapter directly from Realm (#1550)

--- Explorer/Assets/DCL/Multiplayer/Connections/Archipelago/AdapterAddress/Current/ICurrentAdapterAddress.cs
+++ Explorer/Assets/DCL/Multiplayer/Connections/Archipelago/AdapterAddress/Current/ICurrentAdapterAddress.cs
@@ -1,19 +1,17 @@
-using Cysharp.Threading.Tasks;
-using DCL.Multiplayer.Connections.Archipelago.WorldAboutUrl.Current;
-using DCL.WebRequests;
 using ECS;
-using System.Threading;
 
 namespace DCL.Multiplayer.Connections.Archipelago.AdapterAddress.Current
 {
     public interface ICurrentAdapterAddress
     {
-        UniTask<string> AdapterUrlAsync(CancellationToken token);
+        string AdapterUrlAsync();
 
-        public static ICurrentAdapterAddress NewDefault(IWebRequestController webRequestController, IRealmData realmData) =>
-            new CurrentAdapterAddress(
-                IAdapterAddresses.NewDefault(webRequestController),
-                ICurrentWorldAboutUrl.NewDefault(realmData)
+        public static ICurrentAdapterAddress NewDefault(IRealmData realmData)
+        {
+            return new CurrentAdapterAddress(
+                IAdapterAddresses.NewDefault(),
+                realmData
             );
+        }
     }
 }
