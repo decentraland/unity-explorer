bd2a19a26aa49d3bb8e4d710d12a69f1d68f2322
nickkhalow <nickhalow@gmail.com>
2024-08-06 09:51:42 +0400

    URLHealthCheck remove extra attempts

--- Explorer/Assets/DCL/Multiplayer/HealthChecks/URLHealthCheck.cs
+++ Explorer/Assets/DCL/Multiplayer/HealthChecks/URLHealthCheck.cs
@@ -12,11 +12,17 @@ namespace DCL.Multiplayer.HealthChecks
         private readonly IWebRequestController webRequestController;
         private readonly IDecentralandUrlsSource decentralandUrlsSource;
         private readonly DecentralandUrl url;
+
         private static readonly HashSet<int> ERROR_CODES = new ()
         {
             404,
             500,
         };
+
+        /// <summary>
+        ///     Retries should be handles above with RetriesHealthCheck
+        /// </summary>
+        private const int ATTEMPTS = 1;
 
         public URLHealthCheck(IWebRequestController webRequestController, IDecentralandUrlsSource decentralandUrlsSource, DecentralandUrl url)
         {
@@ -28,7 +34,7 @@ namespace DCL.Multiplayer.HealthChecks
         public async UniTask<(bool success, string? errorMessage)> IsRemoteAvailableAsync(CancellationToken ct)
         {
             var urlAddress = Url();
-            int code = await webRequestController.HeadAsync(new CommonArguments(urlAddress), ct).StatusCodeAsync();
+            int code = await webRequestController.HeadAsync(new CommonArguments(urlAddress, attemptsCount: ATTEMPTS), ct).StatusCodeAsync();
             bool success = ERROR_CODES.Contains(code) == false;
             return (success, success ? null : $"Cannot connect to {urlAddress}");
         }
