85a52e065526217672975e40f1390360c2fa7e15
Vitaly Popuzin <popuzin@gmail.com>
2024-08-09 14:37:36 +0200

    encoding parcels in 17 bits

--- Explorer/Assets/DCL/Multiplayer/Movement/MovementTimestampCompressor.cs
+++ Explorer/Assets/DCL/Multiplayer/Movement/MovementTimestampCompressor.cs
@@ -1,6 +1,7 @@
 ﻿using DCL.CharacterMotion.Components;
 using System;
 using UnityEngine;
+using Utility;
 
 namespace DCL.Multiplayer.Movement.Systems
 {
@@ -23,7 +24,29 @@ namespace DCL.Multiplayer.Movement.Systems
     {
         public NetworkAnimState AnimState;
         public long compressedData;
+        public Vector3 remainedPosition;
+
         public NetworkMovementMessage message;
+    }
+
+    public static class ParcelEncoder
+    {
+        public const int PARCEL_SIZE = 16;
+
+        // TODO (Vit): now hardcoded, but it should depend on the Landscape margins and Genesis Parcel data
+        private const int MIN_X = -152;
+        private const int MAX_X = 164;
+
+        private const int MIN_Y = -152;
+        private const int MAX_Y = 160;
+
+        private const int WIDTH = MAX_X - MIN_X + 1;
+
+        public static int EncodeParcel(Vector2Int parcel) =>
+            (parcel.x - MIN_X) + ((parcel.y - MIN_Y) * WIDTH);
+
+        public static Vector2Int DecodeParcel(int index) =>
+            new ((index % WIDTH) + MIN_X, (index / WIDTH) + MIN_Y);
     }
 
     public static class NetworkMessageCompressor
@@ -39,13 +62,27 @@ namespace DCL.Multiplayer.Movement.Systems
 
         public static CompressedNetworkMovementMessage Compress(this NetworkMovementMessage message, int steps = STEPS8_BIT)
         {
+            // Compress timestamp (8 bits)
             float normalizedTimestamp = message.timestamp % ROUND_BUFFER; // Normalize timestamp within the round buffer
             long compressedTimestamp = Mathf.RoundToInt(normalizedTimestamp / SENT_INTERVAL) % steps;
+
+            // Encode parcel (17 bits)
+            var parcel = message.position.ToParcel();
+            int parcelIndex = ParcelEncoder.EncodeParcel(parcel);
+
+            // Combine timestamp and parcel index
+            long compressedData = compressedTimestamp | ((long)parcelIndex << 8);
+
+            Vector2 relativePosition = new Vector2(
+                message.position.x - (parcel.x * ParcelEncoder.PARCEL_SIZE),
+                message.position.z - (parcel.y * ParcelEncoder.PARCEL_SIZE)
+            );
 
             return new CompressedNetworkMovementMessage
             {
                 AnimState = message.ToProtoEnum(),
-                compressedData = compressedTimestamp,
+                compressedData = compressedData,
+                remainedPosition = new Vector3(relativePosition.x, message.position.y, relativePosition.y),
                 message = message,
             };
         }
@@ -75,26 +112,39 @@ namespace DCL.Multiplayer.Movement.Systems
 
         public static NetworkMovementMessage Decompress(this CompressedNetworkMovementMessage compressedMessage, int steps = STEPS8_BIT)
         {
-            int mask = steps == STEPS9_BIT ? 0x1FF :
-                steps == STEPS8_BIT ? 0xFF : 0x7F;
+            int mask = steps switch
+                       {
+                           STEPS9_BIT => 0x1FF,
+                           STEPS8_BIT => 0xFF,
+                           _ => 0x7F, // 7 BITS
+                       };
 
             var compressedTimestamp = (int)(compressedMessage.compressedData & mask);
-
             // Debug.Assert(quantum == RoundBufferSeconds / steps, "VVV should be equal");
             float timestamp = compressedTimestamp * SENT_INTERVAL % ROUND_BUFFER;
 
-            AnimationStates animState = compressedMessage.AnimState.ToAnimState();
-            animState.MovementBlendValue = compressedMessage.message.animState.MovementBlendValue;
-            animState.SlideBlendValue = compressedMessage.message.animState.SlideBlendValue;
-            animState.IsJumping = compressedMessage.message.animState.IsJumping;
-            animState.IsLongJump = compressedMessage.message.animState.IsLongJump;
+            // Extract parcel index (17 bits)
+            int parcelIndex = (int)((compressedMessage.compressedData >> 8) & 0x1FFFF);
+            var parcel = ParcelEncoder.DecodeParcel(parcelIndex);
+            Vector3 worldPosition = new Vector3(
+                                        (parcel.x * ParcelEncoder.PARCEL_SIZE) + compressedMessage.remainedPosition.x,
+                                        compressedMessage.remainedPosition.y,
+                                        (parcel.y * ParcelEncoder.PARCEL_SIZE) + compressedMessage.remainedPosition.z);
 
-            animState = compressedMessage.message.animState;
+            AnimationStates animState = compressedMessage.AnimState.ToAnimState();
+            {
+                animState.MovementBlendValue = compressedMessage.message.animState.MovementBlendValue;
+                animState.SlideBlendValue = compressedMessage.message.animState.SlideBlendValue;
+                animState.IsJumping = compressedMessage.message.animState.IsJumping;
+                animState.IsLongJump = compressedMessage.message.animState.IsLongJump;
+
+                animState = compressedMessage.message.animState;
+            }
 
             return new NetworkMovementMessage
             {
                 timestamp = timestamp,
-                position = compressedMessage.message.position,
+                position = worldPosition,
                 velocity = compressedMessage.message.velocity,
                 animState = animState,
                 isStunned = compressedMessage.message.isStunned,
