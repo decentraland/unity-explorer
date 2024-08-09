57b3137b086f6e8e723a28760fc13ec839a2b6b0
nickkhalow <nickhalow@gmail.com>
2024-08-09 16:43:48 +0400

    BadgeView

---
+++ Explorer/Assets/DCL/UI/ConnectionStatusPanel/Badge/BadgeView.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DCL.UI.ConnectionStatusPanel.Badge
+{
+    public class BadgeView : MonoBehaviour
+    {
+        [SerializeField] private RectTransform self = null!;
+        [SerializeField] private TMP_Text tmpText = null!;
+        [SerializeField] private List<Image> backgroundPieces = new ();
+
+        private void Awake()
+        {
+            tmpText.autoSizeTextContainer = true;
+        }
+
+        public void UpdateText(string text, float? width = null)
+        {
+            tmpText.SetText(text);
+
+            if (width is { } w && Mathf.Approximately(w, 0) == false)
+            {
+                var delta = self.sizeDelta;
+                delta.x = w;
+                self.sizeDelta = delta;
+            }
+        }
+
+        public void ApplyColor(Color textColor, Color backgroundColor)
+        {
+            tmpText.color = textColor;
+            foreach (Image backgroundPiece in backgroundPieces) backgroundPiece.color = backgroundColor;
+        }
+    }
+}
--- Explorer/Assets/DCL/UI/ConnectionStatusPanel/StatusEntry/StatusEntryView.cs
+++ Explorer/Assets/DCL/UI/ConnectionStatusPanel/StatusEntry/StatusEntryView.cs
@@ -1,4 +1,7 @@
+using DCL.UI.ConnectionStatusPanel.Badge;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,31 +9,54 @@ namespace DCL.UI.ConnectionStatusPanel.StatusEntry
 {
     public class StatusEntryView : MonoBehaviour, IStatusEntry
     {
+        [SerializeField] private List<StatusValues> statusValues = new ();
+        [Space]
         [SerializeField] private Button reloadButton = null!;
-        [SerializeField] private GameObject statusEntry = null!;
+        [SerializeField] private BadgeView statusEntry = null!;
+        [Header("Debug")]
+        [SerializeField] private IStatusEntry.Status currentStatus;
 
         private Action? cachedAction;
+        private IReadOnlyDictionary<IStatusEntry.Status, (string title, float predefinedWidth, Color text, Color background)> cachedValues = null!;
 
         public void ShowReloadButton(Action onClick)
         {
             reloadButton.gameObject.SetActive(true);
-            statusEntry.SetActive(false);
+            statusEntry.gameObject.SetActive(false);
             cachedAction = onClick;
         }
 
         public void ShowStatus(IStatusEntry.Status status)
         {
             reloadButton.gameObject.SetActive(false);
-            statusEntry.SetActive(true);
-
-            //TODO update text
+            statusEntry.gameObject.SetActive(true);
+            (string title, float predefinedWidth, Color text, Color background) = cachedValues[status];
+            statusEntry.UpdateText(title, predefinedWidth);
+            statusEntry.ApplyColor(text, background);
         }
 
         private void Awake()
         {
+            cachedValues = statusValues.ToDictionary(e => e.status, e => (e.status.ToString()!, e.predefinedWidth, e.textColor, e.backgroundColor));
             reloadButton.onClick!.AddListener(() => cachedAction?.Invoke());
             reloadButton.gameObject.SetActive(false);
-            statusEntry.SetActive(false);
+            statusEntry.gameObject.SetActive(false);
+        }
+
+        [ContextMenu(nameof(UpdateStatus))]
+        public void UpdateStatus()
+        {
+            Awake();
+            ShowStatus(currentStatus);
+        }
+
+        [Serializable]
+        public class StatusValues
+        {
+            public IStatusEntry.Status status;
+            public float predefinedWidth;
+            public Color textColor;
+            public Color backgroundColor;
         }
     }
 }
