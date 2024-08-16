63121285cf83aa2165c1e48d4def2f278ef5470b
Ashley Canning <ashley.canning@gmail.com>
2024-08-16 14:10:27 +0100

    fix: boxing allocation fix: pre-allocate a much higher buffer for scenes & lods.

--- Explorer/Assets/Scripts/ECS/SceneLifeCycle/IScenesCache.cs
+++ Explorer/Assets/Scripts/ECS/SceneLifeCycle/IScenesCache.cs
@@ -26,8 +26,8 @@ namespace ECS.SceneLifeCycle
 
     public class ScenesCache : IScenesCache
     {
-        private readonly Dictionary<Vector2Int, ISceneFacade> scenesByParcels = new (PoolConstants.SCENES_COUNT * 2);
-        private readonly HashSet<Vector2Int> nonRealSceneByParcel = new (PoolConstants.SCENES_COUNT * 2);
+        private readonly Dictionary<Vector2Int, ISceneFacade> scenesByParcels = new (PoolConstants.SCENES_COUNT);
+        private readonly HashSet<Vector2Int> nonRealSceneByParcel = new (PoolConstants.SCENES_COUNT);
 
 
         private readonly HashSet<ISceneFacade> scenes = new (PoolConstants.SCENES_COUNT);
@@ -44,14 +44,14 @@ namespace ECS.SceneLifeCycle
 
         public void AddNonRealScene(IReadOnlyList<Vector2Int> parcels)
         {
-            foreach (var parcel in parcels)
-                nonRealSceneByParcel.Add(parcel);
+            for (int i = 0; i < parcels.Count; i++)
+                nonRealSceneByParcel.Add(parcels[i]);
         }
 
         public void RemoveNonRealScene(IReadOnlyList<Vector2Int> parcels)
         {
-            foreach (var parcel in parcels)
-                nonRealSceneByParcel.Remove(parcel);
+            for (int i = 0; i < parcels.Count; i++)
+                nonRealSceneByParcel.Remove(parcels[i]);
         }
 
         public void RemoveSceneFacade(IReadOnlyList<Vector2Int> parcels)
