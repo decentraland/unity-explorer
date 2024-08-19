bb238e964fcce700b2676d552095e154c4f36d26
nickkhalow <nickhalow@gmail.com>
2024-08-19 15:31:30 +0400

    summary for ShouldInstanceMaterialComponent

--- Explorer/Assets/Scripts/ECS/Unity/Materials/Components/ShouldInstanceMaterialComponent.cs
+++ Explorer/Assets/Scripts/ECS/Unity/Materials/Components/ShouldInstanceMaterialComponent.cs
@@ -1,5 +1,8 @@
 namespace ECS.Unity.Materials.Components
 {
+    /// <summary>
+    ///     Tag component to indicate that the material should be instanced, should be assigned in StartMaterialLoadingSystem, used by CreateMaterialSystem
+    /// </summary>
     public struct ShouldInstanceMaterialComponent
     {
 
