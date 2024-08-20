27367d82ad3b11890cbdf92c68147216517c5f7d
nickkhalow <nickhalow@gmail.com>
2024-08-20 12:45:52 +0400

    RustEthereumAccount

---
+++ Explorer/Assets/Plugins/RustEthereum/SignServerWrap/Playground/SignServerWrapPlayground.cs
@@ -0,0 +1,35 @@
+using DCL.Web3.Accounts;
+using Nethereum.Signer;
+using UnityEngine;
+
+namespace Plugins.RustEthereum.SignServerWrap.Playground
+{
+    public class SignServerWrapPlayground : MonoBehaviour
+    {
+        public enum Mode
+        {
+            Nethereum,
+            Rust,
+        }
+
+        [SerializeField] private int callsPerSecond = 1000;
+        [SerializeField] private string exampleMessage = "Hello world! Best day ever!";
+        [SerializeField] private Mode mode = Mode.Nethereum;
+
+        private RustEthereumAccount rustEthereumAccount = null!;
+        private NethereumAccount nethereumAccount = null!;
+
+        private void Start()
+        {
+            var key = EthECKey.GenerateKey()!;
+            rustEthereumAccount = new RustEthereumAccount(key);
+            nethereumAccount = new NethereumAccount(key);
+        }
+
+        private void Update()
+        {
+            IWeb3Account account = mode is Mode.Nethereum ? nethereumAccount : rustEthereumAccount;
+            for (var i = 0; i < callsPerSecond; i++) account.Sign(exampleMessage);
+        }
+    }
+}
---
+++ Explorer/Assets/Plugins/RustEthereum/SignServerWrap/RustEthereumAccount.cs
@@ -0,0 +1,58 @@
+using DCL.Utilities.Extensions;
+using DCL.Web3;
+using DCL.Web3.Accounts;
+using Nethereum.Hex.HexConvertors.Extensions;
+using Nethereum.Signer;
+using System;
+
+namespace Plugins.RustEthereum.SignServerWrap
+{
+    public class RustEthereumAccount : IWeb3Account
+    {
+        private readonly IWeb3Account verifierAccount;
+
+        public Web3Address Address { get; }
+
+
+
+        public RustEthereumAccount(EthECKey key)
+        {
+            this.verifierAccount = new NethereumAccount(key);
+
+            Address = new Web3Address(key.GetPublicAddress()!);
+            byte[] bytes = key.GetPrivateKeyAsBytes().EnsureNotNull();
+            var bytesLen = new UIntPtr((ulong)bytes.Length);
+
+            unsafe
+            {
+                fixed (byte* ptr = bytes)
+                {
+                    bool result = NativeMethods.SignServerInitialize(ptr, bytesLen);
+
+                    if (result == false)
+                        throw new Exception("Failed to initialize sign server");
+                }
+            }
+        }
+
+        public string Sign(string message)
+        {
+            const int SIZE_OF_SIGN = 65;
+
+            var buffer = new byte[SIZE_OF_SIGN];
+
+            unsafe
+            {
+                fixed (byte* signature = buffer)
+                {
+                    NativeMethods.SignServerSignMessage(message, signature);
+                }
+            }
+
+           return buffer.ToHex()!;
+        }
+
+        public bool Verify(string message, string signature) =>
+            verifierAccount.Verify(message, signature);
+    }
+}
