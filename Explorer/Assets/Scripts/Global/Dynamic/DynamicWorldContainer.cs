4faba5a7d0a1ecccb6b29ce71dc4f2670848088f
Nicolas Lorusso <nalorusso@gmail.com>
2024-08-22 16:55:19 -0300

    adds --self-forced-emotes program arg so we can test specific emotes in the build

--- Explorer/Assets/Scripts/Global/Dynamic/DynamicWorldContainer.cs
+++ Explorer/Assets/Scripts/Global/Dynamic/DynamicWorldContainer.cs
@@ -78,6 +78,7 @@ using SceneRunner.Debugging.Hub;
 using System;
 using System.Buffers;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading;
 using UnityEngine;
@@ -249,7 +250,8 @@ namespace Global.Dynamic
             var equippedWearables = new EquippedWearables();
             var equippedEmotes = new EquippedEmotes();
             var forceRender = new List<string>();
-            var selfProfile = new SelfProfile(container.ProfileRepository, identityCache, equippedWearables, wearableCatalog, emotesCache, equippedEmotes, forceRender);
+            var selfProfile = new SelfProfile(container.ProfileRepository, identityCache, equippedWearables, wearableCatalog,
+                emotesCache, equippedEmotes, forceRender, ParseSelfForcedEmotes(bootstrapContainer.ApplicationParametersParser));
 
             var metaDataSource = new LogMetaDataSource(new MetaDataSource(staticContainer.RealmData, staticContainer.CharacterContainer.CharacterObject, placesAPIService));
             var gateKeeperSceneRoom = new GateKeeperSceneRoom(staticContainer.WebRequestsContainer.WebRequestController, metaDataSource, bootstrapContainer.DecentralandUrlsSource);
@@ -606,6 +608,15 @@ namespace Global.Dynamic
         {
             reloadSceneController!.Initialize(world, playerEntity);
             wearablesProvider!.Initialize(world);
+        }
+
+        private static URN[]? ParseSelfForcedEmotes(ApplicationParametersParser appParams)
+        {
+            if (!appParams.AppParameters.TryGetValue("--self-force-emotes", out string csv))
+                return null;
+
+            string[] emotes = csv.Split(',');
+            return emotes.Select(s => new URN(s)).ToArray();
         }
     }
 }
