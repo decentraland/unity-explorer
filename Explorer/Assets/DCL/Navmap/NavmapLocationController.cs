f608c76296865aec75353f49d669205475f1b3d0
nickkhalow <nickhalow@gmail.com>
2024-08-23 12:58:39 +0400

    fix camera won't translate

--- Explorer/Assets/DCL/Navmap/NavmapLocationController.cs
+++ Explorer/Assets/DCL/Navmap/NavmapLocationController.cs
@@ -7,12 +7,14 @@ namespace DCL.Navmap
 {
     public class NavmapLocationController
     {
-        private readonly World world;
-        private readonly Entity playerEntity;
         private const float TRANSITION_TIME = 0.5f;
+        private const float IMMEDIATE_TRANSITION = 0;
 
         private IMapCameraController cameraController;
         private CharacterTransform playerTransformComponent;
+
+        private readonly World world;
+        private readonly Entity playerEntity;
 
         public NavmapLocationController(NavmapLocationView view, World world, Entity playerEntity)
         {
@@ -20,18 +22,24 @@ namespace DCL.Navmap
             this.playerEntity = playerEntity;
             world.TryGet(playerEntity, out playerTransformComponent);
 
-            view.CenterToPlayerButton.onClick.AddListener(OnCenterToPlayer);
+            view.CenterToPlayerButton.onClick.AddListener(CenterToPlayer);
         }
 
         public void InjectCameraController(IMapCameraController controller)
         {
             this.cameraController = controller;
+            CenterToPlayer(IMMEDIATE_TRANSITION);
         }
 
-        private void OnCenterToPlayer()
+        private void CenterToPlayer()
         {
-            if(world.TryGet(playerEntity, out playerTransformComponent))
-                cameraController.TranslateTo(ParcelMathHelper.WorldToGridPositionUnclamped(playerTransformComponent.Transform.position),TRANSITION_TIME);
+            CenterToPlayer(TRANSITION_TIME);
+        }
+
+        private void CenterToPlayer(float duration)
+        {
+            if (world.TryGet(playerEntity, out playerTransformComponent))
+                cameraController.TranslateTo(ParcelMathHelper.WorldToGridPositionUnclamped(playerTransformComponent.Transform.position), duration);
         }
     }
 }
