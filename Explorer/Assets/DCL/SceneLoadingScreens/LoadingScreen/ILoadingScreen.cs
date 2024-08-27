908e5af79e64ff7f87859b60f5a22de3af44ff50
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2024-08-27 11:42:47 -0300

    chore: add teleport error handling (#1872)

--- Explorer/Assets/DCL/SceneLoadingScreens/LoadingScreen/ILoadingScreen.cs
+++ Explorer/Assets/DCL/SceneLoadingScreens/LoadingScreen/ILoadingScreen.cs
@@ -2,25 +2,36 @@ using Cysharp.Threading.Tasks;
 using DCL.AsyncLoadReporting;
 using System;
 using System.Threading;
+using UnityEngine.Serialization;
 
 namespace DCL.SceneLoadingScreens.LoadingScreen
 {
     public interface ILoadingScreen
     {
-        enum ShowResult
+        struct LoadResult
         {
-            Success,
-            Timeout,
+            public string ErrorMessage;
+            public bool Success;
+
+            public static LoadResult SuccessResult => new() { Success = true };
+            public static LoadResult TimeoutResult => ExceptionResult("Load timeout!");
+
+            public static LoadResult ExceptionResult(string errorMessage)
+            {
+                return new LoadResult() { Success = false, ErrorMessage = errorMessage };
+            }
         }
 
-        UniTask<ShowResult> ShowWhileExecuteTaskAsync(Func<AsyncLoadProcessReport, UniTask> operation, CancellationToken ct);
+        UniTask<LoadResult> ShowWhileExecuteTaskAsync(Func<AsyncLoadProcessReport, UniTask> operation,
+            CancellationToken ct);
 
         class EmptyLoadingScreen : ILoadingScreen
         {
-            public async UniTask<ShowResult> ShowWhileExecuteTaskAsync(Func<AsyncLoadProcessReport, UniTask> operation, CancellationToken ct)
+            public async UniTask<LoadResult> ShowWhileExecuteTaskAsync(Func<AsyncLoadProcessReport, UniTask> operation,
+                CancellationToken ct)
             {
                 await operation(AsyncLoadProcessReport.Create());
-                return ShowResult.Success;
+                return LoadResult.SuccessResult;
             }
         }
     }
