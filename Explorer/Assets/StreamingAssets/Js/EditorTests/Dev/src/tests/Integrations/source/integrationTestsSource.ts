cf57a3475b4ef0e5e9991864195840936d38f8df
nickkhalow <nickhalow@gmail.com>
2024-09-06 17:16:05 +0700

    remove streaming-assets

--- Explorer/Assets/StreamingAssets/Js/EditorTests/Dev/src/tests/Integrations/source/integrationTestsSource.ts
+++
@@ -1,8 +0,0 @@
-export default interface IntegrationTestsSource {
-    get(key: string): ((context: IntegrationTestContext) => Promise<void>) | undefined
-}
-
-export type IntegrationTestContext = {
-    result: any
-    methodsBundle: any
-}
