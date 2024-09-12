f8782f6fcf45582fb83a00148e12e8b5f6d27aee
Pravus <pravusjif@gmail.com>
2024-09-12 12:45:01 +0200

    improved MainCamera component initialization logic

--- Explorer/Assets/DCL/SDKComponents/CameraControl/MainCamera/Components/MainCameraComponent.cs
+++ Explorer/Assets/DCL/SDKComponents/CameraControl/MainCamera/Components/MainCameraComponent.cs
@@ -6,12 +6,6 @@ namespace DCL.SDKComponents.CameraControl.MainCamera.Components
     public struct MainCameraComponent
     {
         internal CinemachineFreeLook? virtualCameraInstance;
-        internal CRDTEntity virtualCameraCRDTEntity;
-
-        public MainCameraComponent(CRDTEntity virtualCamCRDTEntity)
-        {
-            virtualCameraCRDTEntity = virtualCamCRDTEntity;
-            virtualCameraInstance = null;
-        }
+        internal CRDTEntity? virtualCameraCRDTEntity;
     }
 }
