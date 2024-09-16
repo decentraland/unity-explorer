62c90370459f1ffa15c6a3c5db2613b5dc4479f6
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2024-09-16 14:10:40 -0300

    Reduce percentils to 65/75; using systemUsedInMemoryBytes since it better reflects usage

--- Explorer/Assets/DCL/PluginSystem/Global/StaticSettings.cs
+++ Explorer/Assets/DCL/PluginSystem/Global/StaticSettings.cs
@@ -48,8 +48,8 @@ namespace DCL.PluginSystem.Global
 
         public Dictionary<MemoryUsageStatus, float> MemoryThresholds { get; private set; } = new ()
         {
-            { MemoryUsageStatus.WARNING, 0.8f },
-            { MemoryUsageStatus.FULL, 0.95f },
+            { MemoryUsageStatus.WARNING, 0.65f },
+            { MemoryUsageStatus.FULL, 0.75f }
         };
 
         [Serializable]
