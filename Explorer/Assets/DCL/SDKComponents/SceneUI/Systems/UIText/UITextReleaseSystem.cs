e5d53b8e8535f9b1bdb1751ee525eb5d2c0d5958
davidejensen <davidejensen@live.it>
2024-09-20 13:27:34 +0200

    Fixed double deletion of ui images and labels

--- Explorer/Assets/DCL/SDKComponents/SceneUI/Systems/UIText/UITextReleaseSystem.cs
+++ Explorer/Assets/DCL/SDKComponents/SceneUI/Systems/UIText/UITextReleaseSystem.cs
@@ -27,7 +27,6 @@ namespace DCL.SDKComponents.SceneUI.Systems.UIText
 
         protected override void Update(float t)
         {
-            HandleEntityDestructionQuery(World);
             HandleUITextRemovalQuery(World);
             World.Remove<UITextComponent>(in HandleUITextRemoval_QueryDescription);
         }
@@ -35,11 +34,6 @@ namespace DCL.SDKComponents.SceneUI.Systems.UIText
         [Query]
         [None(typeof(PBUiText), typeof(DeleteEntityIntention))]
         private void HandleUITextRemoval(ref UITextComponent uiTextComponent) =>
-            RemoveLabel(uiTextComponent);
-
-        [Query]
-        [All(typeof(DeleteEntityIntention))]
-        private void HandleEntityDestruction(ref UITextComponent uiTextComponent) =>
             RemoveLabel(uiTextComponent);
 
         private void RemoveLabel(UITextComponent uiTextComponent)
