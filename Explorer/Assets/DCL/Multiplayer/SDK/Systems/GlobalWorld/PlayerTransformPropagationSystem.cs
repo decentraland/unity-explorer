9a7e5bb2ae8f022f5d601c1c8bb63e10e2f61b2e
Mikhail Agapov <118179774+mikhail-dcl@users.noreply.github.com>
2024-09-25 18:21:04 +0300

    Fix transforms propagation outside scenes (#2174)

--- Explorer/Assets/DCL/Multiplayer/SDK/Systems/GlobalWorld/PlayerTransformPropagationSystem.cs
+++ Explorer/Assets/DCL/Multiplayer/SDK/Systems/GlobalWorld/PlayerTransformPropagationSystem.cs
@@ -1,11 +1,13 @@
 using Arch.Core;
 using Arch.System;
 using Arch.SystemGroups;
+using Arch.SystemGroups.DefaultSystemGroups;
 using CrdtEcsBridge.Components;
 using CrdtEcsBridge.Components.Transform;
 using DCL.Character.Components;
 using DCL.Diagnostics;
 using DCL.Multiplayer.SDK.Components;
+using DCL.Optimization.Pools;
 using ECS.Abstract;
 using ECS.Groups;
 using ECS.LifeCycle.Components;
@@ -15,11 +17,16 @@ using Vector3 = UnityEngine.Vector3;
 
 namespace DCL.Multiplayer.SDK.Systems.GlobalWorld
 {
-    [UpdateInGroup(typeof(SyncedPreRenderingSystemGroup))]
+    [UpdateInGroup(typeof(PreRenderingSystemGroup))]
     [LogCategory(ReportCategory.PLAYER_SDK_DATA)]
     public partial class PlayerTransformPropagationSystem : BaseUnityLoopSystem
     {
-        public PlayerTransformPropagationSystem(World world) : base(world) { }
+        private readonly IComponentPool<SDKTransform> sdkTransformPool;
+
+        public PlayerTransformPropagationSystem(World world, IComponentPool<SDKTransform> sdkTransformPool) : base(world)
+        {
+            this.sdkTransformPool = sdkTransformPool;
+        }
 
         protected override void Update(float t)
         {
@@ -32,28 +39,20 @@ namespace DCL.Multiplayer.SDK.Systems.GlobalWorld
         {
             if (!characterTransform.Transform.hasChanged) return;
 
+            if (!playerCRDTEntity.AssignedToScene) return;
+
             // Main player Transform is handled by 'WriteMainPlayerTransformSystem'
             if (playerCRDTEntity.CRDTEntity.Id == SpecialEntitiesID.PLAYER_ENTITY) return;
 
-            World sceneEcsWorld = playerCRDTEntity.SceneFacade.EcsExecutor.World;
+            World sceneEcsWorld = playerCRDTEntity.SceneFacade!.EcsExecutor.World;
 
             // Position is updated to scene-relative on the writer system
-            if (sceneEcsWorld.TryGet(playerCRDTEntity.SceneWorldEntity, out SDKTransform? sdkTransform))
-            {
-                sdkTransform!.Position.Value = characterTransform.Transform.position;
-                sdkTransform.Rotation.Value = characterTransform.Transform.rotation;
-                sdkTransform.IsDirty = true;
-                sceneEcsWorld.Set(playerCRDTEntity.SceneWorldEntity, sdkTransform);
-                return;
-            }
+            if (!sceneEcsWorld.TryGet<SDKTransform>(playerCRDTEntity.SceneWorldEntity, out SDKTransform? sdkTransform))
+                sceneEcsWorld.Add(playerCRDTEntity.SceneWorldEntity, sdkTransform = sdkTransformPool.Get());
 
-            sdkTransform = new SDKTransform
-            {
-                Position = new CanBeDirty<Vector3>(characterTransform.Transform.position),
-                Rotation = new CanBeDirty<Quaternion>(characterTransform.Transform.rotation),
-                IsDirty = true
-            };
-            sceneEcsWorld.Add(playerCRDTEntity.SceneWorldEntity, sdkTransform);
+            sdkTransform!.Position.Value = characterTransform.Transform.position;
+            sdkTransform.Rotation.Value = characterTransform.Transform.rotation;
+            sdkTransform.IsDirty = true;
         }
     }
 }
