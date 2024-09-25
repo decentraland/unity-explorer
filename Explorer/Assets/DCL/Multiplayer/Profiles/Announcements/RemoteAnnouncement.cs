5337f694da300090b672105cd966de72dccb3ac4
Santi Andrade <sandrade@decentraland.org>
2024-09-25 13:25:23 +0200

    fix: Avoid downloading the same remote profile multiple times

--- Explorer/Assets/DCL/Multiplayer/Profiles/Announcements/RemoteAnnouncement.cs
+++ Explorer/Assets/DCL/Multiplayer/Profiles/Announcements/RemoteAnnouncement.cs
@@ -1,6 +1,8 @@
+using System;
+
 namespace DCL.Multiplayer.Profiles.RemoteAnnouncements
 {
-    public readonly struct RemoteAnnouncement
+    public readonly struct RemoteAnnouncement : IEquatable<RemoteAnnouncement>
     {
         public readonly int Version;
         public readonly string WalletId;
@@ -13,5 +15,14 @@ namespace DCL.Multiplayer.Profiles.RemoteAnnouncements
 
         public override string ToString() =>
             $"(RemoteAnnouncement: {{ Version: {Version}, WalletId: {WalletId} }})";
+
+        public bool Equals(RemoteAnnouncement other) =>
+            Version == other.Version && WalletId == other.WalletId;
+
+        public override bool Equals(object? obj) =>
+            obj is RemoteAnnouncement other && Equals(other);
+
+        public override int GetHashCode() =>
+            HashCode.Combine(Version, WalletId);
     }
 }
