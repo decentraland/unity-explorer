d93ceff848c48a91f96d7d2f96345f9f956fb6f6
nickkhalow <nickhalow@gmail.com>
2024-09-27 16:22:48 +0300

    pooled json

--- Explorer/Assets/Scripts/Utility/Json/IJsonObjectBuilder.cs
+++ Explorer/Assets/Scripts/Utility/Json/IJsonObjectBuilder.cs
@@ -1,4 +1,5 @@
 using Segment.Serialization;
+using System;
 
 namespace Utility.Json
 {
@@ -15,5 +16,28 @@ namespace Utility.Json
         JsonObject Build();
 
         void Release(JsonObject jsonObject);
+    }
+
+    public static class JsonObjectBuilderExtensions
+    {
+        public static PooledJsonObject BuildPooled(this IJsonObjectBuilder jsonObjectBuilder) =>
+            new (jsonObjectBuilder);
+    }
+
+    public readonly struct PooledJsonObject : IDisposable
+    {
+        public readonly JsonObject Json;
+        private readonly IJsonObjectBuilder source;
+
+        public PooledJsonObject(IJsonObjectBuilder source)
+        {
+            this.source = source;
+            Json = source.Build();
+        }
+
+        public void Dispose()
+        {
+            source.Release(Json);
+        }
     }
 }
