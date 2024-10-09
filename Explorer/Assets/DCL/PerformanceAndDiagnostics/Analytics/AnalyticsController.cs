5d98993308f69d8e7573d7dde72c82a82c38159f
nickkhalow <nickhalow@gmail.com>
2024-10-09 18:45:09 +0300

    TrackSystemInfo on wallet identify

--- Explorer/Assets/DCL/PerformanceAndDiagnostics/Analytics/AnalyticsController.cs
+++ Explorer/Assets/DCL/PerformanceAndDiagnostics/Analytics/AnalyticsController.cs
@@ -21,21 +21,6 @@ namespace DCL.PerformanceAndDiagnostics.Analytics
             analytics.AddPlugin(new StaticCommonTraitsPlugin(appArgs, launcherTraits));
             analytics.Identify(SystemInfo.deviceUniqueIdentifier!);
             analytics.Flush();
-
-            analytics.Track(AnalyticsEvents.General.SYSTEM_INFO_REPORT, new JsonObject
-            {
-                ["device_model"] = SystemInfo.deviceModel, // "XPS 17 9720 (Dell Inc.)"
-                ["operating_system"] = SystemInfo.operatingSystem, // "Windows 11  (10.0.22631) 64bit"
-                ["system_memory_size"] = SystemInfo.systemMemorySize, // 65220 in [MB]
-
-                ["processor_type"] = SystemInfo.processorType, // "12th Gen Intel(R) Core(TM) i7-12700H"
-                ["processor_count"] = SystemInfo.processorCount, // 20
-
-                ["graphics_device_name"] = SystemInfo.graphicsDeviceName, // "NVIDIA GeForce RTX 3050 Laptop GPU"
-                ["graphics_memory_size"] = SystemInfo.graphicsMemorySize, // 3965 in [MB]
-                ["graphics_device_type"] = SystemInfo.graphicsDeviceType.ToString(), // "Direct3D11", Vulkan, OpenGLCore, XBoxOne...
-                ["graphics_device_version"] = SystemInfo.graphicsDeviceVersion, // "Direct3D 11.0 [level 11.1]"
-            });
         }
 
         public void SetCommonParam(IRealmData realmData, IWeb3IdentityCache? identityCache, IExposedTransform playerTransform)
@@ -55,12 +40,35 @@ namespace DCL.PerformanceAndDiagnostics.Analytics
         public void Identify(IWeb3Identity? identity)
         {
             if (identity != null)
+            {
                 analytics.Identify(identity.Address, new JsonObject
                     {
                         ["dcl_eth_address"] = identity.Address != null ? identity.Address.ToString() : UNDEFINED,
                         ["auth_chain"] = identity.AuthChain != null ? identity.AuthChain.ToString() : UNDEFINED,
                     }
                 );
+
+                TrackSystemInfo();
+                analytics.Flush();
+            }
+        }
+
+        private void TrackSystemInfo()
+        {
+            analytics.Track(AnalyticsEvents.General.SYSTEM_INFO_REPORT, new JsonObject
+            {
+                ["device_model"] = SystemInfo.deviceModel, // "XPS 17 9720 (Dell Inc.)"
+                ["operating_system"] = SystemInfo.operatingSystem, // "Windows 11  (10.0.22631) 64bit"
+                ["system_memory_size"] = SystemInfo.systemMemorySize, // 65220 in [MB]
+
+                ["processor_type"] = SystemInfo.processorType, // "12th Gen Intel(R) Core(TM) i7-12700H"
+                ["processor_count"] = SystemInfo.processorCount, // 20
+
+                ["graphics_device_name"] = SystemInfo.graphicsDeviceName, // "NVIDIA GeForce RTX 3050 Laptop GPU"
+                ["graphics_memory_size"] = SystemInfo.graphicsMemorySize, // 3965 in [MB]
+                ["graphics_device_type"] = SystemInfo.graphicsDeviceType.ToString(), // "Direct3D11", Vulkan, OpenGLCore, XBoxOne...
+                ["graphics_device_version"] = SystemInfo.graphicsDeviceVersion, // "Direct3D 11.0 [level 11.1]"
+            });
         }
 
         public void Flush() =>
