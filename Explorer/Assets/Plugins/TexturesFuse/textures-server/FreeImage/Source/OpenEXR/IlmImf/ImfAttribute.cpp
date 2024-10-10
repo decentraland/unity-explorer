5cf2e3117f70a1b47b5f909c078e18498567c046
Vitaly Popuzin <popuzin@gmail.com>
2024-10-10 18:20:23 +0200

    adjusted files for compilations with MSBuild

--- Explorer/Assets/Plugins/TexturesFuse/textures-server/FreeImage/Source/OpenEXR/IlmImf/ImfAttribute.cpp
+++ Explorer/Assets/Plugins/TexturesFuse/textures-server/FreeImage/Source/OpenEXR/IlmImf/ImfAttribute.cpp
@@ -63,15 +63,14 @@ Attribute::~Attribute () {}
 
 namespace {
 
-struct NameCompare: std::binary_function <const char *, const char *, bool>
-{
-    bool
-    operator () (const char *x, const char *y) const
-    {
-	return strcmp (x, y) < 0;
-    }
-};
 
+    struct NameCompare
+    {
+        bool operator () (const char *x, const char *y) const
+        {
+            return strcmp (x, y) < 0;
+        }
+    };
 
 typedef Attribute* (*Constructor)();
 typedef std::map <const char *, Constructor, NameCompare> TypeMap;
