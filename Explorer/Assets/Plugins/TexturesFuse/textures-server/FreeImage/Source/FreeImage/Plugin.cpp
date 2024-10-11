fc2199419e10e458b54eea4fc339f88178a7e48b
nickkhalow <nickhalow@gmail.com>
2024-10-11 10:51:09 +0300

    support png

--- Explorer/Assets/Plugins/TexturesFuse/textures-server/FreeImage/Source/FreeImage/Plugin.cpp
+++ Explorer/Assets/Plugins/TexturesFuse/textures-server/FreeImage/Source/FreeImage/Plugin.cpp
@@ -275,12 +275,12 @@ FreeImage_Initialise(BOOL load_local_plugins_only)
 			// s_plugins->AddNode(InitPCX);
 			// s_plugins->AddNode(InitPNM, NULL, "PGM", "Portable Greymap (ASCII)", "pgm", "^P2");
 			// s_plugins->AddNode(InitPNM, NULL, "PGMRAW", "Portable Greymap (RAW)", "pgm", "^P5");
-			// s_plugins->AddNode(InitPNG);
+			s_plugins->AddNode(InitPNG);
 			// s_plugins->AddNode(InitPNM, NULL, "PPM", "Portable Pixelmap (ASCII)", "ppm", "^P3");
 			// s_plugins->AddNode(InitPNM, NULL, "PPMRAW", "Portable Pixelmap (RAW)", "ppm", "^P6");
 			// s_plugins->AddNode(InitRAS);
 			// s_plugins->AddNode(InitTARGA);
-			// s_plugins->AddNode(InitTIFF);
+			// s_plugins->AddNode(InitTIFF); TODO
 			// s_plugins->AddNode(InitWBMP);
 			// s_plugins->AddNode(InitPSD);
 			// s_plugins->AddNode(InitCUT);
@@ -289,7 +289,7 @@ FreeImage_Initialise(BOOL load_local_plugins_only)
 			// s_plugins->AddNode(InitDDS);
 			// s_plugins->AddNode(InitGIF);
 			// s_plugins->AddNode(InitHDR);
-			// s_plugins->AddNode(InitG3);
+			// s_plugins->AddNode(InitG3); TODO
 			// s_plugins->AddNode(InitSGI);
 			// s_plugins->AddNode(InitEXR);
 			// s_plugins->AddNode(InitJ2K);
