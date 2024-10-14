0340f7bf397b4f95c04a310e2f321bcbfd75db5c
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2024-10-14 09:44:19 -0300

    Add CompleteLoadingStatus

---
+++ Explorer/Assets/Scripts/Global/Dynamic/TeleportOperations/CompleteLoadingStatus.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using DCL.UserInAppInitializationFlow;
+using Utility.Types;
+
+namespace Global.Dynamic.TeleportOperations
+{
+    public class CompleteLoadingStatus : ITeleportOperation
+    {
+        public UniTask<Result> ExecuteAsync(TeleportParams teleportParams, CancellationToken ct)
+        {
+            teleportParams.LoadingStatus.SetCurrentStage(LoadingStatus.CurrentStage.Done);
+            return UniTask.FromResult(Result.SuccessResult());
+        }
+    }
+}
