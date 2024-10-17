9bef6fd87750b3e3d26c33e8d42b36de4234f031
Fran Colarich <56565104+fcolarich@users.noreply.github.com>
2024-10-17 15:33:20 +0200

    Added comments with more info about new fields

--- Explorer/Assets/DCL/LOD/Settings/LODSettingsAsset.cs
+++ Explorer/Assets/DCL/LOD/Settings/LODSettingsAsset.cs
@@ -13,8 +13,9 @@ namespace DCL.LOD
         {
             5
         };
-
         [field: SerializeField] public int SDK7LodThreshold { get; set; } = 2;
+
+        [Tooltip("The time in seconds before changing from a scene/scene promise to LOD")]
         [field: SerializeField] public float TimeToChangeToLod { get; set; } = 5;
 
         [field: SerializeField] public TextureArrayResolutionDescriptor[] DefaultTextureArrayResolutionDescriptors { get; set; } =
