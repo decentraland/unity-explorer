5ed47ca05b1f222292c04168565cbd6eceb35c32
Mikhail Agapov <mikhail.agapov@decentraland.org>
2024-10-21 11:56:57 +0300

    Restore ConnectiveRoom.Fake

--- Explorer/Assets/DCL/Multiplayer/Connections/Rooms/Connective/IConnectiveRoom.cs
+++ Explorer/Assets/DCL/Multiplayer/Connections/Rooms/Connective/IConnectiveRoom.cs
@@ -20,6 +20,21 @@ namespace DCL.Multiplayer.Connections.Rooms.Connective
         State CurrentState();
 
         IRoom Room();
+
+        class Fake : IConnectiveRoom
+        {
+            public UniTask<bool> StartAsync() =>
+                UniTask.FromResult(false);
+
+            public UniTask StopAsync() =>
+                UniTask.CompletedTask;
+
+            public State CurrentState() =>
+                State.Stopped;
+
+            public IRoom Room() =>
+                NullRoom.INSTANCE;
+        }
     }
 
     public static class GateKeeperSceneRoomExtensions
