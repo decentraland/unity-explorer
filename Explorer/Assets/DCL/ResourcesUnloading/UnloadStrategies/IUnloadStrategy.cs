364b0d718c992d81377f3bbd04d64f20d147ac86
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2024-10-20 23:47:59 -0300

    Cleanup

--- Explorer/Assets/DCL/ResourcesUnloading/UnloadStrategies/IUnloadStrategy.cs
+++ Explorer/Assets/DCL/ResourcesUnloading/UnloadStrategies/IUnloadStrategy.cs
@@ -15,16 +15,16 @@ namespace DCL.ResourcesUnloading.UnloadStrategies
         protected abstract void RunStrategy(ICacheCleaner cacheCleaner);
         protected virtual void ResetStrategy() {}
 
+        private bool FailedOverThreshold()
+        {
+            return currentFailureCount >= FAILURE_THRESHOLD;
+        }
+        
         public void Reset()
         {
             currentFailureCount = 0;
             ResetStrategy();
             PreviousStrategy?.Reset();
-        }
-
-        public bool FailedOverThreshold()
-        {
-            return currentFailureCount >= FAILURE_THRESHOLD;
         }
 
         public void TryUnload(ICacheCleaner cacheCleaner)
