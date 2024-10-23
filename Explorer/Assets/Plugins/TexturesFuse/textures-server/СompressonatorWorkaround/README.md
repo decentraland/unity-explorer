9791204d58821efc7a613391b0a5d8c35df05493
nickkhalow <nickhalow@gmail.com>
2024-10-23 13:50:34 +0300

    update builds_mac

--- Explorer/Assets/Plugins/TexturesFuse/textures-server/СompressonatorWorkaround/README.md
+++ Explorer/Assets/Plugins/TexturesFuse/textures-server/СompressonatorWorkaround/README.md
@@ -6,4 +6,3 @@ To prepare the lib to build
 2. Run ./compressonator/build/fetch_dependencies.py
 3. Run integrate_simde.py
 4. Run builds_mac.sh
-5. Copy libs to ../Compressator/lib
--- Explorer/Assets/Plugins/TexturesFuse/textures-server/СompressonatorWorkaround/builds_mac.sh
+++ Explorer/Assets/Plugins/TexturesFuse/textures-server/СompressonatorWorkaround/builds_mac.sh
@@ -1,7 +1,11 @@
+set -e  # Exit immediately if any command exits with a non-zero status
+
 cd compressonator/cmp_compressonatorlib;
 cmake . -DOPTION_CMP_QT=OFF -DOPTION_CMP_VULKAN=OFF -DOPTION_CMP_DIRECTX=OFF -DOPTION_CMP_OPENCV=OFF -DOPTION_ENABLE_ALL_APPS=OFF -DOPTION_BUILD_CMP_SDK=ON -DOPTION_BUILD_APPS_CMP_CLI=OFF -DOPTION_BUILD_KTX2=OFF -DSIMDE_X86_AVX_ENABLE_NATIVE_ALIASES=ON -DGLM_ENABLE_EXPERIMENTAL=OFF -DGLM_BUILD_LIBRARY=ON; 
 make clean;
 make -j10;
+
+cp libCMP_Compressonator.a ../../../Compressator/lib/libCMP_Compressonator.a
 
 cd -;
 
@@ -9,3 +13,8 @@ cd compressonator/cmp_core;
 cmake . -DOPTION_CMP_QT=OFF -DOPTION_CMP_VULKAN=OFF -DOPTION_CMP_DIRECTX=OFF -DOPTION_CMP_OPENCV=OFF -DOPTION_ENABLE_ALL_APPS=OFF -DOPTION_BUILD_CMP_SDK=ON -DOPTION_BUILD_APPS_CMP_CLI=OFF -DOPTION_BUILD_KTX2=OFF -DSIMDE_X86_AVX_ENABLE_NATIVE_ALIASES=ON -DGLM_ENABLE_EXPERIMENTAL=OFF -DGLM_BUILD_LIBRARY=ON; 
 make clean;
 make -j10;
+
+cp libCMP_Core.a ../../../Compressator/lib/libCMP_Core.a
+cp libCMP_Core_SSE.a ../../../Compressator/lib/libCMP_Core_SSE.a
+cp libCMP_Core_AVX.a ../../../Compressator/lib/libCMP_Core_AVX.a
+cp libCMP_Core_AVX512.a ../../../Compressator/lib/libCMP_Core_AVX512.a
