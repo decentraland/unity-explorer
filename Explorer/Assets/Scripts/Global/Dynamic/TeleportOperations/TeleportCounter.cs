df13c71d307b363c196f0601c9150a221783a24c
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2024-10-24 11:19:50 -0300

    Test coverage

--- Explorer/Assets/Scripts/Global/Dynamic/TeleportOperations/TeleportCounter.cs
+++ Explorer/Assets/Scripts/Global/Dynamic/TeleportOperations/TeleportCounter.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using CommunicationData.URLHelpers;
 using UnityEngine;
 
 namespace Global.Dynamic.TeleportOperations
@@ -6,33 +7,43 @@ namespace Global.Dynamic.TeleportOperations
     public class TeleportCounter
     {
         private readonly List<Vector2Int> successfullTeleports;
+        private readonly HashSet<URLDomain> visitedRealms; 
         private readonly int teleportsBeforeUnload;
-        private int teleportsDone;
+        internal int teleportsDone;
 
         public TeleportCounter(int teleportsBeforeUnload)
         {
             successfullTeleports = new List<Vector2Int>(teleportsBeforeUnload);
+            visitedRealms = new HashSet<URLDomain>(teleportsBeforeUnload);
             this.teleportsBeforeUnload = teleportsBeforeUnload;
         }
 
-        public void AddSuccessfullTeleport(Vector2Int teleportCompleted, bool newRealm)
+        public void AddSuccessfullTeleport(Vector2Int teleportCompleted, URLDomain realmName, bool teleportToNewRealm)
         {
-            //If its a new realm, we ignore the coordinates since they may repeat with Genesis City
-            if (newRealm)
+            if (teleportToNewRealm)
             {
-                teleportsDone++;
+                if (!visitedRealms.Contains(realmName))
+                {
+                    //New realm, we add it to the list since coordinates may repeat with Genesis City
+                    visitedRealms.Add(realmName);
+                    teleportsDone++;
+                }
             }
             //If we have already gone there, we ignore it. We want to unload on new scenes only
-            else if (!successfullTeleports.Contains(teleportCompleted))
+            else
             {
-                successfullTeleports.Add(teleportCompleted);
-                teleportsDone++;
+                if (!successfullTeleports.Contains(teleportCompleted))
+                {
+                    successfullTeleports.Add(teleportCompleted);
+                    teleportsDone++;
+                }
             }
         }
 
         public void ClearSuccessfullTeleports()
         {
             successfullTeleports.Clear();
+            visitedRealms.Clear();
             teleportsDone = 0;
         }
 
