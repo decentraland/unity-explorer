e39802567642625f0ca7b270d5460782648c4bcb
Vitaly Popuzin <popuzin@gmail.com>
2024-10-25 11:12:26 +0200

    adjust names in the build script

--- Explorer/Assets/Plugins/TexturesFuse/textures-server/FreeImage/Source/FFI/build.ps1
+++ Explorer/Assets/Plugins/TexturesFuse/textures-server/FreeImage/Source/FFI/build.ps1
@@ -16,11 +16,11 @@ cmake . -G "Visual Studio 17 2022" `
 
 # Build ASTC encoder
 Set-Location astc-encoder-build
-cmake --build . --clean-first --config Release --parallel 20
+cmake --build . --clean-first --config Release --parallel 10
 Set-Location ..
 
 # Build main project
-cmake --build . --clean-first --config Release --parallel 20
+cmake --build . --clean-first --config Release --parallel 10
 
 # Build configuration
 $RELATIVE_PATH = "..\..\..\..\TexturesServerWrap\Libraries\Windows"
@@ -28,12 +28,8 @@ $ASTC_PATH = "astc-encoder-build\Source\Release"
 
 # Moving files to the dedicated directory
 # Note: In Windows, .dylib becomes .dll
-Move-Item -Force "Release\texturesfuse.dll" "$RELATIVE_PATH\texturesfuse.dll"
-Move-Item -Force "$ASTC_PATH\astcenc-native-shared.dll" "$RELATIVE_PATH\astcenc-shared.dll"
-# Move-Item -Force "$ASTC_PATH\astcenc-avx2-shared.dll" "$RELATIVE_PATH\astcenc-avx2-shared.dll"
-# Move-Item -Force "$ASTC_PATH\astcenc-sse4.1-shared.dll" "$RELATIVE_PATH\astcenc-sse4.1-shared.dll"
-# Note: NEON is ARM-specific and typically not built on Windows x64
-# Move-Item -Force "$ASTC_PATH\Release\astcenc-neon-shared.dll" "$RELATIVE_PATH\astcenc-neon-shared.dll"
+Move-Item -Force "Release\texturesfuse.dll" "$RELATIVE_PATH\libtexturesfuse.dll"
+Move-Item -Force "$ASTC_PATH\astcenc-native-shared.dll" "$RELATIVE_PATH\astcenc-native-shared.dll"
 
 # Windows doesn't need install_name_tool as DLL linking is handled differently
 # DLL dependencies are resolved via the PATH or the same directory at runtime
