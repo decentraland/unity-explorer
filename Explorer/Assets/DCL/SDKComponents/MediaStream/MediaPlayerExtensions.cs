2745511b5052ff46582ddb5eb959797fa82c4d55
davidejensen <davidejensen@live.it>
2024-10-25 08:09:44 +0200

    fix: update audio for streams with no volume set (#2589)
    * fix: update audio for streams with no volume set
    
    * minor fix

--- Explorer/Assets/DCL/SDKComponents/MediaStream/MediaPlayerExtensions.cs
+++ Explorer/Assets/DCL/SDKComponents/MediaStream/MediaPlayerExtensions.cs
@@ -22,7 +22,16 @@ namespace DCL.SDKComponents.MediaStream
             else
             {
                 if (!hasVolume)
+                    //This following part is a workaround applied for the MacOS platform, the reason
+                    //is related to the video and audio streams, the MacOS environment does not support
+                    //the volume control for the video and audio streams, as it doesn’t allow to route audio
+                    //from HLS through to Unity. This is a limitation of Apple’s AVFoundation framework
+                    //Similar issue reported here https://github.com/RenderHeads/UnityPlugin-AVProVideo/issues/1086
+#if UNITY_EDITOR_OSX || UNITY_STANDALONE_OSX
+                    mediaPlayer.AudioVolume = MediaPlayerComponent.DEFAULT_VOLUME * volume;
+#else
                     mediaPlayer.AudioVolume = MediaPlayerComponent.DEFAULT_VOLUME;
+#endif
                 else if (!Mathf.Approximately(mediaPlayer.AudioVolume, volume))
                     mediaPlayer.AudioVolume = volume;
             }
