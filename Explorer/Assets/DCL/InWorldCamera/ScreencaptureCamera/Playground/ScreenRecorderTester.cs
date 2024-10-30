5d40013ad789b8dbb8285881cecbcf235022981a
Vitaly Popuzin <popuzin@gmail.com>
2024-10-30 21:10:23 +0100

    screen capturing is working fine

---
+++ Explorer/Assets/DCL/InWorldCamera/ScreencaptureCamera/Playground/ScreenRecorderTester.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace DCL.InWorldCamera.ScreencaptureCamera.Playground
+{
+    public class ScreenRecorderTester : MonoBehaviour
+    {
+        private ScreenRecorder recorder;
+        public RectTransform canvasRectTransform;
+        public Texture2D Texture;
+
+        [ContextMenu(nameof(SHOOT))]
+        public void SHOOT()
+        {
+            recorder ??= new ScreenRecorder(canvasRectTransform);
+            StartCoroutine(recorder.CaptureScreenshot(Show));
+        }
+
+        private void Show(Texture2D texture)
+        {
+            Texture = texture;
+        }
+    }
+}
