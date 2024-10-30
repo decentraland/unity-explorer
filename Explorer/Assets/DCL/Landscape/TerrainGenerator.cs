4b142d64c0f8a012646371c32404b4d035b981b9
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2024-10-30 08:26:17 -0300

    Bump terrain version

--- Explorer/Assets/DCL/Landscape/TerrainGenerator.cs
+++ Explorer/Assets/DCL/Landscape/TerrainGenerator.cs
@@ -27,7 +27,7 @@ namespace DCL.Landscape
         private const float ROOT_VERTICAL_SHIFT = -0.01f; // fix for not clipping with scene (potential) floor
 
         // increment this number if we want to force the users to generate a new terrain cache
-        private const int CACHE_VERSION = 8;
+        private const int CACHE_VERSION = 9;
 
         private const float PROGRESS_COUNTER_EMPTY_PARCEL_DATA = 0.1f;
         private const float PROGRESS_COUNTER_TERRAIN_DATA = 0.3f;
