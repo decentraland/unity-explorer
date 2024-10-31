2035192f9547814f9c44b427a6d26c791e63a40c
Ashley Canning <ashley.canning@decentraland.org>
2024-10-31 16:04:28 +0000

    Update build.py
    Signed-off-by: Ashley Canning <ashley.canning@decentraland.org>

--- scripts/cloudbuild/build.py
+++ scripts/cloudbuild/build.py
@@ -73,9 +73,7 @@ def clone_current_target(use_cache):
 
         print(f"Using cache strategy target: {remoteCacheStrategy}")
 
-        # Remove cache for new targets
-        if 'buildTargetCopyCache' in body['settings']:
-            del body['settings']['buildTargetCopyCache']
+        del body['settings']['buildTargetCopyCache']
 
         # Remove buildtargetid for new targets
         del body['buildtargetid']
