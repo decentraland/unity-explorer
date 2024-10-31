5229a2236076133ea29bf7fff2ae49b57a25d1c8
Vitaly Popuzin <popuzin@gmail.com>
2024-10-31 15:33:24 +0100

    removed outdated comment

--- Explorer/Assets/DCL/Browser/DecentralandUrls/DecentralandUrlsSource.cs
+++ Explorer/Assets/DCL/Browser/DecentralandUrls/DecentralandUrlsSource.cs
@@ -51,7 +51,7 @@ namespace DCL.Browser.DecentralandUrls
                 DecentralandUrl.FeatureFlags => $"https://feature-flags.decentraland.{ENV}",
                 DecentralandUrl.Help => $"https://decentraland.{ENV}/help/",
                 DecentralandUrl.Market => $"https://market.decentraland.{ENV}",
-                DecentralandUrl.AssetBundlesCDN => $"https://ab-cdn.decentraland.{ENV}", // All ABs are in org
+                DecentralandUrl.AssetBundlesCDN => $"https://ab-cdn.decentraland.{ENV}",
                 DecentralandUrl.ArchipelagoStatus => $"https://archipelago-ea-stats.decentraland.{ENV}/status",
                 DecentralandUrl.GatekeeperStatus => $"https://comms-gatekeeper.decentraland.{ENV}/status",
                 DecentralandUrl.Genesis => $"https://realm-provider-ea.decentraland.{ENV}/main",
