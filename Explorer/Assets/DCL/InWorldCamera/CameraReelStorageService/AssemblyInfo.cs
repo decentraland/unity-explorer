bec80c1104858d2edeeaaa0f106ebc90ab5dfe47
Vitaly Popuzin <popuzin@gmail.com>
2024-11-01 16:58:46 +0100

    fix another test failing

--- Explorer/Assets/DCL/InWorldCamera/CameraReelStorageService/AssemblyInfo.cs
+++ Explorer/Assets/DCL/InWorldCamera/CameraReelStorageService/AssemblyInfo.cs
@@ -2,3 +2,4 @@
 
 [assembly: InternalsVisibleTo("DCL.EditMode.Tests")]
 [assembly: InternalsVisibleTo("DCL.PlayMode.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] // for NSubstitute
