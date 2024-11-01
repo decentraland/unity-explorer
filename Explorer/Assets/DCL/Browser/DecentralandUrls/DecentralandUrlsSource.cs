ebe79872042acad71aa27c19c0b57a567a8e9039
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2024-11-01 15:53:04 -0300

    Cleanup

--- Explorer/Assets/DCL/Browser/DecentralandUrls/DecentralandUrlsSource.cs
+++ Explorer/Assets/DCL/Browser/DecentralandUrls/DecentralandUrlsSource.cs
@@ -7,9 +7,12 @@ namespace DCL.Browser.DecentralandUrls
     //TODO test urls
     public class DecentralandUrlsSource : IDecentralandUrlsSource
     {
-        private static string GENERAL_ENV = "{ENV}";
-        private static string ASSET_BUNDLE_ENV = "{ASSET_BUNDLE_ENV}";
-        private static string GENESIS_URL; 
+        private const string ENV = "{ENV}";
+        private static string ASSET_BUNDLE_URL;
+        private static string GENESIS_URL;
+
+        private const string ASSET_BUNDLE_URL_TEMPLATE = "https://ab-cdn.decentraland.{0}";
+        private const string GENESIS_URL_TEMPLATE = "https://realm-provider-ea.decentraland.{0}/main";
 
 
         private readonly Dictionary<DecentralandUrl, string> cache = new ();
@@ -24,22 +27,19 @@ namespace DCL.Browser.DecentralandUrls
             switch (environment)
             {
                 case DecentralandEnvironment.Org:
-                    GENERAL_ENV = environment.ToString()!.ToLower();
-                    ASSET_BUNDLE_ENV = environment.ToString()!.ToLower();
-                    GENESIS_URL = "https://realm-provider-ea.decentraland.org/main";
-                    break;
                 case DecentralandEnvironment.Zone:
-                    GENERAL_ENV = environment.ToString()!.ToLower();
-                    ASSET_BUNDLE_ENV = environment.ToString()!.ToLower();
-                    GENESIS_URL = "https://realm-provider-ea.decentraland.zone/main";
+                    ASSET_BUNDLE_URL = string.Format(ASSET_BUNDLE_URL_TEMPLATE, environmentDomainLowerCase);
+                    GENESIS_URL = string.Format(GENESIS_URL_TEMPLATE, environmentDomainLowerCase);
                     break;
                 case DecentralandEnvironment.Today:
+                    
                     //The today environemnt is a mixture of the org and today enviroments. 
                     //We want to fetch pointers from org, but asset bundles from today
                     //Thats because how peer-testing.decentraland.org works. 
                     //Its a catalyst that replicates the org environment and eth network, but doesnt propagate back to the production catalysts
-                    GENERAL_ENV = DecentralandEnvironment.Org.ToString()!.ToLower();
-                    ASSET_BUNDLE_ENV = environment.ToString()!.ToLower();
+                    environmentDomainLowerCase = DecentralandEnvironment.Org.ToString()!.ToLower();
+                    ASSET_BUNDLE_URL = "https://ab-cdn.decentraland.today";
+                    
                     //On staging, we hardcode the catalyst because its the only valid one with a valid comms configuration
                     GENESIS_URL = "https://peer-testing.decentraland.org";
                     break;
@@ -50,7 +50,7 @@ namespace DCL.Browser.DecentralandUrls
         {
             if (cache.TryGetValue(decentralandUrl, out string? url) == false)
             {
-                url = RawUrl(decentralandUrl).Replace(GENERAL_ENV, environmentDomainLowerCase);
+                url = RawUrl(decentralandUrl).Replace(ENV, environmentDomainLowerCase);
                 cache[decentralandUrl] = url;
             }
 
@@ -60,34 +60,34 @@ namespace DCL.Browser.DecentralandUrls
         private static string RawUrl(DecentralandUrl decentralandUrl) =>
             decentralandUrl switch
             {
-                DecentralandUrl.DiscordLink => $"https://decentraland.{GENERAL_ENV}/discord/",
-                DecentralandUrl.PrivacyPolicy => $"https://decentraland.{GENERAL_ENV}/privacy",
-                DecentralandUrl.TermsOfUse => $"https://decentraland.{GENERAL_ENV}/terms",
-                DecentralandUrl.ApiPlaces => $"https://places.decentraland.{GENERAL_ENV}/api/places",
-                DecentralandUrl.ApiAuth => $"https://auth-api.decentraland.{GENERAL_ENV}",
-                DecentralandUrl.AuthSignature => $"https://decentraland.{GENERAL_ENV}/auth/requests",
-                DecentralandUrl.POI => $"https://dcl-lists.decentraland.{GENERAL_ENV}/pois",
-                DecentralandUrl.ContentModerationReport => $"https://places.decentraland.{GENERAL_ENV}/api/report",
+                DecentralandUrl.DiscordLink => $"https://decentraland.{ENV}/discord/",
+                DecentralandUrl.PrivacyPolicy => $"https://decentraland.{ENV}/privacy",
+                DecentralandUrl.TermsOfUse => $"https://decentraland.{ENV}/terms",
+                DecentralandUrl.ApiPlaces => $"https://places.decentraland.{ENV}/api/places",
+                DecentralandUrl.ApiAuth => $"https://auth-api.decentraland.{ENV}",
+                DecentralandUrl.AuthSignature => $"https://decentraland.{ENV}/auth/requests",
+                DecentralandUrl.POI => $"https://dcl-lists.decentraland.{ENV}/pois",
+                DecentralandUrl.ContentModerationReport => $"https://places.decentraland.{ENV}/api/report",
                 DecentralandUrl.GateKeeperSceneAdapter =>
-                    $"https://comms-gatekeeper.decentraland.{GENERAL_ENV}/get-scene-adapter",
-                DecentralandUrl.OpenSea => $"https://opensea.decentraland.{GENERAL_ENV}",
-                DecentralandUrl.Host => $"https://decentraland.{GENERAL_ENV}",
-                DecentralandUrl.PeerAbout => $"https://peer.decentraland.{GENERAL_ENV}/about",
-                DecentralandUrl.DAO => $"https://decentraland.{GENERAL_ENV}/dao/",
-                DecentralandUrl.Notification => $"https://notifications.decentraland.{GENERAL_ENV}/notifications",
+                    $"https://comms-gatekeeper.decentraland.{ENV}/get-scene-adapter",
+                DecentralandUrl.OpenSea => $"https://opensea.decentraland.{ENV}",
+                DecentralandUrl.Host => $"https://decentraland.{ENV}",
+                DecentralandUrl.PeerAbout => $"https://peer.decentraland.{ENV}/about",
+                DecentralandUrl.DAO => $"https://decentraland.{ENV}/dao/",
+                DecentralandUrl.Notification => $"https://notifications.decentraland.{ENV}/notifications",
                 DecentralandUrl.NotificationRead =>
-                    $"https://notifications.decentraland.{GENERAL_ENV}/notifications/read",
-                DecentralandUrl.FeatureFlags => $"https://feature-flags.decentraland.{GENERAL_ENV}",
-                DecentralandUrl.Help => $"https://decentraland.{GENERAL_ENV}/help/",
-                DecentralandUrl.Market => $"https://market.decentraland.{GENERAL_ENV}",
-                DecentralandUrl.AssetBundlesCDN => $"https://ab-cdn.decentraland.{ASSET_BUNDLE_ENV}",
-                DecentralandUrl.ArchipelagoStatus => $"https://archipelago-ea-stats.decentraland.{GENERAL_ENV}/status",
-                DecentralandUrl.GatekeeperStatus => $"https://comms-gatekeeper.decentraland.{GENERAL_ENV}/status",
+                    $"https://notifications.decentraland.{ENV}/notifications/read",
+                DecentralandUrl.FeatureFlags => $"https://feature-flags.decentraland.{ENV}",
+                DecentralandUrl.Help => $"https://decentraland.{ENV}/help/",
+                DecentralandUrl.Market => $"https://market.decentraland.{ENV}",
+                DecentralandUrl.AssetBundlesCDN => ASSET_BUNDLE_URL,
+                DecentralandUrl.ArchipelagoStatus => $"https://archipelago-ea-stats.decentraland.{ENV}/status",
+                DecentralandUrl.GatekeeperStatus => $"https://comms-gatekeeper.decentraland.{ENV}/status",
                 DecentralandUrl.Genesis => GENESIS_URL,
-                DecentralandUrl.Badges => $"https://badges.decentraland.{GENERAL_ENV}",
-                DecentralandUrl.CameraReelUsers => $"https://camera-reel-service.decentraland.{GENERAL_ENV}/api/users",
+                DecentralandUrl.Badges => $"https://badges.decentraland.{ENV}",
+                DecentralandUrl.CameraReelUsers => $"https://camera-reel-service.decentraland.{ENV}/api/users",
                 DecentralandUrl.CameraReelImages =>
-                    $"https://camera-reel-service.decentraland.{GENERAL_ENV}/api/images",
+                    $"https://camera-reel-service.decentraland.{ENV}/api/images",
                 _ => throw new ArgumentOutOfRangeException(nameof(decentralandUrl), decentralandUrl, null!)
             };
     }
