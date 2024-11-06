50e462bc3b875dd52774d7abedc056bd0482b820
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2024-11-06 12:10:44 -0300

    feat: testing realms (#2697)
    * Add support for environment validator
    
    * Realm debug view
    
    * Fix compilation issue
    
    * Cleanup
    
    ---------
    
    Signed-off-by: Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
    Co-authored-by: Ashley Canning <ashley.canning@decentraland.org>

---
+++ Explorer/Assets/Scripts/Global/Dynamic/RealmNavigatorDebugView.cs
@@ -0,0 +1,36 @@
+using DCL.DebugUtilities;
+using DCL.DebugUtilities.UIBindings;
+
+namespace Global.Dynamic
+{
+    public class RealmNavigatorDebugView
+    {
+        private readonly ElementBinding<string> currentRealmName;
+        private readonly ElementBinding<string> currentCatalystLambdas;
+        private readonly ElementBinding<string> currentCatalystContent;
+
+
+        public RealmNavigatorDebugView(IDebugContainerBuilder debugContainerBuilder)
+        {
+            currentRealmName = new ElementBinding<string>("");
+            currentCatalystLambdas = new ElementBinding<string>("");
+            currentCatalystContent = new ElementBinding<string>("");
+
+            debugContainerBuilder
+                .TryAddWidget("Realm")?
+                .AddCustomMarker(new ElementBinding<string>("Current Realm"))
+                .AddCustomMarker(currentRealmName)
+                .AddCustomMarker(new ElementBinding<string>("Current Lambdas Catalyst"))
+                .AddCustomMarker(currentCatalystLambdas)
+                .AddCustomMarker(new ElementBinding<string>("Current Content Catalyst"))
+                .AddCustomMarker(currentCatalystContent);
+        }
+
+        public void UpdateRealmName(string newRealm, string catalystLambdas, string catalystContent)
+        {
+            currentRealmName.Value = newRealm;
+            currentCatalystLambdas.Value = catalystLambdas;
+            currentCatalystContent.Value = catalystContent;
+        }
+    }
+}
