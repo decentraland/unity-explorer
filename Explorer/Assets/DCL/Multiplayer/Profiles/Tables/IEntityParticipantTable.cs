4f2431256a92b1e0f7aa4678867ab5c2a4bed5fa
Mikhail Agapov <118179774+mikhail-dcl@users.noreply.github.com>
2024-11-08 15:55:38 +0300

    Fix: multiplayer & avatar issues (#2660)
    * Add more functionality to connective rooms
    
    - Get rid of massive abstraction with intention to simplify hierarchy layers and dependencies
    - Connection Loop can restart if failed on any stage
    - Remove layers that became redundant
    - Extend Room Debug Views to activate and deactivate rooms
    
    * Add Connection Loop Health to debug view
    
    * Add Room Indicators for debugging
    
    * Fix Ghosts
    
    * Fix Nametags and Room Indicators leak
    
    * Add "Async" suffix to new async methods to fix tests
    
    * Add RoomIndicator to addressables
    
    * Fix Gatekeeper room flickering
    
    * Synchronize peers removal with the local state
    
    - Remove reliance on participants reported from the sdk to avoid race condition
    
    * Add protection against `null` in `TryGet`
    
    * Move NametagCleanUpSystem after avatar clean up because of the dependency on DeferDeletion
    
    ---------
    
    Co-authored-by: Ashley Canning <ashley.canning@decentraland.org>

--- Explorer/Assets/DCL/Multiplayer/Profiles/Tables/IEntityParticipantTable.cs
+++ Explorer/Assets/DCL/Multiplayer/Profiles/Tables/IEntityParticipantTable.cs
@@ -1,11 +1,17 @@
 using Arch.Core;
+using DCL.Multiplayer.Connections.Rooms;
 
 namespace DCL.Multiplayer.Profiles.Tables
 {
     public interface IEntityParticipantTable : IReadOnlyEntityParticipantTable
     {
-        void Register(string walletId, Entity entity);
+        void Register(string walletId, Entity entity, RoomSource fromRoom);
 
-        void Release(string walletId);
+        void AddRoomSource(string walletId, RoomSource fromRoom);
+
+        /// <summary>
+        ///     Returns true if the entity is no longer connected to any room
+        /// </summary>
+        bool Release(string walletId, RoomSource fromRoom);
     }
 }
