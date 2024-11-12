1c1dc597fcad2df99a0e364cc1563ca0056589cc
nickkhalow <nickhalow@gmail.com>
2024-11-12 11:07:27 +0200

    update build scripts

--- Explorer/Assets/Plugins/TexturesFuse/textures-server/CompressonatorWorkaround/builds_mac.sh
+++ Explorer/Assets/Plugins/TexturesFuse/textures-server/CompressonatorWorkaround/builds_mac.sh
@@ -18,3 +18,12 @@ cp libCMP_Core.a ../../../Compressator/lib/libCMP_Core.a
 cp libCMP_Core_SSE.a ../../../Compressator/lib/libCMP_Core_SSE.a
 cp libCMP_Core_AVX.a ../../../Compressator/lib/libCMP_Core_AVX.a
 cp libCMP_Core_AVX512.a ../../../Compressator/lib/libCMP_Core_AVX512.a
+
+cd -;
+
+cd compressonator/cmp_framework;
+cmake . -DOPTION_CMP_QT=OFF -DOPTION_CMP_VULKAN=OFF -DOPTION_CMP_DIRECTX=OFF -DOPTION_CMP_OPENCV=OFF -DOPTION_ENABLE_ALL_APPS=OFF -DOPTION_BUILD_CMP_SDK=ON -DOPTION_BUILD_APPS_CMP_CLI=OFF -DOPTION_BUILD_KTX2=OFF -DSIMDE_X86_AVX_ENABLE_NATIVE_ALIASES=ON -DGLM_ENABLE_EXPERIMENTAL=OFF -DGLM_BUILD_LIBRARY=ON -DCMAKE_BUILD_TYPE=Release; 
+make clean;
+make -j10;
+
+cp libCMP_Framework.a ../../../Compressator/lib/libCMP_Framework.a
--- Explorer/Assets/Plugins/TexturesFuse/textures-server/CompressonatorWorkaround/builds_win.ps1
+++ Explorer/Assets/Plugins/TexturesFuse/textures-server/CompressonatorWorkaround/builds_win.ps1
@@ -58,3 +58,33 @@ Copy-Item "Release\CMP_Core_SSE.lib" -Destination "..\..\..\Compressator\lib\lib
 Copy-Item "Release\CMP_Core_AVX.lib" -Destination "..\..\..\Compressator\lib\libCMP_Core_AVX.lib"
 Copy-Item "Release\CMP_Core_AVX512.lib" -Destination "..\..\..\Compressator\lib\libCMP_Core_AVX512.lib"
 
+
+
+Set-Location ..
+Set-Location ..
+
+# Change directory
+Set-Location compressonator/cmp_framework
+
+# Run cmake command (line breaks added for readability)
+cmake . -G "Visual Studio 17 2022" `
+    -DOPTION_CMP_QT=OFF `
+    -DOPTION_CMP_VULKAN=OFF `
+    -DOPTION_CMP_DIRECTX=OFF `
+    -DOPTION_CMP_OPENCV=OFF `
+    -DOPTION_ENABLE_ALL_APPS=OFF `
+    -DOPTION_BUILD_CMP_SDK=ON `
+    -DOPTION_BUILD_APPS_CMP_CLI=OFF `
+    -DOPTION_BUILD_KTX2=OFF `
+    -DSIMDE_X86_AVX_ENABLE_NATIVE_ALIASES=ON `
+    -DGLM_ENABLE_EXPERIMENTAL=OFF `
+    -DGLM_BUILD_LIBRARY=ON
+    
+# Instead of make clean, you'll want to clean the build using cmake
+cmake --build . --target clean
+
+# Build using cmake instead of make
+cmake --build . --config Release --parallel 10
+
+# Copy the library files (using Windows path separator)
+Copy-Item "Release\CMP_Framework.lib" -Destination "..\..\..\Compressator\lib\libCMP_Framework.lib"
