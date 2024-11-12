85e02bd82364205e007c50f8a6669a44fd5d68c1
nickkhalow <nickhalow@gmail.com>
2024-11-12 12:29:30 +0200

    copy files postprocess

--- Explorer/Assets/Plugins/TexturesFuse/Editor/TexturesFuseBuildProcess.cs
+++ Explorer/Assets/Plugins/TexturesFuse/Editor/TexturesFuseBuildProcess.cs
@@ -4,12 +4,14 @@ using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using UnityEditor.Callbacks;
+using UnityEngine;
 
 namespace Plugins.TexturesFuse.Editor
 {
-    public class TexturesFuseBuildProcess : IPreprocessBuildWithReport
+    public class TexturesFuseBuildProcess : IPreprocessBuildWithReport, IPostprocessBuildWithReport
     {
         private const string PATH_TO_MODIFY = "Assets/Plugins/TexturesFuse/textures-server";
+        private const string PLUGINS_DIR = "plugins";
 
         public int callbackOrder => 0;
 
@@ -38,6 +40,33 @@ namespace Plugins.TexturesFuse.Editor
 
             // Refresh the AssetDatabase to reflect changes
             AssetDatabase.Refresh();
+        }
+
+        public void OnPostprocessBuild(BuildReport report)
+        {
+            if (Application.platform is not RuntimePlatform.WindowsEditor)
+                return;
+
+            // Get the build path
+            string buildPath = report!.summary.outputPath!;
+            string targetDir = Path.Combine(buildPath, "plugins");
+
+            // Check if source folder exists
+            if (Directory.Exists(PLUGINS_DIR) == false)
+                Debug.LogError("Source folder does not exist. No files were copied.");
+
+            CopyFilesRecursively(PLUGINS_DIR, targetDir);
+        }
+
+        private static void CopyFilesRecursively(string sourcePath, string targetPath)
+        {
+            //Now Create all of the directories
+            foreach (string dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
+                Directory.CreateDirectory(dirPath.Replace(sourcePath, targetPath));
+
+            //Copy all the files & Replaces any files with the same name
+            foreach (string newPath in Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories))
+                File.Copy(newPath, newPath.Replace(sourcePath, targetPath), true);
         }
     }
 }
