56203ba2a0f714dc7a55214f9502b7926fda0f1c
Nicolas Lorusso <nalorusso@gmail.com>
2024-11-13 16:50:23 -0300

    refactor into consts

--- Explorer/Assets/DCL/EventsApi/GoogleUserCalendar.cs
+++ Explorer/Assets/DCL/EventsApi/GoogleUserCalendar.cs
@@ -6,6 +6,13 @@ namespace DCL.EventsApi
 {
     public class GoogleUserCalendar : IUserCalendar
     {
+        private const string GOOGLE_CALENDAR_DOMAIN = "https://www.google.com/calendar/event";
+        private const string ACTION_PARAM = "action";
+        private const string ACTION_TEMPLATE = "TEMPLATE";
+        private const string TITLE_PARAM = "text";
+        private const string DATES_PARAM = "dates";
+        private const string DESCRIPTION_PARAM = "details";
+
         private readonly IWebBrowser webBrowser;
         private readonly URLBuilder urlBuilder = new ();
 
@@ -18,11 +25,11 @@ namespace DCL.EventsApi
         {
             urlBuilder.Clear();
 
-            urlBuilder.AppendDomain(URLDomain.FromString("https://www.google.com/calendar/event"))
-                      .AppendParameter(new URLParameter("action", "TEMPLATE"))
-                      .AppendParameter(new URLParameter("text", title))
-                      .AppendParameter(new URLParameter("dates", $"{startAt:yyyyMMddTHHmmssZ}/{endAt:yyyyMMddTHHmmssZ}"))
-                      .AppendParameter(new URLParameter("details", description));
+            urlBuilder.AppendDomain(URLDomain.FromString(GOOGLE_CALENDAR_DOMAIN))
+                      .AppendParameter(new URLParameter(ACTION_PARAM, ACTION_TEMPLATE))
+                      .AppendParameter(new URLParameter(TITLE_PARAM, title))
+                      .AppendParameter(new URLParameter(DATES_PARAM, $"{startAt:yyyyMMddTHHmmssZ}/{endAt:yyyyMMddTHHmmssZ}"))
+                      .AppendParameter(new URLParameter(DESCRIPTION_PARAM, description));
 
             webBrowser.OpenUrl(urlBuilder.Build());
         }
