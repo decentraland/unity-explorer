32f1159cc15f07038cb87c4ba684aa429474a73a
nickkhalow <nickhalow@gmail.com>
2024-11-15 23:11:33 +0200

    cmp_config update

--- Explorer/Assets/Plugins/TexturesFuse/textures-server/FreeImage/Source/cmake_configs/cmp_config.cmake
+++ Explorer/Assets/Plugins/TexturesFuse/textures-server/FreeImage/Source/cmake_configs/cmp_config.cmake
@@ -1,4 +1,6 @@
 # Compressor
 set(COMPRESSATOR_ROOT ${CMAKE_SOURCE_DIR}/../../../Compressator)
+set(COMPRESSATOR_ORIGIN_ROOT ${CMAKE_SOURCE_DIR}/../../../CompressonatorWorkaround/compressonator)
 
 message("COMPRESSATOR_ROOT Is: ${COMPRESSATOR_ROOT}")
+message("COMPRESSATOR_ORIGIN_ROOT Is: ${COMPRESSATOR_ORIGIN_ROOT}")
