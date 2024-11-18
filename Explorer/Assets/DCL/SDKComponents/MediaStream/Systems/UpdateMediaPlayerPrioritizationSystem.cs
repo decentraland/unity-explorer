ab4dc41ea81ae4df345f0534a38136afa114a052
Alejandro Villalba <lince3d@gmail.com>
2024-11-18 12:26:00 +0100

    Prioritization debugging disabled

--- Explorer/Assets/DCL/SDKComponents/MediaStream/Systems/UpdateMediaPlayerPrioritizationSystem.cs
+++ Explorer/Assets/DCL/SDKComponents/MediaStream/Systems/UpdateMediaPlayerPrioritizationSystem.cs
@@ -1,4 +1,4 @@
-﻿#define DEBUG_VIDEO_PRIORITIES
+﻿//#define DEBUG_VIDEO_PRIORITIES
 // When the definition is enabled, a colored cube will be created next to each video's mesh renderer. Its color corresponds to the current priority of the video.
 // Green means higher priority, red means lower priority. Blue means that it was prioritized but it is not allowed to play due to the maximum limit.
 // Black means it has been discarded from prioritization.
