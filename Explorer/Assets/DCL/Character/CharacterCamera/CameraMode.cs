1f75eff4886d93bef25ee0da4389fc3e2af784f2
Fran Colarich <56565104+fcolarich@users.noreply.github.com>
2024-11-19 16:58:53 +0100

    fix: Unknown camera mode Enum breaks scenes (#2830)
    * Update CameraMode.cs
    
    * Changed default camera to third person

--- Explorer/Assets/DCL/Character/CharacterCamera/CameraMode.cs
+++ Explorer/Assets/DCL/Character/CharacterCamera/CameraMode.cs
@@ -2,15 +2,14 @@
 {
     public enum CameraMode : byte
     {
-        Unknown = 0,
-        FirstPerson = 1,
-        ThirdPerson = 2,
-        DroneView = 3,
-        SDKCamera = 4,
+        FirstPerson = 0,
+        ThirdPerson = 1,
+        DroneView = 2,
+        SDKCamera = 3,
 
         /// <summary>
         ///     Free-fly, does not follow character, intercepts controls designated for character movement
         /// </summary>
-        Free = 5,
+        Free = 4,
     }
 }
