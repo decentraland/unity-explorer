02621d7aebae64ad63d34dbfdb67af1b922b5f4c
nickkhalow <nickhalow@gmail.com>
2024-11-20 01:02:50 +0200

    compress shader interface

---
+++ Explorer/Assets/Plugins/TexturesFuse/TexturesServerWrap/CompressShaders/ICompressShaders.cs
@@ -0,0 +1,29 @@
+using Cysharp.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using UnityEngine;
+
+namespace Plugins.TexturesFuse.TexturesServerWrap.CompressShaders
+{
+    public interface ICompressShaders
+    {
+        const string CMD_ARGS = "compile-shaders-and-exit";
+
+        bool AreReady();
+
+        UniTask WarmUpAsync(CancellationToken token);
+
+        static string ShadersDir() =>
+            Application.isEditor
+                ? Path.Combine(Directory.GetCurrentDirectory(), "plugins")
+                : "plugins";
+    }
+
+    public static class CompressShadersExtensions
+    {
+        public static UniTask WarmUpIfRequiredAsync(this ICompressShaders compressShaders, CancellationToken token) =>
+            compressShaders.AreReady()
+                ? UniTask.CompletedTask
+                : compressShaders.WarmUpAsync(token);
+    }
+}
