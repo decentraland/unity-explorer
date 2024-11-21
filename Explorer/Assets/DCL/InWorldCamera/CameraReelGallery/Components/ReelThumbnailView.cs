2a291e7665a5927bb54cae8e36d31983ebd813a3
lorenzo-ranciaffi <lorenzo.ranciaffi@decentraland.org>
2024-11-21 11:56:22 +0100

    fix attribute visibility

--- Explorer/Assets/DCL/InWorldCamera/CameraReelGallery/Components/ReelThumbnailView.cs
+++ Explorer/Assets/DCL/InWorldCamera/CameraReelGallery/Components/ReelThumbnailView.cs
@@ -9,14 +9,14 @@ namespace DCL.InWorldCamera.CameraReelGallery.Components
     public class ReelThumbnailView : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IDisposable
     {
         [Header("References")]
-        public Image thumbnailImage;
+        [SerializeField] internal Image thumbnailImage;
         [SerializeField] internal LoadingBrightView loadingBrightView;
         [SerializeField] internal RectTransform optionButtonContainer;
         [SerializeField] internal Button button;
         [SerializeField] internal GameObject outline;
 
         [Header("Configuration")]
-        [SerializeField] internal Vector3 optionButtonOffset = new (-15.83997f, -22f, 0);
+        [SerializeField] internal Vector3 optionButtonOffset = new (-11f, -23f, 0);
         [SerializeField] internal float scaleFactorOnHover = 1.03f;
         [SerializeField] internal float scaleAnimationDuration = 0.3f;
         [SerializeField] internal float thumbnailLoadedAnimationDuration = 0.3f;
