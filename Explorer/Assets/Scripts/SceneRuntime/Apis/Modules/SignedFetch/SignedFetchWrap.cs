6135f5c0f2e364c882f0e86676f3ab5e9bf92bee
Ashley Canning <ashley.canning@gmail.com>
2024-11-27 15:19:32 +0000

    Remove signed fetch origin & referer

--- Explorer/Assets/Scripts/SceneRuntime/Apis/Modules/SignedFetch/SignedFetchWrap.cs
+++ Explorer/Assets/Scripts/SceneRuntime/Apis/Modules/SignedFetch/SignedFetchWrap.cs
@@ -88,11 +88,7 @@ namespace SceneRuntime.Apis.Modules.SignedFetch
             string hashBody = request.init.body.Length > 0 ? sha256_hash(request.init.body) : null;
             string signatureMetadata = CreateSignatureMetadata(hashBody);
 
-            string hostUrl = decentralandUrlsSource.Url(DecentralandUrl.Host);
-
             var headers = new WebRequestHeadersInfo(request.init?.headers)
-                         .Add("Origin", hostUrl)
-                         .Add("Referer", hostUrl)
                          .WithSign(signatureMetadata, unixTimestamp);
 
             var signInfo = WebRequestSignInfo.NewFromRaw(
@@ -184,7 +180,6 @@ namespace SceneRuntime.Apis.Modules.SignedFetch
 
             var metadata = new SignatureMetadata
             {
-                origin = decentralandUrlsSource.Url(DecentralandUrl.Host),
                 sceneId = sceneData.SceneEntityDefinition.id!,
                 parcel = $"{parcel.x},{parcel.y}",
                 tld = decentralandUrlsSource.DecentralandDomain,
@@ -210,7 +205,6 @@ namespace SceneRuntime.Apis.Modules.SignedFetch
         [Serializable]
         private struct SignatureMetadata
         {
-            public string origin;
             public string sceneId;
             public string parcel;
 
