ba2c44b3d1ddbb68eabe12a43a7fcba7bd7915d4
Vitaly Popuzin <popuzin@gmail.com>
2024-11-29 19:25:33 +0100

    left only unhandled exceptions

--- Explorer/Assets/DCL/PerformanceAndDiagnostics/Analytics/CriticalLogsAnalyticsHandler.cs
+++ Explorer/Assets/DCL/PerformanceAndDiagnostics/Analytics/CriticalLogsAnalyticsHandler.cs
@@ -1,14 +1,10 @@
-﻿using DCL.Diagnostics;
-using Segment.Serialization;
+﻿using Segment.Serialization;
 using System;
 using System.Runtime.CompilerServices;
-using System.Text;
-using UnityEngine;
-using Object = UnityEngine.Object;
 
 namespace DCL.PerformanceAndDiagnostics.Analytics
 {
-    public class CriticalLogsAnalyticsHandler : IReportHandler
+    public class CriticalLogsAnalyticsHandler
     {
         private const int PAYLOAD_LIMIT = 28 * 1024; // Segment == 32 KB, leaving some room for headers
         private const int SAFE_CHAR_LIMIT = PAYLOAD_LIMIT / 4; // 7,680 characters
@@ -26,60 +22,12 @@ namespace DCL.PerformanceAndDiagnostics.Analytics
             AppDomain.CurrentDomain.UnhandledException -= TrackUnhandledException;
         }
 
-        private void TrackUnhandledException(object sender, UnhandledExceptionEventArgs args)
-        {
-            var e = (Exception)args.ExceptionObject;
-
+        private void TrackUnhandledException(object sender, UnhandledExceptionEventArgs args) =>
             analytics.Track(AnalyticsEvents.General.ERROR, new JsonObject
             {
                 { "type", "unhandled exception" },
-                { "category", IAnalyticsController.UNDEFINED },
-                { "scene_hash", IAnalyticsController.UNDEFINED },
-                { "message", TrimToPayloadLimit(e.Message) },
+                { "message", TrimToPayloadLimit(((Exception)args.ExceptionObject).Message) },
             });
-        }
-
-        public void Log(LogType logType, ReportData reportData, Object context, object messageObj)
-        {
-            if (logType != LogType.Error && logType != LogType.Exception) return;
-
-            analytics.Track(AnalyticsEvents.General.ERROR, new JsonObject
-            {
-                { "type", logType.ToString() },
-                { "category", reportData.Category },
-                { "scene_hash", reportData.SceneShortInfo.Name },
-                { "message", messageObj is string messageString ? TrimToPayloadLimit(messageString) : TrimToPayloadLimit(messageObj.ToString()) },
-            });
-        }
-
-        public void LogFormat(LogType logType, ReportData reportData, Object context, object message, params object[] args)
-        {
-            if (logType != LogType.Error && logType != LogType.Exception) return;
-
-            Log(logType, reportData, context, string.Format(message.ToString(), args));
-        }
-
-        public void LogException<T>(T ecsSystemException) where T: Exception, IDecentralandException
-        {
-            analytics.Track(AnalyticsEvents.General.ERROR, new JsonObject
-            {
-                { "type", "exception" },
-                { "category", "ecs" },
-                { "scene_hash", IAnalyticsController.UNDEFINED },
-                { "message", TrimToPayloadLimit(ecsSystemException.Message) },
-            });
-        }
-
-        public void LogException(Exception exception, ReportData reportData, Object context)
-        {
-            analytics.Track(AnalyticsEvents.General.ERROR, new JsonObject
-            {
-                { "type", "exception" },
-                { "category", reportData.Category },
-                { "scene_hash", reportData.SceneShortInfo.Name },
-                { "message", TrimToPayloadLimit(exception.Message) },
-            });
-        }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         private static string TrimToPayloadLimit(string message)
