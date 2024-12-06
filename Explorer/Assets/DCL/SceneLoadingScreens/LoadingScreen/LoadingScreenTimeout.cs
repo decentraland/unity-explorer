f89be98b524d4df91f1e82fbefae24f6afc26877
nickkhalow <nickhalow@gmail.com>
2024-12-06 13:30:09 +0200

    LoadingScreenTimeout Set(float seconds)

--- Explorer/Assets/DCL/SceneLoadingScreens/LoadingScreen/LoadingScreenTimeout.cs
+++ Explorer/Assets/DCL/SceneLoadingScreens/LoadingScreen/LoadingScreenTimeout.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using UnityEngine;
 
 namespace DCL.SceneLoadingScreens.LoadingScreen
 {
     public class LoadingScreenTimeout
     {
-        public TimeSpan Value { get; set; } = TimeSpan.FromMinutes(2);
+        public TimeSpan Value { get; private set; } = TimeSpan.FromMinutes(2);
+
+        public void Set(float seconds)
+        {
+            Value = TimeSpan.FromSeconds(Mathf.Max(seconds, 0));
+        }
     }
 }
