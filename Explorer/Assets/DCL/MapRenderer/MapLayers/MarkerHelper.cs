98dffcd7da0f228693344eccb00f5450d4c97275
davidejensen <davidejensen@live.it>
2024-12-09 13:10:52 +0100

    Added fade utils for map pins

--- Explorer/Assets/DCL/MapRenderer/MapLayers/MarkerHelper.cs
+++ Explorer/Assets/DCL/MapRenderer/MapLayers/MarkerHelper.cs
@@ -4,7 +4,9 @@ using DG.Tweening.Core;
 using DG.Tweening.Plugins.Options;
 using NBitcoin;
 using System;
+using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Tasks;
 using UnityEngine;
 
 namespace DCL.MapRenderer.MapLayers
@@ -49,5 +51,106 @@ namespace DCL.MapRenderer.MapLayers
                 throw;
             }
         }
+
+        public static void SetAlpha(SpriteRenderer[] spriteRenderers, TMPro.TextMeshPro[] textMeshes, float alpha)
+        {
+            for (var i = 0; i < spriteRenderers.Length; i++)
+            {
+                var sr = spriteRenderers[i];
+                Color resetColor = sr.color;
+                resetColor.a = alpha;
+                sr.color = resetColor;
+            }
+
+            for (var i = 0; i < textMeshes.Length; i++)
+            {
+                var tm = textMeshes[i];
+                Color resetColor = tm.color;
+                resetColor.a = alpha;
+                tm.color = resetColor;
+            }
+        }
+
+        public static async UniTask FadeToAsync(
+            SpriteRenderer[] spriteRenderers,
+            TMPro.TextMeshPro[] textMeshes,
+            float targetAlpha,
+            float duration,
+            Ease ease,
+            CancellationToken cancellationToken,
+            float? resetToAlpha = null)
+        {
+            var originalSpriteAlphas = new float[spriteRenderers.Length];
+            var originalTextAlphas = new float[textMeshes.Length];
+
+            for (var i = 0; i < spriteRenderers.Length; i++)
+                originalSpriteAlphas[i] = spriteRenderers[i].color.a;
+
+            for (var i = 0; i < textMeshes.Length; i++)
+                originalTextAlphas[i] = textMeshes[i].color.a;
+
+            var spriteTweens = new List<TweenerCore<Color, Color, ColorOptions>>(spriteRenderers.Length);
+            var textTweens = new List<TweenerCore<Color, Color, ColorOptions>>(textMeshes.Length);
+
+            for (var i = 0; i < spriteRenderers.Length; i++)
+            {
+                var sr = spriteRenderers[i];
+                Color targetColor = sr.color;
+                targetColor.a = targetAlpha;
+                spriteTweens.Add(sr.DOColor(targetColor, duration).SetEase(ease));
+            }
+
+            for (var i = 0; i < textMeshes.Length; i++)
+            {
+                var tm = textMeshes[i];
+                Color targetColor = tm.color;
+                targetColor.a = targetAlpha;
+                textTweens.Add(tm.DOColor(targetColor, duration).SetEase(ease));
+            }
+
+            try
+            {
+                var spriteTasks = new Task[spriteTweens.Count];
+
+                for (var i = 0; i < spriteTweens.Count; i++)
+                    spriteTasks[i] = spriteTweens[i].AsyncWaitForCompletion().WithCancellation(cancellationToken);
+
+                var textTasks = new Task[textTweens.Count];
+
+                for (var i = 0; i < textTweens.Count; i++)
+                    textTasks[i] = textTweens[i].AsyncWaitForCompletion().WithCancellation(cancellationToken);
+
+                await Task.WhenAll(spriteTasks);
+                await Task.WhenAll(textTasks);
+            }
+            catch (OperationCanceledException)
+            {
+                Debug.Log("Fade cancelled");
+                for (int i = 0; i < spriteRenderers.Length; i++)
+                {
+                    var sr = spriteRenderers[i];
+                    Color resetColor = sr.color;
+                    resetColor.a = resetToAlpha ?? originalSpriteAlphas[i];
+                    sr.color = resetColor;
+                }
+
+                for (int i = 0; i < textMeshes.Length; i++)
+                {
+                    var tm = textMeshes[i];
+                    Color resetColor = tm.color;
+                    resetColor.a = resetToAlpha ?? originalTextAlphas[i];
+                    tm.color = resetColor;
+                }
+
+                for (var i = 0; i < spriteTweens.Count; i++)
+                    spriteTweens[i].Kill();
+
+                for (var i = 0; i < textTweens.Count; i++)
+                    textTweens[i].Kill();
+
+                throw;
+            }
+        }
+
     }
 }
