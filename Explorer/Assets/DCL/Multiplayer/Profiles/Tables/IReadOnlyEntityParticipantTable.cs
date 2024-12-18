739d888ee56190fd452eff71434f66747c485a7e
Nick Khalow <71646502+NickKhalow@users.noreply.github.com>
2024-12-19 01:44:11 +0800

    immutable entry for IReadOnlyEntityParticipantTable (#3021)

--- Explorer/Assets/DCL/Multiplayer/Profiles/Tables/IReadOnlyEntityParticipantTable.cs
+++ Explorer/Assets/DCL/Multiplayer/Profiles/Tables/IReadOnlyEntityParticipantTable.cs
@@ -1,23 +1,33 @@
 using Arch.Core;
 using DCL.Multiplayer.Connections.Rooms;
-using DCL.Multiplayer.Profiles.Entities;
 using System.Collections.Generic;
+using Utility;
 
 namespace DCL.Multiplayer.Profiles.Tables
 {
     public interface IReadOnlyEntityParticipantTable
     {
-        public struct Entry
+        public readonly struct Entry
         {
-            public string WalletId;
-            public Entity Entity;
-            public RoomSource ConnectedTo;
+            public readonly string WalletId;
+            public readonly Entity Entity;
+            public readonly RoomSource ConnectedTo;
 
             internal Entry(string walletId, Entity entity, RoomSource connectedTo)
             {
                 WalletId = walletId;
                 Entity = entity;
                 ConnectedTo = connectedTo;
+            }
+
+            public Entry WithRoomSource(RoomSource fromRoom) =>
+                new (WalletId, Entity, ConnectedTo | fromRoom);
+
+            public Entry WithoutRoomSource(RoomSource fromRoom)
+            {
+                RoomSource newRoomSource = ConnectedTo;
+                newRoomSource.RemoveFlag(fromRoom);
+                return new Entry(WalletId, Entity, newRoomSource);
             }
         }
 
