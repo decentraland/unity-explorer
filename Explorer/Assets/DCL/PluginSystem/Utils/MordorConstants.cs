e6374984646117303fad5a86ded3da34ca0d1b9c
Fran Colarich <56565104+fcolarich@users.noreply.github.com>
2024-12-19 21:14:32 +0100

    Unified mordor constants and moved the file to a better location

--- Explorer/Assets/DCL/PluginSystem/Utils/MordorConstants.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public static class MordorConstants
-{
-    public static Vector3 PLAYER_MORDOR_POSITION = new(15000, 0, 15000);
-    public static Vector3 SCENE_MORDOR_POSITION = new(0, -10000, 0);
-}
