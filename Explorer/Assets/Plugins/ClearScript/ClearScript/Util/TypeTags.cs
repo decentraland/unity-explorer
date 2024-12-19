399964e66d708a06930590c217b6997b11fe6d9a
Ansis <ansis.malins@decentraland.org>
2024-12-19 19:41:04 +0100

    Replace ClearScript binaries with source, use debug binaries

---
+++ Explorer/Assets/Plugins/ClearScript/ClearScript/Util/TypeTags.cs
@@ -0,0 +1,18 @@
+namespace Microsoft.ClearScript.Util
+{
+    internal interface INothingTag
+    {
+    }
+
+    internal interface IWindowsScriptEngineTag
+    {
+    }
+
+    internal interface IVBScriptEngineTag
+    {
+    }
+
+    internal interface IWindowsScriptItemTag
+    {
+    }
+}
