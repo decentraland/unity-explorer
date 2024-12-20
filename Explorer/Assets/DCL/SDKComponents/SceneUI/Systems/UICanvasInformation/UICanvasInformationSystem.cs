02d496d1373e0526a023e68b78fbe739652c9ea0
Fran Colarich <56565104+fcolarich@users.noreply.github.com>
2024-12-21 00:00:44 +0100

    Fixed UI not updating correctly using PBUiCanvasInformation

--- Explorer/Assets/DCL/SDKComponents/SceneUI/Systems/UICanvasInformation/UICanvasInformationSystem.cs
+++ Explorer/Assets/DCL/SDKComponents/SceneUI/Systems/UICanvasInformation/UICanvasInformationSystem.cs
@@ -23,6 +23,8 @@ namespace DCL.SDKComponents.SceneUI.Systems.UICanvasInformation
         private BorderRect interactableArea;
         private int lastViewportResolutionWidth = -1;
         private int lastScreenRealResolutionWidth = -1;
+        private const int TOTAL_ATTEMPTS = 3;
+        private int currentAttempts = 0;
 
         public override void Initialize()
         {
@@ -36,6 +38,7 @@ namespace DCL.SDKComponents.SceneUI.Systems.UICanvasInformation
         {
             this.sceneStateProvider = sceneStateProvider;
             this.ecsToCRDTWriter = ecsToCRDTWriter;
+            currentAttempts = 0;
         }
 
         protected override void Update(float t)
@@ -53,12 +56,17 @@ namespace DCL.SDKComponents.SceneUI.Systems.UICanvasInformation
         private void UpdateUICanvasInformationComponent()
         {
             if (lastViewportResolutionWidth == Screen.width && lastScreenRealResolutionWidth == Screen.mainWindowDisplayInfo.width)
-                return;
+            {
+                //We add this logic because in the first frames the message might get lost and we wont send correctly the size of the screen
+                //to the scene, causing breaking UIs
+                if (currentAttempts > TOTAL_ATTEMPTS) { return; }
+                currentAttempts++;
+            }
 
             lastScreenRealResolutionWidth = Screen.mainWindowDisplayInfo.width;
             lastViewportResolutionWidth = Screen.width;
 
-            ecsToCRDTWriter.PutMessage<PBUiCanvasInformation, UICanvasInformationSystem>(static (component, system) =>
+            ecsToCRDTWriter.PutMessage<PBUiCanvasInformation, UICanvasInformationSystem>((component, system) =>
             {
                 component.InteractableArea = system.interactableArea;
                 component.Width = Screen.width;
