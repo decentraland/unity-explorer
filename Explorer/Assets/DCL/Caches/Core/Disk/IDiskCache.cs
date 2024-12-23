4dccc294cc2bdbef391e5cb38e1d577610b406c2
nickkhalow <nickhalow@gmail.com>
2024-12-23 16:07:45 +0200

    core cache

---
+++ Explorer/Assets/DCL/Caches/Core/Disk/IDiskCache.cs
@@ -0,0 +1,16 @@
+using Cysharp.Threading.Tasks;
+using System;
+using System.Threading;
+using Utility.Types;
+
+namespace DCL.Caches.Core.Disk
+{
+    public interface IDiskCache
+    {
+        UniTask<EnumResult<TaskError>> PutAsync(string key, string extension, ReadOnlyMemory<byte> data, CancellationToken token);
+
+        UniTask<EnumResult<byte[]?, TaskError>> ContentAsync(string key, string extension, CancellationToken token);
+
+        UniTask<EnumResult<TaskError>> RemoveAsync(string key, string extension, CancellationToken token);
+    }
+}
