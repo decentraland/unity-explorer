278bc57a567f64f72b7504e7cae3a3bc9a2617b0
lorenzo-ranciaffi <lorenzo.ranciaffi@decentraland.org>
2025-01-06 12:08:03 +0100

    added readonly serialized attribute drawer

--- Explorer/Assets/DCL/UI/GenericContextMenu/Controls/Configs/ContextMenuControlSettings.cs
+++ Explorer/Assets/DCL/UI/GenericContextMenu/Controls/Configs/ContextMenuControlSettings.cs
@@ -1,12 +1,13 @@
 using System;
 using UnityEngine;
+using Utility;
 
 namespace DCL.UI.GenericContextMenu.Controls.Configs
 {
     [Serializable]
     public class ContextMenuControlSettings : ScriptableObject
     {
-        [SerializeField] protected ContextMenuControlTypes controlTypeType;
+        [SerializeField, ReadOnly] protected ContextMenuControlTypes controlTypeType;
 
         public ContextMenuControlTypes ControlTypeType => controlTypeType;
     }
--- Explorer/Assets/Scripts/Utility/CustomPropertyDrawers.cs
+++ Explorer/Assets/Scripts/Utility/CustomPropertyDrawers.cs
@@ -29,4 +29,7 @@ namespace Utility
             FieldName = fieldName;
         }
     }
+
+    [AttributeUsage(AttributeTargets.Field)]
+    public class ReadOnlyAttribute : PropertyAttribute { }
 }
