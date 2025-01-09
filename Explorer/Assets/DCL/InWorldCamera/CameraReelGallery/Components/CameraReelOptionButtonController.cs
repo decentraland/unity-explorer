18c2b995ae08ff7e8e217fe4cd7cc6bd1a42d261
lorenzo-ranciaffi <lorenzo.ranciaffi@decentraland.org>
2025-01-09 15:01:00 +0100

    use asset references instead of indexes

--- Explorer/Assets/DCL/InWorldCamera/CameraReelGallery/Components/CameraReelOptionButtonController.cs
+++ Explorer/Assets/DCL/InWorldCamera/CameraReelGallery/Components/CameraReelOptionButtonController.cs
@@ -11,12 +11,6 @@ namespace DCL.InWorldCamera.CameraReelGallery.Components
 {
     public class CameraReelOptionButtonController : IDisposable
     {
-        private const int PUBLIC_CONTROL_INDEX = 0;
-        private const int SHARE_CONTROL_INDEX = 2;
-        private const int COPY_CONTROL_INDEX = 3;
-        private const int DOWNLOAD_CONTROL_INDEX = 4;
-        private const int DELETE_CONTROL_INDEX = 5;
-
         private readonly CameraReelOptionButtonView view;
         private readonly RectTransform buttonRectTransform;
         private readonly IMVCManager mvcManager;
@@ -49,11 +43,11 @@ namespace DCL.InWorldCamera.CameraReelGallery.Components
 
             controlsActions = new ()
             {
-                { contextMenuConfig.ContextMenuSettings[PUBLIC_CONTROL_INDEX], new Action<bool>(toggleValue => SetPublicRequested?.Invoke(currentReelData, toggleValue)) },
-                { contextMenuConfig.ContextMenuSettings[SHARE_CONTROL_INDEX], new Action(() => ShareToXRequested?.Invoke(currentReelData)) },
-                { contextMenuConfig.ContextMenuSettings[COPY_CONTROL_INDEX], new Action(() => CopyPictureLinkRequested?.Invoke(currentReelData)) },
-                { contextMenuConfig.ContextMenuSettings[DOWNLOAD_CONTROL_INDEX], new Action(() => DownloadRequested?.Invoke(currentReelData)) },
-                { contextMenuConfig.ContextMenuSettings[DELETE_CONTROL_INDEX], new Action(() => DeletePictureRequested?.Invoke(currentReelData)) },
+                { this.view.publicControl, new Action<bool>(toggleValue => SetPublicRequested?.Invoke(currentReelData, toggleValue)) },
+                { this.view.shareControl, new Action(() => ShareToXRequested?.Invoke(currentReelData)) },
+                { this.view.copyControl, new Action(() => CopyPictureLinkRequested?.Invoke(currentReelData)) },
+                { this.view.downloadControl, new Action(() => DownloadRequested?.Invoke(currentReelData)) },
+                { this.view.deleteControl, new Action(() => DeletePictureRequested?.Invoke(currentReelData)) },
             };
         }
 
@@ -65,7 +59,7 @@ namespace DCL.InWorldCamera.CameraReelGallery.Components
             view.transform.localPosition = offsetPosition;
             view.gameObject.SetActive(true);
             currentReelData = cameraReelResponse;
-            initialValues[contextMenuConfig.ContextMenuSettings[PUBLIC_CONTROL_INDEX]] = cameraReelResponse.isPublic;
+            initialValues[view.publicControl] = cameraReelResponse.isPublic;
             closeContextMenuTask = new UniTaskCompletionSource();
         }
 
--- Explorer/Assets/DCL/InWorldCamera/CameraReelGallery/Components/CameraReelOptionButtonView.cs
+++ Explorer/Assets/DCL/InWorldCamera/CameraReelGallery/Components/CameraReelOptionButtonView.cs
@@ -1,3 +1,4 @@
+using DCL.UI.GenericContextMenu.Controls.Configs;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,5 +7,12 @@ namespace DCL.InWorldCamera.CameraReelGallery.Components
     public class CameraReelOptionButtonView : MonoBehaviour
     {
         [field: SerializeField] internal Button optionButton { get; private set; }
+
+        [field: Header("Context menu settings")]
+        [field: SerializeField] internal ContextMenuControlSettings publicControl { get; private set; }
+        [field: SerializeField] internal ContextMenuControlSettings shareControl { get; private set; }
+        [field: SerializeField] internal ContextMenuControlSettings copyControl { get; private set; }
+        [field: SerializeField] internal ContextMenuControlSettings downloadControl { get; private set; }
+        [field: SerializeField] internal ContextMenuControlSettings deleteControl { get; private set; }
     }
 }
