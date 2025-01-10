29c7c7ff5e07d79f31d7fdba878ea170f2dd21bd
Nick Khalow <71646502+NickKhalow@users.noreply.github.com>
2025-01-10 14:29:14 +0400

    Feat: full url retrieval (#3026)
    * initial realm format
    
    * DynamicWorldParams move init
    
    * Bootstrap integrate IRealmUrls
    
    * remove redundant methods
    
    * BootstrapContainer integrate IRealmUrls
    
    * RealmUrls and RealmNamesMap implementation
    
    * remove redundant comment
    
    * fix casting
    
    * post merge fix
    
    ---------
    
    Co-authored-by: Ashley Canning <ashley.canning@decentraland.org>

--- Explorer/Assets/Scripts/Global/Dynamic/IBootstrap.cs
+++ Explorer/Assets/Scripts/Global/Dynamic/IBootstrap.cs
@@ -21,8 +21,15 @@ namespace Global.Dynamic
     {
         UniTask PreInitializeSetupAsync(UIDocument cursorRoot, UIDocument debugUiRoot, CancellationToken ct);
 
-        UniTask<(StaticContainer?, bool)> LoadStaticContainerAsync(BootstrapContainer bootstrapContainer, PluginSettingsContainer globalPluginSettingsContainer, DebugViewsCatalog debugViewsCatalog, Entity playerEntity, ITexturesFuse texturesFuse,
-            bool isTextureCompressionEnabled, ISystemMemoryCap memoryCap, CancellationToken ct);
+        UniTask<(StaticContainer?, bool)> LoadStaticContainerAsync(
+            BootstrapContainer bootstrapContainer,
+            PluginSettingsContainer globalPluginSettingsContainer,
+            IDebugContainerBuilder debugContainerBuilder,
+            Entity playerEntity,
+            ITexturesFuse texturesFuse,
+            ISystemMemoryCap memoryCap,
+            CancellationToken ct
+        );
 
         UniTask<(DynamicWorldContainer?, bool)> LoadDynamicWorldContainerAsync(BootstrapContainer bootstrapContainer, StaticContainer staticContainer, PluginSettingsContainer scenePluginSettingsContainer, DynamicSceneLoaderSettings settings, DynamicSettings dynamicSettings,
             UIDocument uiToolkitRoot, UIDocument cursorRoot, AudioClipConfig backgroundMusic,
