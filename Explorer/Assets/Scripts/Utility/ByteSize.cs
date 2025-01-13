012658d99b7f79be827119a040264a2584b974e5
Nick Khalow <nickhalow@gmail.com>
2025-01-13 12:45:31 +0200

    bytesize utility

---
+++ Explorer/Assets/Scripts/Utility/ByteSize.cs
@@ -0,0 +1,19 @@
+﻿namespace Utility
+{
+    public static class ByteSize
+    {
+        private const ulong KB = 1024;
+        private const ulong MB = KB * 1024;
+        private const ulong GB = MB * 1024;
+        private const ulong TB = GB * 1024;
+
+        public static string ToReadableString(ulong bytes)
+        {
+            if (bytes < KB) return $"{bytes} B";
+            if (bytes < MB) return $"{(double)bytes / KB:0.##} KB";
+            if (bytes < GB) return $"{(double)bytes / MB:0.##} MB";
+            if (bytes < TB) return $"{(double)bytes / GB:0.##} GB";
+            return $"{(double)bytes / TB:0.##} TB";
+        }
+    }
+}
