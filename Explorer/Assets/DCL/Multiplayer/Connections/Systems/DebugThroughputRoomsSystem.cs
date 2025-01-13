66ad67c4af6a6fbd2264215396bf88eb7d345875
nickkhalow <nickhalow@gmail.com>
2025-01-13 14:08:18 +0200

    migrate to markers

--- Explorer/Assets/DCL/Multiplayer/Connections/Systems/DebugThroughputRoomsSystem.cs
+++ Explorer/Assets/DCL/Multiplayer/Connections/Systems/DebugThroughputRoomsSystem.cs
@@ -17,6 +17,7 @@ namespace DCL.Multiplayer.Connections.Systems
     {
         private readonly BufferBinding island;
         private readonly BufferBinding scene;
+        private readonly bool buffersInitialized;
         private readonly float stepSeconds;
         private float current;
 
@@ -32,14 +33,21 @@ namespace DCL.Multiplayer.Connections.Systems
             DebugWidgetBuilder? infoWidget = debugBuilder.TryAddWidget(IDebugContainerBuilder.Categories.ROOM_THROUGHPUT);
 
             if (infoWidget == null)
+            {
+                buffersInitialized = false;
                 return;
+            }
 
             this.island = BufferBinding.CreateAndAttach(infoWidget, islandBufferBunch, "island");
             this.scene = BufferBinding.CreateAndAttach(infoWidget, sceneBufferBunch, "scene");
+            buffersInitialized = true;
         }
 
         protected override void Update(float t)
         {
+            if (buffersInitialized == false)
+                return;
+
             current += t;
 
             if (current >= stepSeconds)
@@ -53,10 +61,10 @@ namespace DCL.Multiplayer.Connections.Systems
         private readonly struct BufferBinding
         {
             private readonly ThroughputBufferBunch bufferBunch;
-            private readonly ElementBinding<string> incoming;
-            private readonly ElementBinding<string> outgoing;
+            private readonly ElementBinding<ulong> incoming;
+            private readonly ElementBinding<ulong> outgoing;
 
-            private BufferBinding(ThroughputBufferBunch bufferBunch, ElementBinding<string> incoming, ElementBinding<string> outgoing)
+            private BufferBinding(ThroughputBufferBunch bufferBunch, ElementBinding<ulong> incoming, ElementBinding<ulong> outgoing)
             {
                 this.bufferBunch = bufferBunch;
                 this.incoming = incoming;
@@ -65,10 +73,10 @@ namespace DCL.Multiplayer.Connections.Systems
 
             public static BufferBinding CreateAndAttach(DebugWidgetBuilder widgetBuilder, ThroughputBufferBunch bunch, string name)
             {
-                var incoming = new ElementBinding<string>(string.Empty);
-                var outgoing = new ElementBinding<string>(string.Empty);
-                widgetBuilder.AddControl(new DebugConstLabelDef($"{name} - incoming"), new DebugTextFieldDef(incoming));
-                widgetBuilder.AddControl(new DebugConstLabelDef($"{name} - outgoing"), new DebugTextFieldDef(outgoing));
+                var incoming = new ElementBinding<ulong>(0);
+                var outgoing = new ElementBinding<ulong>(0);
+                widgetBuilder.AddMarker($"{name} - incoming", incoming, DebugLongMarkerDef.Unit.Bytes);
+                widgetBuilder.AddMarker($"{name} - outgoing", outgoing, DebugLongMarkerDef.Unit.Bytes);
                 return new BufferBinding(bunch, incoming, outgoing);
             }
 
@@ -78,11 +86,11 @@ namespace DCL.Multiplayer.Connections.Systems
                 CollectAndDraw(bufferBunch.Outgoing, outgoing);
             }
 
-            private static void CollectAndDraw(IThroughputBuffer buffer, ElementBinding<string> binding)
+            private static void CollectAndDraw(IThroughputBuffer buffer, ElementBinding<ulong> binding)
             {
                 ulong incomingBytes = buffer.CurrentAmount();
                 buffer.Clear();
-                binding.Value = Utility.ByteSize.ToReadableString(incomingBytes);
+                binding.Value = incomingBytes;
             }
         }
     }
