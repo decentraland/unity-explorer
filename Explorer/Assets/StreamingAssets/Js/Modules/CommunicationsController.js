e5c8d9a08efb54b5eafc2ceb2192aa121a396af7
Gonzalo DCL <gonzalo@decentraland.org>
2025-01-14 10:10:05 -0300

    fix sendBinary peerData message

--- Explorer/Assets/StreamingAssets/Js/Modules/CommunicationsController.js
+++ Explorer/Assets/StreamingAssets/Js/Modules/CommunicationsController.js
@@ -1,12 +1,12 @@
 module.exports.sendBinary = async function(message) {
-    const resultData = UnityCommunicationsControllerApi.SendBinary(message.data, message.messagesToAddress)
+    const resultData = UnityCommunicationsControllerApi.SendBinary(message.data, message.peerData)
     return {
         data: resultData
     };
 }
 
 // Needed for scenes own MessageBus through 'comms' observable
-module.exports.send = async function(message) { 
+module.exports.send = async function(message) {
     UnitySDKMessageBusCommsControllerApi.Send(message.message)
     return {};
 }
