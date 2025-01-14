b3c5c3f90836e7b9727665954deaa2e5e3785592
Nicolas Lorusso <56365551+lorux0@users.noreply.github.com>
2025-01-14 12:19:49 -0300

    feat: friends rpc integration (#3076)

--- Explorer/Assets/DCL/Friends/IFriendsService.cs
+++ Explorer/Assets/DCL/Friends/IFriendsService.cs
@@ -1,42 +1,70 @@
 using Cysharp.Threading.Tasks;
 using DCL.Profiles;
+using System;
 using System.Collections.Generic;
 using System.Threading;
+using UnityEngine.Pool;
 
 namespace DCL.Friends
 {
-    public interface IFriendsService
+    public interface IFriendsService : IDisposable
     {
         UniTask<PaginatedFriendsResult> GetFriendsAsync(int pageNum, int pageSize, CancellationToken ct);
 
-        UniTask<bool> IsFriendAsync(string friendId, CancellationToken ct);
+        UniTask<FriendshipStatus> GetFriendshipStatusAsync(string userId, CancellationToken ct);
 
         UniTask<PaginatedFriendRequestsResult> GetReceivedFriendRequestsAsync(int pageNum, int pageSize, CancellationToken ct);
 
         UniTask<PaginatedFriendRequestsResult> GetSentFriendRequestsAsync(int pageNum, int pageSize, CancellationToken ct);
 
-        UniTask RejectFriendshipAsync(string friendId, CancellationToken cancellationToken = default);
+        UniTask RejectFriendshipAsync(string friendId, CancellationToken ct);
 
-        UniTask CancelFriendshipAsync(string friendId, CancellationToken cancellationToken = default);
+        UniTask CancelFriendshipAsync(string friendId, CancellationToken ct);
 
-        UniTask AcceptFriendshipAsync(string friendId, CancellationToken cancellationToken = default);
+        UniTask AcceptFriendshipAsync(string friendId, CancellationToken ct);
 
-        UniTask DeleteFriendshipAsync(string friendId, CancellationToken cancellationToken = default);
+        UniTask DeleteFriendshipAsync(string friendId, CancellationToken ct);
 
-        UniTask<FriendRequest> AddFriendshipAsync(string friendId, string messageBody, CancellationToken cancellationToken = default);
-
-        UniTask RemoveFriendAsync(string friendId, CancellationToken ct);
+        UniTask<FriendRequest> RequestFriendshipAsync(string friendId, string messageBody, CancellationToken ct);
     }
 
-    public struct PaginatedFriendsResult
+    public readonly struct PaginatedFriendsResult : IDisposable
     {
-        public IReadOnlyList<Profile> Friends;
-        public int TotalAmount;
+        private readonly List<Profile> friends;
+
+        public IReadOnlyList<Profile> Friends => friends;
+        public int TotalAmount { get; }
+
+        public PaginatedFriendsResult(IEnumerable<Profile> profiles, int totalAmount)
+        {
+            friends = ListPool<Profile>.Get();
+            friends.AddRange(profiles);
+            TotalAmount = totalAmount;
+        }
+
+        public void Dispose()
+        {
+            ListPool<Profile>.Release(friends);
+        }
     }
 
-    public struct PaginatedFriendRequestsResult
+    public readonly struct PaginatedFriendRequestsResult : IDisposable
     {
-        public IReadOnlyList<FriendRequest> Requests;
-        public int TotalAmount;
+        private readonly List<FriendRequest> requests;
+
+        public IReadOnlyList<FriendRequest> Requests => requests;
+        public int TotalAmount { get; }
+
+        public PaginatedFriendRequestsResult(IEnumerable<FriendRequest> requests, int totalAmount)
+        {
+            this.requests = ListPool<FriendRequest>.Get();
+            this.requests.AddRange(requests);
+            TotalAmount = totalAmount;
+        }
+
+        public void Dispose()
+        {
+            ListPool<FriendRequest>.Release(requests);
+        }
     }
 }
