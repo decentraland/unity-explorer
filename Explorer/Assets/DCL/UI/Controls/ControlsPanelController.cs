c51dc4baabe991ad27f925456e8bf74966be08f0
Miha Krajnc <miha.krajnc@decentraland.org>
2025-01-20 13:23:17 +0100

    chore: change closing logic for controls panel

--- Explorer/Assets/DCL/UI/Controls/ControlsPanelController.cs
+++ Explorer/Assets/DCL/UI/Controls/ControlsPanelController.cs
@@ -11,6 +11,8 @@ namespace DCL.UI.Controls
         private readonly DCLInput input;
         public override CanvasOrdering.SortingLayer Layer => CanvasOrdering.SortingLayer.Fullscreen;
 
+        private bool closePanel;
+
         public ControlsPanelController(ViewFactoryMethod viewFactory, IMVCManager mvcManager, DCLInput input) : base(viewFactory)
         {
             this.mvcManager = mvcManager;
@@ -21,14 +23,16 @@ namespace DCL.UI.Controls
 
         private void OnShortcutPressed(InputAction.CallbackContext ctx)
         {
-            mvcManager.ShowAsync(IssueCommand()).Forget();
+            if (State == ControllerState.ViewFocused)
+                closePanel = true;
+            else
+                mvcManager.ShowAsync(IssueCommand()).Forget();
         }
 
         protected override UniTask WaitForCloseIntentAsync(CancellationToken ct) =>
             UniTask.WhenAny(
                 viewInstance!.closeButton.OnClickAsync(ct),
-                UniTask.WaitUntil(() => input.UI.Close.WasPerformedThisFrame(), cancellationToken: ct),
-                UniTask.NextFrame(ct).ContinueWith(() => UniTask.WaitUntil(() => input.Shortcuts.Controls.WasPerformedThisFrame(), cancellationToken: ct))
+                UniTask.WaitUntil(() => input.UI.Close.WasPerformedThisFrame() || closePanel, cancellationToken: ct)
             );
 
         public override void Dispose()
