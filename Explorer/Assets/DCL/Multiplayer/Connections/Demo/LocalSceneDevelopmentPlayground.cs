b77ef83cdb12d6b427d52b14047f09c1577c4e70
NickKhalow <nickhalow@gmail.com>
2025-02-07 05:39:08 +0200

    const metadata for room

--- Explorer/Assets/DCL/Multiplayer/Connections/Demo/LocalSceneDevelopmentPlayground.cs
+++ Explorer/Assets/DCL/Multiplayer/Connections/Demo/LocalSceneDevelopmentPlayground.cs
@@ -2,42 +2,27 @@ using Arch.Core;
 using Cysharp.Threading.Tasks;
 using DCL.Browser.DecentralandUrls;
 using DCL.Character.Components;
-using DCL.DemoWorlds;
-using DCL.GlobalPartitioning;
-using DCL.Ipfs;
 using DCL.Multiplayer.Connections.DecentralandUrls;
 using DCL.Multiplayer.Connections.FfiClients;
 using DCL.Multiplayer.Connections.GateKeeper.Meta;
 using DCL.Multiplayer.Connections.GateKeeper.Rooms;
-using DCL.Optimization.PerformanceBudgeting;
 using DCL.Web3.Accounts.Factory;
 using DCL.Web3.Identities;
 using DCL.WebRequests;
 using DCL.WebRequests.Analytics;
 using DCL.WebRequests.RequestsHub;
-using ECS;
 using ECS.SceneLifeCycle;
-using ECS.SceneLifeCycle.SceneDefinition;
-using ECS.StreamableLoading.Cache;
 using LiveKit.Internal.FFIClients;
 using Plugins.TexturesFuse.TexturesServerWrap.Unzips;
 using UnityEngine;
-using Utility;
 
 namespace DCL.Multiplayer.Connections.Demo
 {
     public class LocalSceneDevelopmentPlayground : MonoBehaviour
     {
-        private IDemoWorld demoWorld = null!;
-
         private void Start()
         {
             LaunchAsync().Forget();
-        }
-
-        private void Update()
-        {
-            demoWorld.Update();
         }
 
         private async UniTaskVoid LaunchAsync()
@@ -50,26 +35,15 @@ namespace DCL.Multiplayer.Connections.Demo
             var urlsSource = new DecentralandUrlsSource(DecentralandEnvironment.Org);
 
             IWeb3IdentityCache? identityCache = await ArchipelagoFakeIdentityCache.NewAsync(urlsSource, new Web3AccountFactory());
-            var character = new ExposedTransform();
             var webRequests = new LogWebRequestController(new WebRequestController(new WebRequestsAnalyticsContainer(), identityCache, new RequestHub(ITexturesFuse.NewDefault(), false)));
 
             new GateKeeperSceneRoom(
                     webRequests,
-                    new SceneRoomLogMetaDataSource(new SceneRoomMetaDataSource(new IRealmData.Fake(), character, world, false)),
+                    new SceneRoomLogMetaDataSource(new ConstSceneRoomMetaDataSource("random-name")),
                     urlsSource,
                     new ScenesCache()
-                   // ,
-                   //  DecentralandUrl.LocalGateKeeperSceneAdapter
                 ).StartAsync()
                  .Forget();
-
-            demoWorld = new DemoWorld(
-                world,
-                w => { },
-                w => new LoadSceneDefinitionListSystem(w, webRequests, new NoCache<SceneDefinitions, GetSceneDefinitionList>(false, false)),
-                w => new GlobalDeferredLoadingSystem(w, new NullPerformanceBudget(), new NullPerformanceBudget(), new SceneAssetLock())
-            );
-            demoWorld.SetUp();
         }
     }
 }
---
+++ Explorer/Assets/DCL/Multiplayer/Connections/GateKeeper/Meta/ConstSceneRoomMetaDataSource.cs
@@ -0,0 +1,33 @@
+using Cysharp.Threading.Tasks;
+using DCL.Diagnostics;
+using System.Threading;
+using UnityEngine;
+using Utility.Types;
+
+namespace DCL.Multiplayer.Connections.GateKeeper.Meta
+{
+    public class ConstSceneRoomMetaDataSource : ISceneRoomMetaDataSource
+    {
+        private readonly MetaData.Input metadataInput;
+        private readonly MetaData metaData;
+
+        public ConstSceneRoomMetaDataSource(string name)
+        {
+            metadataInput = new MetaData.Input(name, Vector2Int.zero);
+            metaData = new MetaData(name, metadataInput);
+        }
+
+        public bool ScenesCommunicationIsIsolated => true;
+
+        public bool MetadataIsDirty => false;
+
+        public MetaData.Input GetMetadataInput() =>
+            metadataInput;
+
+        public UniTask<Result<MetaData>> MetaDataAsync(MetaData.Input input, CancellationToken token)
+        {
+            ReportHub.LogWarning(ReportCategory.LIVEKIT, "I'm static, I won't consider the input param");
+            return UniTask.FromResult(Result<MetaData>.SuccessResult(metaData));
+        }
+    }
+}
