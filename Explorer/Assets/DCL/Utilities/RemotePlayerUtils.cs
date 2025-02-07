24c10ecbe9bbfc9e915ae2cc12882f7f3dc574e6
Santi Andrade <sandrade@decentraland.org>
2025-02-07 12:44:59 +0100

    Apply feedback: Create GetBlendValueFromSpeed (II)

--- Explorer/Assets/DCL/Utilities/RemotePlayerUtils.cs
+++ Explorer/Assets/DCL/Utilities/RemotePlayerUtils.cs
@@ -5,13 +5,13 @@ namespace DCL.Utilities
         public const float JUMP_EPSILON = 0.01f;
         public const float MOVEMENT_EPSILON = 0.01f;
 
-        // Found empirically and diverges a bit from the character settings (where speeds are RUN = 10, JOG = 8, WALK = 1.5)
-        public const float RUN_SPEED_THRESHOLD = 9.5f;
-        public const float JOG_SPEED_THRESHOLD = 4f;
-
         // Amount of positions with timestamp that older than timestamp of the last passed message, that will be skip in one frame.
         public const int BEHIND_EXTRAPOLATION_BATCH = 10;
         public const float ZERO_VELOCITY_SQR_THRESHOLD = 0.01f * 0.01f;
+
+        // Found empirically and diverges a bit from the character settings (where speeds are RUN = 10, JOG = 8, WALK = 1.5)
+        private const float RUN_SPEED_THRESHOLD = 9.5f;
+        private const float JOG_SPEED_THRESHOLD = 4f;
 
         public static float GetBlendValueFromSpeed(float speed) =>
             speed > RUN_SPEED_THRESHOLD ? 3 : speed > JOG_SPEED_THRESHOLD ? 2 : 1;
