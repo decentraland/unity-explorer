42ec6b9d37ff2ae3ccbd10ee3e5582184c5f5410
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2025-02-10 17:09:48 -0300

    Rollback
    Signed-off-by: Juan Ignacio Molteni <juanignaciomolteni@gmail.com>

--- Explorer/Assets/DCL/AvatarRendering/AvatarShape/ComputeShader/ComputeShaderSkinning.cs
+++ Explorer/Assets/DCL/AvatarRendering/AvatarShape/ComputeShader/ComputeShaderSkinning.cs
@@ -55,7 +55,12 @@ namespace DCL.AvatarRendering.AvatarShape.ComputeShader
         {
             //Note (Juani): Using too many BeginWrite in Mac caused a crash. So I ve set up this switch that changes the way in which we
             //set up the buffers depending on the platform
+
+#if UNITY_STANDALONE_WIN
+            return new ComputeSkinningBufferContainerWrite(vertCount, skinnedMeshRendererBoneCount);
+#else
             return new ComputeSkinningBufferContainerSetData(vertCount, skinnedMeshRendererBoneCount);
+#endif
         }
 
         private AvatarCustomSkinningComponent.Buffers SetupComputeShader(IReadOnlyList<MeshData> meshesData, UnityEngine.ComputeShader skinningShader, int vertCount, int skinnedMeshRendererBoneCount)
