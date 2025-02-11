a1535024d99224ba2917c93a05bdfbc9a4b72a06
NickKhalow <nickhalow@gmail.com>
2025-02-11 15:43:26 +0200

    PMutex (Platform Mutex)

--- Explorer/Assets/DCL/Web3/Identities/MemoryMappedFilePlayerPrefsIdentityProviderKeyStrategy.cs
+++ Explorer/Assets/DCL/Web3/Identities/MemoryMappedFilePlayerPrefsIdentityProviderKeyStrategy.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO.MemoryMappedFiles;
-using System.Threading;
+using System.Runtime.InteropServices;
 using UnityEngine.Pool;
 
 namespace DCL.Web3.Identities
@@ -14,12 +14,12 @@ namespace DCL.Web3.Identities
     public class MemoryMappedFilePlayerPrefsIdentityProviderKeyStrategy : IPlayerPrefsIdentityProviderKeyStrategy
     {
         private const string MMF_NAME = "Local\\DCL_AppInstancesTracking";
-        private const string MUTEX_NAME = "DCL_AppInstancesTrackingMutex";
+        private const string MUTEX_NAME = "Local\\DCL_AppInstancesTrackingMutex";
         private const int MAX_INSTANCES_COUNT = 64;
-        private const int MUTEX_TIMEOUT_MILLISECONDS = 5000;
+        private const uint MUTEX_TIMEOUT_MILLISECONDS = 5000;
 
         private readonly MemoryMappedFile memoryMappedFile;
-        private readonly Mutex mutex;
+        private readonly PMutex mutex;
         private readonly int selfPID;
         private readonly string selfProcessName;
 
@@ -28,7 +28,7 @@ namespace DCL.Web3.Identities
         public MemoryMappedFilePlayerPrefsIdentityProviderKeyStrategy()
         {
             memoryMappedFile = MemoryMappedFile.CreateOrOpen(MMF_NAME, MemoryMappedFileSize(), MemoryMappedFileAccess.ReadWrite);
-            mutex = new Mutex(false, MUTEX_NAME);
+            mutex = new PMutex(MUTEX_NAME);
             var self = Process.GetCurrentProcess();
             selfProcessName = self.ProcessName;
             selfPID = Process.GetCurrentProcess().Id;
@@ -209,6 +209,66 @@ namespace DCL.Web3.Identities
                     unsafe { return sizeof(Entry); }
                 }
             }
+        }
+    }
+
+    /// <summary>
+    /// Created due named mutex is not supported by IL2CPP
+    /// </summary>
+    internal class PMutex : IDisposable
+    {
+        private const int ERROR_ALREADY_EXISTS = 183;
+        /// <summary>
+        /// From https://learn.microsoft.com/en-us/windows/win32/api/synchapi/nf-synchapi-waitforsingleobject
+        /// </summary>
+        private const uint WAIT_OBJECT_0 = (uint)0x00000000L;
+
+        private readonly string name;
+        private readonly IntPtr pMutex;
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern IntPtr CreateMutex(IntPtr lpMutexAttributes, bool bInitialOwner, string lpName);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern uint WaitForSingleObject(IntPtr hHandle, uint dwMillisecond);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool ReleaseMutex(IntPtr hMutex);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool CloseHandle(IntPtr hObject);
+
+        public PMutex(string name)
+        {
+            pMutex = CreateMutex(IntPtr.Zero, false, name);
+            int lastError = Marshal.GetLastWin32Error();
+
+            if (pMutex == IntPtr.Zero)
+                throw new Exception("Failed to create mutex.");
+
+            if (lastError == ERROR_ALREADY_EXISTS)
+                throw new Exception("Mutex already exists, another process is using it.");
+        }
+
+        public void WaitOne(uint milliseconds)
+        {
+            uint result = WaitForSingleObject(pMutex, milliseconds);
+
+            if (result != WAIT_OBJECT_0)
+                throw new Exception("Cannot acquire mutex");
+        }
+
+        public void ReleaseMutex()
+        {
+            bool result = ReleaseMutex(pMutex);
+
+            if (result == false)
+                throw new Exception("Cannot release mutex");
+        }
+
+        public void Dispose()
+        {
+            CloseHandle(pMutex);
         }
     }
 }
