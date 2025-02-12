4b9d96ec9763ccf40f4a8decca0d57a6b97299a8
Ashley Canning <ashley.canning@gmail.com>
2025-02-12 16:27:38 +0000

    Print system information on startup.

--- Explorer/Assets/Scripts/Global/Dynamic/MainSceneLoader.cs
+++ Explorer/Assets/Scripts/Global/Dynamic/MainSceneLoader.cs
@@ -78,6 +78,8 @@ namespace Global.Dynamic
 
         private void Awake()
         {
+            SystemInfoUtils.Log();
+
             InitializeFlowAsync(destroyCancellationToken).Forget();
         }
 
@@ -317,7 +319,7 @@ namespace Global.Dynamic
 
         private static IDiskCache NewInstanceDiskCache(IAppArgs appArgs)
         {
-            if (appArgs.HasFlag(AppArgsFlags.DISABLE_DISK_CACHE))
+            //if (appArgs.HasFlag(AppArgsFlags.DISABLE_DISK_CACHE))
             {
                 ReportHub.Log(ReportData.UNSPECIFIED, $"Disable disk cache, flag --{AppArgsFlags.DISABLE_DISK_CACHE} is passed");
                 return new IDiskCache.Fake();
