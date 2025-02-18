fafb1a32e39eeb440447fecb2b74a2de696026bb
Vitaly Popuzin <popuzin@gmail.com>
2025-02-18 12:37:53 +0100

    extracated helper class

--- Explorer/Assets/DCL/Rendering/GPUInstancing/InstancingData/GPUInstancingPrefabData.cs
+++ Explorer/Assets/DCL/Rendering/GPUInstancing/InstancingData/GPUInstancingPrefabData.cs
@@ -1,42 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
 namespace DCL.Roads.GPUInstancing.Playground
 {
-    [Serializable]
-    public class GPUInstancingLODGroupWithBuffer : IEquatable<GPUInstancingLODGroupWithBuffer>
-    {
-        public string Name;
-        public GPUInstancingLODGroup LODGroup;
-        public List<PerInstanceBuffer> InstancesBuffer;
-
-        public GPUInstancingLODGroupWithBuffer(GPUInstancingLODGroup lodGroup, List<PerInstanceBuffer> instances)
-        {
-            Name = lodGroup.Name;
-            LODGroup = lodGroup;
-            InstancesBuffer = instances;
-        }
-
-        public bool Equals(GPUInstancingLODGroupWithBuffer other)
-        {
-            if (other is null) return false;
-            if (ReferenceEquals(this, other)) return true;
-            return Name == other.Name && Equals(LODGroup, other.LODGroup);
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (obj is null) return false;
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj.GetType() != GetType()) return false;
-            return Equals((GPUInstancingLODGroupWithBuffer) obj);
-        }
-
-        public override int GetHashCode() =>
-            HashCode.Combine(Name, LODGroup);
-    }
-
     public class GPUInstancingPrefabData : MonoBehaviour
     {
         public List<GPUInstancingLODGroupWithBuffer> IndirectCandidates;
