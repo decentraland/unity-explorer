1f0988ba439326c854743c7877bb7734f3fbb64f
Vitaly Popuzin <popuzin@gmail.com>
2025-02-19 21:22:13 +0100

    created runtime collection of data for roads

--- Explorer/Assets/DCL/Roads/Settings/RoadSettingsAsset.cs
+++ Explorer/Assets/DCL/Roads/Settings/RoadSettingsAsset.cs
@@ -24,6 +24,47 @@ namespace DCL.Roads.Settings
         IReadOnlyList<RoadDescription> IRoadSettingsAsset.RoadDescriptions => RoadDescriptions;
         IReadOnlyList<AssetReferenceGameObject> IRoadSettingsAsset.RoadAssetsReference => RoadAssetsReference;
 
+        public void CollectGPUInstancingLODGroupsRuntime(Dictionary<string, GPUInstancingPrefabData> loadedPrefabs)
+        {
+            var tempIndirectCandidates = new Dictionary<GPUInstancingLODGroupWithBuffer, HashSet<PerInstanceBuffer>>();
+
+            foreach (RoadDescription roadDescription in RoadDescriptions)
+            {
+                if (!loadedPrefabs.TryGetValue(roadDescription.RoadModel, out GPUInstancingPrefabData prefab))
+                {
+                    Debug.LogWarning($"Can't find prefab {roadDescription.RoadModel}");
+                    continue;
+                }
+
+                var roadRoot = Matrix4x4.TRS(
+                    roadDescription.RoadCoordinate.ParcelToPositionFlat() + ParcelMathHelper.RoadPivotDeviation,
+                    roadDescription.Rotation.SelfOrIdentity(),
+                    Vector3.one);
+
+                ProcessCandidates(prefab.IndirectCandidates, roadRoot, tempIndirectCandidates);
+            }
+
+            IndirectLODGroups = tempIndirectCandidates
+                               .Select(kvp => new GPUInstancingLODGroupWithBuffer(kvp.Key.LODGroup, kvp.Value.ToList()))
+                               .OrderBy(group => group.LODGroup.Name)
+                               .ToList();
+        }
+
+        private void ProcessCandidates(List<GPUInstancingLODGroupWithBuffer> sourceCandidates, Matrix4x4 roadRoot, Dictionary<GPUInstancingLODGroupWithBuffer, HashSet<PerInstanceBuffer>> targetDict)
+        {
+            foreach (GPUInstancingLODGroupWithBuffer candidate in sourceCandidates)
+            {
+                if (!targetDict.TryGetValue(candidate, out HashSet<PerInstanceBuffer> matrices))
+                {
+                    matrices = new HashSet<PerInstanceBuffer>();
+                    targetDict.Add(candidate, matrices);
+                }
+
+                foreach (PerInstanceBuffer instanceData in candidate.InstancesBuffer)
+                    matrices.Add(new PerInstanceBuffer { instMatrix = roadRoot * instanceData.instMatrix });
+            }
+        }
+
 #if UNITY_EDITOR
         public void HideAll()
         {
@@ -105,21 +146,6 @@ namespace DCL.Roads.Settings
             }
 
             return loadedPrefabs;
-        }
-
-        private void ProcessCandidates(List<GPUInstancingLODGroupWithBuffer> sourceCandidates, Matrix4x4 roadRoot, Dictionary<GPUInstancingLODGroupWithBuffer, HashSet<PerInstanceBuffer>> targetDict)
-        {
-            foreach (GPUInstancingLODGroupWithBuffer candidate in sourceCandidates)
-            {
-                if (!targetDict.TryGetValue(candidate, out HashSet<PerInstanceBuffer> matrices))
-                {
-                    matrices = new HashSet<PerInstanceBuffer>();
-                    targetDict.Add(candidate, matrices);
-                }
-
-                foreach (PerInstanceBuffer instanceData in candidate.InstancesBuffer)
-                    matrices.Add(new PerInstanceBuffer { instMatrix = roadRoot * instanceData.instMatrix });
-            }
         }
 #endif
     }
