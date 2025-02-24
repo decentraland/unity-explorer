207bbb22e8a40c69869caa92451f9032ee4eb4c1
NickKhalow <nickhalow@gmail.com>
2025-02-24 11:25:46 +0200

    log schedule operations

--- Explorer/Assets/Plugins/RustSegment/SegmentServerWrap/RustSegmentAnalyticsService.cs
+++ Explorer/Assets/Plugins/RustSegment/SegmentServerWrap/RustSegmentAnalyticsService.cs
@@ -33,6 +33,9 @@ namespace Plugins.RustSegment.SegmentServerWrap
 
         private static volatile RustSegmentAnalyticsService? current;
 
+        private long trackId;
+        private long flushId;
+
         public RustSegmentAnalyticsService(string writerKey)
         {
             if (string.IsNullOrWhiteSpace(writerKey))
@@ -111,6 +114,9 @@ namespace Plugins.RustSegment.SegmentServerWrap
                 list.Add(mContext);
 
                 afterClean.Add(operationId, (Operation.Track, list));
+
+                trackId++;
+                ReportHub.Log(ReportCategory.ANALYTICS, $"{nameof(RustSegmentAnalyticsService)} Track scheduled operationId: {operationId} trackId: {trackId}");
             }
         }
 
@@ -126,6 +132,9 @@ namespace Plugins.RustSegment.SegmentServerWrap
                 ulong operationId = NativeMethods.SegmentServerFlush();
                 AlertIfInvalid(operationId);
                 afterClean.Add(operationId, (Operation.Flush, ListPool<MarshaledString>.Get()!));
+
+                flushId++;
+                ReportHub.Log(ReportCategory.ANALYTICS, $"{nameof(RustSegmentAnalyticsService)} Flush scheduled operationId: {operationId} flushId: {flushId}");
             }
         }
 
