8e8d3eecebff0e4d0a7dbf787c559540a2df792b
Ansis Māliņš <ansis.malins@decentraland.org>
2025-02-28 17:52:07 +0100

    Implement IV8HostObject for JSTaskResolverResetable (#3452)

--- Explorer/Assets/Scripts/SceneRuntime/Apis/JSTaskResolverResetable.cs
+++ Explorer/Assets/Scripts/SceneRuntime/Apis/JSTaskResolverResetable.cs
@@ -1,12 +1,22 @@
 ﻿using Cysharp.Threading.Tasks;
-using JetBrains.Annotations;
 using Microsoft.ClearScript;
+using Microsoft.ClearScript.V8.SplitProxy;
+using System;
 
 namespace SceneRuntime.Apis
 {
-    public class JSTaskResolverResetable
+    public class JSTaskResolverResetable : IV8HostObject
     {
         private AutoResetUniTaskCompletionSource source;
+
+        private readonly InvokeHostObject completed;
+        private readonly InvokeHostObject reject;
+
+        public JSTaskResolverResetable()
+        {
+            completed = Completed;
+            reject = Reject;
+        }
 
         public UniTask Task => source.Task;
 
@@ -15,16 +25,38 @@ namespace SceneRuntime.Apis
             source = AutoResetUniTaskCompletionSource.Create();
         }
 
-        [UsedImplicitly]
-        public void Completed()
+        private void Completed(ReadOnlySpan<V8Value.Decoded> args, V8Value result)
+        {
+            Completed();
+        }
+
+        private void Completed()
         {
             source.TrySetResult();
         }
 
-        [UsedImplicitly]
-        public void Reject(string message)
+        private void Reject(ReadOnlySpan<V8Value.Decoded> args, V8Value result)
+        {
+            string message = args[0].GetString();
+            Reject(message);
+        }
+
+        private void Reject(string message)
         {
             source.TrySetException(new ScriptEngineException(message));
+        }
+
+        void IV8HostObject.GetNamedProperty(StdString name, V8Value value, out bool isConst)
+        {
+            isConst = true;
+
+            if (name.Equals(nameof(Completed)))
+                value.SetHostObject(completed);
+            else if (name.Equals(nameof(Reject)))
+                value.SetHostObject(reject);
+            else
+                throw new NotImplementedException(
+                    $"Named property {name.ToString()} is not implemented");
         }
     }
 }
