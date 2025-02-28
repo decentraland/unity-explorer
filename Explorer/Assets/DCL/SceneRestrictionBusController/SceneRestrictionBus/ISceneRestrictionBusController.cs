4c53bce6fa079b924ebbf0c405c25385542e2351
lorenzo-ranciaffi <lorenzo.ranciaffi@decentraland.org>
2025-02-28 11:32:23 +0100

    dispose pattern for scene restriction bus controller

--- Explorer/Assets/DCL/SceneRestrictionBusController/SceneRestrictionBus/ISceneRestrictionBusController.cs
+++ Explorer/Assets/DCL/SceneRestrictionBusController/SceneRestrictionBus/ISceneRestrictionBusController.cs
@@ -1,7 +1,8 @@
+using System;
 
 namespace DCL.SceneRestrictionBusController.SceneRestrictionBus
 {
-    public interface ISceneRestrictionBusController
+    public interface ISceneRestrictionBusController : IDisposable
     {
         void PushSceneRestriction(SceneRestriction.SceneRestriction sceneRestriction);
         void SubscribeToSceneRestriction(SceneRestrictionBusController.SceneRestrictionReceivedDelegate callback);
--- Explorer/Assets/DCL/SceneRestrictionBusController/SceneRestrictionBus/SceneRestrictionBusController.cs
+++ Explorer/Assets/DCL/SceneRestrictionBusController/SceneRestrictionBus/SceneRestrictionBusController.cs
@@ -12,5 +12,8 @@ namespace DCL.SceneRestrictionBusController.SceneRestrictionBus
 
         public void SubscribeToSceneRestriction(SceneRestrictionReceivedDelegate callback) =>
             sceneRestrictionReceivedDelegate += callback;
+
+        public void Dispose() =>
+            sceneRestrictionReceivedDelegate = null;
     }
 }
