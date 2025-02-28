6b369a8e3b1cbd54056dd89be287c146285dc740
Nick Khalow <71646502+NickKhalow@users.noreply.github.com>
2025-02-28 18:35:52 +0200

    Release/release 25 02 2025 chore disk cache (#3453)
    * iterator base
    
    * iterator impl
    
    * remove redundant code
    
    * Fix playground
    
    * fix tests
    
    * fix partial cache
    
    * fix ownership
    
    * explicit ownership attribute
    
    * MemoryIterator total size
    
    * TextureDiskSerializer dispose data
    
    * NativeAllocator
    
    * fix tests
    
    * reuse UnmanagedMemoryManager
    
    * partial download with sliced memory owner
    
    * remove logging

--- Explorer/Assets/Scripts/ECS/StreamableLoading/Common/PartialDiskSerializer.cs
+++ Explorer/Assets/Scripts/ECS/StreamableLoading/Common/PartialDiskSerializer.cs
@@ -2,43 +2,76 @@ using Cysharp.Threading.Tasks;
 using ECS.StreamableLoading.Cache.Disk;
 using ECS.StreamableLoading.Common.Components;
 using System;
-using System.Buffers;
 using System.Threading;
 
 namespace ECS.StreamableLoading.Common
 {
-    public class PartialDiskSerializer : IDiskSerializer<PartialLoadingState>
+    public class PartialDiskSerializer : IDiskSerializer<PartialLoadingState, SerializeMemoryIterator<PartialDiskSerializer.State>>
     {
-        public SlicedOwnedMemory<byte> Serialize(PartialLoadingState data) =>
+        public SerializeMemoryIterator<State> Serialize(PartialLoadingState data) =>
             SerializeInternal(data);
 
-        private static SlicedOwnedMemory<byte> SerializeInternal(PartialLoadingState data)
+        private static SerializeMemoryIterator<State> SerializeInternal(PartialLoadingState data)
         {
             var meta = new Meta(data.FullFileSize, data.IsFileFullyDownloaded);
-            Span<byte> metaData = stackalloc byte[Meta.META_SIZE];
-            meta.ToSpan(metaData);
+            var slice = data.FullData.Slice(0, data.NextRangeStart);
+            var state = new State(meta, slice);
 
-            int targetSize = Meta.META_SIZE + data.NextRangeStart;
-            var memoryOwner = new SlicedOwnedMemory<byte>(MemoryPool<byte>.Shared.Rent(targetSize), targetSize);
-            var memory = memoryOwner.Memory;
+            return SerializeMemoryIterator<State>.New(
+                state,
+                static (source, index, buffer) =>
+                {
+                    if (index == 0)
+                    {
+                        source.Meta.ToSpan(buffer.Span);
+                        return Meta.META_SIZE;
+                    }
 
-            metaData.CopyTo(memory.Span);
-            data.FullData.Span.Slice(0, data.NextRangeStart).CopyTo(memory.Slice(Meta.META_SIZE, data.NextRangeStart).Span);
+                    // Address meta offset
+                    index -= 1;
 
-            return memoryOwner;
+                    var span = source.FullData.Span;
+                    return SerializeMemoryIterator.ReadNextData(index, span, buffer);
+                },
+                static (source, index, bufferLength) =>
+                {
+                    if (index == 0)
+                        return true;
+
+                    // Address meta offset
+                    index -= 1;
+
+                    return SerializeMemoryIterator.CanReadNextData(index, source.FullData.Length, bufferLength);
+                }
+            );
         }
 
         public UniTask<PartialLoadingState> DeserializeAsync(SlicedOwnedMemory<byte> data, CancellationToken token)
         {
-            var meta = Meta.FromSpan(data.Memory.Span);
-            var fileData = data.Memory.Slice(Meta.META_SIZE);
+            using (data)
+            {
+                var meta = Meta.FromSpan(data.Memory.Span);
+                var fileData = data.Memory.Slice(Meta.META_SIZE);
 
-            var partialLoadingState = new PartialLoadingState(meta.MaxFileSize, meta.IsFullyDownloaded);
-            partialLoadingState.AppendData(fileData);
-            return UniTask.FromResult(partialLoadingState);
+                var partialLoadingState = new PartialLoadingState(meta.MaxFileSize, meta.IsFullyDownloaded);
+                partialLoadingState.AppendData(fileData);
+                return UniTask.FromResult(partialLoadingState);
+            }
         }
 
-        private readonly struct Meta
+        public readonly struct State
+        {
+            public readonly Meta Meta;
+            public readonly ReadOnlyMemory<byte> FullData;
+
+            public State(Meta meta, ReadOnlyMemory<byte> fullData)
+            {
+                Meta = meta;
+                FullData = fullData;
+            }
+        }
+
+        public readonly struct Meta
         {
             public const int META_SIZE = 5;
             public readonly int MaxFileSize;
@@ -53,6 +86,7 @@ namespace ECS.StreamableLoading.Common
             public void ToSpan(Span<byte> span)
             {
                 span[0] = (byte)(IsFullyDownloaded ? 1 : 0);
+
                 for (int i = 1; i < 5; i++)
                     span[i] = (byte)((MaxFileSize >> (i * 8)) & 0xFF);
             }
@@ -61,6 +95,7 @@ namespace ECS.StreamableLoading.Common
             {
                 var maxFileSize = 0;
                 var isFullyDownloaded = array[0] == 1;
+
                 for (var i = 1; i < 5; i++)
                     maxFileSize |= array[i] << (i * 8);
 
