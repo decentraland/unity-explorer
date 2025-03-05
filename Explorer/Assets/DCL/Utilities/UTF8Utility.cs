aaf8e5ddd6de393960c3b66c5413ca788879fe90
Ansis <ansis.malins@decentraland.org>
2025-03-05 11:52:04 +0100

    Obtain the OriginalJson string without allocating

---
+++ Explorer/Assets/DCL/Utilities/UTF8Utility.cs
@@ -0,0 +1,26 @@
+﻿namespace DCL.Utilities
+{
+    public class UTF8Utility
+    {
+        public static readonly byte[] UTF8_CHAR_SIZE;
+
+        static UTF8Utility()
+        {
+            UTF8_CHAR_SIZE = new byte[256];
+
+            for (int i = 0; i < 256; i++)
+            {
+                if ((i & 0b10000000) == 0)
+                    UTF8_CHAR_SIZE[i] = 1;
+                else if ((i & 0b11100000) == 0b11000000)
+                    UTF8_CHAR_SIZE[i] = 2;
+                else if ((i & 0b11110000) == 0b11100000)
+                    UTF8_CHAR_SIZE[i] = 3;
+                else if ((i & 0b11111000) == 0b11110000)
+                    UTF8_CHAR_SIZE[i] = 4;
+                else
+                    UTF8_CHAR_SIZE[i] = 1;
+            }
+        }
+    }
+}
--- Explorer/Assets/Scripts/ECS/SceneLifeCycle/SceneDefinition/Systems/LoadSceneDefinitionListSystem.cs
+++ Explorer/Assets/Scripts/ECS/SceneLifeCycle/SceneDefinition/Systems/LoadSceneDefinitionListSystem.cs
@@ -4,16 +4,18 @@ using Arch.SystemGroups.DefaultSystemGroups;
 using Cysharp.Threading.Tasks;
 using DCL.Diagnostics;
 using DCL.Ipfs;
-using DCL.Optimization.PerformanceBudgeting;
+using DCL.Utilities;
 using DCL.WebRequests;
 using ECS.Prioritization.Components;
 using ECS.StreamableLoading.Cache;
 using ECS.StreamableLoading.Common.Components;
 using ECS.StreamableLoading.Common.Systems;
-using Ipfs;
-using System.Collections.Generic;
+using Newtonsoft.Json;
+using System;
+using System.IO;
 using System.Text;
 using System.Threading;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Mathematics;
 
 namespace ECS.SceneLifeCycle.SceneDefinition
@@ -60,9 +62,80 @@ namespace ECS.SceneLifeCycle.SceneDefinition
 
             bodyBuilder.Append("]}");
 
-            List<SceneEntityDefinition> targetList = await
-                webRequestController.PostAsync(intention.CommonArguments, GenericPostArguments.CreateJson(bodyBuilder.ToString()), ct, GetReportData())
-                                    .OverwriteFromJsonAsync(intention.TargetCollection, WRJsonParser.Newtonsoft, WRThreadFlags.SwitchToThreadPool);
+            var adapter = webRequestController.PostAsync(intention.CommonArguments,
+                GenericPostArguments.CreateJson(bodyBuilder.ToString()), ct, GetReportData());
+
+            using var downloadHandler = await adapter.ExposeDownloadHandlerAsync();
+            var nativeData = downloadHandler.nativeData;
+            var serializer = JsonSerializer.CreateDefault();
+            var targetList = intention.TargetCollection;
+
+            unsafe
+            {
+                var dataPtr = (byte*)nativeData.GetUnsafeReadOnlyPtr();
+
+                using var stream = new UnmanagedMemoryStream(dataPtr, nativeData.Length,
+                    nativeData.Length, FileAccess.Read);
+
+                using var textReader = new StreamReader(stream, Encoding.UTF8);
+                using var jsonReader = new JsonTextReader(textReader);
+
+                jsonReader.Read();
+
+                if (jsonReader.TokenType != JsonToken.StartArray)
+                    throw new JsonReaderException(
+                        $"Expected token StartArray, got {jsonReader.TokenType}", jsonReader.Path,
+                        jsonReader.LineNumber, jsonReader.LinePosition, null);
+
+                int charPosition = 0;
+                int startByte = 0;
+
+                while (jsonReader.Read() && jsonReader.TokenType != JsonToken.EndArray)
+                {
+                    if (jsonReader.TokenType != JsonToken.StartObject)
+                        throw new JsonReaderException(
+                            $"Expected token StartObject, got {jsonReader.TokenType}", jsonReader.Path,
+                            jsonReader.LineNumber, jsonReader.LinePosition, null);
+
+                    int readerPosition = jsonReader.LinePosition;
+
+                    while (charPosition < readerPosition)
+                    {
+                        int charSize = UTF8Utility.UTF8_CHAR_SIZE[dataPtr[startByte]];
+                        charPosition += (charSize >> 2) + 1;
+                        startByte += charSize;
+                    }
+
+                    var scene = serializer.Deserialize<SceneEntityDefinition>(jsonReader);
+
+                    if (jsonReader.LineNumber != 1)
+                        throw new NotImplementedException("Can't parse json that contains newlines");
+
+                    int endByte = startByte;
+                    readerPosition = jsonReader.LinePosition;
+
+                    while (charPosition < readerPosition)
+                    {
+                        int charSize = UTF8Utility.UTF8_CHAR_SIZE[dataPtr[endByte]];
+                        charPosition += (charSize >> 2) + 1;
+                        endByte += charSize;
+                    }
+
+                    if (scene != null)
+                    {
+                        // All the complexity here is so that we can obtain this one OriginalJson string
+                        // without excess of allocations. Because the sole purpose of this string is to
+                        // be passed on to JavaScript, it would be even better if we created a V8Value
+                        // directly without decoding the bytes at all.
+                        scene.metadata.OriginalJson = Encoding.UTF8.GetString(dataPtr + startByte - 1,
+                            endByte - startByte + 1);
+
+                        targetList.Add(scene);
+                    }
+
+                    startByte = endByte;
+                }
+            }
 
             return new StreamableLoadingResult<SceneDefinitions>(new SceneDefinitions(targetList));
         }
