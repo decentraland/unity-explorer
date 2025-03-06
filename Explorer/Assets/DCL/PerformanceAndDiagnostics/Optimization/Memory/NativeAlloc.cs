59550b75958f4180f40758b9f28626dbb1c75996
NickKhalow <nickhalow@gmail.com>
2025-03-06 15:14:06 +0200

    windows alloc

--- Explorer/Assets/DCL/PerformanceAndDiagnostics/Optimization/Memory/NativeAlloc.cs
+++ Explorer/Assets/DCL/PerformanceAndDiagnostics/Optimization/Memory/NativeAlloc.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 using System.Runtime.InteropServices;
 
 namespace DCL.Optimization.Memory
@@ -9,10 +10,17 @@ namespace DCL.Optimization.Memory
     /// </summary>
     public static class NativeAlloc
     {
-        [DllImport("libc", EntryPoint = "malloc", CallingConvention = CallingConvention.Cdecl)]
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN || PLATFORM_STANDALONE_WIN
+        private const string LIBRARY = "msvcrt.dll";
+#else
+        private const string LIBRARY = "libc";
+#endif
+
+
+        [DllImport(LIBRARY, EntryPoint = "malloc", CallingConvention = CallingConvention.Cdecl)]
         public static extern IntPtr Malloc(nuint size);
 
-        [DllImport("libc", EntryPoint = "free", CallingConvention = CallingConvention.Cdecl)]
+        [DllImport(LIBRARY, EntryPoint = "free", CallingConvention = CallingConvention.Cdecl)]
         public static extern void Free(IntPtr ptr);
     }
 }
