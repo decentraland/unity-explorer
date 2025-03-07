64b713c2acc2223f868c48faf970aae3b6b4af6d
Fran Colarich <56565104+fcolarich@users.noreply.github.com>
2025-03-07 13:08:53 +0100

    Added new profile picture to profile suggestions

--- Explorer/Assets/DCL/UI/InputSuggestions/ProfileInputSuggestionElement.cs
+++ Explorer/Assets/DCL/UI/InputSuggestions/ProfileInputSuggestionElement.cs
@@ -6,17 +6,14 @@ namespace DCL.UI.SuggestionPanel
 {
     public class ProfileInputSuggestionElement : BaseInputSuggestionElement<ProfileInputSuggestionData>
     {
-        [field: SerializeField] private Image faceSnapshotImage;
-        [field: SerializeField] private Image faceFrame;
+        [field: SerializeField] private ProfilePictureView profilePictureView;
         [field: SerializeField] private SimpleUserNameElement simpleUserNameElement;
 
         protected override void SetupContinuation(ProfileInputSuggestionData data)
         {
             SuggestionId = data.ProfileData.DisplayName;
             simpleUserNameElement.Setup(data.ProfileData, data.ProfileData.UserNameColor);
-            faceFrame.color = data.ProfileData.UserNameColor;
-            //faceSnapshotImage.sprite = data.ProfileData.ProfilePicture
-            //TODO FRAN issue #3276-> Disabled for now, we need to implement the profile picture fetching properly and reuse of profile element.
+            profilePictureView.SetupWithDependencies(data.ViewDependencies, data.ProfileData.UserNameColor, data.ProfileData.Avatar.FaceSnapshotUrl, data.ProfileData.UserId);
         }
     }
 }
