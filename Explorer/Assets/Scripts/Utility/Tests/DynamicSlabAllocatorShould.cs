20eb9876ef486c71a6a947ffad04bcfbdc3bc067
nickkhalow <nickhalow@gmail.com>
2025-03-10 13:02:04 +0200

    tests

--- Explorer/Assets/Scripts/Utility/Tests/DynamicSlabAllocatorShould.cs
+++ Explorer/Assets/Scripts/Utility/Tests/DynamicSlabAllocatorShould.cs
@@ -19,5 +19,26 @@ namespace Utility.Tests
             for (int i = 0; i < SIZE; i++)
                 span[i] = (byte)Random.Range(0, byte.MaxValue);
         }
+
+        [Test]
+        public void Release()
+        {
+            const int SIZE = 128 * 1024;
+            using var a = new DynamicSlabAllocator(SIZE, 2);
+
+            int count = Random.Range(10, 100);
+            SlabItem[] items = new SlabItem[count];
+
+            for (int i = 0; i < count; i++)
+                items[i] = a.Allocate();
+
+            for (int i = 0; i < count; i++)
+                a.Release(items[i]);
+
+            var info = a.Info;
+            Assert.AreEqual(0, info.ChunksInUseCount);
+            Assert.AreEqual(count, info.AllocatedTimes);
+            Assert.AreEqual(count, info.ReturnedTimes);
+        }
     }
 }
