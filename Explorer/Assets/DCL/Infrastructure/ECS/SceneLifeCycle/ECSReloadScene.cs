a2162cfa667f5149919e68d08ef39a46a76c8d63
Pravus <pravusjif@gmail.com>
2025-03-17 13:26:19 +0100

    fix: local scene dev reload memory leak (#3595)
    * fixed GLTFData not being disposed of correctly; Introduced cache cleaner call on LSD scene reload
    
    * fogot to commit DynamicWorldContainer update
    
    * Added UnloadUnusedAssets() call for LSD scene reload
    
    * implemented basic disposal at GltfDownloadResult

--- Explorer/Assets/DCL/Infrastructure/ECS/SceneLifeCycle/ECSReloadScene.cs
+++ Explorer/Assets/DCL/Infrastructure/ECS/SceneLifeCycle/ECSReloadScene.cs
@@ -1,6 +1,7 @@
 ﻿using Arch.Core;
 using Cysharp.Threading.Tasks;
 using DCL.Character.Components;
+using DCL.ResourcesUnloading;
 using ECS.LifeCycle.Components;
 using ECS.Prioritization.Components;
 using ECS.SceneLifeCycle.Components;
@@ -8,6 +9,7 @@ using ECS.SceneLifeCycle.SceneDefinition;
 using ECS.StreamableLoading.Common;
 using SceneRunner.Scene;
 using System.Threading;
+using UnityEngine;
 using Utility;
 
 namespace ECS.SceneLifeCycle
@@ -15,6 +17,7 @@ namespace ECS.SceneLifeCycle
     public class ECSReloadScene : IReloadScene
     {
         private readonly IScenesCache scenesCache;
+        private readonly ICacheCleaner cacheCleaner;
 
         private readonly Entity playerEntity;
         private readonly World world;
@@ -23,12 +26,14 @@ namespace ECS.SceneLifeCycle
         public ECSReloadScene(IScenesCache scenesCache,
             World world,
             Entity playerEntity,
-            bool localSceneDevelopment)
+            bool localSceneDevelopment,
+            ICacheCleaner cacheCleaner)
         {
             this.scenesCache = scenesCache;
             this.world = world;
             this.playerEntity = playerEntity;
             this.localSceneDevelopment = localSceneDevelopment;
+            this.cacheCleaner = cacheCleaner;
         }
 
         public async UniTask<bool> TryReloadSceneAsync(CancellationToken ct)
@@ -89,6 +94,11 @@ namespace ECS.SceneLifeCycle
                     {
                         staticScenePointers.Promise = null;
                     });
+
+                Resources.UnloadUnusedAssets();
+
+                // Nothing can be called after cacheCleaner.UnloadCache(), as that code becomes unreachable.
+                cacheCleaner.UnloadCache(false);
             }
             else
             {
