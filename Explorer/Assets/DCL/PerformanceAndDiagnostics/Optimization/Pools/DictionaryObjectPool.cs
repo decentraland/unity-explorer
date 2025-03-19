9bdc81ab354c0444dd8e1924c7751653e59f33d5
Mikhail Agapov <mikhail.agapov@decentraland.org>
2025-03-19 18:30:47 +0300

    Dictionary Pool

---
+++ Explorer/Assets/DCL/PerformanceAndDiagnostics/Optimization/Pools/DictionaryObjectPool.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.Pool;
+
+namespace DCL.Optimization.Pools
+{
+    /// <summary>
+    ///     Provides functionality similarly <see cref="ObjectPool{T}" /> in an instance manner (unlike static <see cref="DictionaryPool{TKey, TValue}" />)
+    /// </summary>
+    public class DictionaryObjectPool<TKey, TValue> : ObjectPool<Dictionary<TKey, TValue>>
+    {
+        public DictionaryObjectPool(
+            Action<Dictionary<TKey, TValue>> actionOnGet = null,
+            Action<Dictionary<TKey, TValue>> actionOnDestroy = null,
+            bool collectionCheck = PoolConstants.CHECK_COLLECTIONS,
+            int dictionaryInstanceDefaultCapacity = 100,
+            int defaultCapacity = 10,
+            int maxSize = 10000) : base(() => new Dictionary<TKey, TValue>(dictionaryInstanceDefaultCapacity),
+            actionOnGet,
+            l => l.Clear(),
+            actionOnDestroy,
+            collectionCheck,
+            defaultCapacity,
+            maxSize) { }
+    }
+}
