85d42b513fbd819be6910d83efec0c061f9ad05d
Ansis <ansis.malins@decentraland.org>
2025-03-20 09:25:01 +0100

    Added empty line

--- Explorer/Assets/DCL/PluginSystem/Global/ProfilingPlugin.cs
+++ Explorer/Assets/DCL/PluginSystem/Global/ProfilingPlugin.cs
@@ -36,6 +36,7 @@ namespace DCL.PluginSystem.Global
         public void InjectToWorld(ref ArchSystemsWorldBuilder<Arch.Core.World> builder, in GlobalPluginArguments arguments)
         {
             UpdateProfilerSystem.InjectToWorld(ref builder, profiler, scenesCache);
+
             DebugViewProfilingSystem.InjectToWorld(ref builder, realmData, profiler, memoryBudget,
                 debugContainerBuilder, dclVersion);
         }
