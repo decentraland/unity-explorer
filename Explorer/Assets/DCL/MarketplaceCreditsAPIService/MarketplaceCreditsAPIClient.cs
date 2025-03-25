3ca67b54e2db2fce51094298a7ca7666e6c66062
Santi Andrade <sandrade@decentraland.org>
2025-03-25 11:13:43 +0100

    extract mocked data into its own class

--- Explorer/Assets/DCL/MarketplaceCreditsAPIService/MarketplaceCreditsAPIClient.cs
+++ Explorer/Assets/DCL/MarketplaceCreditsAPIService/MarketplaceCreditsAPIClient.cs
@@ -4,7 +4,6 @@ using DCL.Diagnostics;
 using DCL.Multiplayer.Connections.DecentralandUrls;
 using DCL.WebRequests;
 using Plugins.TexturesFuse.TexturesServerWrap.Unzips;
-using System.Collections.Generic;
 using System.Globalization;
 using System.Threading;
 using UnityEngine;
@@ -16,9 +15,6 @@ namespace DCL.MarketplaceCreditsAPIService
     {
         private readonly IWebRequestController webRequestController;
         private readonly IDecentralandUrlsSource decentralandUrlsSource;
-
-        private static string mockedEmail = "test@test.com";
-        private static bool mockedEmailConfirmed = true;
 
         private string marketplaceCreditsBaseUrl => decentralandUrlsSource.Url(DecentralandUrl.MarketplaceCredits);
 
@@ -35,7 +31,7 @@ namespace DCL.MarketplaceCreditsAPIService
             // CreditsProgramProgressResponse creditsProgramProgressResponse = await webRequestController.SignedFetchGetAsync(url, string.Empty, ct)
             //                                                                                           .CreateFromJson<CreditsProgramProgressResponse>(WRJsonParser.Newtonsoft);
 
-            CreditsProgramProgressResponse creditsProgramProgressResponse = await MockCreditsProgramProgressAsync(mockedEmail, mockedEmailConfirmed, ct);
+            CreditsProgramProgressResponse creditsProgramProgressResponse = await MarketplaceCreditsMockedData.MockCreditsProgramProgressAsync(MarketplaceCreditsMockedData.CurrentMockedEmail, MarketplaceCreditsMockedData.CurrentMockedEmailConfirmed, ct);
 
             return creditsProgramProgressResponse;
         }
@@ -44,12 +40,23 @@ namespace DCL.MarketplaceCreditsAPIService
         {
             var url = $"{marketplaceCreditsBaseUrl}/users/{walletId}/captcha";
 
-            // Sprite captchaSprite = await webRequestController.SignedFetchGetAsync(url, string.Empty, ct)
-            //                                                  .CreateFromJson<Sprite>(WRJsonParser.Newtonsoft);
+            // CaptchaResponse captchaResponse = await webRequestController.SignedFetchGetAsync(url, string.Empty, ct)
+            //                                                             .CreateFromJson<CaptchaResponse>(WRJsonParser.Newtonsoft);
 
-            Sprite captchaSprite = await GetSpriteFromUrlAsync("https://i.ibb.co/RG48r508/Test-Captcha.png", ct);
+            CaptchaResponse captchaResponse = await MarketplaceCreditsMockedData.MockCaptchaAsync(ct);
 
-            return captchaSprite;
+            IOwnedTexture2D ownedTexture = await webRequestController.GetTextureAsync(
+                new CommonArguments(URLAddress.FromString(captchaResponse.captchaUrl)),
+                new GetTextureArguments(TextureType.Albedo),
+                GetTextureWebRequest.CreateTexture(TextureWrapMode.Clamp),
+                ct,
+                ReportCategory.UI
+            );
+
+            var texture = ownedTexture.Texture;
+            texture.filterMode = FilterMode.Bilinear;
+
+            return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), VectorUtilities.OneHalf, 50, 0, SpriteMeshType.FullRect, Vector4.one, false);
         }
 
         public async UniTask<ClaimCreditsResponse> ClaimCreditsAsync(string walletId, float captchaValue, CancellationToken ct)
@@ -59,7 +66,7 @@ namespace DCL.MarketplaceCreditsAPIService
             // ClaimCreditsResponse claimCreditsResponseData = await webRequestController.SignedFetchPostAsync(url, captchaValue.ToString(CultureInfo.InvariantCulture), ct)
             //                                                                           .CreateFromJson<ClaimCreditsResponse>(WRJsonParser.Newtonsoft);
 
-            ClaimCreditsResponse claimCreditsResponseData = await MockClaimCreditsAsync(ct);
+            ClaimCreditsResponse claimCreditsResponseData = await MarketplaceCreditsMockedData.MockClaimCreditsAsync(ct);
 
             return claimCreditsResponseData;
         }
@@ -71,7 +78,7 @@ namespace DCL.MarketplaceCreditsAPIService
             // CreditsProgramProgressResponse creditsProgramProgressResponse = await webRequestController.SignedFetchPostAsync(url, string.Empty, ct)
             //                                                                                           .CreateFromJson<CreditsProgramProgressResponse>(WRJsonParser.Newtonsoft);
 
-            CreditsProgramProgressResponse programRegistrationResponse = await MockCreditsProgramProgressAsync(email, false, ct);
+            CreditsProgramProgressResponse programRegistrationResponse = await MarketplaceCreditsMockedData.MockCreditsProgramProgressAsync(email, false, ct);
 
             return programRegistrationResponse;
         }
@@ -83,7 +90,7 @@ namespace DCL.MarketplaceCreditsAPIService
             // await webRequestController.SignedFetchPostAsync(url, string.Empty, ct)
             //                           .CreateFromJson<CreditsProgramProgressResponse>(WRJsonParser.Newtonsoft);
 
-            await MockRemoveRegistrationAsync(ct);
+            await MarketplaceCreditsMockedData.MockRemoveRegistrationAsync(ct);
         }
 
         public async UniTask ResendVerificationEmailAsync(string walletId, CancellationToken ct)
@@ -93,150 +100,7 @@ namespace DCL.MarketplaceCreditsAPIService
             // await webRequestController.SignedFetchPostAsync(url, string.Empty, ct)
             //                           .CreateFromJson<CreditsProgramProgressResponse>(WRJsonParser.Newtonsoft);
 
-            await MockResendVerificationEmailAsync(ct);
-        }
-
-        private async UniTask<Sprite> GetSpriteFromUrlAsync(string url, CancellationToken ct)
-        {
-            IOwnedTexture2D ownedTexture = await webRequestController.GetTextureAsync(
-                new CommonArguments(URLAddress.FromString(url)),
-                new GetTextureArguments(TextureType.Albedo),
-                GetTextureWebRequest.CreateTexture(TextureWrapMode.Clamp),
-                ct,
-                ReportCategory.UI
-            );
-
-            var texture = ownedTexture.Texture;
-            texture.filterMode = FilterMode.Bilinear;
-            Sprite sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), VectorUtilities.OneHalf, 50, 0, SpriteMeshType.FullRect, Vector4.one, false);
-
-            return sprite;
-        }
-
-        private static async UniTask<CreditsProgramProgressResponse> MockCreditsProgramProgressAsync(string email, bool isEmailConfirmed, CancellationToken ct)
-        {
-            int randomDelay = new System.Random().Next(1000, 3000);
-            await UniTask.Delay(randomDelay, cancellationToken: ct);
-
-            CreditsProgramProgressResponse programRegistration = new CreditsProgramProgressResponse
-            {
-                season = new SeasonData
-                {
-                    startDate = "2025-03-01T00:00:00Z",
-                    endDate = "2025-05-31T23:59:59Z",
-                    timeLeft = 601200000,
-                    isOutOfFunds = false,
-                },
-                currentWeek = new CurrentWeekData
-                {
-                    timeLeft = 601200000,
-                },
-                user = new UserData
-                {
-                    email = email,
-                    isEmailConfirmed = isEmailConfirmed,
-                },
-                credits = new CreditsData
-                {
-                    available = 8.5f,
-                    expireIn = 2630016000,
-                },
-                goals = new List<GoalData>
-                {
-                    new ()
-                        {
-                            title = "Jump Into Decentraland On 3 Separate Days (Min. 10 min)",
-                            thumbnail = "https://picsum.photos/100/100",
-                            progress = new GoalProgressData
-                            {
-                                totalSteps = 3,
-                                completedSteps = 1,
-                            },
-                            reward = 4,
-                            isClaimed = false,
-                        },
-                        new ()
-                        {
-                            title = "Attend 2 Events (Min. 10 min)",
-                            thumbnail = "https://picsum.photos/100/100",
-                            progress = new GoalProgressData
-                            {
-                                totalSteps = 2,
-                                completedSteps = 1,
-                            },
-                            reward = 2,
-                            isClaimed = false,
-                        },
-                        new ()
-                        {
-                            title = "View 3 New Profiles",
-                            thumbnail = "https://picsum.photos/100/100",
-                            progress = new GoalProgressData
-                            {
-                                totalSteps = 3,
-                                completedSteps = 3,
-                            },
-                            reward = 1,
-                            isClaimed = true,
-                        },
-                        new ()
-                        {
-                            thumbnail = "https://picsum.photos/100/100",
-                            title = "Visit 3 New Parcels",
-                            progress = new GoalProgressData
-                            {
-                                totalSteps = 3,
-                                completedSteps = 3,
-                            },
-                            reward = 1,
-                            isClaimed = false,
-                        },
-                },
-            };
-
-            mockedEmail = email;
-            mockedEmailConfirmed = isEmailConfirmed;
-
-            return programRegistration;
-        }
-
-        private static async UniTask MockRemoveRegistrationAsync(CancellationToken ct)
-        {
-            int randomDelay = new System.Random().Next(1000, 3000);
-            await UniTask.Delay(randomDelay, cancellationToken: ct);
-
-            mockedEmail = string.Empty;
-            mockedEmailConfirmed = false;
-        }
-
-        private static async UniTask MockResendVerificationEmailAsync(CancellationToken ct)
-        {
-            int randomDelay = new System.Random().Next(1000, 3000);
-            await UniTask.Delay(randomDelay, cancellationToken: ct);
-
-            MockEmailVerifiedAsync(ct).Forget();
-        }
-
-        private static async UniTask MockEmailVerifiedAsync(CancellationToken ct)
-        {
-            await UniTask.Delay(8000, cancellationToken: ct);
-            mockedEmailConfirmed = true;
-        }
-
-        private static async UniTask<ClaimCreditsResponse> MockClaimCreditsAsync(CancellationToken ct)
-        {
-            bool randomSuccess = new System.Random().Next(0, 2) == 1;
-            float randomClaimedCredits = ((float)new System.Random().NextDouble() * 4) + 1;
-            int randomDelay = new System.Random().Next(1000, 3000);
-            await UniTask.Delay(randomDelay, cancellationToken: ct);
-
-            ClaimCreditsResponse responseData = new ClaimCreditsResponse
-            {
-                success = randomSuccess,
-                claimedCredits = randomClaimedCredits,
-            };
-
-            return responseData;
+            await MarketplaceCreditsMockedData.MockResendVerificationEmailAsync(ct);
         }
     }
 }
---
+++ Explorer/Assets/DCL/MarketplaceCreditsAPIService/MarketplaceCreditsMockedData.cs
@@ -0,0 +1,149 @@
+using Cysharp.Threading.Tasks;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DCL.MarketplaceCreditsAPIService
+{
+    public static class MarketplaceCreditsMockedData
+    {
+        public static string CurrentMockedEmail = "test@test.com";
+        public static bool CurrentMockedEmailConfirmed = true;
+
+        internal static async UniTask<CreditsProgramProgressResponse> MockCreditsProgramProgressAsync(string email, bool isEmailConfirmed, CancellationToken ct)
+        {
+            int randomDelay = new System.Random().Next(1000, 3000);
+            await UniTask.Delay(randomDelay, cancellationToken: ct);
+
+            CreditsProgramProgressResponse programRegistration = new CreditsProgramProgressResponse
+            {
+                season = new SeasonData
+                {
+                    startDate = "2025-03-01T00:00:00Z",
+                    endDate = "2025-05-31T23:59:59Z",
+                    timeLeft = 601200000,
+                    isOutOfFunds = false,
+                },
+                currentWeek = new CurrentWeekData
+                {
+                    timeLeft = 601200000,
+                },
+                user = new UserData
+                {
+                    email = email,
+                    isEmailConfirmed = isEmailConfirmed,
+                },
+                credits = new CreditsData
+                {
+                    available = 8.5f,
+                    expireIn = 2630016000,
+                },
+                goals = new List<GoalData>
+                {
+                    new ()
+                    {
+                        title = "Jump Into Decentraland On 3 Separate Days (Min. 10 min)",
+                        thumbnail = "https://picsum.photos/100/100",
+                        progress = new GoalProgressData
+                        {
+                            totalSteps = 3,
+                            completedSteps = 1,
+                        },
+                        reward = 4,
+                        isClaimed = false,
+                    },
+                    new ()
+                    {
+                        title = "Attend 2 Events (Min. 10 min)",
+                        thumbnail = "https://picsum.photos/100/100",
+                        progress = new GoalProgressData
+                        {
+                            totalSteps = 2,
+                            completedSteps = 1,
+                        },
+                        reward = 2,
+                        isClaimed = false,
+                    },
+                    new ()
+                    {
+                        title = "View 3 New Profiles",
+                        thumbnail = "https://picsum.photos/100/100",
+                        progress = new GoalProgressData
+                        {
+                            totalSteps = 3,
+                            completedSteps = 3,
+                        },
+                        reward = 1,
+                        isClaimed = true,
+                    },
+                    new ()
+                    {
+                        thumbnail = "https://picsum.photos/100/100",
+                        title = "Visit 3 New Parcels",
+                        progress = new GoalProgressData
+                        {
+                            totalSteps = 3,
+                            completedSteps = 3,
+                        },
+                        reward = 1,
+                        isClaimed = false,
+                    },
+                },
+            };
+
+            CurrentMockedEmail = email;
+            CurrentMockedEmailConfirmed = isEmailConfirmed;
+
+            return programRegistration;
+        }
+
+        internal static async Task<CaptchaResponse> MockCaptchaAsync(CancellationToken ct)
+        {
+            int randomDelay = new System.Random().Next(1000, 3000);
+            await UniTask.Delay(randomDelay, cancellationToken: ct);
+
+            return new CaptchaResponse
+            {
+                captchaUrl = "https://i.ibb.co/RG48r508/Test-Captcha.png",
+            };
+        }
+
+        internal static async UniTask<ClaimCreditsResponse> MockClaimCreditsAsync(CancellationToken ct)
+        {
+            float randomClaimedCredits = ((float)new System.Random().NextDouble() * 4) + 1;
+            int randomDelay = new System.Random().Next(1000, 3000);
+            await UniTask.Delay(randomDelay, cancellationToken: ct);
+
+            ClaimCreditsResponse responseData = new ClaimCreditsResponse
+            {
+                success = true,
+                claimedCredits = randomClaimedCredits,
+            };
+
+            return responseData;
+        }
+
+        internal static async UniTask MockRemoveRegistrationAsync(CancellationToken ct)
+        {
+            int randomDelay = new System.Random().Next(1000, 3000);
+            await UniTask.Delay(randomDelay, cancellationToken: ct);
+
+            CurrentMockedEmail = string.Empty;
+            CurrentMockedEmailConfirmed = false;
+        }
+
+        internal static async UniTask MockResendVerificationEmailAsync(CancellationToken ct)
+        {
+            int randomDelay = new System.Random().Next(1000, 3000);
+            await UniTask.Delay(randomDelay, cancellationToken: ct);
+
+            MockEmailVerifiedAsync(ct).Forget();
+        }
+
+        private static async UniTask MockEmailVerifiedAsync(CancellationToken ct)
+        {
+            await UniTask.Delay(8000, cancellationToken: ct);
+            CurrentMockedEmailConfirmed = true;
+        }
+    }
+}
