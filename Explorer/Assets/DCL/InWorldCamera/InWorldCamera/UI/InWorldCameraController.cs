42c438798ad219c8f55d82a1c11dce08098551c0
Alejandro Villalba <lince3d@gmail.com>
2025-03-27 16:03:50 +0000

    The layout of the camera does not appear when triggering the C key multiple times (#3752)
    Now the InWorldCameraController ignores requests to be shown if it is already visible, and ignores requests to hide if it is not visible.
    Now the flow of closing is the same when pressing the close button and when triggering the C key shortcut, it now leaves the WaitForCloseIntentAsync method, which was not being left when the shortcut was used to hide the controller.
    Fixed also: Now InWorldCameraController is not hidden and shown when the screen is capture, instead the view is just disabled and enabled.

--- Explorer/Assets/DCL/InWorldCamera/InWorldCamera/UI/InWorldCameraController.cs
+++ Explorer/Assets/DCL/InWorldCamera/InWorldCamera/UI/InWorldCameraController.cs
@@ -43,6 +43,7 @@ namespace DCL.InWorldCamera.UI
 
         public bool IsVfxInProgress => viewInstance != null && viewInstance.IsVfxInProgress;
 
+        private UniTaskCompletionSource? closeViewTask;
 
         public InWorldCameraController(ViewFactoryMethod viewFactory, Button sidebarButton, World world, IMVCManager mvcManager, ICameraReelStorageService storageService, ISharedSpaceManager sharedSpaceManager) : base(viewFactory)
         {
@@ -53,6 +54,7 @@ namespace DCL.InWorldCamera.UI
             this.sharedSpaceManager = sharedSpaceManager;
 
             ctx = new CancellationTokenSource();
+            closeViewTask = new UniTaskCompletionSource();
 
             storageService.ScreenshotUploaded += OnScreenshotUploaded;
             sidebarButton.onClick.AddListener(ToggleInWorldCamera);
@@ -114,10 +116,21 @@ namespace DCL.InWorldCamera.UI
             viewInstance?.HideAsync(default(CancellationToken), isInstant).Forget();
         }
 
-        protected override UniTask WaitForCloseIntentAsync(CancellationToken ct) =>
-            UniTask.WhenAny(
-                viewInstance!.CloseButton.OnClickAsync(ct),
-                viewInstance.CameraReelButton.OnClickAsync(ct));
+        public void Close()
+        {
+            // Effectively hides the controller in the MVC system (otherwise it waits forever in WaitForCloseIntentAsync when it has not been closed using the UI buttons)
+            closeViewTask?.TrySetResult();
+        }
+
+        protected override UniTask WaitForCloseIntentAsync(CancellationToken ct)
+        {
+            closeViewTask?.TrySetCanceled(ct);
+            closeViewTask = new UniTaskCompletionSource();
+
+            return UniTask.WhenAny(closeViewTask.Task,
+                                        viewInstance!.CloseButton.OnClickAsync(ct),
+                                        viewInstance.CameraReelButton.OnClickAsync(ct));
+        }
 
         public void PlayScreenshotFX(Texture2D image, float splashDuration, float middlePauseDuration, float transitionDuration)
         {
