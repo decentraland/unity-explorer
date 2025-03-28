78c5450000a1372d855524fb13296db326b2f332
Ansis Māliņš <ansis.malins@decentraland.org>
2025-03-28 09:22:36 +0100

    Add a Sentry breadcrumb for quitting (#3733)

--- Explorer/Assets/DCL/PerformanceAndDiagnostics/Optimization/UnityObjectUtils.cs
+++ Explorer/Assets/DCL/PerformanceAndDiagnostics/Optimization/UnityObjectUtils.cs
@@ -1,4 +1,5 @@
-﻿using Cysharp.Threading.Tasks;
+﻿using DCL.Diagnostics;
+using Sentry;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
@@ -7,18 +8,16 @@ namespace Utility
 {
     public static class UnityObjectUtils
     {
-        private static bool isQuitting;
-
-        // Can't check Application.isPlaying if called from the background thread
-        public static bool IsQuitting => (!PlayerLoopHelper.IsMainThread || Application.isPlaying) && isQuitting;
+        public static bool IsQuitting { get; private set; }
 
         [RuntimeInitializeOnLoadMethod]
         private static void StartTrackingApplicationStatus()
         {
             void SetQuitting()
             {
-                isQuitting = true;
-                Application.quitting -= SetQuitting;
+                IsQuitting = true;
+                ReportHub.Log(LogType.Log, ReportCategory.ALWAYS, "Application is quitting");
+                SentrySdk.AddBreadcrumb("Application is quitting");
             }
 
             Application.quitting += SetQuitting;
