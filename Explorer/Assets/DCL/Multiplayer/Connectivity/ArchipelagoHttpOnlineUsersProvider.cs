c7445cea8f1663346c314bac0044e91114d4162e
davidejensen <davidejensen@live.it>
2025-04-02 09:35:03 +0200

    Minor code improvements

--- Explorer/Assets/DCL/Multiplayer/Connectivity/ArchipelagoHttpOnlineUsersProvider.cs
+++ Explorer/Assets/DCL/Multiplayer/Connectivity/ArchipelagoHttpOnlineUsersProvider.cs
@@ -10,22 +10,18 @@ namespace DCL.Multiplayer.Connectivity
 {
     public class ArchipelagoHttpOnlineUsersProvider : IOnlineUsersProvider
     {
-        private const string USER_ID_FIELD = "[USER-ID]";
-        private static readonly JsonSerializerSettings SERIALIZER_SETTINGS = new () { Converters = new JsonConverter[] { new OnlinePlayersJsonDtoConverter(), new OnlinePlayerInWorldJsonDtoConverter() } };
+        private static readonly JsonSerializerSettings SERIALIZER_SETTINGS = new () { Converters = new JsonConverter[] { new OnlinePlayersJsonDtoConverter() } };
 
         private readonly IWebRequestController webRequestController;
         private readonly URLAddress baseUrl;
-        private readonly URLAddress baseUrlWorlds;
         private readonly URLBuilder urlBuilder = new ();
 
         public ArchipelagoHttpOnlineUsersProvider(
             IWebRequestController webRequestController,
-            URLAddress baseUrl,
-            URLAddress baseUrlWorlds)
+            URLAddress baseUrl)
         {
             this.webRequestController = webRequestController;
             this.baseUrl = baseUrl;
-            this.baseUrlWorlds = baseUrlWorlds;
         }
 
         public async UniTask<IReadOnlyCollection<OnlineUserData>> GetAsync(CancellationToken ct) =>
@@ -40,19 +36,8 @@ namespace DCL.Multiplayer.Connectivity
             foreach (string userId in userIds)
                 urlBuilder.AppendParameter(new URLParameter("id", userId));
 
-            List<OnlineUserData> onlineUsers = await webRequestController.GetAsync(urlBuilder.Build(), ct, ReportCategory.MULTIPLAYER)
-                                                                                     .CreateFromNewtonsoftJsonAsync<List<OnlineUserData>>(serializerSettings: SERIALIZER_SETTINGS);
-
-            foreach (string userId in userIds)
-            {
-                urlBuilder.Clear();
-                urlBuilder.AppendDomain(URLDomain.FromString(baseUrlWorlds.Value.Replace(USER_ID_FIELD, userId)));
-
-                onlineUsers.Add(await webRequestController.GetAsync(urlBuilder.Build(), ct, ReportCategory.MULTIPLAYER, ignoreErrorCodes: IWebRequestController.IGNORE_NOT_FOUND)
-                                                          .CreateFromNewtonsoftJsonAsync<OnlineUserData>(serializerSettings: SERIALIZER_SETTINGS));
-            }
-
-            return onlineUsers;
+            return await webRequestController.GetAsync(urlBuilder.Build(), ct, ReportCategory.MULTIPLAYER)
+                                         .CreateFromNewtonsoftJsonAsync<List<OnlineUserData>>(serializerSettings: SERIALIZER_SETTINGS);
         }
     }
 }
