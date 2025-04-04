2af0f7c5cfbe09758fb7d69f30cea1ef572165d2
davidejensen <davidejensen@live.it>
2025-04-04 16:21:59 +0200

    Added base view and controller

---
+++ Explorer/Assets/DCL/ApplicationMinimumSpecsGuard/MinimumSpecsScreenController.cs
@@ -0,0 +1,57 @@
+using Cysharp.Threading.Tasks;
+using DCL.Browser;
+using DCL.Multiplayer.Connections.DecentralandUrls;
+using MVC;
+using System.Threading;
+using UnityEngine;
+
+namespace DCL.ApplicationMinimumSpecsGuard
+{
+    public class MinimumSpecsScreenController : ControllerBase<MinimumSpecsScreenView>
+    {
+        private readonly IWebBrowser webBrowser;
+        public override CanvasOrdering.SortingLayer Layer => CanvasOrdering.SortingLayer.Overlay;
+
+        public MinimumSpecsScreenController(ViewFactoryMethod viewFactory, IWebBrowser webBrowser) : base(viewFactory)
+        {
+            this.webBrowser = webBrowser;
+        }
+
+        protected override void OnViewInstantiated()
+        {
+            viewInstance.ExitButton.onClick.AddListener(OnExitClicked);
+            viewInstance.ContinueButton.onClick.AddListener(OnContinueClicked);
+            viewInstance.ReadMoreButton.onClick.AddListener(OnReadMoreClicked);
+        }
+
+        public override void Dispose()
+        {
+            if (viewInstance == null)
+                return;
+
+            viewInstance.ExitButton.onClick.RemoveListener(OnExitClicked);
+            viewInstance.ContinueButton.onClick.RemoveListener(OnContinueClicked);
+            viewInstance.ReadMoreButton.onClick.RemoveListener(OnReadMoreClicked);
+        }
+
+        private void OnContinueClicked()
+        {
+
+        }
+
+        private static void OnExitClicked()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            UnityEngine.Application.Quit();
+#endif
+        }
+
+        private void OnReadMoreClicked() =>
+            webBrowser.OpenUrl(DecentralandUrl.MinimumSpecs);
+
+        protected override UniTask WaitForCloseIntentAsync(CancellationToken ct) =>
+            UniTask.Never(ct);
+    }
+}
