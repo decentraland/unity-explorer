b46046e7b608f16d0070cbaf672f249434b398c4
Geoff Birch <geoff.birch@decentraland.org>
2025-04-07 15:38:42 +0100

    Scene shader changes for Resident Drawer and Outline Render Feature converted to RenderGraph

---
+++ Explorer/Assets/DCL/Rendering/RenderGraph/RenderFeatures/AvatarOutline/RenderFeature_AvatarOutline.RenderPass_OutlineDraw.cs
@@ -0,0 +1,96 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.RenderGraphModule;
+using UnityEngine.Rendering.Universal;
+using DCL.Diagnostics;
+using System.Collections.Generic;
+
+namespace DCL.Rendering.RenderGraph.RenderFeatures.AvatarOutline
+{
+    public class RenderPass_OutlineDraw : ScriptableRenderPass
+    {
+        private const string profilerTag = "_OutlineDrawPass";
+        private readonly ShaderTagId m_ShaderTagId = new ("Outline");
+        private ReportData m_ReportData = new ("DCL_RenderFeature_Outline_OutlineDrawPass", ReportHint.SessionStatic);
+        private const string DrawOutlineObjectsPassName = "DrawOutlineObjectsPass";
+
+        private ProfilingSampler m_Sampler = new (profilerTag);
+
+        private FilteringSettings m_FilteringSettings;
+        private List<Renderer> m_OutlineRenderers;
+        public RenderPass_OutlineDraw(List<Renderer> _OutlineRenderers)
+        {
+            m_OutlineRenderers = _OutlineRenderers;
+        }
+
+        public override void RecordRenderGraph(UnityEngine.Rendering.RenderGraphModule.RenderGraph renderGraph, ContextContainer frameData)
+        {
+            var resourceData = frameData.Get<UniversalResourceData>();
+
+            // The following line ensures that the render pass doesn't blit
+            // from the back buffer.
+            // if (resourceData.isActiveTargetBackBuffer)
+            //     return;
+
+            UniversalCameraData cameraData = frameData.Get<UniversalCameraData>();
+
+            var screenColorHandle = resourceData.activeColorTexture;
+            var screenDepthStencilHandle = resourceData.activeDepthTexture;
+
+            // This check is to avoid an error from the material preview in the scene
+            if (!screenColorHandle.IsValid() || !screenDepthStencilHandle.IsValid())
+                return;
+
+            // Draw objects-to-outline pass
+            using (var builder = renderGraph.AddRasterRenderPass<RenderObjectsPassData>(DrawOutlineObjectsPassName, out var passData))
+            {
+                // Configure pass data
+                passData.outlineRenderers = m_OutlineRenderers.ToArray();
+
+                builder.SetRenderAttachment(screenColorHandle, 0);
+
+                // Make sure we also read from the active stencil buffer,
+                // which was written to in the Draw objects-to-outline pass
+                // and is used here to cut out the inside of the outline.
+                builder.SetRenderAttachmentDepth(screenDepthStencilHandle, AccessFlags.ReadWrite);
+
+                builder.SetRenderFunc((RenderObjectsPassData data, RasterGraphContext context) =>
+                    ExecuteDrawOutlineObjects(data, context));
+            }
+        }
+
+        private static void ExecuteDrawOutlineObjects(RenderObjectsPassData data, RasterGraphContext context)
+        {
+            //int originalMaterialOutlinerPass = objectRenderer.sharedMaterial.FindPass("Outline");
+            // Render all the outlined objects to the temp texture
+            foreach (Renderer objectRenderer in data.outlineRenderers)
+            {
+                // Skip null renderers
+                //if (objectRenderer)
+                {
+                    // if (!objectRenderer.enabled || objectRenderer.forceRenderingOff)
+                    //     continue;
+                    //
+                    // if (objectRenderer.sharedMaterial == null)
+                    //     continue;
+
+
+                    //if (originalMaterialOutlinerPass != -1)
+                    {
+                        context.cmd.DrawRenderer(objectRenderer, objectRenderer.sharedMaterial, 0, 0);
+                    }
+                }
+            }
+        }
+
+        public override void OnCameraCleanup(CommandBuffer cmd)
+        {
+            m_OutlineRenderers.Clear();
+        }
+
+        class RenderObjectsPassData
+        {
+            internal Renderer[] outlineRenderers;
+        }
+    }
+}
---
+++ Explorer/Assets/DCL/Rendering/RenderGraph/RenderFeatures/AvatarOutline/RenderFeature_AvatarOutline.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.Universal;
+using System.Collections.Generic;
+using DCL.Diagnostics;
+
+namespace DCL.Rendering.RenderGraph.RenderFeatures.AvatarOutline
+{
+    public class RendererFeature_AvatarOutline : ScriptableRendererFeature
+    {
+        private readonly ReportData m_ReportData = new ("RendererFeature_AvatarOutline", ReportHint.SessionStatic);
+        public static List<Renderer> m_AvatarOutlineRenderers = new ();
+        private RenderPass_OutlineDraw m_OutlineDrawPass;
+
+        public override void Create()
+        {
+            name = "RendererFeature_AvatarOutline";
+
+            // Pass in constructor variables which don't/shouldn't need to be updated every frame.
+            m_OutlineDrawPass = new RenderPass_OutlineDraw(m_AvatarOutlineRenderers);
+            m_OutlineDrawPass.renderPassEvent = RenderPassEvent.BeforeRenderingOpaques;;
+        }
+
+        public override void OnCameraPreCull(ScriptableRenderer renderer, in CameraData cameraData)
+        {
+
+        }
+
+        public override void SetupRenderPasses(ScriptableRenderer renderer, in RenderingData renderingData)
+        {
+
+        }
+
+        public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+        {
+            if (m_OutlineDrawPass == null)
+                return;
+
+            renderer.EnqueuePass(m_OutlineDrawPass);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+
+        }
+    }
+}
