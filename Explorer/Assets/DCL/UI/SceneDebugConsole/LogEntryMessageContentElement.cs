8d2e0de79fc92313a2284f71debbfb91edf72bdc
Pravus <pravusjif@gmail.com>
2025-04-10 21:40:23 +0200

    updated layout and ui elements configuration

--- Explorer/Assets/DCL/UI/SceneDebugConsole/LogEntryMessageContentElement.cs
+++ Explorer/Assets/DCL/UI/SceneDebugConsole/LogEntryMessageContentElement.cs
@@ -8,7 +8,7 @@ namespace DCL.UI.SceneDebugConsole
     public class LogEntryMessageContentElement : MonoBehaviour
     {
         [field: SerializeField] internal RectTransform messageContentRectTransform { get; private set; }
-        [field: SerializeField] internal ContentSizeFitter messageContentSizeFitter { get; private set; }
+        // [field: SerializeField] internal ContentSizeFitter messageContentSizeFitter { get; private set; }
         [field: SerializeField] internal TMP_Text messageContentText { get; private set; }
         [field: SerializeField] internal TextHyperlinkHandlerElement textHyperlinkHandler { get; private set; }
 
@@ -19,7 +19,7 @@ namespace DCL.UI.SceneDebugConsole
             //Force mesh is needed otherwise entryText.GetParsedText() in CalculatePreferredWidth will return the original text
             //of the previous frame, also data for links would not be updated either.
             messageContentText.ForceMeshUpdate(true, true);
-            messageContentSizeFitter.SetLayoutVertical();
+            // messageContentSizeFitter.SetLayoutVertical();
         }
     }
 }
