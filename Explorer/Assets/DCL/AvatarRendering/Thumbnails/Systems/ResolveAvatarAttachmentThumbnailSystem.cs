88f44e1e7417f10da999bb39ec5d1bc1dfc989db
Juan Molteni <juanignaciomolteni@gmail.com>
2025-04-10 08:21:10 -0300

    Cleanup

--- Explorer/Assets/DCL/AvatarRendering/Thumbnails/Systems/ResolveAvatarAttachmentThumbnailSystem.cs
+++ Explorer/Assets/DCL/AvatarRendering/Thumbnails/Systems/ResolveAvatarAttachmentThumbnailSystem.cs
@@ -31,13 +31,12 @@ namespace DCL.AvatarRendering.Thumbnails.Systems
         [Query]
         private void CompleteWearableABThumbnailDownload(Entity entity, ref IAvatarAttachment wearable, ref AssetBundlePromise promise)
         {
-            if (promise.IsCancellationRequested(World!))
+            if (promise.IsCancellationRequested(World))
             {
                 wearable.ThumbnailAssetResult = null;
                 World.Destroy(entity);
                 return;
             }
-
 
             if (promise.TryConsume(World, out var result))
             {
@@ -49,7 +48,7 @@ namespace DCL.AvatarRendering.Thumbnails.Systems
         [Query]
         private void CompleteWearableThumbnailDownload(Entity entity, ref IAvatarAttachment wearable, ref Promise promise)
         {
-            if (promise.IsCancellationRequested(World!))
+            if (promise.IsCancellationRequested(World))
             {
                 wearable.ThumbnailAssetResult = null;
                 World.Destroy(entity);
