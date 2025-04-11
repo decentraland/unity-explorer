e65a916d62c90f4223353f1ffafda98354d3f360
Ansis <ansis.malins@decentraland.org>
2025-04-11 16:15:33 +0200

    Show where photo was saved

--- Explorer/Assets/DCL/ApplicationVersionGuard/ApplicationVersionGuard.cs
+++ Explorer/Assets/DCL/ApplicationVersionGuard/ApplicationVersionGuard.cs
@@ -5,13 +5,11 @@ using DCL.Multiplayer.Connections.DecentralandUrls;
 using DCL.WebRequests;
 using SceneRuntime.Apis.Modules.SignedFetch.Messages;
 using System;
-using System.ComponentModel;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
 using System.Threading;
 using UnityEngine;
+using Utility;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -68,7 +66,7 @@ namespace DCL.ApplicationVersionGuard
                 try
                 {
                     await UniTask.Delay(1000, cancellationToken: ct);
-                    ShellExecute(launcherPath);
+                    PlatformUtils.ShellExecute(launcherPath);
                 }
                 catch (Exception e)
                 {
@@ -163,55 +161,6 @@ namespace DCL.ApplicationVersionGuard
             return possiblePaths.FirstOrDefault(path => File.Exists(path)
                                                         || (Directory.Exists(path) && (Application.platform == RuntimePlatform.OSXEditor || Application.platform == RuntimePlatform.OSXPlayer)));
         }
-
-        private static void ShellExecute(string fileName)
-        {
-#if UNITY_STANDALONE_WIN
-            if ((int)ShellExecute(IntPtr.Zero, null, fileName, null, null, SW_NORMAL) <= 16)
-            {
-                int error = Marshal.GetLastWin32Error();
-                var sb = new StringBuilder(1024);
-
-                uint length = FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, IntPtr.Zero, error, 0, sb,
-                    sb.Capacity, IntPtr.Zero);
-
-                string message = length > 0 ? sb.ToString() : $"error {error}";
-                throw new Win32Exception(error, message.TrimEnd());
-            }
-#elif UNITY_STANDALONE_OSX
-            string cmd = "open \"" + fileName + "\"";
-            int code = ExecuteSystemCommand(cmd);
-            if (code != 0)
-            {
-                var sb = new StringBuilder(1024);
-
-                string message = code == -1 && strerror_r(code, sb, sb.Capacity) == 0
-                    ? sb.ToString()
-                    : "Unknown error";
-                throw new Exception($"error {code}: {message}");
-            }
-#endif
-        }
-
-#if UNITY_STANDALONE_WIN
-        [DllImport("kernel32", CharSet = CharSet.Unicode, SetLastError = true)]
-        private static extern uint FormatMessage(uint dwFlags, IntPtr lpSource, int dwMessageId,
-            uint dwLanguageId, StringBuilder lpBuffer, int nSize, IntPtr Arguments);
-
-        private const uint FORMAT_MESSAGE_FROM_SYSTEM = 0x00001000;
-
-        [DllImport("shell32", CharSet = CharSet.Unicode, SetLastError = true)]
-        private static extern IntPtr ShellExecute(IntPtr hWnd, string lpOperation, string lpFile,
-            string lpParameters, string lpDirectory, int nShowCmd);
-
-        private const int SW_NORMAL = 1;
-#elif UNITY_STANDALONE_OSX
-        [DllImport("libc", EntryPoint = "system")]
-        private static extern int ExecuteSystemCommand([MarshalAs(UnmanagedType.LPStr)] string command);
-
-        [DllImport("libc")]
-        private static extern int strerror_r(int errnum, StringBuilder buf, int buflen);
-#endif
 
         [Serializable]
         private struct GitHubRelease
---
+++ Explorer/Assets/DCL/InWorldCamera/CameraReelToast/DownloadNotificationView.cs
@@ -0,0 +1,33 @@
+﻿using DCL.InWorldCamera.ReelActions;
+using DCL.UI;
+using TMPro;
+using UnityEngine;
+using Utility;
+
+namespace DCL.InWorldCamera.CameraReelToast
+{
+    public sealed class DownloadNotificationView : MonoBehaviour
+    {
+        [SerializeField] private WarningNotificationView notificationView;
+        [SerializeField] private TMP_Text fileLocationText;
+
+        private bool wasClicked;
+
+        public WarningNotificationView NotificationView => notificationView;
+
+        internal void OnShow()
+        {
+            fileLocationText.text = ReelCommonActions.ReelsPath;
+            wasClicked = false;
+        }
+
+        public void OpenFileLocation()
+        {
+            if (wasClicked)
+                return;
+
+            PlatformUtils.ShellExecute(ReelCommonActions.ReelsPath);
+            wasClicked = true;
+        }
+    }
+}
