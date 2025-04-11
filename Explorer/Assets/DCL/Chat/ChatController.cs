02bac0adfe43176490de51644a7fcd2b6903ebbe
Fran Colarich <56565104+fcolarich@users.noreply.github.com>
2025-04-11 17:08:08 +0200

    Update ChatController.cs

--- Explorer/Assets/DCL/Chat/ChatController.cs
+++ Explorer/Assets/DCL/Chat/ChatController.cs
@@ -65,7 +65,6 @@ namespace DCL.Chat
         private readonly ChatUserStateUpdater chatUserStateUpdater;
         private readonly ChatUsersStateCache chatUsersStateCache;
         private readonly IChatUserStateEventBus chatUserStateEventBus;
-        private readonly ChatHistoryStorage? chatStorage;
 
         private SingleInstanceEntity cameraEntity;
         private CancellationTokenSource memberListCts;
