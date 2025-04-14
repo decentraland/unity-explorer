af4e38123936924c463020b673d1bad9d0056c23
Santi Andrade <sandrade@decentraland.org>
2025-04-14 14:48:55 +0200

    fix DecentralandUrlsSource

--- Explorer/Assets/DCL/Browser/DecentralandUrls/DecentralandUrlsSource.cs
+++ Explorer/Assets/DCL/Browser/DecentralandUrls/DecentralandUrlsSource.cs
@@ -94,8 +94,8 @@ namespace DCL.Browser.DecentralandUrls
                 DecentralandUrl.RemotePeers => $"https://archipelago-ea-stats.decentraland.{ENV}/comms/peers",
                 DecentralandUrl.RemotePeersWorld => $"https://worlds-content-server.decentraland.org/wallet/[USER-ID]/connected-world",
                 DecentralandUrl.DAO => $"https://decentraland.{ENV}/dao/",
-                DecentralandUrl.Notification => $"https://notifications.decentraland.zone/notifications", // TODO (Santi): This will be changed to {ENV} before merging
-                DecentralandUrl.NotificationRead => $"https://notifications.decentraland.zone/notifications/read", // TODO (Santi): This will be changed to {ENV} before merging
+                DecentralandUrl.Notification => $"https://notifications.decentraland.{ENV}/notifications",
+                DecentralandUrl.NotificationRead => $"https://notifications.decentraland.{ENV}/notifications/read",
                 DecentralandUrl.FeatureFlags => $"https://feature-flags.decentraland.{ENV}",
                 DecentralandUrl.Help => $"https://decentraland.{ENV}/help/",
                 DecentralandUrl.MinimumSpecs => $"https://docs.decentraland.{ENV}/player/FAQs/decentraland-101/#what-hardware-do-i-need-to-run-decentraland",
@@ -116,8 +116,8 @@ namespace DCL.Browser.DecentralandUrls
                 DecentralandUrl.MarketplaceClaimName => $"https://decentraland.{ENV}/marketplace/names/claim",
                 DecentralandUrl.Servers => $"https://peer.decentraland.{ENV}/lambdas/contracts/servers",
                 DecentralandUrl.MediaConverter => $"https://media-opticonverter.decentraland.{ENV}/convert?ktx2=true&fileUrl={{0}}",
-                DecentralandUrl.MarketplaceCredits => $"https://credits.decentraland.zone", // TODO (Santi): This will be changed to {ENV} before merging
-                DecentralandUrl.EmailSubscriptions => $"https://notifications.decentraland.zone", // TODO (Santi): This will be changed to {ENV} before merging
+                DecentralandUrl.MarketplaceCredits => $"https://credits.decentraland.{ENV}",
+                DecentralandUrl.EmailSubscriptions => $"https://notifications.decentraland.{ENV}",
                 _ => throw new ArgumentOutOfRangeException(nameof(decentralandUrl), decentralandUrl, null!)
             };
     }
