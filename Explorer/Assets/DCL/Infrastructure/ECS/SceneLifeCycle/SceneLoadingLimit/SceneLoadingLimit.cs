32495502e47389b44e6c86b178bfdfc3d348a327
Juan Molteni <juanignaciomolteni@gmail.com>
2025-04-22 15:20:27 -0300

    Code reosortings

--- Explorer/Assets/DCL/Infrastructure/ECS/SceneLifeCycle/SceneLoadingLimit/SceneLoadingLimit.cs
+++ Explorer/Assets/DCL/Infrastructure/ECS/SceneLifeCycle/SceneLoadingLimit/SceneLoadingLimit.cs
@@ -10,27 +10,7 @@ using Utility;
 
 namespace ECS.SceneLifeCycle.IncreasingRadius
 {
-    // Following the limits from the docs. One parcel gives 15MB, capped at 300MB. Im considering here the 'worst scenarios' possible
-    // https://docs.decentraland.org/creator/development-guide/sdk7/scene-limitations/#scene-limitation-rules
-    // We add an estimation factor (1.1f); assets loaded in memory do not have the same size as in disk
 
-    // IE:
-    // A single scene can take 330MB
-    // A single high quality LOD can take 121MB (MaxSceneSize/3 + MaxSceneSize/30)
-    // A single low quality LOD can take 11MB (MaxSceneSize/30)
-
-    // The following values take into consideration the 'worst scenarios', built using GP as reference.
-    // Since all scenes dont take do the worst scenario, more will be loaded. This just ensures the upper limit
-    public class SceneLoadingMemoryConstants
-    {
-        private static readonly float RUNTIME_MEMORY_COEFFICENT = 1.1f;
-        public static float LOD_REDUCTION = 3;
-        public static float QUALITY_REDUCTED_LOD_REDUCTION = 30;
-
-        public static float MAX_SCENE_SIZE = 300 * RUNTIME_MEMORY_COEFFICENT;
-        public static float MAX_SCENE_LOD = (MAX_SCENE_SIZE / LOD_REDUCTION) + (MAX_SCENE_SIZE / QUALITY_REDUCTED_LOD_REDUCTION);
-        public static float MAX_SCENE_LOWQUALITY_LOD = MAX_SCENE_SIZE / QUALITY_REDUCTED_LOD_REDUCTION;
-    }
 
     public class SceneLoadingLimit
     {
@@ -115,9 +95,9 @@ namespace ECS.SceneLifeCycle.IncreasingRadius
         {
             if (isEnabled)
             {
-                if (systemMemoryCap.MemoryCapInMB < 10_000)
+                if (systemMemoryCap.MemoryCapInMB < SceneLoadingMemoryConstants.LOW_MEMORY_RIG_THRESHOLD)
                     initialKey = SceneLimitsKey.LOW_MEMORY;
-                else if (systemMemoryCap.MemoryCapInMB < 17_000)
+                else if (systemMemoryCap.MemoryCapInMB < SceneLoadingMemoryConstants.MEDIUM_MEMORY_RIGH_THRESHOLD)
                     initialKey = SceneLimitsKey.MEDIUM_MEMORY;
                 else
                     initialKey = SceneLimitsKey.MAX_MEMORY;
@@ -183,39 +163,10 @@ namespace ECS.SceneLifeCycle.IncreasingRadius
         }
 
 
-        private enum SceneLimitsKey
-        {
-            WARNING,
-            LOW_MEMORY,
-            MEDIUM_MEMORY,
-            MAX_MEMORY,
-        }
 
-        private enum SceneTransitionState
-        {
-            NORMAL,
-            REDUCED,
-            TRANSITIONING_TO_REDUCED,
-            TRANSITIONING_TO_NORMAL,
-        }
 
-        public struct SceneLimits
-        {
-            public readonly float SceneMaxAmountOfUsableMemoryInMB;
-            public readonly float QualityReductedLODMaxAmountOfUsableMemoryInMB;
 
-            public SceneLimits(float sceneMaxAmountOfUsableMemoryInMB, float qualityReductedLODMaxAmountOfUsableMemoryInMB)
-            {
-                SceneMaxAmountOfUsableMemoryInMB = sceneMaxAmountOfUsableMemoryInMB;
-                QualityReductedLODMaxAmountOfUsableMemoryInMB = qualityReductedLODMaxAmountOfUsableMemoryInMB;
-            }
 
-            public static SceneLimits Lerp(SceneLimits a, SceneLimits b, float t) =>
-                new (
-                    Mathf.Lerp(a.SceneMaxAmountOfUsableMemoryInMB, b.SceneMaxAmountOfUsableMemoryInMB, t),
-                    Mathf.Lerp(a.QualityReductedLODMaxAmountOfUsableMemoryInMB, b.QualityReductedLODMaxAmountOfUsableMemoryInMB, t)
-                );
-        }
 
     }
 }
---
+++ Explorer/Assets/DCL/Infrastructure/ECS/SceneLifeCycle/SceneLoadingLimit/SceneLoadingMemoryConstants.cs
@@ -0,0 +1,27 @@
+namespace ECS.SceneLifeCycle.IncreasingRadius
+{
+    // Following the limits from the docs. One parcel gives 15MB, capped at 300MB. Im considering here the 'worst scenarios' possible
+    // https://docs.decentraland.org/creator/development-guide/sdk7/scene-limitations/#scene-limitation-rules
+    // We add an estimation factor (1.1f); assets loaded in memory do not have the same size as in disk
+
+    // IE:
+    // A single scene can take 330MB
+    // A single high quality LOD can take 121MB (MaxSceneSize/3 + MaxSceneSize/30)
+    // A single low quality LOD can take 11MB (MaxSceneSize/30)
+
+    // The following values take into consideration the 'worst scenarios', built using GP as reference.
+    // Since all scenes dont take do the worst scenario, more will be loaded. This just ensures the upper limit
+    public class SceneLoadingMemoryConstants
+    {
+        private static readonly float RUNTIME_MEMORY_COEFFICENT = 1.1f;
+        public static float LOD_REDUCTION = 3;
+        public static float QUALITY_REDUCTED_LOD_REDUCTION = 30;
+
+        public static float MAX_SCENE_SIZE = 300 * RUNTIME_MEMORY_COEFFICENT;
+        public static float MAX_SCENE_LOD = (MAX_SCENE_SIZE / LOD_REDUCTION) + (MAX_SCENE_SIZE / QUALITY_REDUCTED_LOD_REDUCTION);
+        public static float MAX_SCENE_LOWQUALITY_LOD = MAX_SCENE_SIZE / QUALITY_REDUCTED_LOD_REDUCTION;
+
+        public static int LOW_MEMORY_RIG_THRESHOLD = 10_000;
+        public static int MEDIUM_MEMORY_RIGH_THRESHOLD = 17_000;
+    }
+}
