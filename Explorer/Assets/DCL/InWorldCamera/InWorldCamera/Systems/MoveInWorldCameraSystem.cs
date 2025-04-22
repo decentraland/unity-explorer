7e56ba08ef5febf811a172e54b8830e76331a58a
lorenzo-ranciaffi <lorenzo.ranciaffi@decentraland.org>
2025-04-22 10:09:15 +0200

    adjust windows sensitivity

--- Explorer/Assets/DCL/InWorldCamera/InWorldCamera/Systems/MoveInWorldCameraSystem.cs
+++ Explorer/Assets/DCL/InWorldCamera/InWorldCamera/Systems/MoveInWorldCameraSystem.cs
@@ -16,7 +16,7 @@ namespace DCL.InWorldCamera.Systems
     [LogCategory(ReportCategory.IN_WORLD_CAMERA)]
     public partial class MoveInWorldCameraSystem : BaseUnityLoopSystem
     {
-        private const float ZOOM_SPEED_SCALAR_WINDOWS = 3f;
+        private const float ZOOM_SPEED_SCALAR_WINDOWS = 20f;
         private const float ZOOM_SPEED_SCALAR_MAC = 1f;
 
         private readonly float zoomSpeedScalar;
