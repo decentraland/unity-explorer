7e765a4f76d6558d43ca82b33e4c2459111e57af
Vitaly Popuzin <popuzin@gmail.com>
2025-04-30 10:26:04 +0200

    moving spheres torso

---
+++ Explorer/Assets/MediaPipeUnity/Samples/Scenes/Pose Landmark Detection/RightArmLogger.cs
@@ -0,0 +1,10 @@
+using Mediapipe.Tasks.Vision.PoseLandmarker;
+using Mediapipe.Unity.Sample.PoseLandmarkDetection;
+using UnityEngine;
+
+public class RightArmLogger : MonoBehaviour
+{
+    [SerializeField] PoseLandmarkerRunner runner;   // drag in Inspector
+
+
+}
