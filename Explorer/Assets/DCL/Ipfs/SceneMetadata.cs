3d2a7c4bc5e4284cdd4e63d7d6078624c2c4e0f7
Mirko Jugurdzija <mirko.jugurdzija@gmail.com>
2025-05-05 16:20:14 +0200

    feat: add world configuration support to scene metadata
    Add WorldConfiguration class to the SceneMetadata class to support skybox configuration in the project. World configuration data now is correctly deserialized from scene.json

--- Explorer/Assets/DCL/Ipfs/SceneMetadata.cs
+++ Explorer/Assets/DCL/Ipfs/SceneMetadata.cs
@@ -15,7 +15,8 @@ namespace DCL.Ipfs
         public List<string> requiredPermissions;
         public List<SpawnPoint>? spawnPoints;
         public bool isPortableExperience;
-
+        public WorldConfiguration? worldConfiguration;
+        
         [JsonIgnore]
         public string OriginalJson { get; set; }
 
@@ -49,6 +50,58 @@ namespace DCL.Ipfs
                 public float? SingleValue { get; internal set; }
                 public float[]? MultiValue { get; internal set; }
             }
+        }
+
+        /// <summary>
+        /// Configuration specific to Decentraland Worlds (Realms).
+        /// {
+        ///     "worldConfiguration": {
+        ///     "name": "my-name.dcl.eth",
+        ///     "skyboxConfig": {
+        ///         "fixedTime": 36000
+        ///     },
+        ///     "fixedAdapter": "offline:offline"
+        /// }
+        /// 
+        /// skyboxConfig.fixedTime: This property indicates how many
+        /// seconds have passed (in Decentraland time) since the start of the day,
+        /// assuming the full cycle lasts 24 hours. Divide the seconds value by 60 to obtain minutes,
+        /// and by 60 again to obtain the hours since the start of the day.
+        /// For example, if the seconds value is 36000, it corresponds to 10:00 am.
+        /// If no value is set for this field, the world will follow the same day/night cycle as Genesis City.
+        /// 
+        /// NOTE: more about the setup: https://docs.decentraland.org/creator/worlds/about/
+        /// </summary>
+        [Serializable]
+        public class WorldConfiguration
+        {
+            /// <summary>
+            /// The unique name of the world (e.g., an ENS name).
+            /// </summary>
+            public string? Name { get; set; }
+
+            /// <summary>
+            /// Configuration for the world's skybox.
+            /// </summary>
+            public SkyboxConfig? SkyboxConfig { get; set; }
+            
+            /// <summary>
+            /// Specifies the adapter used for scene deployment or loading (e.g., "offline:offline").
+            /// </summary>
+            public string? FixedAdapter { get; set; }
+        }
+
+        /// <summary>
+        /// Defines settings for the world's skybox.
+        /// </summary>
+        [Serializable]
+        public class SkyboxConfig
+        {
+            /// <summary>
+            /// A fixed time value (potentially in seconds or another unit) for the skybox visuals.
+            /// Null if not fixed.
+            /// </summary>
+            public float? FixedTime { get; set; }
         }
     }
 }
