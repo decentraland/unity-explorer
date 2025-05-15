7a8519a49108e556fe9df1d1fe7b3b3e46c00193
Fran Colarich <56565104+fcolarich@users.noreply.github.com>
2025-05-15 13:57:14 +0200

    Revert "Restored Context menu behaviour"
    This reverts commit cca6672ad90dd06caa1f9e328c90ff57e9867c12.

--- Explorer/Assets/DCL/Infrastructure/MVC/MVCFacade/MVCManagerMenusAccessFacade.cs
+++ Explorer/Assets/DCL/Infrastructure/MVC/MVCFacade/MVCManagerMenusAccessFacade.cs
@@ -11,7 +11,6 @@ using DCL.TeleportPrompt;
 using DCL.UI;
 using DCL.UI.GenericContextMenu;
 using DCL.UI.GenericContextMenu.Controllers;
-using DCL.UI.GenericContextMenu.Controls.Configs;
 using DCL.UI.SharedSpaceManager;
 using DCL.Utilities;
 using DCL.Web3;
@@ -118,27 +117,26 @@ namespace MVC
             await genericUserProfileContextMenuController.ShowUserProfileContextMenuAsync(profile, position, offset, ct, closeMenuTask, onContextMenuHide, ConvertMenuAnchorPoint(anchorPoint));
         }
 
-        private GenericContextMenuAnchorPoint ConvertMenuAnchorPoint(MenuAnchorPoint anchorPoint)
+        private ContextMenuOpenDirection ConvertMenuAnchorPoint(MenuAnchorPoint anchorPoint)
         {
             switch (anchorPoint)
             {
                 case MenuAnchorPoint.TOP_LEFT:
-                    return GenericContextMenuAnchorPoint.TOP_LEFT;
+                    return ContextMenuOpenDirection.TOP_LEFT;
                 case MenuAnchorPoint.TOP_RIGHT:
-                    return GenericContextMenuAnchorPoint.TOP_RIGHT;
+                    return ContextMenuOpenDirection.TOP_RIGHT;
                 case MenuAnchorPoint.BOTTOM_LEFT:
-                    return GenericContextMenuAnchorPoint.BOTTOM_LEFT;
+                    return ContextMenuOpenDirection.BOTTOM_LEFT;
                 case MenuAnchorPoint.BOTTOM_RIGHT:
-                    return GenericContextMenuAnchorPoint.BOTTOM_RIGHT;
+                    return ContextMenuOpenDirection.BOTTOM_RIGHT;
                 case MenuAnchorPoint.CENTER_LEFT:
-                    return GenericContextMenuAnchorPoint.CENTER_LEFT;
+                    return ContextMenuOpenDirection.CENTER_LEFT;
                 case MenuAnchorPoint.CENTER_RIGHT:
-                    return GenericContextMenuAnchorPoint.CENTER_RIGHT;
+                    return ContextMenuOpenDirection.CENTER_RIGHT;
                 default:
                 case MenuAnchorPoint.DEFAULT:
-                    return GenericContextMenuAnchorPoint.DEFAULT;
+                    return ContextMenuOpenDirection.BOTTOM_RIGHT;
             }
         }
-
     }
 }
