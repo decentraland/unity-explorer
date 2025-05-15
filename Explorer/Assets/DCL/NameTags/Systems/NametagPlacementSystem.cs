3272535039f58a89c91396d9216e256351ab0913
Fran Colarich <56565104+fcolarich@users.noreply.github.com>
2025-05-15 13:52:12 +0200

    Fixed NametagPlacementSystem missing hiddenByModifierArea check

--- Explorer/Assets/DCL/NameTags/Systems/NametagPlacementSystem.cs
+++ Explorer/Assets/DCL/NameTags/Systems/NametagPlacementSystem.cs
@@ -106,7 +106,8 @@ namespace DCL.Nametags
         [All(typeof(PBAvatarShape))]
         private void AddTagForNonPlayerAvatars([Data] in CameraComponent camera, [Data] in float3 cameraForward, [Data] in float3 cameraUp, Entity e, in AvatarShapeComponent avatarShape, in CharacterTransform characterTransform, in PartitionComponent partitionComponent)
         {
-            if (partitionComponent.IsBehind ||
+            if (avatarShape.HiddenByModifierArea ||
+                partitionComponent.IsBehind ||
                 NametagMathHelper.IsOutOfRenderRange(camera.Camera.transform.position, characterTransform.Position, maxDistanceSqr) ||
                 string.IsNullOrEmpty(avatarShape.Name))
                 return;
@@ -163,8 +164,8 @@ namespace DCL.Nametags
         [None(typeof(DeleteEntityIntention))]
         private void UpdateTag([Data] in CameraComponent camera, [Data] in float fovScaleFactor, [Data] in float3 cameraForward, [Data] in float3 cameraUp, Entity e, NametagView nametagView, in AvatarCustomSkinningComponent avatarSkinningComponent, in CharacterTransform characterTransform, in PartitionComponent partitionComponent, in AvatarShapeComponent avatarShape)
         {
-            if (partitionComponent.IsBehind
-                || avatarShape.HiddenByModifierArea
+            if (avatarShape.HiddenByModifierArea ||
+                partitionComponent.IsBehind
                 || NametagMathHelper.IsOutOfRenderRange(camera.Camera.transform.position, characterTransform.Position, maxDistanceSqr)
                 || (camera.Mode == CameraMode.FirstPerson && World.Has<PlayerComponent>(e))
                 || World.Has<BlockedPlayerComponent>(e))
