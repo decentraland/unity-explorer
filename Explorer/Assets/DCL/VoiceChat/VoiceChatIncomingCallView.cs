4432961097c0acb6ceb71204acf6e04f0bd68d0d
davidejensen <davidejensen@live.it>
2025-05-28 16:27:39 +0200

    Added small animation for incoming call accept button

--- Explorer/Assets/DCL/VoiceChat/VoiceChatIncomingCallView.cs
+++ Explorer/Assets/DCL/VoiceChat/VoiceChatIncomingCallView.cs
@@ -1,3 +1,5 @@
+using DG.Tweening;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -10,5 +12,43 @@ namespace DCL.VoiceChat
 
         [field: SerializeField]
         public Button RefuseCallButton;
+
+        [field: SerializeField]
+        public Image AnimatingBackground;
+
+        private const float END_SCALE_ANIMATION_VALUE = 1.3f;
+        private const float END_FADE_ANIMATION_VALUE = 0.8f;
+        private const float ANIMATION_DURATION = 0.8f;
+
+        private Sequence pulseAnimation;
+        private Color initialColor;
+
+        private void OnEnable()
+        {
+            CreatePulseAnimation();
+        }
+
+        private void OnDisable()
+        {
+            pulseAnimation?.Kill(true);
+        }
+
+        private void CreatePulseAnimation()
+        {
+            initialColor = AnimatingBackground.color;
+            initialColor.a = 0;
+            AnimatingBackground.transform.localScale = Vector3.zero;
+            AnimatingBackground.color = initialColor;
+
+            pulseAnimation = DOTween.Sequence();
+
+            pulseAnimation.Append(AnimatingBackground.transform.DOScale(END_SCALE_ANIMATION_VALUE, ANIMATION_DURATION).SetEase(Ease.OutQuad));
+            pulseAnimation.Join(AnimatingBackground.DOFade(END_FADE_ANIMATION_VALUE, ANIMATION_DURATION).SetEase(Ease.OutQuad));
+            pulseAnimation.AppendInterval(0.2f);
+            pulseAnimation.Append(AnimatingBackground.transform.DOScale(0f, ANIMATION_DURATION / 2).SetEase(Ease.InQuad));
+            pulseAnimation.Join(AnimatingBackground.DOFade(0f, ANIMATION_DURATION / 2).SetEase(Ease.InQuad));
+            pulseAnimation.SetLoops(-1, LoopType.Restart);
+            pulseAnimation.Play();
+        }
     }
 }
