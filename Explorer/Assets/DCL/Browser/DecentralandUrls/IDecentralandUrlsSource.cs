a3a4610ace492961973c980012ac08c10184d489
Agnieszka Besz <aga@besz.me>
2025-05-28 17:05:33 +0100

    fix: added support for new laucher in application version guard

--- Explorer/Assets/DCL/Browser/DecentralandUrls/IDecentralandUrlsSource.cs
+++ Explorer/Assets/DCL/Browser/DecentralandUrls/IDecentralandUrlsSource.cs
@@ -3,8 +3,8 @@ namespace DCL.Multiplayer.Connections.DecentralandUrls
     public interface IDecentralandUrlsSource
     {
         const string EXPLORER_LATEST_RELEASE_URL = "https://api.github.com/repos/decentraland/unity-explorer/releases/latest";
-        const string LAUNCHER_LATEST_RELEASE_URL = "https://api.github.com/repos/decentraland/launcher/releases/latest";
-        const string LAUNCHER_DOWNLOAD_URL = "https://github.com/decentraland/launcher/releases/download";
+        const string LAUNCHER_DOWNLOAD_URL = "https://explorer-artifacts.decentraland.org/launcher-rust/";
+        const string LEGACY_LAUNCHER_DOWNLOAD_URL = "https://explorer-artifacts.decentraland.org/launcher/dcl/";
 
         string DecentralandDomain { get; }
         DecentralandEnvironment Environment { get; }
