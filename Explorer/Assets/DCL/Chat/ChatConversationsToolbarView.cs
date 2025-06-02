46454ac52d2d0476a9d8f8ecf6de58f830377887
Mirko Jugurdzija <mirko.jugurdzija@gmail.com>
2025-06-02 14:17:55 +0200

    fix: move Initialize method just after item is instantiated

--- Explorer/Assets/DCL/Chat/ChatConversationsToolbarView.cs
+++ Explorer/Assets/DCL/Chat/ChatConversationsToolbarView.cs
@@ -75,6 +75,7 @@ namespace DCL.Chat
             if (items.TryGetValue(channel.Id, out var item)) return;
 
             ChatConversationsToolbarViewItem newItem = Instantiate(itemPrefab, itemsContainer);
+            newItem.Initialize();
             newItem.OpenButtonClicked += OpenButtonClicked;
             newItem.RemoveButtonClicked += OnRemoveButtonClicked;
             newItem.TooltipShown += OnItemTooltipShown;
@@ -101,7 +102,6 @@ namespace DCL.Chat
             }
 
             newItem.SetConversationType(channel.ChannelType == ChatChannel.ChatChannelType.USER);
-            newItem.Initialize();
             
             items.Add(channel.Id, newItem);
 
