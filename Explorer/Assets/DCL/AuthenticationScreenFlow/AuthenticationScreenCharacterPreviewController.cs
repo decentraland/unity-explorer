d8f3c3e2c636a25fded50348b3534b54913443f0
Vitaly Popuzin <popuzin@gmail.com>
2025-06-13 19:30:45 +0200

    playing equipped emotes

--- Explorer/Assets/DCL/AuthenticationScreenFlow/AuthenticationScreenCharacterPreviewController.cs
+++ Explorer/Assets/DCL/AuthenticationScreenFlow/AuthenticationScreenCharacterPreviewController.cs
@@ -1,12 +1,13 @@
 ﻿using Arch.Core;
 using CommunicationData.URLHelpers;
+using Cysharp.Threading.Tasks;
 using DCL.AvatarRendering.Emotes;
 using DCL.AvatarRendering.Emotes.Equipped;
 using DCL.AvatarRendering.Loading.Components;
 using DCL.CharacterPreview;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using Avatar = DCL.Profiles.Avatar;
 
 namespace DCL.AuthenticationScreenFlow
@@ -16,6 +17,7 @@ namespace DCL.AuthenticationScreenFlow
         private readonly List<URN> shortenedWearables = new ();
         private readonly HashSet<URN> shortenedEmotes = new ();
         private readonly IEquippedEmotes equippedEmotes;
+        private Avatar avatar;
 
         public AuthenticationScreenCharacterPreviewController(CharacterPreviewView view,  IEquippedEmotes equippedEmotes, ICharacterPreviewFactory previewFactory, World world, CharacterPreviewEventBus characterPreviewEventBus)
             : base(view, previewFactory, world, true, characterPreviewEventBus)
@@ -25,6 +27,8 @@ namespace DCL.AuthenticationScreenFlow
 
         public override void Initialize(Avatar avatar)
         {
+            this.avatar = avatar;
+
             shortenedWearables.Clear();
             shortenedEmotes.Clear();
 
@@ -43,27 +47,36 @@ namespace DCL.AuthenticationScreenFlow
             previewAvatarModel.Emotes = shortenedEmotes;
 
             base.Initialize(avatar);
+
+            EmoteTaskLoop().Forget();
+            checkTime = Time.unscaledTime;
         }
 
-        private int i;
-        protected void OnPointerDown(PointerEventData pointerEventData)
+        private float checkTime;
+
+        private async UniTask EmoteTaskLoop()
         {
-            Debug.Log("VVV pointer selected");
-            OnEmoteSlotSelected(i);
-            i++;
-            base.OnPointerDown(pointerEventData);
+            int i = 0;
+
+            while (true)
+            {
+                await UniTask.Yield(PlayerLoopTiming.PreLateUpdate);
+                if(Time.unscaledTime - checkTime > 3f)
+                {
+                    checkTime = Time.unscaledTime;
+                    PlayEmote(avatar.Emotes[i].Shorten());
+
+                    i++;
+                    if (i >= avatar.Emotes.Count)
+                        i = 0;
+                }
+            }
         }
+
         public int PlayJumpInEmote()
         {
             PlayEmote("wave");
             return 3;
-        }
-
-        private void OnEmoteSlotSelected(int slot)
-        {
-            IEmote? emote = equippedEmotes.EmoteInSlot(slot);
-            if (emote == null) return;
-            PlayEmote(emote.GetUrn().Shorten());
         }
     }
 }
