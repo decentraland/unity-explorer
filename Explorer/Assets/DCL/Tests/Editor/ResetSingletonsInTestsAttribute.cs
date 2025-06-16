dbc6215ba7bcd2a7baad83fe8d615545190a26c7
Mikhail Agapov <mikhail.agapov@decentraland.org>
2025-06-17 00:24:10 +0300

    Automatically reset singletons between tests

---
+++ Explorer/Assets/DCL/Tests/Editor/ResetSingletonsInTestsAttribute.cs
@@ -0,0 +1,18 @@
+﻿using CodeLess.Singletons;
+using DCL.FeatureFlags;
+using DCL.Tests.Editor;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+[assembly: ResetSingletonsInTests]
+
+namespace DCL.Tests.Editor
+{
+    public class ResetSingletonsInTestsAttribute : TestActionAttribute
+    {
+        public override void AfterTest(ITest test)
+        {
+            SingletonRegistry.Reset();
+        }
+    }
+}
