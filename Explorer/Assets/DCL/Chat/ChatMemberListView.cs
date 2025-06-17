981946d385398bd4d9e8c0f4be87f0faf5c73eae
Alex Villalba <lince3d@gmail.com>
2025-06-17 10:55:46 +0100

    Community conversations Member list (#4436)
    * Added titlebar for community member list
    
    * Chat titlebar prefabs refactored and simplified, buttons now belong to main bar
    
    * Now the Member list is reused in both Nearby and Communities
    
    It shows the actual members in the list in each community.
    It updates the number of members dynamically.
    The member list data structure has been renamed to ChatUserData, so it does not matter the origin of the data (communities endpoints or profile repository), the same method and data layout is sent to the member list and to the input box for comparing the cached profiles with the members.
    
    Also replaces the Members with Online in the title bars.
    
    * Added category to the result error trace for communities

--- Explorer/Assets/DCL/Chat/ChatMemberListView.cs
+++ Explorer/Assets/DCL/Chat/ChatMemberListView.cs
@@ -15,19 +15,6 @@ namespace DCL.Chat
 {
     public class ChatMemberListView : MonoBehaviour, IViewWithGlobalDependencies
     {
-        /// <summary>
-        /// A subset of a Profile, stores only the necessary data to be presented by the view.
-        /// </summary>
-        public struct MemberData
-        {
-            public string Id;
-            public string Name;
-            public string FaceSnapshotUrl;
-            public string WalletId;
-            public ChatMemberConnectionStatus ConnectionStatus;
-            public Color ProfileColor;
-        }
-
         public delegate void VisibilityChangedDelegate(bool isVisible);
 
         /// <summary>
@@ -38,7 +25,7 @@ namespace DCL.Chat
         [SerializeField]
         private LoopListView2 loopListView;
 
-        private List<MemberData> members = new ();
+        private List<ChatUserData> members = new ();
 
         private ViewDependencies viewDependencies;
         private ProfileRepositoryWrapper profileRepositoryWrapper;
@@ -57,6 +44,12 @@ namespace DCL.Chat
                 {
                     isVisible = value;
                     gameObject.SetActive(value);
+
+                    if (!IsVisible)
+                    {
+                        loopListView.SetListItemCount(0);
+                    }
+
                     VisibilityChanged?.Invoke(value);
                 }
             }
@@ -86,7 +79,7 @@ namespace DCL.Chat
         /// Replaces the data to be represented by the view.
         /// </summary>
         /// <param name="memberData">The data related to the members of the list.</param>
-        public void SetData(List<MemberData> memberData)
+        public void SetData(List<ChatUserData> memberData)
         {
             members = memberData;
 
@@ -107,9 +100,9 @@ namespace DCL.Chat
 
             LoopListViewItem2 newItem = loopListView.NewListViewItem(loopListView.ItemPrefabDataList[0].mItemPrefab.name);
             ChatMemberListViewItem memberItem = newItem.GetComponent<ChatMemberListViewItem>();
-            memberItem.Id = members[index].Id;
+            memberItem.Id = members[index].WalletAddress;
             memberItem.Name = members[index].Name;
-            memberItem.SetupProfilePicture(profileRepositoryWrapper, members[index].ProfileColor, members[index].FaceSnapshotUrl, members[index].Id);
+            memberItem.SetupProfilePicture(profileRepositoryWrapper, members[index].ProfileColor, members[index].FaceSnapshotUrl, members[index].WalletAddress);
             memberItem.ConnectionStatus = members[index].ConnectionStatus;
             memberItem.Tag = members[index].WalletId;
             memberItem.NameTextColor = members[index].ProfileColor;
