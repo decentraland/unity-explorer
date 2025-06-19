c606c09995723db915a06fc0bdf2b7c5588cbeab
Vitaly Popuzin <35366872+popuz@users.noreply.github.com>
2025-06-19 18:32:10 +0300

    Chore: network profiler module (#4359)
    # Pull Request Description
    
    Added already tracked WebRequests and LiveKit data web-traffic into a Profiler Module. Additionally, added monitoring for full WiFi ipv4 traffic as for raw bandwidth estimation.
    
    Default module contains only per-frame data. Accumulative data is tracked, but not included in the module (can be observed by adding it via in-editor Module creation, if needed).
    
    NetworkInterface API used to show OS WiFi IPv4 bandwidth consumption as high-level estimate. It includes all OS process, not only the client.
    
    This panel is disabled by default due to it high CPU cost (~5 ms for getting `NetworkInterfaces`)
    
    NOTE: improvements can be done to track only process bandwidth by using 
    1. ETW - requires admin rights to start application.
    2. SharpPcap
    3. NetwrokInterface overhead potentially could be optimized by using `System.Diagnostics.PerformanceCounter.dll`.
    
    both requires .dll import and accurate setup.
    
    ## Test Instructions
    It should not affect build, so just a sanity check.

---
+++ Explorer/Assets/DCL/PerformanceAndDiagnostics/Profiling/Editor/DCLNetworkProfilerModule.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Unity.Profiling.Editor;
+using static DCL.Profiling.NetworkProfilerCounters;
+
+namespace DCL.Profiling.Editor
+{
+    [Serializable, ProfilerModuleMetadata(CATEGORY_NAME)]
+    public sealed class DCLNetworkProfilerModule: ProfilerModule
+    {
+        private static readonly ProfilerCounterDescriptor[] CHART_COUNTERS =
+        {
+            new(WIFI_IPV4_MBPS_SENT_NAME, CATEGORY),
+            new(WIFI_IPV4_MBPS_RECEIVED_NAME, CATEGORY),
+
+            new(WIFI_IPV4_BYTES_SENT_FRAME_NAME, CATEGORY),
+            new(WIFI_IPV4_BYTES_RECEIVED_FRAME_NAME, CATEGORY),
+
+            new(LIVEKIT_ISLAND_SEND_NAME, CATEGORY),
+            new(LIVEKIT_SCENE_SEND_NAME, CATEGORY),
+            new(LIVEKIT_ISLAND_RECEIVED_NAME, CATEGORY),
+            new(LIVEKIT_SCENE_RECEIVED_NAME, CATEGORY),
+
+            new(WEB_REQUESTS_UPLOADED_FRAME_NAME, CATEGORY),
+            new(WEB_REQUESTS_DOWNLOADED_FRAME_NAME, CATEGORY),
+        };
+
+        public DCLNetworkProfilerModule() : base(CHART_COUNTERS) { }
+    }
+}
