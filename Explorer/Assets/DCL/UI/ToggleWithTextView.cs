1c0c6be811ded53abd9c437e61e534795f4508ed
Krzysztof Murczyński <krzysztof.m@decentraland.org>
2025-06-27 16:24:34 +0200

    Added SetInteractable method to ToggleWithText
    This method swaps also additionally added sprites.

--- Explorer/Assets/DCL/UI/ToggleWithTextView.cs
+++ Explorer/Assets/DCL/UI/ToggleWithTextView.cs
@@ -7,10 +7,20 @@ namespace DCL.UI
     {
         [field: SerializeField]
         public TMP_Text toggleText { get; private set; }
+        [field: SerializeField]
+        private Color interactableTextColor { get; set; }
+        [field: SerializeField]
+        private Color nonInteractableTextColor { get; set; }
 
         public void SetToggleText(string text)
         {
             toggleText.text = text;
+        }
+
+        public void SetInteractable(bool isInteractable)
+        {
+            toggleText.color = isInteractable ? interactableTextColor : nonInteractableTextColor;
+            Toggle.interactable = isInteractable;
         }
     }
 }
