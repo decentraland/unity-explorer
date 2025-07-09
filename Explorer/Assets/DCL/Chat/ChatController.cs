0fe40514e92e9c02cb2734aff7b7c2fe642e3e53
Alex Villalba <lince3d@gmail.com>
2025-07-09 10:14:30 +0100

    Fixed: User needs to close and re-open the app to see their new community (#4642) (#4706)
    Now when the community is created, the chat conversation is created too.
    When the community deleted, the conversation is removed for the owner only. New messages coming from the server are needed for other members to remove the conversation too.

--- Explorer/Assets/DCL/Chat/ChatController.cs
+++ Explorer/Assets/DCL/Chat/ChatController.cs
@@ -489,6 +489,29 @@ namespace DCL.Chat
             viewInstance.Focus();
         }
 
+        private void OnCommunitiesDataProviderCommunityCreated(CreateOrUpdateCommunityResponse.CommunityData newCommunity)
+        {
+            ChatChannel.ChannelId channelId = ChatChannel.NewCommunityChannelId(newCommunity.id);
+            userCommunities[channelId] = new GetUserCommunitiesData.CommunityData()
+                {
+                    id = newCommunity.id,
+                    thumbnails = newCommunity.thumbnails,
+                    description = newCommunity.description,
+                    ownerAddress = newCommunity.ownerAddress,
+                    name = newCommunity.name,
+                    privacy = newCommunity.privacy,
+                    role = CommunityMemberRole.owner,
+                    membersCount = 1
+                };
+            chatHistory.AddOrGetChannel(channelId, ChatChannel.ChatChannelType.COMMUNITY);
+        }
+
+        private void OnCommunitiesDataProviderCommunityDeleted(string communityId)
+        {
+            ChatChannel.ChannelId channelId = ChatChannel.NewCommunityChannelId(communityId);
+            chatHistory.RemoveChannel(channelId);
+        }
+
         private void OnSelectConversation(ChatChannel.ChannelId channelId)
         {
             if(!IsViewReady)
@@ -985,6 +1008,9 @@ namespace DCL.Chat
             DCLInput.Instance.Shortcuts.ToggleNametags.performed += OnToggleNametagsShortcutPerformed;
             DCLInput.Instance.Shortcuts.OpenChatCommandLine.performed += OnOpenChatCommandLineShortcutPerformed;
 
+            communitiesDataProvider.CommunityCreated += OnCommunitiesDataProviderCommunityCreated;
+            communitiesDataProvider.CommunityDeleted += OnCommunitiesDataProviderCommunityDeleted;
+
             SubscribeToCommunitiesBusEventsAsync().Forget();
         }
 
@@ -1070,6 +1096,9 @@ namespace DCL.Chat
 
             communitiesEventBus.UserConnectedToCommunity -= OnCommunitiesEventBusUserConnectedToCommunity;
             communitiesEventBus.UserDisconnectedFromCommunity -= OnCommunitiesEventBusUserDisconnectedToCommunity;
+
+            communitiesDataProvider.CommunityCreated -= OnCommunitiesDataProviderCommunityCreated;
+            communitiesDataProvider.CommunityDeleted -= OnCommunitiesDataProviderCommunityDeleted;
         }
 
         private async UniTaskVoid ShowErrorNotificationAsync(string errorMessage, CancellationToken ct)
