742c9886148c1d8b10bd7ed3121255438ce0da3c
Santi Andrade <sandrade@decentraland.org>
2025-07-11 11:59:26 +0200

    [Communities Polishing] Remove FakeCommunitiesDataProvider & ICommunitiesDataProvider (#4743)

--- Explorer/Assets/DCL/Communities/CommunitiesCard/CommunityCardController.cs
+++ Explorer/Assets/DCL/Communities/CommunitiesCard/CommunityCardController.cs
@@ -47,7 +47,7 @@ namespace DCL.Communities.CommunitiesCard
         private readonly ICameraReelStorageService cameraReelStorageService;
         private readonly ICameraReelScreenshotsStorage cameraReelScreenshotsStorage;
         private readonly ObjectProxy<IFriendsService> friendServiceProxy;
-        private readonly ICommunitiesDataProvider communitiesDataProvider;
+        private readonly CommunitiesDataProvider communitiesDataProvider;
         private readonly IWebRequestController webRequestController;
         private readonly ProfileRepositoryWrapper profileRepositoryWrapper;
         private readonly IPlacesAPIService placesAPIService;
@@ -78,7 +78,7 @@ namespace DCL.Communities.CommunitiesCard
             ICameraReelStorageService cameraReelStorageService,
             ICameraReelScreenshotsStorage cameraReelScreenshotsStorage,
             ObjectProxy<IFriendsService> friendServiceProxy,
-            ICommunitiesDataProvider communitiesDataProvider,
+            CommunitiesDataProvider communitiesDataProvider,
             IWebRequestController webRequestController,
             ProfileRepositoryWrapper profileDataProvider,
             IPlacesAPIService placesAPIService,
