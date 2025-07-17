3c8c7916e20d2d06803edb4c23317b3332169209
davidejensen <davidejensen@live.it>
2025-07-17 09:17:02 +0200

    feat: added base structure for community voice chat rpc service

---
+++ Explorer/Assets/DCL/VoiceChat/Services/ICommunityVoiceService.cs
@@ -0,0 +1,16 @@
+using Cysharp.Threading.Tasks;
+using Decentraland.SocialService.V2;
+using System;
+using System.Threading;
+
+namespace DCL.VoiceChat.Services
+{
+    public interface ICommunityVoiceService : IDisposable
+    {
+        UniTask<StartCommunityVoiceChatResponse> StartCommunityVoiceChatAsync(string communityId, CancellationToken ct);
+
+        UniTask<JoinCommunityVoiceChatResponse> JoinCommunityVoiceChatAsync(string communityId, CancellationToken ct);
+
+        UniTask SubscribeToCommunityVoiceChatUpdatesAsync(CancellationToken ct);
+    }
+}
