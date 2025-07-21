678b9178ac286d1ae0683c8bd7900942790c5598
lorenzo-ranciaffi <lorenzo.ranciaffi@decentraland.org>
2025-07-21 10:46:26 +0200

    sub menu on hover

--- Explorer/Assets/DCL/UI/GenericContextMenu/Controls/GenericContextMenuSubMenuButtonView.cs
+++ Explorer/Assets/DCL/UI/GenericContextMenu/Controls/GenericContextMenuSubMenuButtonView.cs
@@ -1,18 +1,25 @@
+using Cysharp.Threading.Tasks;
 using DCL.UI.GenericContextMenu.Controls.Configs;
 using System;
+using System.Threading;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using Utility;
 
 namespace DCL.UI.GenericContextMenu.Controls
 {
-    public class GenericContextMenuSubMenuButtonView : GenericContextMenuComponentBase
+    public class GenericContextMenuSubMenuButtonView : GenericContextMenuComponentBase, IPointerEnterHandler, IPointerExitHandler
     {
         [field: SerializeField] public Button ButtonComponent { get; private set; }
         [field: SerializeField] public TMP_Text TextComponent { get; private set; }
         [field: SerializeField] public Image ImageComponent { get; private set; }
+        [field: SerializeField] public int UnHoverDebounceDurationMs { get; private set; } = 300;
 
         private ControlsContainerView container;
+        private bool isHovering;
+        private CancellationTokenSource hoverCts;
 
         public void SetContainer(ControlsContainerView container)
         {
@@ -27,12 +34,13 @@ namespace DCL.UI.GenericContextMenu.Controls
 
         private void OnDisable()
         {
+            hoverCts.SafeCancelAndDispose();
             UnregisterListeners();
         }
 
         public void Configure(SubMenuContextMenuButtonSettings settings)
         {
-            TextComponent.SetText(settings!.buttonText);
+            TextComponent.SetText(settings.buttonText);
             TextComponent.color = settings.textColor;
             ImageComponent.sprite = settings.buttonIcon;
             ImageComponent.color = settings.iconColor;
@@ -46,5 +54,30 @@ namespace DCL.UI.GenericContextMenu.Controls
 
         public override void RegisterCloseListener(Action listener) {}
 
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            isHovering = true;
+            hoverCts.SafeCancelAndDispose();
+            container.gameObject.SetActive(true);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            isHovering = false;
+            hoverCts = hoverCts.SafeRestart();
+            WaitAndTriggerExitAsync(hoverCts.Token).Forget();
+        }
+
+        private async UniTaskVoid WaitAndTriggerExitAsync(CancellationToken token)
+        {
+            try
+            {
+                await UniTask.Delay(UnHoverDebounceDurationMs, cancellationToken: token);
+
+                if (!isHovering)
+                    container.gameObject.SetActive(false);
+            }
+            catch (OperationCanceledException) { }
+        }
     }
 }
