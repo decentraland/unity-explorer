97d28356a0eabd2cfcf2a6c0f9308f26a8335e55
lorenzo-ranciaffi <lorenzo.ranciaffi@decentraland.org>
2025-07-28 14:45:48 +0200

    add loading pulse effect

--- Explorer/Assets/DCL/UI/SkeletonLoadingView.cs
+++ Explorer/Assets/DCL/UI/SkeletonLoadingView.cs
@@ -5,6 +5,8 @@ namespace DCL.UI
 {
     public class SkeletonLoadingView : MonoBehaviour
     {
+        private const float PULSE_BUFFER_SCALAR = 1.3f;
+
         [SerializeField] private CanvasGroup loadedCanvasGroup = null!;
         [SerializeField] private CanvasGroup loadingCanvasGroup = null!;
 
@@ -30,7 +32,7 @@ namespace DCL.UI
             {
                 bone.DOKill();
                 bone.anchoredPosition = new Vector2(0f, bone.anchoredPosition.y);
-                bone.DOAnchorPos3DX(bone.sizeDelta.x, tweenDuration)
+                bone.DOAnchorPosX(bone.sizeDelta.x * PULSE_BUFFER_SCALAR, tweenDuration)
                     .SetEase(Ease.Linear)
                     .SetLoops(-1, LoopType.Restart);
             }
