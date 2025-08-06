4b32459705f0505333ee65a6da20de5dc8f70969
Vitaly Popuzin <popuzin@gmail.com>
2025-08-06 15:45:47 +0200

    clean up view

--- Explorer/Assets/DCL/ApplicationsGuards/ApplicationMinimumSpecsGuard/UI/Table/MinimumSpecsTableView.cs
+++ Explorer/Assets/DCL/ApplicationsGuards/ApplicationMinimumSpecsGuard/UI/Table/MinimumSpecsTableView.cs
@@ -4,13 +4,10 @@ namespace DCL.ApplicationMinimumSpecsGuard
 {
     public class MinimumSpecsTableView : MonoBehaviour
     {
-        // Always presented
+        [field: Tooltip("Row that closes the table grid. Always presented since we have at least one entry when showing this modal.")]
         [field: SerializeField] public MinimumSpecsRowView LastRow { get; private set; }
 
-        // If we need more rows in between
+        [field: Tooltip("Template to spawn when user have more than one requirements not met.")]
         [field: SerializeField] public MinimumSpecsRowView RowTemplate { get; private set; }
-
-        // [field: SerializeField] public MinimumSpecsRowView LastRow { get; private set; }
-        // public MinimumSpecsRowView[] Rows => GetComponentsInChildren<MinimumSpecsRowView>();
     }
 }
