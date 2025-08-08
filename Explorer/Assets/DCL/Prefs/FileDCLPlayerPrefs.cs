1cdbdf570ac302d0bb040e93e3dff6b3deb3b975
Miha Krajnc <miha.krajnc@decentraland.org>
2025-08-08 18:58:00 +0200

    fix: lock concurrent prefs writing

--- Explorer/Assets/DCL/Prefs/FileDCLPlayerPrefs.cs
+++ Explorer/Assets/DCL/Prefs/FileDCLPlayerPrefs.cs
@@ -53,32 +53,44 @@ namespace DCL.Prefs
         {
             if (userData.Strings.TryGetValue(key, out string existing) && existing == value) return;
 
-            userData.Strings[key] = value;
-            dataChanged = true;
+            lock (userData)
+            {
+                userData.Strings[key] = value;
+                dataChanged = true;
+            }
         }
 
         public void SetInt(string key, int value)
         {
             if (userData.Ints.TryGetValue(key, out int existing) && existing == value) return;
 
-            userData.Ints[key] = value;
-            dataChanged = true;
+            lock (userData)
+            {
+                userData.Ints[key] = value;
+                dataChanged = true;
+            }
         }
 
         public void SetFloat(string key, float value)
         {
             if (userData.Floats.TryGetValue(key, out float existing) && Mathf.Approximately(existing, value)) return;
 
-            userData.Floats[key] = value;
-            dataChanged = true;
+            lock (userData)
+            {
+                userData.Floats[key] = value;
+                dataChanged = true;
+            }
         }
 
         public void SetBool(string key, bool value)
         {
             if (userData.Bools.TryGetValue(key, out bool existing) && existing == value) return;
 
-            userData.Bools[key] = value;
-            dataChanged = true;
+            lock (userData)
+            {
+                userData.Bools[key] = value;
+                dataChanged = true;
+            }
         }
 
         public string GetString(string key, string defaultValue)
@@ -105,15 +117,6 @@ namespace DCL.Prefs
             return userData.Bools.GetValueOrDefault(key, defaultValue);
         }
 
-        private void MigrateBool(string key)
-        {
-            if (unityPrefs == null || !unityPrefs.HasKey(key)) return;
-
-            userData.Bools.TryAdd(key, unityPrefs!.GetBool(key, false));
-            unityPrefs.DeleteKey(key);
-            unityPrefs.Save();
-        }
-
         public bool HasKey(string key) =>
             (unityPrefs != null && unityPrefs.HasKey(key)) || userData.Strings.ContainsKey(key) || userData.Ints.ContainsKey(key) || userData.Floats.ContainsKey(key);
 
@@ -121,20 +124,26 @@ namespace DCL.Prefs
         {
             if (!HasKey(key)) return;
 
-            userData.Strings.Remove(key);
-            userData.Ints.Remove(key);
-            userData.Floats.Remove(key);
-            PlayerPrefs.DeleteKey(key);
-            dataChanged = true;
+            lock (userData)
+            {
+                userData.Strings.Remove(key);
+                userData.Ints.Remove(key);
+                userData.Floats.Remove(key);
+                PlayerPrefs.DeleteKey(key);
+                dataChanged = true;
+            }
         }
 
         public void DeleteAll()
         {
-            userData.Strings.Clear();
-            userData.Ints.Clear();
-            userData.Floats.Clear();
-            PlayerPrefs.DeleteAll();
-            dataChanged = true;
+            lock (userData)
+            {
+                userData.Strings.Clear();
+                userData.Ints.Clear();
+                userData.Floats.Clear();
+                PlayerPrefs.DeleteAll();
+                dataChanged = true;
+            }
         }
 
         public void Save()
@@ -147,6 +156,7 @@ namespace DCL.Prefs
             Task.Run(() =>
             {
                 lock (fileStream)
+                lock (userData)
                 {
                     fileStream.Seek(0, SeekOrigin.Begin);
                     fileStream.SetLength(0);
@@ -163,7 +173,12 @@ namespace DCL.Prefs
         {
             if (unityPrefs == null || !unityPrefs.HasKey(key)) return;
 
-            userData.Strings.TryAdd(key, unityPrefs!.GetString(key, null));
+            lock (userData)
+            {
+                userData.Strings.TryAdd(key, unityPrefs!.GetString(key, null));
+                dataChanged = true;
+            }
+
             unityPrefs.DeleteKey(key);
             unityPrefs.Save();
         }
@@ -172,7 +187,12 @@ namespace DCL.Prefs
         {
             if (unityPrefs == null || !unityPrefs.HasKey(key)) return;
 
-            userData.Ints.TryAdd(key, unityPrefs!.GetInt(key, 0));
+            lock (userData)
+            {
+                userData.Ints.TryAdd(key, unityPrefs!.GetInt(key, 0));
+                dataChanged = true;
+            }
+
             unityPrefs.DeleteKey(key);
             unityPrefs.Save();
         }
@@ -181,7 +201,26 @@ namespace DCL.Prefs
         {
             if (unityPrefs == null || !unityPrefs.HasKey(key)) return;
 
-            userData.Floats.TryAdd(key, unityPrefs!.GetFloat(key, 0f));
+            lock (userData)
+            {
+                userData.Floats.TryAdd(key, unityPrefs!.GetFloat(key, 0f));
+                dataChanged = true;
+            }
+
+            unityPrefs.DeleteKey(key);
+            unityPrefs.Save();
+        }
+
+        private void MigrateBool(string key)
+        {
+            if (unityPrefs == null || !unityPrefs.HasKey(key)) return;
+
+            lock (userData)
+            {
+                userData.Bools.TryAdd(key, unityPrefs!.GetBool(key, false));
+                dataChanged = true;
+            }
+
             unityPrefs.DeleteKey(key);
             unityPrefs.Save();
         }
