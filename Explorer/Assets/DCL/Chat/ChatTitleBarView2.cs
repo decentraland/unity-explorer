32cfb268d7617ec4f9002dcf61be797ef1503ac1
Mirko Jugurdzija <mirko.jugurdzija@gmail.com>
2025-08-10 21:09:20 +0200

    fix: adjust title bar design

--- Explorer/Assets/DCL/Chat/ChatTitleBarView2.cs
+++ Explorer/Assets/DCL/Chat/ChatTitleBarView2.cs
@@ -82,7 +82,7 @@ namespace DCL.Chat
         public void SetFocusedState(bool isFocused, bool animate, float duration, Ease easing)
         {
             titlebarCanvasGroup.DOKill();
-            float targetAlpha = isFocused ? 1.0f : 0.0f;
+            float targetAlpha = isFocused ? 0.9f : 0.0f;
             titlebarCanvasGroup.DOFade(targetAlpha, animate ? duration : 0f);
         }
     }
