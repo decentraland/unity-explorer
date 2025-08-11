b8e102624cfb1cff9da80b1fb61e5580b1da4044
Mirko Jugurdzija <mirko.jugurdzija@gmail.com>
2025-08-11 10:05:39 +0200

    fix: prepare for chat reset (login/logout)

--- Explorer/Assets/DCL/Chat/_Refactor/ChatCommands/ResetChatCommand.cs
+++ Explorer/Assets/DCL/Chat/_Refactor/ChatCommands/ResetChatCommand.cs
@@ -1,10 +1,14 @@
-﻿using Utility;
+﻿using DCL.Chat.ChatServices;
+using DCL.Chat.History;
+using Utility;
 
 namespace DCL.Chat.ChatCommands
 {
     public class ResetChatCommand
     {
         private readonly IEventBus eventBus;
+        private readonly IChatHistory chatHistory;
+        private readonly CurrentChannelService currentChannelService;
 
         public ResetChatCommand(IEventBus eventBus)
         {
@@ -14,6 +18,10 @@ namespace DCL.Chat.ChatCommands
         public void Execute()
         {
             eventBus.Publish(new ChatEvents.ChatResetEvent());
+
+            chatHistory.DeleteAllChannels();
+            currentChannelService.Dispose();
+            
         }
     }
 }
