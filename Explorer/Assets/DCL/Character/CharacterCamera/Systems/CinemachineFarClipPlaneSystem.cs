201470e68db4bf042b07a4516ffc025e872e8eb8
Maurizio-dcl <maurizio.farmi@decentraland.org>
2025-08-20 14:16:46 +0200

    Switched Camera.Follow with Camera.transform to fix null reference.

--- Explorer/Assets/DCL/Character/CharacterCamera/Systems/CinemachineFarClipPlaneSystem.cs
+++ Explorer/Assets/DCL/Character/CharacterCamera/Systems/CinemachineFarClipPlaneSystem.cs
@@ -51,10 +51,10 @@ namespace DCL.CharacterCamera.Systems
 
                 case CameraMode.InWorld:
                     ICinemachineFreeCameraData inWorldCamera = cinemachinePreset.InWorldCameraData;
-                    UpdateFarClipPlane(inWorldCamera.Camera.Follow, settings, ref inWorldCamera.Camera.m_Lens);
+                    // Here we use camera transform because Camera.Follow could be null
+                    UpdateFarClipPlane(inWorldCamera.Camera.transform, settings, ref inWorldCamera.Camera.m_Lens);
                     break;
             }
-
         }
 
         private void UpdateFarClipPlane(Transform target, CameraFarClipPlaneSettings settings, ref LensSettings lens)
