8e85f9b648dc63ee7799e8890534181649957d52
Nick Khalow <71646502+NickKhalow@users.noreply.github.com>
2025-08-20 21:00:42 +0300

    Opti: eliminate TransformAccessArray from AvatarTransformMatrixJobWrapper and mitigate performance degradation InterpolateCharacterSystem (#4570)
    * migrate TransformArray to Matrix4x4
    
    * migrate for loop to memcpy
    
    * WorldMatrixCalculation on jobs
    
    * fix tests
    
    * NewDefault for tests only
    
    * calculate offset once
    
    * remove raw pointers, remove unsafe
    
    * Revert "remove raw pointers, remove unsafe"
    
    This reverts commit be96f542c504821b17d1fa041454516044e843aa.
    
    * quick array impl
    
    * move unsafe
    
    * remove doable dispose
    
    * job dispose
    
    * FromOrDefault array
    
    * private avatar amount supported
    
    * remove WorldMatrixCalculationJob

--- Explorer/Assets/DCL/AvatarRendering/AvatarShape/ComputeShader/BoneMatrixCalculationJob.cs
+++ Explorer/Assets/DCL/AvatarRendering/AvatarShape/ComputeShader/BoneMatrixCalculationJob.cs
@@ -1,40 +1,52 @@
-﻿using Unity.Burst;
+﻿using System;
+using Unity.Burst;
 using Unity.Collections;
+using Unity.Jobs;
 using Unity.Mathematics;
 using UnityEngine;
-using UnityEngine.Jobs;
 
 namespace DCL.AvatarRendering.AvatarShape.ComputeShader
 {
     [BurstCompile]
-    public struct BoneMatrixCalculationJob : IJobParallelForTransform
+    public struct BoneMatrixCalculationJob : IJobParallelFor, IDisposable
     {
-        private readonly int BONE_COUNT;
-        private int AvatarIndex;
+        private readonly int boneCount;
 
-        public NativeArray<float4x4> BonesMatricesResult;
+        private NativeArray<float4x4> bonesMatricesResult;
         [NativeDisableParallelForRestriction]
         public NativeArray<Matrix4x4> AvatarTransform;
 
+        private NativeArray<Matrix4x4> boneWorldMatrixArray;
+
         [NativeDisableParallelForRestriction] public NativeArray<bool> UpdateAvatar;
 
-        public BoneMatrixCalculationJob(int boneCount, int bonesPerAvatarLength)
+        public NativeArray<float4x4> BonesMatricesResult => bonesMatricesResult;
+
+        public BoneMatrixCalculationJob(int boneCount, int bonesPerAvatarLength, NativeArray<Matrix4x4> boneWorldMatrixArray)
         {
-            BONE_COUNT = boneCount;
-            BonesMatricesResult = new NativeArray<float4x4>(bonesPerAvatarLength, Allocator.Persistent);
+            this.boneCount = boneCount;
+            bonesMatricesResult = new NativeArray<float4x4>(bonesPerAvatarLength, Allocator.Persistent);
             AvatarTransform = default;
             UpdateAvatar = default;
-            AvatarIndex = 0;
+
+            this.boneWorldMatrixArray = boneWorldMatrixArray;
         }
 
-        public void Execute(int index, TransformAccess transform)
+        public void Dispose()
+        {
+            bonesMatricesResult.Dispose();
+        }
+
+        public void Execute(int index)
         {
             // The avatarIndex is calculated by dividing the index by the amount of bones per avatar
             // Therefore, all of the indexes between 0 and ComputeShaderConstants.BONE_COUNT correlates to a single avatar
-            AvatarIndex = index / BONE_COUNT;
-            if (!UpdateAvatar[AvatarIndex])
+            int avatarIndex = index / boneCount;
+
+            if (!UpdateAvatar[avatarIndex])
                 return;
-            BonesMatricesResult[index] = AvatarTransform[AvatarIndex] * transform.localToWorldMatrix;
+
+            bonesMatricesResult[index] = AvatarTransform[avatarIndex] * boneWorldMatrixArray[index];
         }
     }
 }
