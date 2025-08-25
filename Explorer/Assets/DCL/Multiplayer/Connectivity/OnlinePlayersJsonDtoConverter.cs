9987dad2654a9042d5d07c46b36aec89a640feff
Miha Krajnc <miha.krajnc@decentraland.org>
2025-08-25 18:21:14 +0200

    fix: code stripping issues and chat ui

--- Explorer/Assets/DCL/Multiplayer/Connectivity/OnlinePlayersJsonDtoConverter.cs
+++ Explorer/Assets/DCL/Multiplayer/Connectivity/OnlinePlayersJsonDtoConverter.cs
@@ -41,15 +41,19 @@ namespace DCL.Multiplayer.Connectivity
         private static Vector3 ToVector3(float x, float z) =>
             new (Convert.ToInt32(x), 0, Convert.ToInt32(z));
 
-        [Preserve]
+        [Serializable, Preserve]
         private class RootObject
         {
+            [Preserve]
             public List<DataObject> peers { get; set; }
         }
 
+        [Serializable, Preserve]
         private class DataObject
         {
+            [Preserve]
             public string address { get; set; }
+            [Preserve]
             public float[] position { get; set; }
         }
     }
