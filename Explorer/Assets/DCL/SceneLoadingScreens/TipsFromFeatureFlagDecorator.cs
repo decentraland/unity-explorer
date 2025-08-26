fb71bc1291864d4522e9760806333d3bd28a3b99
Nicolas Lorusso <56365551+lorux0@users.noreply.github.com>
2025-08-26 11:22:37 -0300

    feat: filter loading screen tips from feature flag (#5156)

---
+++ Explorer/Assets/DCL/SceneLoadingScreens/TipsFromFeatureFlagDecorator.cs
@@ -0,0 +1,53 @@
+using Cysharp.Threading.Tasks;
+using DCL.FeatureFlags;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace DCL.SceneLoadingScreens
+{
+    public class TipsFromFeatureFlagDecorator : ISceneTipsProvider
+    {
+        private readonly FeatureFlagsConfiguration featureFlags;
+        private readonly ISceneTipsProvider source;
+        private readonly List<SceneTips.Tip> filteredTipList = new ();
+        private Tips tipsJson;
+        private bool featureFlagChecked;
+        private bool tipsParseSuccess;
+
+        public TipsFromFeatureFlagDecorator(ISceneTipsProvider source, FeatureFlagsConfiguration featureFlags)
+        {
+            this.featureFlags = featureFlags;
+            this.source = source;
+        }
+
+        public async UniTask<SceneTips> GetAsync(CancellationToken ct)
+        {
+            SceneTips originTips = await source.GetAsync(ct);
+
+            if (!featureFlagChecked)
+            {
+                tipsParseSuccess = featureFlags.TryGetJsonPayload(FeatureFlagsStrings.LOADING_SCREEN_TIPS, "tips", out tipsJson);
+                featureFlagChecked = true;
+            }
+
+            if (!tipsParseSuccess) return originTips;
+
+            filteredTipList.Clear();
+            SceneTips newTips = new SceneTips(originTips.Duration, originTips.Random, filteredTipList);
+            filteredTipList.AddRange(originTips.Tips.Where(t => Contains(tipsJson, t)));
+
+            return newTips;
+
+            bool Contains(Tips tips, SceneTips.Tip tip) =>
+                tips.displayed.Any(title => string.Equals(title, tip.Title, StringComparison.OrdinalIgnoreCase));
+        }
+
+        [Serializable]
+        private struct Tips
+        {
+            public string[] displayed;
+        }
+    }
+}
