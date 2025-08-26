4090e6871dac894b5149896639a4ad67269da09f
Agnieszka Besz <aga@besz.me>
2025-08-26 16:56:05 +0100

    chore: add exception for block list json parsing

--- Explorer/Assets/DCL/ApplicationsGuards/ApplicationBlocklistGuard/ApplicationBlocklistGuard.cs
+++ Explorer/Assets/DCL/ApplicationsGuards/ApplicationBlocklistGuard/ApplicationBlocklistGuard.cs
@@ -3,6 +3,7 @@ using DCL.Diagnostics;
 using DCL.Multiplayer.Connections.DecentralandUrls;
 using DCL.WebRequests;
 using SceneRuntime.Apis.Modules.SignedFetch.Messages;
+using System;
 using System.Linq;
 using System.Threading;
 using UnityEngine;
@@ -13,16 +14,24 @@ namespace DCL.ApplicationBlocklistGuard
     {
         public static async UniTask<bool> IsUserBlocklistedAsync(IWebRequestController webRequestController, IDecentralandUrlsSource urlsSource, string userID, CancellationToken ct)
         {
-            FlatFetchResponse response = await webRequestController.GetAsync<FlatFetchResponse<GenericGetRequest>, FlatFetchResponse>(
-                urlsSource.Url(DecentralandUrl.Blocklist),
-                new FlatFetchResponse<GenericGetRequest>(),
-                ct,
-                ReportCategory.STARTUP,
-                new WebRequestHeadersInfo());
+            try
+            {
+                FlatFetchResponse response = await webRequestController.GetAsync<FlatFetchResponse<GenericGetRequest>, FlatFetchResponse>(
+                    urlsSource.Url(DecentralandUrl.Blocklist),
+                    new FlatFetchResponse<GenericGetRequest>(),
+                    ct,
+                    ReportCategory.STARTUP,
+                    new WebRequestHeadersInfo());
 
-            BlocklistData bd = JsonUtility.FromJson<BlocklistData>(response.body);
+                BlocklistData bd = JsonUtility.FromJson<BlocklistData>(response.body);
 
-            return bd.users.Any(u => u.wallet == userID);
+                return bd?.users?.Any(u => u.wallet == userID) ?? false;
+            }
+            catch (Exception ex)
+            {
+                ReportHub.LogError(ReportCategory.STARTUP, $"Failed to parse blocklist JSON: {ex.Message}. Skipping blocklist check.");
+                return false;
+            }
         }
     }
 }
