dbbf56cefbd5261a4f61eb33257cfda3d19b9b6b
gon boedo <gonzalo@decentraland.org>
2025-09-05 12:42:14 -0300

    fix auth server peer id

--- Explorer/Assets/DCL/Infrastructure/CrdtEcsBridge/JsModulesImplementation/Communications/SceneCommunicationPipe.cs
+++ Explorer/Assets/DCL/Infrastructure/CrdtEcsBridge/JsModulesImplementation/Communications/SceneCommunicationPipe.cs
@@ -85,8 +85,8 @@ namespace CrdtEcsBridge.JsModulesImplementation.Communications
 
             MessageWrap<Scene> sceneMessage = messagePipe.NewMessage<Scene>();
 
-            // Always send scene packages to authorative-server instead of broadcasting
-            sceneMessage.AddSpecialRecipient("authorative-server");
+            // Always send scene packages to authoritative-server instead of broadcasting
+            sceneMessage.AddSpecialRecipient("authoritative-server");
 
             sceneMessage.Payload.Data = ByteString.CopyFrom(message);
             sceneMessage.Payload.SceneId = sceneId;
