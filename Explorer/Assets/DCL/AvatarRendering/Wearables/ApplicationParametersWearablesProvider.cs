e29b30702a3a9f69e4afe08f7301d87049b71296
Pravus <pravusjif@gmail.com>
2025-09-09 02:39:10 +0200

    removed unneeded import

--- Explorer/Assets/DCL/AvatarRendering/Wearables/ApplicationParametersWearablesProvider.cs
+++ Explorer/Assets/DCL/AvatarRendering/Wearables/ApplicationParametersWearablesProvider.cs
@@ -10,7 +10,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using UnityEngine.Pool;
-using DCL.AvatarRendering.Wearables.Helpers;
 
 namespace DCL.AvatarRendering.Wearables
 {
