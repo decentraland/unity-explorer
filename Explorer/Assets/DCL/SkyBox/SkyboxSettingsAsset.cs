9c30afddcb06a443be5b087451f8700f5f9774ac
Vitaly Popuzin <popuzin@gmail.com>
2025-09-16 10:09:11 +0200

    changed update cycle to match design vision - 1 day cycle happens in 2 hour now

--- Explorer/Assets/DCL/SkyBox/SkyboxSettingsAsset.cs
+++ Explorer/Assets/DCL/SkyBox/SkyboxSettingsAsset.cs
@@ -2,6 +2,7 @@
 using System;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using UnityEngine.Serialization;
 
 namespace DCL.SkyBox
 {
@@ -14,16 +15,15 @@ namespace DCL.SkyBox
     [CreateAssetMenu(menuName = "DCL/SO/Skybox Settings", fileName = "SkyboxSettings")]
     public class SkyboxSettingsAsset : ScriptableObject
     {
-        private const float DEFAULT_SPEED = 1 * 60f; // 1 minute per second
+        private const int SECONDS_IN_DAY = 86400;
 
         // We need to subtract 1 minute to make the slider range is between 00:00 and 23:59
         public const int TOTAL_MINUTES_IN_DAY = 1439; // 23:59 in minutes
-        public const int SECONDS_IN_DAY = 86400;
         public const float INITIAL_TIME_OF_DAY = 0.5f; // Midday
-        public const float REFRESH_INTERVAL = 5f;
 
-        [SerializeField] private float speedMultiplier = DEFAULT_SPEED;
+        [SerializeField] private float fullDayCycleInMinutes = 120;
         [SerializeField] private float transitionSpeed = 1f;
+        [field: SerializeField] public float RefreshInterval { get; private set; } = 5f;
 
         private float timeOfDayNormalized;
         private bool isDayCycleEnabled;
@@ -35,6 +35,7 @@ namespace DCL.SkyBox
         public Material SkyboxMaterial = null!;
         public AssetReferenceT<AnimationClip> SkyboxAnimationCycle = null!;
 
+        public float FullCycleSpeed => 1f / (fullDayCycleInMinutes * 60f);
         public bool IsUIControlled { get; set; }
         public float UIOverrideTimeOfDayNormalized { get; set; }
         public Vector2Int? CurrentSDKControlledScene { get; set; }
@@ -51,8 +52,6 @@ namespace DCL.SkyBox
 
         }
         public TransitionMode TransitionMode { get; set; }
-
-        public float SpeedMultiplier => speedMultiplier;
 
         public float TransitionSpeed => transitionSpeed;
 
