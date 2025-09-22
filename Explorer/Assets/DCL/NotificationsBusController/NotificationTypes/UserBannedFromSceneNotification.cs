61d03b2ede0293b961bb81eb9bd4d8b600a8a401
Santi Andrade <sandrade@decentraland.org>
2025-09-22 12:38:02 +0200

    Notifications implemented

---
+++ Explorer/Assets/DCL/NotificationsBusController/NotificationTypes/UserBannedFromSceneNotification.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace DCL.NotificationsBusController.NotificationTypes
+{
+    public class UserBannedFromSceneNotification : NotificationBase
+    {
+        private const string NOTIFICATION_TITLE = "Your access to this scene has been restricted.";
+
+        [JsonProperty("metadata")]
+        public UserBannedFromSceneNotificationMetadata Metadata { get; set; }
+
+        public override string GetHeader() =>
+            Metadata.PlaceTitle;
+
+        public override string GetTitle() =>
+            NOTIFICATION_TITLE;
+    }
+
+    [Serializable]
+    public struct UserBannedFromSceneNotificationMetadata
+    {
+        [JsonProperty("placeTitle")]
+        public string PlaceTitle { get; set; }
+
+        [JsonProperty("userAddress")]
+        public string UserAddress { get; set; }
+    }
+}
---
+++ Explorer/Assets/DCL/NotificationsBusController/NotificationTypes/UserUnbannedFromSceneNotification.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace DCL.NotificationsBusController.NotificationTypes
+{
+    public class UserUnbannedFromSceneNotification : NotificationBase
+    {
+        private const string NOTIFICATION_TITLE = "This scene is no longer restricting access to chat and interactions.";
+
+        [JsonProperty("metadata")]
+        public UserUnbannedFromSceneNotificationMetadata Metadata { get; set; }
+
+        public override string GetTitle() =>
+            NOTIFICATION_TITLE;
+    }
+
+    [Serializable]
+    public struct UserUnbannedFromSceneNotificationMetadata
+    {
+        [JsonProperty("placeTitle")]
+        public string PlaceTitle { get; set; }
+
+        [JsonProperty("userAddress")]
+        public string UserAddress { get; set; }
+    }
+}
