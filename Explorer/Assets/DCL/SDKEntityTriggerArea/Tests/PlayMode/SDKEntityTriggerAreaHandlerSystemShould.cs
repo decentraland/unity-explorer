cae0ba15dbe1d7df38d9586249070e5756e60e2e
Pravus <pravusjif@gmail.com>
2025-09-26 02:32:41 +0200

    corrected and added tests

--- Explorer/Assets/DCL/SDKEntityTriggerArea/Tests/PlayMode/SDKEntityTriggerAreaHandlerSystemShould.cs
+++ Explorer/Assets/DCL/SDKEntityTriggerArea/Tests/PlayMode/SDKEntityTriggerAreaHandlerSystemShould.cs
@@ -17,6 +17,8 @@ using SceneRunner.Scene;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
+using Utility.Multithreading;
+using CrdtEcsBridge.Physics;
 
 namespace DCL.SDKEntityTriggerArea.Tests
 {
@@ -59,6 +61,7 @@ namespace DCL.SDKEntityTriggerArea.Tests
             // Setup system
             sceneStateProvider = Substitute.For<ISceneStateProvider>();
             sceneStateProvider.IsCurrent.Returns(true);
+            sceneStateProvider.State.Returns(new Atomic<SceneState>(SceneState.Running));
 
             poolsRegistry = new ComponentPoolsRegistry();
             poolsRegistry.AddGameObjectPool(() => sdkEntityTriggerArea, onRelease: area => area.Dispose());
@@ -66,7 +69,7 @@ namespace DCL.SDKEntityTriggerArea.Tests
 
             system = new SDKEntityTriggerAreaHandlerSystem(world, sdkEntityTriggerAreaPool, mainPlayerAvatarBaseProxy, sceneStateProvider, characterObject);
 
-            UnityEngine.Physics.simulationMode = SimulationMode.Script;
+            Physics.simulationMode = SimulationMode.Script;
         }
 
         protected override void OnTearDown()
@@ -83,10 +86,9 @@ namespace DCL.SDKEntityTriggerArea.Tests
             // Workaround for Unity bug not awaiting async Setup correctly
             await UniTask.WaitUntil(() => system != null);
 
-            var pbComponent = new PBCameraModeArea();
             var component = new SDKEntityTriggerAreaComponent(areaSize: new Vector3(15, 6, 18));
 
-            world.Add(entity, component, pbComponent);
+            world.Add(entity, component);
 
             system.Update(0);
 
@@ -103,10 +105,9 @@ namespace DCL.SDKEntityTriggerArea.Tests
 
             Assert.AreNotEqual(sdkEntityTriggerArea.BoxCollider.size, targetAreaSize);
 
-            var pbComponent = new PBCameraModeArea();
             var component = new SDKEntityTriggerAreaComponent(areaSize: targetAreaSize);
 
-            world.Add(entity, component, pbComponent);
+            world.Add(entity, component);
 
             system.Update(0);
 
@@ -121,10 +122,9 @@ namespace DCL.SDKEntityTriggerArea.Tests
 
             var targetAreaSize = new Vector3(15, 6, 18);
 
-            var pbComponent = new PBCameraModeArea();
             var component = new SDKEntityTriggerAreaComponent(areaSize: targetAreaSize);
 
-            world.Add(entity, component, pbComponent);
+            world.Add(entity, component);
 
             system.Update(0);
 
@@ -148,10 +148,9 @@ namespace DCL.SDKEntityTriggerArea.Tests
             // Workaround for Unity bug not awaiting async Setup correctly
             await UniTask.WaitUntil(() => system != null);
 
-            var pbComponent = new PBCameraModeArea();
             var component = new SDKEntityTriggerAreaComponent(areaSize: Vector3.one);
 
-            world.Add(entity, component, pbComponent);
+            world.Add(entity, component);
 
             system.Update(0);
 
@@ -180,11 +179,9 @@ namespace DCL.SDKEntityTriggerArea.Tests
             // Workaround for Unity bug not awaiting async Setup correctly
             await UniTask.WaitUntil(() => system != null);
 
-            var pbComponent = new PBCameraModeArea();
-
             var component = new SDKEntityTriggerAreaComponent(areaSize: Vector3.one * 4);
 
-            world.Add(entity, component, pbComponent);
+            world.Add(entity, component);
 
             system.Update(0);
             component = world.Get<SDKEntityTriggerAreaComponent>(entity);
@@ -216,11 +213,9 @@ namespace DCL.SDKEntityTriggerArea.Tests
             // Workaround for Unity bug not awaiting async Setup correctly
             await UniTask.WaitUntil(() => system != null);
 
-            var pbComponent = new PBCameraModeArea();
-
             var component = new SDKEntityTriggerAreaComponent(areaSize: Vector3.one * 4);
 
-            world.Add(entity, component, pbComponent);
+            world.Add(entity, component);
 
             system.Update(0);
             component = world.Get<SDKEntityTriggerAreaComponent>(entity);
@@ -239,12 +234,21 @@ namespace DCL.SDKEntityTriggerArea.Tests
 
             // Simulate "getting outside current scene"
             sceneStateProvider.IsCurrent.Returns(false);
+            system.OnSceneIsCurrentChanged(false);
 
             system.Update(0);
 
             Assert.AreEqual(0, component.EnteredEntitiesToBeProcessed.Count);
             Assert.AreEqual(1, component.ExitedEntitiesToBeProcessed.Count);
             Assert.IsFalse(sdkEntityTriggerArea.BoxCollider.enabled);
+
+            // Simulate "returning to the scene"
+            sceneStateProvider.IsCurrent.Returns(true);
+            system.OnSceneIsCurrentChanged(true);
+
+            system.Update(0);
+
+            Assert.IsTrue(sdkEntityTriggerArea.BoxCollider.enabled);
         }
 
         [Test]
@@ -256,11 +260,9 @@ namespace DCL.SDKEntityTriggerArea.Tests
             // Move character inside area
             fakeMainPlayerGO.transform.position = entityTransformComponent.Transform.position;
 
-            var pbComponent = new PBCameraModeArea();
-
             var component = new SDKEntityTriggerAreaComponent(areaSize: Vector3.one * 4);
 
-            world.Add(entity, component, pbComponent);
+            world.Add(entity, component);
 
             Assert.AreEqual(0, component.EnteredEntitiesToBeProcessed.Count);
             Assert.AreEqual(0, component.ExitedEntitiesToBeProcessed.Count);
@@ -286,11 +288,9 @@ namespace DCL.SDKEntityTriggerArea.Tests
             mainPlayerAvatarBaseProxy = new ObjectProxy<AvatarBase>();
             system = new SDKEntityTriggerAreaHandlerSystem(world, sdkEntityTriggerAreaPool, mainPlayerAvatarBaseProxy, sceneStateProvider, characterObject);
 
-            var pbComponent = new PBCameraModeArea();
-
             var component = new SDKEntityTriggerAreaComponent(areaSize: Vector3.one * 4);
 
-            world.Add(entity, component, pbComponent);
+            world.Add(entity, component);
 
             Assert.AreEqual(0, component.EnteredEntitiesToBeProcessed.Count);
             Assert.AreEqual(0, component.ExitedEntitiesToBeProcessed.Count);
@@ -324,10 +324,9 @@ namespace DCL.SDKEntityTriggerArea.Tests
             fakeOtherPlayerGO = Object.Instantiate(fakeOtherPlayerGO.GetComponent<CharacterObject>()).gameObject;
             fakeOtherPlayerGO.transform.position = Vector3.zero;
 
-            var pbComponent = new PBCameraModeArea();
             var component = new SDKEntityTriggerAreaComponent(areaSize: Vector3.one * 4, targetOnlyMainPlayer: onlyMainPlayer);
 
-            world.Add(entity, component, pbComponent);
+            world.Add(entity, component);
 
             system.Update(0);
             component = world.Get<SDKEntityTriggerAreaComponent>(entity);
@@ -356,6 +355,107 @@ namespace DCL.SDKEntityTriggerArea.Tests
 
             // Cleanup
             Object.DestroyImmediate(fakeOtherPlayerGO);
+        }
+
+        [Test]
+        [TestCase(SDKEntityTriggerAreaMeshType.BOX)]
+        [TestCase(SDKEntityTriggerAreaMeshType.SPHERE)]
+        public async Task SetupColliderTypeCorrectly(SDKEntityTriggerAreaMeshType meshType)
+        {
+            // Workaround for Unity bug not awaiting async Setup correctly
+            await UniTask.WaitUntil(() => system != null);
+            var targetAreaSize = new Vector3(15, 6, 18);
+
+            var component = new SDKEntityTriggerAreaComponent(areaSize: targetAreaSize, meshType: meshType);
+
+            world.Add(entity, component);
+
+            system.Update(0);
+
+            if (meshType == SDKEntityTriggerAreaMeshType.BOX)
+            {
+                Assert.IsTrue(sdkEntityTriggerArea.BoxCollider.enabled);
+                Assert.IsFalse(sdkEntityTriggerArea.SphereCollider.enabled);
+                Assert.AreEqual(targetAreaSize, sdkEntityTriggerArea.BoxCollider.size);
+            }
+            else
+            {
+                Assert.IsFalse(sdkEntityTriggerArea.BoxCollider.enabled);
+                Assert.IsTrue(sdkEntityTriggerArea.SphereCollider.enabled);
+                var expectedRadius = targetAreaSize.magnitude / 2f;
+                Assert.AreEqual(expectedRadius, sdkEntityTriggerArea.SphereCollider.radius, 1e-4f);
+            }
+        }
+
+        [Test]
+        public async Task UsePlayerLayerByDefault()
+        {
+            // Workaround for Unity bug not awaiting async Setup correctly
+            await UniTask.WaitUntil(() => system != null);
+            var component = new SDKEntityTriggerAreaComponent(areaSize: Vector3.one * 4);
+            Assert.AreEqual(ColliderLayer.ClPlayer, component.LayerMask);
+        }
+
+        [Test]
+        public async Task UseAllAvatarsPhysicsLayerWhenOnlyTargetingPlayers()
+        {
+            // Workaround for Unity bug not awaiting async Setup correctly
+            await UniTask.WaitUntil(() => system != null);
+            var component = new SDKEntityTriggerAreaComponent(areaSize: Vector3.one * 4, layerMask: ColliderLayer.ClPlayer);
+
+            world.Add(entity, component);
+
+            system.Update(0);
+
+            Assert.AreEqual(PhysicsLayers.ALL_AVATARS, sdkEntityTriggerArea.gameObject.layer);
+
+            component = new SDKEntityTriggerAreaComponent(areaSize: Vector3.one * 4, layerMask: ColliderLayer.ClPlayer | ColliderLayer.ClCustom6);
+            world.Set(entity, component);
+
+            system.Update(0);
+
+            Assert.AreEqual(PhysicsLayers.SDK_ENTITY_TRIGGER_AREA, sdkEntityTriggerArea.gameObject.layer);
+        }
+
+        [Test]
+        public async Task DetectSDKEntitiesWithTargetSDKLayer()
+        {
+            // Workaround for Unity bug not awaiting async Setup correctly
+            await UniTask.WaitUntil(() => system != null);
+            var component = new SDKEntityTriggerAreaComponent(areaSize: Vector3.one * 4, layerMask: ColliderLayer.ClCustom1);
+
+            world.Add(entity, component);
+
+            system.Update(0);
+            component = world.Get<SDKEntityTriggerAreaComponent>(entity);
+
+            Assert.AreEqual(0, component.EnteredEntitiesToBeProcessed.Count);
+            Assert.AreEqual(0, component.ExitedEntitiesToBeProcessed.Count);
+
+            // Create an SDK entity collider on the SDK custom layer
+            var sdkEntityGO = new GameObject("FakeSDKEntity");
+            sdkEntityGO.layer = PhysicsLayers.SDK_CUSTOM_LAYER;
+            sdkEntityGO.AddComponent<BoxCollider>();
+
+            // Move SDK entity inside area
+            sdkEntityGO.transform.position = entityTransformComponent.Transform.position;
+            await WaitForPhysics();
+
+            component = world.Get<SDKEntityTriggerAreaComponent>(entity);
+            Assert.AreEqual(1, component.EnteredEntitiesToBeProcessed.Count);
+            Assert.AreEqual(0, component.ExitedEntitiesToBeProcessed.Count);
+            component.TryClear();
+
+            // Move SDK entity outside area
+            sdkEntityGO.transform.position = entityTransformComponent.Transform.position + (Vector3.one * 50);
+            await WaitForPhysics();
+
+            component = world.Get<SDKEntityTriggerAreaComponent>(entity);
+            Assert.AreEqual(0, component.EnteredEntitiesToBeProcessed.Count);
+            Assert.AreEqual(1, component.ExitedEntitiesToBeProcessed.Count);
+
+            // Cleanup
+            Object.DestroyImmediate(sdkEntityGO);
         }
 
         private static async Task WaitForPhysics()
