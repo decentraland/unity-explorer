713bb92f2a7daa531c1b24bddca6310307c5cc35
nickkhalow <nickhalow@gmail.com>
2025-10-02 14:22:03 +0300

    extract crdt message type length

--- Explorer/Assets/DCL/Infrastructure/CRDT/CRDTFilter.cs
+++ Explorer/Assets/DCL/Infrastructure/CRDT/CRDTFilter.cs
@@ -40,7 +40,7 @@ namespace CRDT
                 if (remainingBytesToRead > memory.Length)
                     break;
 
-                uint bodyLength = TypeLengthBytes(messageType, memory);
+                uint bodyLength = CRDTMessageTypeUtils.TypeLengthBytes(messageType, memory);
 
                 if (messageType is not CRDTMessageType.PUT_COMPONENT_NETWORK || ComponentIdOfPutNetworkComponentType(memory) != NO_SYNC_COMPONENT_ID)
                 {
@@ -56,24 +56,6 @@ namespace CRDT
                 memory = memory.Slice((int)bodyLength);
             }
         }
-
-        /// <summary>
-        /// Component header + variable (if appliable), input is after general header
-        /// </summary>
-        private static uint TypeLengthBytes(CRDTMessageType type, ReadOnlySpan<byte> memory) =>
-            type switch
-            {
-                CRDTMessageType.NONE => 0,
-                CRDTMessageType.PUT_COMPONENT => 16 + memory.Slice(12).ReadConst<uint>(), // 16 bytes - header, 12 bytes - dataLength offset
-                CRDTMessageType.DELETE_COMPONENT => 12, // 12 bytes - header
-                CRDTMessageType.DELETE_ENTITY => 4, // 4 bytes - header (entity)
-                CRDTMessageType.APPEND_COMPONENT => 16 + memory.Slice(12).ReadConst<uint>(), // 16 bytes - header, 12 bytes - dataLength offset
-                CRDTMessageType.PUT_COMPONENT_NETWORK => 20 + memory.Slice(16).ReadConst<uint>(), // 20 bytes - header, 16 bytes - dataLength offset
-                CRDTMessageType.DELETE_COMPONENT_NETWORK => 16, // header
-                CRDTMessageType.DELETE_ENTITY_NETWORK => 8, // header
-                CRDTMessageType.MAX_MESSAGE_TYPE => 0, // header
-                _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
-            };
 
         private static uint ComponentIdOfPutNetworkComponentType(ReadOnlySpan<byte> memory) =>
             memory.Slice(4).ReadConst<uint>(); // offset entityId
