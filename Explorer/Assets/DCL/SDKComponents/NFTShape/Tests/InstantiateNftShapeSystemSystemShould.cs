f163805f2a0c566738c5cb55f40ce931cf0e1e48
Nicolas Lorusso <nalorusso@gmail.com>
2025-10-10 14:05:37 -0300

    test coverage on InstantiateNftShapeSystemSystemShould

--- Explorer/Assets/DCL/SDKComponents/NFTShape/Tests/InstantiateNftShapeSystemSystemShould.cs
+++ Explorer/Assets/DCL/SDKComponents/NFTShape/Tests/InstantiateNftShapeSystemSystemShould.cs
@@ -1,4 +1,5 @@
 ﻿using Arch.Core;
+using CommunicationData.URLHelpers;
 using DCL.ECSComponents;
 using DCL.Optimization.PerformanceBudgeting;
 using DCL.SDKComponents.NFTShape.Component;
@@ -9,60 +10,68 @@ using DCL.SDKComponents.NFTShape.System;
 using ECS.Abstract;
 using ECS.Prioritization.Components;
 using ECS.StreamableLoading.NFTShapes;
-using ECS.StreamableLoading.NFTShapes.URNs;
 using ECS.TestSuite;
 using NSubstitute;
 using NUnit.Framework;
-// using Promise = ECS.StreamableLoading.Common.AssetPromise<ECS.StreamableLoading.Textures.Texture2DData, ECS.StreamableLoading.NFTShapes.GetNFTShapeIntention>;
+using NftTypePromise = ECS.StreamableLoading.Common.AssetPromise<ECS.StreamableLoading.NFTShapes.NftTypeResult, ECS.StreamableLoading.NFTShapes.GetNFTTypeIntention>;
+using NftImagePromise = ECS.StreamableLoading.Common.AssetPromise<ECS.StreamableLoading.Textures.Texture2DData, ECS.StreamableLoading.NFTShapes.GetNFTImageIntention>;
+using NftVideoPromise = ECS.StreamableLoading.Common.AssetPromise<ECS.StreamableLoading.Textures.Texture2DData, ECS.StreamableLoading.NFTShapes.GetNFTVideoIntention>;
 
 namespace DCL.SDKComponents.NFTShape.Tests
 {
     public class InstantiateNftShapeSystemSystemShould : UnitySystemTestBase<InstantiateNftShapeSystem>
     {
-        // private const string INITIAL_URN = "INITIAL_URN";
-        //
-        // private EntityEventBuffer<NftShapeRendererComponent>? changedNftShapes;
-        //
-        // [SetUp]
-        // public void Setup()
-        // {
-        //     system = new InstantiateNftShapeSystem(world,
-        //         Substitute.For<INFTShapeRendererFactory>(),
-        //         Substitute.For<IPerformanceBudget>(),
-        //         Substitute.For<IReadOnlyFramePrefabs>(),
-        //         changedNftShapes = new EntityEventBuffer<NftShapeRendererComponent>(1));
-        // }
-        //
-        // [Test]
-        // [TestCase("NEW_URN")]
-        // [TestCase(INITIAL_URN)]
-        // public void ReconfigureNftShape(string newURN)
-        // {
-        //     var shape = new PBNftShape { Urn = INITIAL_URN };
-        //     INftShapeRenderer? renderer = Substitute.For<INftShapeRenderer>();
-        //     var component = new NftShapeRendererComponent(renderer);
-        //     var loadingComponent = new NFTLoadingComponent(Promise.Create(world, new GetNFTShapeIntention(INITIAL_URN, Substitute.For<IURNSource>()), PartitionComponent.TOP_PRIORITY));
-        //
-        //     Entity entity = world.Create(shape,
-        //         component,
-        //         loadingComponent);
-        //
-        //     shape.Urn = newURN;
-        //     shape.IsDirty = true;
-        //
-        //     system.Update(0);
-        //
-        //     bool urnChanged = newURN != INITIAL_URN;
-        //
-        //     renderer.Received(1).Apply(shape, urnChanged);
-        //
-        //     if (urnChanged)
-        //     {
-        //         Assert.That(loadingComponent.TypePromise.LoadingIntention.CancellationTokenSource.IsCancellationRequested, Is.True);
-        //
-        //         CollectionAssert.AreEqual(new EntityRelation<NftShapeRendererComponent>[] { new (entity, component) }, changedNftShapes!.Relations);
-        //         Assert.That(world.Has<NFTLoadingComponent>(entity), Is.False);
-        //     }
-        // }
+        private const string INITIAL_URN = "INITIAL_URN";
+
+        private EntityEventBuffer<NftShapeRendererComponent>? changedNftShapes;
+
+        [SetUp]
+        public void Setup()
+        {
+            system = new InstantiateNftShapeSystem(world,
+                Substitute.For<INFTShapeRendererFactory>(),
+                Substitute.For<IPerformanceBudget>(),
+                Substitute.For<IReadOnlyFramePrefabs>(),
+                changedNftShapes = new EntityEventBuffer<NftShapeRendererComponent>(1));
+        }
+
+        [Test]
+        [TestCase("NEW_URN")]
+        [TestCase(INITIAL_URN)]
+        public void ReconfigureNftShape(string newURN)
+        {
+            var shape = new PBNftShape { Urn = INITIAL_URN };
+            INftShapeRenderer? renderer = Substitute.For<INftShapeRenderer>();
+            var component = new NftShapeRendererComponent(renderer);
+            var loadingComponent = new NFTLoadingComponent(INITIAL_URN,
+                NftTypePromise.Create(world, new GetNFTTypeIntention(URLAddress.FromString(INITIAL_URN)), PartitionComponent.TOP_PRIORITY))
+            {
+                VideoPromise = NftVideoPromise.Create(world, new GetNFTVideoIntention(URLAddress.FromString(INITIAL_URN)), PartitionComponent.TOP_PRIORITY),
+                ImagePromise = NftImagePromise.Create(world, new GetNFTImageIntention(URLAddress.FromString(INITIAL_URN)), PartitionComponent.TOP_PRIORITY),
+            };
+
+            Entity entity = world.Create(shape,
+                component,
+                loadingComponent);
+
+            shape.Urn = newURN;
+            shape.IsDirty = true;
+
+            system!.Update(0);
+
+            bool urnChanged = newURN != INITIAL_URN;
+
+            renderer.Received(1).Apply(shape, urnChanged);
+
+            if (urnChanged)
+            {
+                Assert.That(loadingComponent.TypePromise.LoadingIntention.CancellationTokenSource.IsCancellationRequested, Is.True);
+                Assert.That(loadingComponent.VideoPromise.Value.LoadingIntention.CancellationTokenSource.IsCancellationRequested, Is.True);
+                Assert.That(loadingComponent.ImagePromise.Value.LoadingIntention.CancellationTokenSource.IsCancellationRequested, Is.True);
+
+                CollectionAssert.AreEqual(new EntityRelation<NftShapeRendererComponent>[] { new (entity, component) }, changedNftShapes!.Relations);
+                Assert.That(world.Has<NFTLoadingComponent>(entity), Is.False);
+            }
+        }
     }
 }
