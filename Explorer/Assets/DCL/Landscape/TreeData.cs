143ac28c0bc842c4652bbc0e23e7c8f3b16a75be
Ansis Māliņš <ansis.malins@decentraland.org>
2025-10-13 18:03:15 +0200

    Log tree placement for parcel -114,-36

--- Explorer/Assets/DCL/Landscape/TreeData.cs
+++ Explorer/Assets/DCL/Landscape/TreeData.cs
@@ -75,7 +75,14 @@ namespace DCL.Landscape
                              .LerpScale(float2(instance.ScaleXZ, instance.ScaleY) * (1f / 255f))
                              .xyx;
 
-            if (OverlapsOccupiedParcel(float2(position.x, position.z), prototype.radius * scale.x))
+            bool overlaps = OverlapsOccupiedParcel(float2(position.x, position.z),
+                prototype.radius * scale.x);
+
+            if (all(parcel == int2(-114, -36)))
+                ReportHub.Log(ReportCategory.ALWAYS,
+                    $"x: {position.x}, z: {position.z}, prototype: {prototype.name}, radius: {prototype.radius}, scale: {scale.x}, overlaps: {overlaps}");
+
+            if (overlaps)
             {
                 position.y = 0f;
                 rotation = default(Quaternion);
