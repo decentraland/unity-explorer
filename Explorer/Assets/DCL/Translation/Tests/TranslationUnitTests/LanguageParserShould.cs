2adf7790ae8374456fb7d396b6757eee060fdcc8
Mirko Jugurdzija <mirko.jugurdzija@gmail.com>
2025-10-15 21:39:33 +0200

    fix: change how we parse detected languages, especially Chinese (for example, this couldn't be detected zh-Hans as Chinese and defaulted to English)

---
+++ Explorer/Assets/DCL/Translation/Tests/TranslationUnitTests/LanguageParserShould.cs
@@ -0,0 +1,60 @@
+﻿using DCL.Utilities;
+using NUnit.Framework;
+
+namespace DCL.Translation
+{
+    [TestFixture]
+    public class LanguageCodeParserShould
+    {
+        [TestCase("zh-Hans", LanguageCode.ZH)]
+        [TestCase("ZH-Hant", LanguageCode.ZH)]
+        [TestCase("zh-CN",   LanguageCode.ZH)]
+        [TestCase("zh_TW",   LanguageCode.ZH)]
+        [TestCase("pt-BR",   LanguageCode.PT)]
+        [TestCase("PT-pt",   LanguageCode.PT)]
+        [TestCase("es-419",  LanguageCode.ES)]
+        [TestCase("en-US",   LanguageCode.EN)]
+        [TestCase("EN",      LanguageCode.EN)]
+        [TestCase("ru",      LanguageCode.RU)]
+        [TestCase("Ko",      LanguageCode.KO)]
+        public void MapCommonBcp47VariantsToSupportedLanguage(string input, LanguageCode expected)
+        {
+            var result = LanguageCodeParser.Parse(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("   ")]
+        [TestCase("xx-YY")]
+        [TestCase("cmn-Hans-CN")] // Mandarin variant
+        public void FallbackToEnglishOnUnknownOrEmpty(string input)
+        {
+            var result = LanguageCodeParser.Parse(input);
+            Assert.AreEqual(LanguageCode.EN, result);
+        }
+
+        [Test]
+        public void UsePrimarySubtagWhenNoDirectMapExists()
+        {
+            Assert.AreEqual(LanguageCode.FR, LanguageCodeParser.Parse("fr-CA"));
+            Assert.AreEqual(LanguageCode.DE, LanguageCodeParser.Parse("de-Latn-DE"));
+            Assert.AreEqual(LanguageCode.IT, LanguageCodeParser.Parse("it_IT"));
+        }
+
+        [Test]
+        public void BeCaseAndSeparatorInsensitive()
+        {
+            Assert.AreEqual(LanguageCode.ZH, LanguageCodeParser.Parse("Zh_hAnS"));
+            Assert.AreEqual(LanguageCode.EN, LanguageCodeParser.Parse("en-us"));
+            Assert.AreEqual(LanguageCode.PT, LanguageCodeParser.Parse("PT_br"));
+        }
+
+        [Test]
+        public void IgnoreExtensionsAndPrivateUseSubtags()
+        {
+            Assert.AreEqual(LanguageCode.EN, LanguageCodeParser.Parse("en-US-x-custom"));
+            Assert.AreEqual(LanguageCode.ZH, LanguageCodeParser.Parse("zh-Hans-CN-u-ca-chinese"));
+        }
+    }
+}
