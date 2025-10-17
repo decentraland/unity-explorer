072497536ce7701b12ab649f222119b6acc6996e
Daniele Marotta <daniele.marotta@decentraland.org>
2025-10-17 15:40:37 +0200

    fixed jitter on moving platforms

---
+++ Explorer/Assets/DCL/Character/CharacterMotion/Systems/CharacterPlatformUpdateSceneTickSystem.cs
@@ -0,0 +1,36 @@
+﻿using Arch.Core;
+using Arch.System;
+using Arch.SystemGroups;
+using Arch.SystemGroups.DefaultSystemGroups;
+using DCL.CharacterMotion.Components;
+using ECS.Abstract;
+using ECS.SceneLifeCycle;
+using SceneRunner.Scene;
+
+namespace DCL.CharacterMotion.Systems
+{
+    [UpdateInGroup(typeof(PresentationSystemGroup))]
+    [UpdateAfter(typeof(ChangeCharacterPositionGroup))]
+    [UpdateAfter(typeof(RotateCharacterSystem))]
+    public partial class CharacterPlatformUpdateSceneTickSystem : BaseUnityLoopSystem
+    {
+        private readonly IScenesCache scenesCache;
+
+        public CharacterPlatformUpdateSceneTickSystem(World world, IScenesCache scenesCache) : base(world)
+        {
+            this.scenesCache = scenesCache;
+        }
+
+        protected override void Update(float t)
+        {
+            UpdateTickQuery(World);
+        }
+
+        [Query]
+        private void UpdateTick(ref CharacterPlatformComponent platformComponent)
+        {
+            ISceneFacade? currentScene = scenesCache.CurrentScene.Value;
+            platformComponent.LastUpdateTick = currentScene?.SceneStateProvider.TickNumber ?? 0;
+        }
+    }
+}
