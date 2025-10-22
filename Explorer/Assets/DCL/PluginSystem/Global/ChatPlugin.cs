232b2d0dab299041a7ce92ad12948ddff761ef93
lorenzo-ranciaffi <lorenzo.ranciaffi@decentraland.org>
2025-10-22 11:00:27 +0200

    fix: safer chat disposal

--- Explorer/Assets/DCL/PluginSystem/Global/ChatPlugin.cs
+++ Explorer/Assets/DCL/PluginSystem/Global/ChatPlugin.cs
@@ -180,8 +180,7 @@ namespace DCL.PluginSystem.Global
         {
             chatStorage?.Dispose();
             pluginScope.Dispose();
-            pluginCts.Cancel();
-            pluginCts.Dispose();
+            pluginCts.SafeCancelAndDispose();
             fallbackFontsProvider.Dispose();
         }
 
