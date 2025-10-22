c557e599674052a63dbfa4451398a8501d9af804
Daniele Marotta <daniele.marotta@decentraland.org>
2025-10-22 10:00:44 +0200

    added docs to new component

--- Explorer/Assets/DCL/AvatarRendering/Emotes/Components/CharacterWaitingSceneEmoteLoading.cs
+++ Explorer/Assets/DCL/AvatarRendering/Emotes/Components/CharacterWaitingSceneEmoteLoading.cs
@@ -1,5 +1,13 @@
 ﻿namespace DCL.AvatarRendering.Emotes
 {
+    /// <summary>
+    /// Added to an entity that wants to play a <b>SCENE</b> emote for the time the emote is still being loaded.
+    /// <p/>
+    /// The component purpose is to let other systems know that the scene is trying to play an emote, but that loading
+    /// of the emote isn't completed yet.
+    /// </summary>
+    /// <seealso cref="GetSceneEmoteFromLocalSceneIntention"/>
+    /// <seealso cref="GetSceneEmoteFromRealmIntention"/>
     public struct CharacterWaitingSceneEmoteLoading
     {
     }
