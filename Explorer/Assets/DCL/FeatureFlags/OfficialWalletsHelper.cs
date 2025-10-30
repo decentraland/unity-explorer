8dd811a4d0cd6b9fe03c0f34c0fb6234406d30d0
Miha Krajnc <miha.krajnc@decentraland.org>
2025-10-30 13:37:40 +0100

    fix: revert renderer change

--- Explorer/Assets/DCL/FeatureFlags/OfficialWalletsHelper.cs
+++ Explorer/Assets/DCL/FeatureFlags/OfficialWalletsHelper.cs
@@ -13,8 +13,6 @@ namespace DCL.FeatureFlags
             if (FeatureFlagsConfiguration.Instance.TryGetCsvPayload(FeatureFlagsStrings.OFFICIAL_WALLETS, FeatureFlagsStrings.WALLETS_VARIANT, out var csv) && csv is { Count: >= 1 })
                 foreach (string wallet in csv[0])
                     officialWallets.Add(wallet.ToLowerInvariant());
-
-            officialWallets.Add("0x3f574d05ec670fe2c92305480b175654ca512005".ToLowerInvariant());
         }
 
         public bool IsOfficialWallet(string wallet) =>
