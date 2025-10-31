7c782bf61655307f2ae397729a98649f2d65566d
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2025-10-31 12:41:08 -0300

    Removed secondary checkl

--- Explorer/Assets/DCL/LOD/Systems/UnloadSceneLODSystem.cs
+++ Explorer/Assets/DCL/LOD/Systems/UnloadSceneLODSystem.cs
@@ -65,7 +65,7 @@ namespace ECS.SceneLifeCycle.Systems
 
                 //TODO(Juani) : This `if` is required for retro-compatibility with non Initial Scene State scenes.
                 //If all scenes were built with SAB scenes, we could remove it
-                if (initialSceneStateDescriptor.HasProperABManifestVersion && !initialSceneStateDescriptor.AssetBundleFailed())
+                if (initialSceneStateDescriptor.HasProperABManifestVersion)
                 {
                     if (sceneLODInfo.IsInitialized())
                         sceneLODInfo.metadata.SuccessfullLODs = SceneLODInfoUtils.ClearLODResult(sceneLODInfo.metadata.SuccessfullLODs, 0);
