2cee0cbafaca71b231fa10d47bd4e1e3fbeeb2b1
Santi Andrade <sandrade@decentraland.org>
2025-11-18 00:56:27 +0100

    Update NotificationJsonDtoConverter.cs

--- Explorer/Assets/DCL/Notifications/Serialization/NotificationJsonDtoConverter.cs
+++ Explorer/Assets/DCL/Notifications/Serialization/NotificationJsonDtoConverter.cs
@@ -36,7 +36,7 @@ namespace DCL.Notifications.Serialization
         private const string COMMUNITY_REQUEST_TO_JOIN_ACCEPTED_TYPE = "community_request_to_join_accepted";
         private const string COMMUNITY_DELETED_CONTENT_VIOLATION_TYPE = "community_deleted_content_violation";
         private const string COMMUNITY_POST_ADDED_TYPE = "community_post_added";
-        private const string COMMUNITY_OWNERSHIP_TRANSFERRED_TYPE = "community_ownership_transferred_added";
+        private const string COMMUNITY_OWNERSHIP_TRANSFERRED_TYPE = "community_ownership_transferred";
         private const string USER_BANNED_FROM_SCENE_TYPE = "user_banned_from_scene";
         private const string USER_UNBANNED_FROM_SCENE_TYPE = "user_unbanned_from_scene";
 
