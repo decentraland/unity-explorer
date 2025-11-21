136521aee75a489c7d1bfd2808912f8fe0e58770
Santi Andrade <sandrade@decentraland.org>
2025-11-21 16:24:16 +0100

    Auto hide non-interactable tooltip after 5 seconds

--- Explorer/Assets/DCL/UI/GenericContextMenu/Controls/ControlsContainerView.cs
+++ Explorer/Assets/DCL/UI/GenericContextMenu/Controls/ControlsContainerView.cs
@@ -1,11 +1,17 @@
+using Cysharp.Threading.Tasks;
+using System.Threading;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using Utility;
 
 namespace DCL.UI.Controls
 {
     public class ControlsContainerView : MonoBehaviour
     {
+        private const float NON_INTERACTABLE_TOOLTIP_ANCHORED_Y_POSITION = 7f;
+        private const int NON_INTERACTABLE_TOOLTIP_AUTO_HIDE_DELAY_MS = 5000;
+
         [SerializeField] internal RectTransform controlsContainer;
         [SerializeField] internal VerticalLayoutGroup controlsLayoutGroup;
         [SerializeField] internal GameObject containerRim;
@@ -13,18 +19,33 @@ namespace DCL.UI.Controls
         [SerializeField] private RectTransform nonInteractableTooltipContainer;
         [SerializeField] private TMP_Text nonInteractableTooltipText;
 
+        private CancellationTokenSource cts;
+
         public void SetLoadingAnimationVisibility(bool isVisible) =>
             loadingAnimation.gameObject.SetActive(isVisible);
 
         public void ShowNonInteractableTooltip(Transform parent, string text)
         {
-            nonInteractableTooltipContainer.gameObject.SetActive(true);
-            nonInteractableTooltipContainer.SetParent(parent);
-            nonInteractableTooltipContainer.anchoredPosition = new Vector2(nonInteractableTooltipContainer.anchoredPosition.x, 7f);
-            nonInteractableTooltipText.text = text;
+            cts = cts.SafeRestart();
+            ShowNonInteractableTooltipAsync(cts.Token).Forget();
+            return;
+
+            async UniTaskVoid ShowNonInteractableTooltipAsync(CancellationToken ct)
+            {
+                nonInteractableTooltipContainer.gameObject.SetActive(true);
+                nonInteractableTooltipContainer.SetParent(parent);
+                nonInteractableTooltipContainer.anchoredPosition = new Vector2(nonInteractableTooltipContainer.anchoredPosition.x, NON_INTERACTABLE_TOOLTIP_ANCHORED_Y_POSITION);
+                nonInteractableTooltipText.text = text;
+
+                await UniTask.Delay(NON_INTERACTABLE_TOOLTIP_AUTO_HIDE_DELAY_MS, cancellationToken: ct);
+                HideNonInteractableTooltip();
+            }
         }
 
-        public void HideNonInteractableTooltip() =>
+        public void HideNonInteractableTooltip()
+        {
+            cts.SafeCancelAndDispose();
             nonInteractableTooltipContainer.gameObject.SetActive(false);
+        }
     }
 }
