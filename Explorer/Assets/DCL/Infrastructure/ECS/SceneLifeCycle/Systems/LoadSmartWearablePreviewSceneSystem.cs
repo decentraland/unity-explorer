ff917fa75c59e3a14505a17f7e0cf0752317bc1c
Daniele Marotta <daniele.marotta@decentraland.org>
2025-12-02 11:36:16 +0100

    fixed landscape loading when previewing smart wearables

--- Explorer/Assets/DCL/Infrastructure/ECS/SceneLifeCycle/Systems/LoadSmartWearablePreviewSceneSystem.cs
+++ Explorer/Assets/DCL/Infrastructure/ECS/SceneLifeCycle/Systems/LoadSmartWearablePreviewSceneSystem.cs
@@ -124,18 +124,6 @@ namespace ECS.SceneLifeCycle.Systems
         }
 
         /// <summary>
-        ///     Attached to the realm entity.
-        ///     Signals to the system that scene loading has started and stores a reference to the loaded scene entity.
-        /// </summary>
-        private struct SmartWearablePreviewScene
-        {
-            /// <summary>
-            ///     The scene entity that was loaded.
-            /// </summary>
-            public Entity Value;
-        }
-
-        /// <summary>
         ///     Used to parse the json response from the /preview-wearables endpoint.
         /// </summary>
         [Serializable]
