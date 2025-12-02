13e3a26ed7e6ea93459cb77f879144befe83d534
Geoff Birch <geoff.birch@decentraland.org>
2025-12-02 10:34:57 +0000

    return of the toggle

--- Explorer/Assets/DCL/PluginSystem/Global/RenderingSystemPlugin.cs
+++ Explorer/Assets/DCL/PluginSystem/Global/RenderingSystemPlugin.cs
@@ -1,11 +1,8 @@
 ﻿using Arch.SystemGroups;
 using Cysharp.Threading.Tasks;
 using DCL.DebugUtilities;
-using DCL.DebugUtilities.UIBindings;
+using DCL.Rendering.RenderSystem;
 using System.Threading;
-using UnityEngine;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
 
 namespace DCL.PluginSystem.Global
 {
@@ -16,33 +13,10 @@ namespace DCL.PluginSystem.Global
         public RenderingSystemPlugin(IDebugContainerBuilder debugContainerBuilder)
         {
             this.debugContainerBuilder = debugContainerBuilder;
-            //
         }
 
         public UniTask Initialize(IPluginSettingsContainer container, CancellationToken ct)
         {
-            UniversalRenderPipelineAsset urpAsset = GraphicsSettings.currentRenderPipeline as UniversalRenderPipelineAsset;
-
-            var version = new ElementBinding<string>(urpAsset.gpuResidentDrawerMode.ToString());
-            debugContainerBuilder.TryAddWidget(IDebugContainerBuilder.Categories.RENDERING)
-                                 .AddCustomMarker("GRD State:", version)
-                                 .AddControl(
-                                      new DebugConstLabelDef("Enabled GPU Resident Drawer"),
-                                      new DebugButtonDef("GRD", EnableResidentDrawer)
-                                  );
-
-            void EnableResidentDrawer()
-            {
-                if (urpAsset.gpuResidentDrawerMode == GPUResidentDrawerMode.Disabled)
-                {
-                    urpAsset.gpuResidentDrawerMode = GPUResidentDrawerMode.InstancedDrawing;
-                }
-                else if (urpAsset.gpuResidentDrawerMode == GPUResidentDrawerMode.InstancedDrawing)
-                {
-                    urpAsset.gpuResidentDrawerMode = GPUResidentDrawerMode.Disabled;
-                }
-            }
-
             return UniTask.CompletedTask;
         }
 
@@ -53,17 +27,7 @@ namespace DCL.PluginSystem.Global
 
         public void InjectToWorld(ref ArchSystemsWorldBuilder<Arch.Core.World> builder, in GlobalPluginArguments arguments)
         {
-
-
-
-            //RenderingSystem.InjectToWorld();
-
-            // UpdateProfilerSystem.InjectToWorld(ref builder, profiler, scenesCache);
-            //
-            // DebugViewProfilingSystem.InjectToWorld(ref builder, realmData, profiler, memoryBudget,
-            //     debugContainerBuilder, dclVersion, adaptivePhysicsSettings, sceneLoadingLimit);
+            DebugRenderSystem.InjectToWorld(ref builder, debugContainerBuilder);
         }
-
-
     }
 }
---
+++ Explorer/Assets/DCL/Rendering/RenderSystem/RenderDebugSystem.cs
@@ -0,0 +1,60 @@
+﻿using Arch.Core;
+using Arch.SystemGroups;
+using Arch.SystemGroups.DefaultSystemGroups;
+using DCL.DebugUtilities;
+using DCL.DebugUtilities.UIBindings;
+using ECS.Abstract;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace DCL.Rendering.RenderSystem
+{
+    [UpdateInGroup(typeof(InitializationSystemGroup))]
+    public partial class DebugRenderSystem : BaseUnityLoopSystem
+    {
+        private ElementBinding<bool> bEnableResidentDrawer;
+        private ElementBinding<bool> bEnableGPUOcclusion;
+
+        private DebugRenderSystem(World world, IDebugContainerBuilder debugBuilder)
+            : base(world)
+        {
+            bEnableResidentDrawer = new ElementBinding<bool>(false);
+            bEnableGPUOcclusion = new ElementBinding<bool>(false);
+
+            debugBuilder.TryAddWidget(IDebugContainerBuilder.Categories.RENDERING)
+                        .AddControl(
+                             new DebugConstLabelDef("Enabled GPU Resident Drawer"),
+                             new DebugToggleDef(bEnableResidentDrawer)
+                         )
+                        .AddControl(
+                             new DebugConstLabelDef("Enabled GPU Occlusion - Requires GRD"),
+                             new DebugToggleDef(bEnableGPUOcclusion)
+                         );
+        }
+
+        protected override void Update(float t)
+        {
+            if (bEnableResidentDrawer != null && bEnableGPUOcclusion != null)
+            {
+                bool bGRDEnabled = EnableResidentDrawer(bEnableResidentDrawer.Value);
+                EnableGPUOcclusion(bGRDEnabled, bEnableGPUOcclusion.Value);
+            }
+        }
+
+        private bool EnableResidentDrawer(bool _bEnableResidentDrawer)
+        {
+            UniversalRenderPipelineAsset urpAsset = GraphicsSettings.currentRenderPipeline as UniversalRenderPipelineAsset;
+            urpAsset.gpuResidentDrawerMode = _bEnableResidentDrawer ? GPUResidentDrawerMode.InstancedDrawing : GPUResidentDrawerMode.Disabled;
+            return urpAsset.gpuResidentDrawerMode == GPUResidentDrawerMode.InstancedDrawing;
+        }
+
+        private void EnableGPUOcclusion(bool _bGRDEnabled, bool _bEnableGPUOcclusion)
+        {
+            if (_bGRDEnabled)
+            {
+                UniversalRenderPipelineAsset urpAsset = GraphicsSettings.currentRenderPipeline as UniversalRenderPipelineAsset;
+                urpAsset.gpuResidentDrawerEnableOcclusionCullingInCameras = _bEnableGPUOcclusion;
+            }
+        }
+    }
+}
