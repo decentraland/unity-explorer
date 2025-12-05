9bf7557656b8d97a4dd317e8c12e59cc24d78dd8
Alejandro Villalba <lince3d@gmail.com>
2025-12-05 21:05:57 +0000

    Fixed: The animation transition from start to outcome looked bad
    The flag was not being set so the proper transition in the Animation controller (the one without interpolation) was not being triggered.

--- Explorer/Assets/DCL/AvatarRendering/Emotes/Systems/Play/EmotePlayer.cs
+++ Explorer/Assets/DCL/AvatarRendering/Emotes/Systems/Play/EmotePlayer.cs
@@ -300,7 +300,7 @@ namespace DCL.AvatarRendering.Emotes.Play
             view.SetAnimatorTrigger(view.IsAnimatorInTag(AnimationHashes.EMOTE) || view.IsAnimatorInTag(AnimationHashes.EMOTE_LOOP) ? AnimationHashes.EMOTE_RESET : AnimationHashes.EMOTE);
             view.SetAnimatorBool(AnimationHashes.EMOTE_LOOP, emoteComponent.EmoteLoop);
             // This flag makes the animator choose a different transition to Emote, which does not have an interpolation between animations
-            view.SetAnimatorBool(AnimationHashes.IS_SOCIAL_EMOTE_OUTCOME, emoteComponent.SocialEmote.IsReacting);
+            view.SetAnimatorBool(AnimationHashes.IS_SOCIAL_EMOTE_OUTCOME, emoteComponent.SocialEmote.IsPlayingOutcome);
 
             // Prop
             AnimationClip? propClip = null;
