a64c96105763499426553629bc7c39497bf4fa72
NickKhalow <nickhalow@gmail.com>
2025-12-05 12:56:22 +0200

    performance marker

--- Explorer/Assets/Plugins/NativeAudioAnalysis/NativeMethods.cs
+++ Explorer/Assets/Plugins/NativeAudioAnalysis/NativeMethods.cs
@@ -1,3 +1,4 @@
+using Unity.Profiling;
 using System;
 using System.Runtime.InteropServices;
 
@@ -33,6 +34,9 @@ namespace Plugins.NativeAudioAnalysis
 
     public static class NativeMethods
     {
+        public static readonly ProfilerMarker AnalyzeMarker = 
+            new ProfilerMarker("AudioAnalysis.AnalyzeBuffer");
+
         public const int BANDS = 8;
         public const float DEFAULT_AMPLITUDE_GAIN = 5f;
         public const float DEFAULT_BANDS_GAIN = 0.05f;
@@ -44,39 +48,40 @@ namespace Plugins.NativeAudioAnalysis
 
         [DllImport(LIBRARY_NAME, CallingConvention = CallingConvention.Cdecl)]
         internal extern static unsafe AudioAnalysis audio_analysis_analyze_audio_buffer(
-            float* buffer,
-            UIntPtr len,
-            float sample_rate,
+                float* buffer,
+                UIntPtr len,
+                float sample_rate,
 
-            AnalysisResultMode mode,
+                AnalysisResultMode mode,
 
-            // gain values are used only in the logarithmic mode
-            float amplitude_gain,
-            float bands_gain
+                // gain values are used only in the logarithmic mode
+                float amplitude_gain,
+                float bands_gain
 
-            // float onset_threshold = DEFAULT_ONSET_THRESHOLD
-        );
+                // float onset_threshold = DEFAULT_ONSET_THRESHOLD
+                );
 
         public static AudioAnalysis AnalyzeAudioBuffer(
-            float[] data, 
-            float sampleRate,
-            AnalysisResultMode mode = AnalysisResultMode.Logarithmic,
-            float amplitudeGain = DEFAULT_AMPLITUDE_GAIN,
-            float bandsGain = DEFAULT_BANDS_GAIN
-        ) 
+                ReadOnlySpan<float> data, 
+                float sampleRate,
+                AnalysisResultMode mode = AnalysisResultMode.Logarithmic,
+                float amplitudeGain = DEFAULT_AMPLITUDE_GAIN,
+                float bandsGain = DEFAULT_BANDS_GAIN
+                ) 
         {
+            using var _ = AnalyzeMarker.Auto();
             unsafe 
             {
                 fixed (float* ptr = data)
                 {
                     return NativeMethods.audio_analysis_analyze_audio_buffer(
-                        ptr, 
-                        (UIntPtr) data.Length, 
-                        sampleRate,
-                        mode,
-                        amplitudeGain,
-                        bandsGain
-                    );
+                            ptr, 
+                            (UIntPtr) data.Length, 
+                            sampleRate,
+                            mode,
+                            amplitudeGain,
+                            bandsGain
+                            );
                 }
             }
         }
