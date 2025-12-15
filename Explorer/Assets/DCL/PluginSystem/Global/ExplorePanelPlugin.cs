3aecac59740ad052274ae069e2336e1f368eef08
Santi Andrade <sandrade@decentraland.org>
2025-12-15 17:06:39 +0100

    feat: [Communities++] Communities deep link (#6392)
    * first commit
    
    * Manage the click on the deep link notification
    
    * Receive cimmunity deep link from app args
    
    * Renamings
    
    * Update DecentralandUrlsSource.cs
    
    * Apply feedback: Rename GetCommunityIdFromAppArgs() to TryGetCommunityIdFromAppArgs()
    
    * Apply feedback: Simplify code in TryGetCommunityIdFromAppArgs()
    
    * Apply feedback: <summary> added for COMMUNITY app arg flag
    
    * Apply feedback: Fix HandleDeepLink
    
    * Update CommunitiesBrowserController.cs

--- Explorer/Assets/DCL/PluginSystem/Global/ExplorePanelPlugin.cs
+++ Explorer/Assets/DCL/PluginSystem/Global/ExplorePanelPlugin.cs
@@ -41,6 +41,7 @@ using System.Threading;
 using DCL.Backpack.AvatarSection.Outfits.Repository;
 using DCL.Chat.MessageBus;
 using DCL.Clipboard;
+using DCL.Communities;
 using DCL.Communities.CommunitiesBrowser;
 using DCL.Communities.CommunitiesDataProvider;
 using DCL.EventsApi;
@@ -55,6 +56,7 @@ using DCL.MapRenderer.MapLayers.HomeMarker;
 using DCL.Multiplayer.Connections.DecentralandUrls;
 using DCL.Optimization.PerformanceBudgeting;
 using DCL.Passport;
+using DCL.RealmNavigation;
 using DCL.UI.Profiles.Helpers;
 using DCL.SDKComponents.MediaStream.Settings;
 using DCL.Settings.Settings;
@@ -154,6 +156,8 @@ namespace DCL.PluginSystem.Global
         private readonly IVoiceChatOrchestrator communityCallOrchestrator;
         private readonly IPassportBridge passportBridge;
         private readonly SmartWearableCache smartWearableCache;
+        private readonly CommunityDataService communityDataService;
+        private readonly ILoadingStatus loadingStatus;
 
         public ExplorePanelPlugin(IEventBus eventBus,
             FeatureFlagsConfiguration featureFlags,
@@ -214,7 +218,9 @@ namespace DCL.PluginSystem.Global
             IPassportBridge passportBridge,
             IChatEventBus chatEventBus,
             HomePlaceEventBus homePlaceEventBus,
-            SmartWearableCache smartWearableCache)
+            SmartWearableCache smartWearableCache,
+            CommunityDataService communityDataService,
+            ILoadingStatus loadingStatus)
         {
             this.eventBus = eventBus;
             this.featureFlags = featureFlags;
@@ -276,6 +282,8 @@ namespace DCL.PluginSystem.Global
             this.homePlaceEventBus = homePlaceEventBus;
             this.passportBridge = passportBridge;
             this.smartWearableCache = smartWearableCache;
+            this.communityDataService = communityDataService;
+            this.loadingStatus = loadingStatus;
         }
 
         public void Dispose()
@@ -467,7 +475,9 @@ namespace DCL.PluginSystem.Global
                 nftNamesProvider,
                 communityCallOrchestrator,
                 sharedSpaceManager,
-                chatEventBus);
+                chatEventBus,
+                communityDataService,
+                loadingStatus);
 
             ExplorePanelController explorePanelController = new
                 ExplorePanelController(viewFactoryMethod, navmapController, settingsController, backpackSubPlugin.backpackController!, cameraReelController,
