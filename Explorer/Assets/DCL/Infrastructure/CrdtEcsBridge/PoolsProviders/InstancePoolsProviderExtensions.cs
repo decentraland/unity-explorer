cd06aaacad664ffd11e4126548c8139561b063f1
NickKhalow <nickhalow@gmail.com>
2025-12-15 21:35:44 +0200

    remove obsolete method

--- Explorer/Assets/DCL/Infrastructure/CrdtEcsBridge/PoolsProviders/InstancePoolsProviderExtensions.cs
+++ Explorer/Assets/DCL/Infrastructure/CrdtEcsBridge/PoolsProviders/InstancePoolsProviderExtensions.cs
@@ -4,34 +4,6 @@ namespace CrdtEcsBridge.PoolsProviders
 {
     public static class InstancePoolsProviderExtensions
     {
-        public static void RenewCrdtRawDataPoolFromScriptArray(
-            this IInstancePoolsProvider instancePoolsProvider, ITypedArray<byte> scriptArray,
-            ref PoolableByteArray lastInput)
-        {
-            EnsureArrayLength(instancePoolsProvider, (int)scriptArray.Length, ref lastInput);
-
-            // V8ScriptItem does not support zero length
-            if (scriptArray.Length > 0)
-                scriptArray.Read(0, scriptArray.Length, lastInput.Array, 0);
-        }
-
-        private static void EnsureArrayLength(IInstancePoolsProvider instancePoolsProvider,
-            int scriptArrayLength, ref PoolableByteArray lastInput)
-        {
-            // if the rented array can't keep the desired data, replace it
-            if (lastInput.Array.Length < scriptArrayLength)
-            {
-                // Release the old one
-                lastInput.Dispose();
-
-                // Rent a new one
-                lastInput = instancePoolsProvider.GetAPIRawDataPool(scriptArrayLength);
-            }
-            // Otherwise set the desired length to the existing array so it provides a correct span
-            else
-                lastInput.SetLength(scriptArrayLength);
-        }
-
         /// <summary>
         /// Releases the old array if it can't fit the new size anymore, and rents a new one.
         /// Copies data from the old array to the beginning of the new one
