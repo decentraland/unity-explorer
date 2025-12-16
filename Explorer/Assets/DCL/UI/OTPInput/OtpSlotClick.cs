271e355717848f7d66d3944f2c3e902566346a99
Vitaly Popuzin <popuzin@gmail.com>
2025-12-16 20:17:25 +0100

    OTP input field prototyping

---
+++ Explorer/Assets/DCL/UI/OTPInput/OtpSlotClick.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public sealed class OtpSlotClick : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private OtpCodeInput otp;
+    [SerializeField] private OtpCodeInput2 otp2;
+    [SerializeField] private int index;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (otp) otp.FocusSlot(index);
+        if (otp2) otp2.FocusSlot(index);
+    }
+}
