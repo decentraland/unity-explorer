9a30464ffcc52a6288d1fe15bdd493d3fb6f3d8a
Geoff Birch <137780197+GBirch33@users.noreply.github.com>
2025-12-17 19:41:08 +0000

    feat: Shaders Update for GPU Resident Drawer feature (#6311)
    Adds GPU Resident Drawer to the shaders, and a toggle to turn it on/off in the debug panel (off by default)
    
    
    So there's 2 tests:
    
    1) nothing changes visually when switching GRD on/off
    2) perf check differences - we're not overly bothered right now as the materials and mesh setup doesn't get the most out of GRD, so we just want to know roughly what the difference is, and you can enabled Occlusion too, see if that brings any performance value.

---
+++ Explorer/Assets/DCL/PluginSystem/Global/RenderingSystemPlugin.cs
@@ -0,0 +1,28 @@
+﻿using Arch.SystemGroups;
+using Cysharp.Threading.Tasks;
+using DCL.DebugUtilities;
+using DCL.Rendering.RenderSystem;
+using System.Threading;
+
+namespace DCL.PluginSystem.Global
+{
+    public class RenderingSystemPlugin : IDCLGlobalPluginWithoutSettings
+    {
+        private readonly IDebugContainerBuilder debugContainerBuilder;
+
+        public RenderingSystemPlugin(IDebugContainerBuilder debugContainerBuilder)
+        {
+            this.debugContainerBuilder = debugContainerBuilder;
+        }
+
+        public void InjectToWorld(ref ArchSystemsWorldBuilder<Arch.Core.World> builder, in GlobalPluginArguments arguments)
+        {
+            DebugRenderSystem.InjectToWorld(ref builder, debugContainerBuilder);
+        }
+
+        public UniTask Initialize(IPluginSettingsContainer container, CancellationToken ct) =>
+            UniTask.CompletedTask;
+
+        public void Dispose() { }
+    }
+}
