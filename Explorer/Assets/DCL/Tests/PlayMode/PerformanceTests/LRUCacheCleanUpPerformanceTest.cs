4cf1ba822c86f8951accdb2c5c32046c29f753e7
NickKhalow <nickhalow@gmail.com>
2025-12-19 18:07:21 +0200

    performance test

---
+++ Explorer/Assets/DCL/Tests/PlayMode/PerformanceTests/LRUCacheCleanUpPerformanceTest.cs
@@ -0,0 +1,35 @@
+﻿using DCL.Diagnostics;
+using DCL.Optimization.PerformanceBudgeting;
+using ECS.StreamableLoading.Common.Components;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using Unity.Profiling;
+using ECS.StreamableLoading.Cache.Disk;
+using ECS.StreamableLoading.Cache.Disk.CleanUp;
+using ECS.StreamableLoading.Cache.Disk.Lock;
+using Unity.PerformanceTesting;
+
+namespace ECS.StreamableLoading.Cache.Tests
+{
+    public class LRUCacheCleanUpPerformanceTest
+    {
+        private LRUDiskCleanUp cache;
+
+        [SetUp]
+        public void Setup()
+        {
+            CacheDirectory directory = CacheDirectory.NewDefault();
+            FilesLock filesLock = new FilesLock();
+            cache = new LRUDiskCleanUp(directory, filesLock);
+        }
+
+        [Test, Performance]
+        public void CleanUpIfNeeded()
+        {
+            cache.CleanUpIfNeeded();
+        }
+    }
+}
