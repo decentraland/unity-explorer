9c4645e839d73e364c2b457f861fb626b9e324bd
daniele-dcl <daniele.marotta@decentraland.org>
2026-01-05 13:05:44 +0100

    feat: head sync (#6347)

---
+++ Explorer/Assets/DCL/Settings/ModuleControllers/HeadSyncController.cs
@@ -0,0 +1,30 @@
+﻿using DCL.Prefs;
+using DCL.Settings.ModuleViews;
+
+namespace DCL.Settings.ModuleControllers
+{
+    public class HeadSyncController : SettingsFeatureController
+    {
+        private SettingsToggleModuleView view;
+
+        public HeadSyncController(SettingsToggleModuleView view)
+        {
+            this.view = view;
+
+            bool value = DCLPlayerPrefs.GetBool(DCLPrefKeys.SETTINGS_HEAD_SYNC_ENABLED, true);
+            view.ToggleView.Toggle.SetIsOnWithoutNotify(value);
+            view.ToggleView.Toggle.onValueChanged.AddListener(OnToggleValueChanged);
+
+            Apply(value);
+        }
+
+        public override void Dispose() =>
+            view.ToggleView.Toggle.onValueChanged.RemoveAllListeners();
+
+        private void OnToggleValueChanged(bool isOn) =>
+            Apply(isOn);
+
+        private static void Apply(bool headSyncEnabled) =>
+            DCLPlayerPrefs.SetBool(DCLPrefKeys.SETTINGS_HEAD_SYNC_ENABLED, headSyncEnabled, true);
+    }
+}
