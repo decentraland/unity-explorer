02c7273de7d8e85a55836452052c84bd616e5409
Vitaly Popuzin <popuzin@gmail.com>
2026-01-19 13:00:31 +0100

    add rpc overrides

--- Explorer/Assets/ThirdWebUnity/ThirdWebManager.cs
+++ Explorer/Assets/ThirdWebUnity/ThirdWebManager.cs
@@ -1,4 +1,6 @@
 using DCL.Prefs;
+using System.Collections.Generic;
+using System.Numerics;
 using System.Threading.Tasks;
 using Thirdweb;
 using UnityEngine;
@@ -43,7 +45,19 @@ namespace ThirdWebUnity
                 sdkName: "UnitySDK",
                 sdkOs: Application.platform.ToString(),
                 sdkPlatform: "unity",
-                sdkVersion: THIRDWEB_UNITY_SDK_VERSION);
+                sdkVersion: THIRDWEB_UNITY_SDK_VERSION,
+                rpcOverrides: new Dictionary<BigInteger, string>
+                {
+                    { 1, "https://rpc.decentraland.org/mainnet" }, // Ethereum Mainnet
+                    { 11155111, "https://rpc.decentraland.org/sepolia" }, // Ethereum Sepolia
+                    { 137, "https://rpc.decentraland.org/polygon" }, // Polygon Mainnet
+                    { 80002, "https://rpc.decentraland.org/amoy" }, // Polygon Amoy
+                    { 42161, "https://rpc.decentraland.org/arbitrum" }, // Arbitrum Mainnet
+                    { 10, "https://rpc.decentraland.org/optimism" }, // Optimism Mainnet
+                    { 43114, "https://rpc.decentraland.org/avalanche" }, // Avalanche Mainnet
+                    { 56, "https://rpc.decentraland.org/binance" }, // BSC Mainnet
+                    { 250, "https://rpc.decentraland.org/fantom" }, // Fantom Mainnet
+                });
 
             if (Client == null)
                 Debug.LogError("VVV Failed to initialize ThirdwebManager.");
