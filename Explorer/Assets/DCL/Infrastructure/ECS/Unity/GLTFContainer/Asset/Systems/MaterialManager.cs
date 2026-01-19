1e93ec8cb1a53dcf90c31bb170ce1b6bac41627c
Geoff Birch <geoff.birch@decentraland.org>
2026-01-19 16:34:02 +0000

    Minor perf improvements

--- Explorer/Assets/DCL/Infrastructure/ECS/Unity/GLTFContainer/Asset/Systems/MaterialManager.cs
+++ Explorer/Assets/DCL/Infrastructure/ECS/Unity/GLTFContainer/Asset/Systems/MaterialManager.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
+using Unity.Collections;
 using UnityEngine;
 using UnityEngine.Rendering;
 
@@ -43,9 +44,9 @@ namespace DCL.Rendering.RenderSystem
 
         private GraphicsBuffer GPUBuffer_PerMaterial;
         private UInt16 currentPosition = 0;
-        private List<PerRSUVMaterial> perMaterials;
+        private NativeList<PerRSUVMaterial> perMaterials;
         PerRSUVMaterial perMat = new PerRSUVMaterial();
-        private SortedDictionary<uint, Material> materialSortedDictionary = new SortedDictionary<uint, Material>();
+        private Dictionary<uint, Material> materialDictionary = new Dictionary<uint, Material>(512);
         private UInt32 nMaterialCount = 0;
         private int nFrameGPUStartingPosition = 0;
 
@@ -53,14 +54,14 @@ namespace DCL.Rendering.RenderSystem
         {
             GPUBuffer_PerMaterial = new GraphicsBuffer(GraphicsBuffer.Target.Structured, GraphicsBuffer.UsageFlags.None, UInt16.MaxValue, Marshal.SizeOf(typeof(PerRSUVMaterial)));
             Shader.SetGlobalBuffer(RSUV_BUFFER_ID, GPUBuffer_PerMaterial);
-            perMaterials = new List<PerRSUVMaterial>();
+            perMaterials = new NativeList<PerRSUVMaterial>(1024,Allocator.Persistent);
         }
 
         public void AddMaterial(Renderer renderer)
         {
             Material newMaterial = SRUV_Adjustment(renderer);
             uint matCRC = MaterialCRC.ComputeCustomMaterialCRC(newMaterial);
-            if (materialSortedDictionary.TryGetValue(matCRC, out Material _mat))
+            if (materialDictionary.TryGetValue(matCRC, out Material _mat))
             {
                 renderer.sharedMaterials = new Material[] { _mat };
                 UnityEngine.Object.Destroy(newMaterial);
@@ -69,32 +70,34 @@ namespace DCL.Rendering.RenderSystem
             {
                 newMaterial.name = "Material_" + ++nMaterialCount + "_" + matCRC;
                 renderer.sharedMaterials = new Material[] { newMaterial };
-                materialSortedDictionary.Add(matCRC, newMaterial);
+                materialDictionary.Add(matCRC, newMaterial);
             }
         }
 
         private Material SRUV_Adjustment(Renderer renderer)
         {
-            perMat._BaseMap_ST = renderer.sharedMaterial.GetVector(BASEMAP_ST_ID);
-            perMat._BaseColor = renderer.sharedMaterial.GetVector(BASE_COLOR_ID);
-            perMat._SpecColor = renderer.sharedMaterial.GetVector(SPEC_COLOR_ID);
-            perMat._EmissionColor = renderer.sharedMaterial.GetVector(EMISSION_COLOR_ID);
-            perMat._PlaneClipping = renderer.sharedMaterial.GetVector(PLANE_CLIPPING_ID);
-            perMat._VerticalClipping = renderer.sharedMaterial.GetVector(VERTICAL_CLIPPING_ID);
-            perMat._Cutoff = renderer.sharedMaterial.GetFloat(CUTOFF_ID);
-            perMat._Smoothness = renderer.sharedMaterial.GetFloat(SMOOTHNESS_ID);
-            perMat._Metallic = renderer.sharedMaterial.GetFloat(METALLIC_ID);
-            perMat._BumpScale = renderer.sharedMaterial.GetFloat(BUMP_SCALE_ID);
-            perMat._Parallax = renderer.sharedMaterial.GetFloat(PARALLAX_ID);
-            perMat._OcclusionStrength = renderer.sharedMaterial.GetFloat(OCCLUSION_STRENGTH_ID);
-            perMat._Surface = renderer.sharedMaterial.GetFloat(SURFACE_ID);
+            MeshRenderer meshRenderer = (MeshRenderer)renderer;
+            Material mat = meshRenderer.sharedMaterial;
+            perMat._BaseMap_ST = mat.GetVector(BASEMAP_ST_ID);
+            perMat._BaseColor = mat.GetVector(BASE_COLOR_ID);
+            perMat._SpecColor = mat.GetVector(SPEC_COLOR_ID);
+            perMat._EmissionColor = mat.GetVector(EMISSION_COLOR_ID);
+            perMat._PlaneClipping = mat.GetVector(PLANE_CLIPPING_ID);
+            perMat._VerticalClipping = mat.GetVector(VERTICAL_CLIPPING_ID);
+            perMat._Cutoff = mat.GetFloat(CUTOFF_ID);
+            perMat._Smoothness = mat.GetFloat(SMOOTHNESS_ID);
+            perMat._Metallic = mat.GetFloat(METALLIC_ID);
+            perMat._BumpScale = mat.GetFloat(BUMP_SCALE_ID);
+            perMat._Parallax = mat.GetFloat(PARALLAX_ID);
+            perMat._OcclusionStrength = mat.GetFloat(OCCLUSION_STRENGTH_ID);
+            perMat._Surface = mat.GetFloat(SURFACE_ID);
             perMat._padding = 0.0f;
 
             perMaterials.Add(perMat);
 
-            ((MeshRenderer)renderer).SetShaderUserValue(currentPosition++);
+            meshRenderer.SetShaderUserValue(currentPosition++);
 
-            Material newMaterial = new Material(renderer.sharedMaterial);
+            Material newMaterial = new Material(mat);
             newMaterial.enableInstancing = true;
             newMaterial.SetVector(BASEMAP_ST_ID, Vector4.zero);
             newMaterial.SetVector(BASE_COLOR_ID, Vector4.one);
@@ -118,12 +121,25 @@ namespace DCL.Rendering.RenderSystem
 
         public void EndOfFramePushtoGPU()
         {
-            if (perMaterials.Count > 0)
+            if (perMaterials.Length > 0)
             {
-                GPUBuffer_PerMaterial.SetData(perMaterials.ToArray(), 0, nFrameGPUStartingPosition, perMaterials.Count);
-                nFrameGPUStartingPosition += perMaterials.Count;
+                // Check if hitting capacity issues
+                if (perMaterials.Length > perMaterials.Capacity * 0.9f)
+                {
+                    Debug.LogWarning($"PerMaterial NativeList near capacity: {perMaterials.Length}/{perMaterials.Capacity}");
+                }
+                GPUBuffer_PerMaterial.SetData(perMaterials.AsArray(), 0, nFrameGPUStartingPosition, perMaterials.Length);
+                nFrameGPUStartingPosition += perMaterials.Length;
                 perMaterials.Clear();
             }
+        }
+
+        public void Dispose()
+        {
+            if (perMaterials.IsCreated)
+                perMaterials.Dispose();
+
+            GPUBuffer_PerMaterial?.Dispose();
         }
 
         private void DefaultNonRequiredMaterialValues()
