ec6f086d42f4bbc5d5da9e915e77e5d7b40ef889
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2026-01-20 14:13:26 -0300

    Update SceneEntityDefinition.cs
    Signed-off-by: Juan Ignacio Molteni <juanignaciomolteni@gmail.com>

--- Explorer/Assets/DCL/Ipfs/SceneEntityDefinition.cs
+++ Explorer/Assets/DCL/Ipfs/SceneEntityDefinition.cs
@@ -20,8 +20,6 @@ namespace DCL.Ipfs
 
         public bool SupportInitialSceneState()
         {
-            return true;
-            
             if (assetBundleManifestVersion != null)
                 return assetBundleManifestVersion.SupportsInitialSceneState();
 
