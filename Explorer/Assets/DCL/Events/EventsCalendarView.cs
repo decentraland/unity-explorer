d5d5743a07887d2657ee1dacc5d6ad642afc8536
Santi Andrade <sandrade@decentraland.org>
2026-02-10 13:10:43 +0100

    GoToToday button moved to the main header

--- Explorer/Assets/DCL/Events/EventsCalendarView.cs
+++ Explorer/Assets/DCL/Events/EventsCalendarView.cs
@@ -33,8 +33,6 @@ namespace DCL.Events
         [SerializeField] private List<EventsDaySelectorButton> daySelectorButtons = null!;
         [SerializeField] private Button previousDateRangeButton = null!;
         [SerializeField] private Button nextDateRangeButton = null!;
-        [SerializeField] private Button goToTodayButtonLeftSide = null!;
-        [SerializeField] private Button goToTodayButtonRightSide = null!;
 
         [Header("Events")]
         [SerializeField] private List<EventListConfiguration> eventsLists = null!;
@@ -56,26 +54,13 @@ namespace DCL.Events
         private DateTime currentFromDate;
         private int currentNumberOfDaysShowed;
         private EventsStateService eventsStateService = null!;
-        private bool showGoToTodayButtonOnTheRight;
         private ThumbnailLoader? eventCardsThumbnailLoader;
         private ProfileRepositoryWrapper? profileRepositoryWrapper;
 
         private void Awake()
         {
-            previousDateRangeButton.onClick.AddListener(() =>
-            {
-                showGoToTodayButtonOnTheRight = false;
-                SetupDaysSelector(currentFromDate.AddDays(-currentNumberOfDaysShowed), currentNumberOfDaysShowed);
-            });
-            nextDateRangeButton.onClick.AddListener(() =>
-            {
-                showGoToTodayButtonOnTheRight = true;
-                SetupDaysSelector(currentFromDate.AddDays(currentNumberOfDaysShowed), currentNumberOfDaysShowed);
-            });
-
-            DateTime todayAtTheBeginningOfTheDay = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day, 0, 0, 0, DateTimeKind.Local);
-            goToTodayButtonLeftSide.onClick.AddListener(() => SetupDaysSelector(todayAtTheBeginningOfTheDay, currentNumberOfDaysShowed));
-            goToTodayButtonRightSide.onClick.AddListener(() => SetupDaysSelector(todayAtTheBeginningOfTheDay, currentNumberOfDaysShowed));
+            previousDateRangeButton.onClick.AddListener(() => SetupDaysSelector(currentFromDate.AddDays(-currentNumberOfDaysShowed), currentNumberOfDaysShowed));
+            nextDateRangeButton.onClick.AddListener(() => SetupDaysSelector(currentFromDate.AddDays(currentNumberOfDaysShowed), currentNumberOfDaysShowed));
 
             foreach (EventsDaySelectorButton daySelectorButton in daySelectorButtons)
                 daySelectorButton.ButtonClicked += OnDaySelectorButtonClicked;
@@ -85,8 +70,6 @@ namespace DCL.Events
         {
             previousDateRangeButton.onClick.RemoveAllListeners();
             nextDateRangeButton.onClick.RemoveAllListeners();
-            goToTodayButtonLeftSide.onClick.RemoveAllListeners();
-            goToTodayButtonRightSide.onClick.RemoveAllListeners();
 
             foreach (EventsDaySelectorButton daySelectorButton in daySelectorButtons)
                 daySelectorButton.ButtonClicked -= OnDaySelectorButtonClicked;
@@ -109,8 +92,6 @@ namespace DCL.Events
             bool isToday = fromDate.Date == DateTime.Today;
             nextDateRangeButton.interactable = !deactivateArrows;
             previousDateRangeButton.interactable = !deactivateArrows && !isToday;
-            goToTodayButtonLeftSide.gameObject.SetActive(!isToday && !showGoToTodayButtonOnTheRight);
-            goToTodayButtonRightSide.gameObject.SetActive(!isToday && showGoToTodayButtonOnTheRight);
 
             for (var i = 0; i < daySelectorButtons.Count; i++)
             {
