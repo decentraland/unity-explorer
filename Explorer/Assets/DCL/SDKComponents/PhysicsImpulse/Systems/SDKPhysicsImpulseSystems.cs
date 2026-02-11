0c09ee0b17ee729d28fe8dc250419b0a57e34860
Vitaly Popuzin <popuzin@gmail.com>
2026-02-11 22:04:49 +0100

    exposed settings to the debug panel

--- Explorer/Assets/DCL/SDKComponents/PhysicsImpulse/Systems/SDKPhysicsImpulseSystems.cs
+++ Explorer/Assets/DCL/SDKComponents/PhysicsImpulse/Systems/SDKPhysicsImpulseSystems.cs
@@ -1,35 +1,47 @@
 using Arch.Core;
 using Arch.System;
 using Arch.SystemGroups;
-using Arch.SystemGroups.DefaultSystemGroups;
 using CRDT;
 using CrdtEcsBridge.Components;
 using CrdtEcsBridge.Components.Conversion;
+using DCL.CharacterMotion;
 using DCL.CharacterMotion.Components;
+using DCL.CharacterMotion.Settings;
 using DCL.Diagnostics;
 using DCL.ECSComponents;
 using ECS.Abstract;
+using ECS.Groups;
+using SceneRunner.Scene;
 using UnityEngine;
 
 namespace DCL.SDKComponents.PhysicsImpulse.Systems
 {
-    [UpdateInGroup(typeof(PhysicsSystemGroup))]
+    [UpdateInGroup(typeof(SyncedSimulationSystemGroup))]
     [LogCategory(ReportCategory.MOTION)]
     public partial class SDKPhysicsImpulseSystems : BaseUnityLoopSystem
     {
-        private const float MAX_EXTERNAL_VELOCITY = 50f;
-
         private readonly World globalWorld;
         private readonly Entity globalPlayerEntity;
+        private readonly ISceneStateProvider sceneStateProvider;
 
-        internal SDKPhysicsImpulseSystems(World world, World globalWorld, Entity globalPlayerEntity) : base(world)
+        private SingleInstanceEntity characterSettings;
+
+        internal SDKPhysicsImpulseSystems(World world, World globalWorld, Entity globalPlayerEntity, ISceneStateProvider sceneStateProvider) : base(world)
         {
             this.globalWorld = globalWorld;
             this.globalPlayerEntity = globalPlayerEntity;
+            this.sceneStateProvider = sceneStateProvider;
+        }
+
+        public override void Initialize()
+        {
+            characterSettings = globalWorld.CacheCharacterSettings();
         }
 
         protected override void Update(float t)
         {
+            if (!sceneStateProvider.IsCurrent) return;
+
             ApplyPhysicsImpulseQuery(World!);
         }
 
@@ -39,20 +51,26 @@ namespace DCL.SDKComponents.PhysicsImpulse.Systems
         {
             if (crdtEntity.Id != SpecialEntitiesID.PLAYER_ENTITY) return;
             if (pbPhysicsImpulse.IsDirty == false) return;
+
             if (pbPhysicsImpulse.Direction == null)
             {
                 pbPhysicsImpulse.IsDirty = false;
                 return;
             }
 
-            Vector3 impulseVelocity = pbPhysicsImpulse.Direction.ToUnityVector();
+            ICharacterControllerSettings settings = characterSettings.GetCharacterSettings(globalWorld);
+
+            // Δv = impulse / mass
+            Vector3 impulseVelocity = pbPhysicsImpulse.Direction.ToUnityVector() / settings.CharacterMass;
 
             var rigidTransform = globalWorld.Get<CharacterRigidTransform>(globalPlayerEntity);
             rigidTransform.ExternalVelocity += impulseVelocity;
 
             // Clamp to max external velocity
-            if (rigidTransform.ExternalVelocity.sqrMagnitude > MAX_EXTERNAL_VELOCITY * MAX_EXTERNAL_VELOCITY)
-                rigidTransform.ExternalVelocity = rigidTransform.ExternalVelocity.normalized * MAX_EXTERNAL_VELOCITY;
+            float maxVelocity = settings.MaxExternalVelocity;
+
+            if (rigidTransform.ExternalVelocity.sqrMagnitude > maxVelocity * maxVelocity)
+                rigidTransform.ExternalVelocity = rigidTransform.ExternalVelocity.normalized * maxVelocity;
 
             // Unground the character if impulse has upward component
             if (impulseVelocity.y > 0)
