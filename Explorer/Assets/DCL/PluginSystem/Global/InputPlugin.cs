d0d30784f5b8bc66e9702a01b61afee689a1c5a5
Vitaly Popuzin <popuzin@gmail.com>
2026-02-19 11:30:17 +0100

    fixed compilation error

--- Explorer/Assets/DCL/PluginSystem/Global/InputPlugin.cs
+++ Explorer/Assets/DCL/PluginSystem/Global/InputPlugin.cs
@@ -87,8 +87,6 @@ namespace DCL.PluginSystem.Global
 
             ApplyInputMapsSystem.InjectToWorld(ref builder);
             UpdateInputJumpSystem.InjectToWorld(ref builder, DCLInput.Instance.Player.Jump);
-            UpdateInputImpulseSystem.InjectToWorld(ref builder, DCLInput.Instance.Player.Impulse);
-            UpdateInputExternalForceSystem.InjectToWorld(ref builder, DCLInput.Instance.Player.ExternalForce);
             UpdateInputMovementSystem.InjectToWorld(ref builder);
             UpdateCameraInputSystem.InjectToWorld(ref builder);
             DropPlayerFromFreeCameraSystem.InjectToWorld(ref builder, DCLInput.Instance.FreeCamera.DropPlayer);
