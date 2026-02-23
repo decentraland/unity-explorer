d5403f2d16eab37edf233dd3f122d6697adf1913
Alejandro Jimenez <alejandro.jimenez@decentraland.org>
2026-02-23 08:30:25 +0100

    fix: reenable saturation and value slider in backpack. fix: use feature flag also for the processing of the color

--- Explorer/Assets/DCL/Profiles/SharedAPI/Profile.CompactInfo.cs
+++ Explorer/Assets/DCL/Profiles/SharedAPI/Profile.CompactInfo.cs
@@ -1,5 +1,6 @@
 ﻿using CommunicationData.URLHelpers;
 using DCL.Diagnostics;
+using DCL.FeatureFlags;
 using DCL.Utilities;
 using DCL.Web3;
 using ECS.StreamableLoading.Common.Components;
@@ -261,7 +262,7 @@ namespace DCL.Profiles
                     MentionName = new string(mentionBuffer);
                 }
 
-                if (claimedNameColor.HasValue)
+                if (FeaturesRegistry.Instance.IsEnabled(FeatureId.NAME_COLOR_CHANGE) && claimedNameColor is { a: > 0 })
                     UserNameColor = claimedNameColor.Value;
                 else
                     UserNameColor = NameColorHelper.GetNameColor(DisplayName);
