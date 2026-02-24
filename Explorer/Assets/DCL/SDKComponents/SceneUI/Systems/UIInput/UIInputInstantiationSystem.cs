4d1b9e23e0b3b48834688569a629038180e47aa7
Pravus <pravusjif@gmail.com>
2026-02-24 20:25:30 +0100

    chore: sdk ui improve default styles and hover feedback (#6846)
    Additions
    * Updated theme file to include ScenesUIStyleSheet at root level for Scenes UI panel. That change already makes some default UI Toolkit behaviours from Unity to affect some UI components.
    * Implemented support for dynamic manipulation of selectedIndex by the scene
    * Added Opacity transition logic for Dropdown panel activation
    * Added default hover feedback for UiDropdown, UiInput and UiButton:
      * UiTransform border color darkening
      * UiTransform background color darkening
    * Added visual feedback for the disabling of UiDropdown, UiInput and UiButton
    * Added UiTransform default Border Width, Radius and Color values when those are missing for UiDropdown, UiInput and UiButton
    * Added UiTransform default Background Color value when that is missing for UiDropdown and UiInput (UiButton already injects default background from the SDK runtime side depending on the button being "primary" or "secondary")
    * Test coverage
    
    Fixes
    * Fixed bug that re-applied the selectedIndex property of the Dropdown on any dirty state of the component
    * Refactored how registered pointer event callbacks are handled for UiDropdown to avoid closure allocations
    * Fixed font property being ignored for UiDropdown and UiInput
    * Fixed textAlign property being ignored by UiInput

--- Explorer/Assets/DCL/SDKComponents/SceneUI/Systems/UIInput/UIInputInstantiationSystem.cs
+++ Explorer/Assets/DCL/SDKComponents/SceneUI/Systems/UIInput/UIInputInstantiationSystem.cs
@@ -1,4 +1,4 @@
-﻿using Arch.Core;
+using Arch.Core;
 using Arch.System;
 using Arch.SystemGroups;
 using CRDT;
@@ -13,57 +13,92 @@ using DCL.SDKComponents.SceneUI.Groups;
 using DCL.SDKComponents.SceneUI.Utils;
 using DCL.Utilities.Extensions;
 using ECS.Abstract;
+using ECS.LifeCycle.Components;
+using UnityEngine.UIElements;
 
 namespace DCL.SDKComponents.SceneUI.Systems.UIInput
 {
+    /*
+     * As defined in the SDK, UiInput entities composition breakdown:
+     * https://github.com/decentraland/js-sdk-toolchain/blob/main/packages/@dcl/react-ecs/src/components/Input/index.tsx#L43-L55
+     * - PBUiInput
+     * - (optional, but Explorer queries require it) PBUiTransform
+     * - (optional) PBUiBackground
+     * - (optional) PBPointerEvents
+     */
+
     [UpdateInGroup(typeof(SceneUIComponentInstantiationGroup))]
     [LogCategory(ReportCategory.SCENE_UI)]
     public partial class UIInputInstantiationSystem : BaseUnityLoopSystem
     {
         private const string COMPONENT_NAME = "UIInput";
+        private const float HOVER_BORDER_DARKEN_FACTOR = 0.3f;
+        private const float HOVER_BACKGROUND_DARKEN_FACTOR = 0f;
 
         private readonly IComponentPool<UIInputComponent> inputTextsPool;
         private readonly IECSToCRDTWriter ecsToCRDTWriter;
         private readonly IInputBlock inputBlock;
+        private readonly StyleFontDefinition[] styleFontDefinitions;
 
-        public UIInputInstantiationSystem(World world, IComponentPoolsRegistry poolsRegistry, IECSToCRDTWriter ecsToCRDTWriter, IInputBlock inputBlock) : base(world)
+        public UIInputInstantiationSystem(World world, IComponentPoolsRegistry poolsRegistry, IECSToCRDTWriter ecsToCRDTWriter, IInputBlock inputBlock, in StyleFontDefinition[] styleFontDefinitions) : base(world)
         {
             inputTextsPool = poolsRegistry.GetReferenceTypePool<UIInputComponent>().EnsureNotNull();
             this.ecsToCRDTWriter = ecsToCRDTWriter;
             this.inputBlock = inputBlock;
+            this.styleFontDefinitions = styleFontDefinitions;
         }
 
         protected override void Update(float t)
         {
+            UpdateUIInputTransformDefaultsQuery(World!);
+
             InstantiateUIInputQuery(World!);
             UpdateUIInputQuery(World!);
+
             TriggerInputResultsQuery(World!);
         }
 
         [Query]
-        [All(typeof(PBUiInput), typeof(UITransformComponent))]
+        [All(typeof(PBUiInput))]
         [None(typeof(UIInputComponent))]
-        private void InstantiateUIInput(in Entity entity, ref PBUiInput sdkModel, ref UITransformComponent uiTransformComponent)
+        private void InstantiateUIInput(in Entity entity, in PBUiInput sdkModel, in PBUiTransform pbUiTransform, ref UITransformComponent uiTransformComponent)
         {
             var newUIInputComponent = inputTextsPool.Get()!;
             newUIInputComponent.Initialize(inputBlock,
                 UiElementUtils.BuildElementName(COMPONENT_NAME, entity),
-                "dcl-input",
                 sdkModel.Value,
                 sdkModel.Placeholder,
                 sdkModel.GetPlaceholderColor());
             uiTransformComponent.Transform.Add(newUIInputComponent.TextField);
+
+            UiElementUtils.ApplyDefaultUiTransformValues(in pbUiTransform, uiTransformComponent.Transform);
+            UiElementUtils.ApplyDefaultUiBackgroundValues(World, entity, uiTransformComponent.Transform);
+            UiElementUtils.ConfigureHoverStylesBehaviour(World, entity, in uiTransformComponent, newUIInputComponent.TextField, HOVER_BORDER_DARKEN_FACTOR, HOVER_BACKGROUND_DARKEN_FACTOR);
+
             World!.Add(entity, newUIInputComponent);
         }
 
         [Query]
+        [None(typeof(DeleteEntityIntention))]
         private void UpdateUIInput(ref UIInputComponent uiInputComponent, ref PBUiInput sdkModel)
         {
             if (!sdkModel.IsDirty)
                 return;
 
-            UiElementUtils.SetupUIInputComponent(ref uiInputComponent, ref sdkModel);
+            UiElementUtils.SetupUIInputComponent(ref uiInputComponent, in sdkModel, in styleFontDefinitions);
             sdkModel.IsDirty = false;
+        }
+
+        [Query]
+        [All(typeof(UIInputComponent))]
+        [None(typeof(DeleteEntityIntention))]
+        private void UpdateUIInputTransformDefaults(in Entity entity, in UITransformComponent uiTransformComponent, in PBUiTransform pbUiTransform, ref UIInputComponent uiInputComponent)
+        {
+            if (!pbUiTransform.IsDirty) return;
+
+            UiElementUtils.ApplyDefaultUiTransformValues(in pbUiTransform, uiTransformComponent.Transform);
+            // Re-configure hover so the stored border colors match the updated transform (e.g. defaults applied above); avoids wrong restore on hover leave when PBUiTransform changed while hovered.
+            UiElementUtils.ConfigureHoverStylesBehaviour(World!, entity, in uiTransformComponent, uiInputComponent.TextField, HOVER_BORDER_DARKEN_FACTOR, HOVER_BACKGROUND_DARKEN_FACTOR);
         }
 
         [Query]
