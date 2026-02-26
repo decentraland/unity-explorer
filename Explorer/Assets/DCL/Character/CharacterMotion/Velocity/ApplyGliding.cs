2087725d53f1a14e82e2e1511f12120ef782688a
Daniele Marotta <daniele.marotta@decentraland.org>
2026-02-26 11:03:06 +0100

    added delay between double jump and gliding

--- Explorer/Assets/DCL/Character/CharacterMotion/Velocity/ApplyGliding.cs
+++ Explorer/Assets/DCL/Character/CharacterMotion/Velocity/ApplyGliding.cs
@@ -12,27 +12,32 @@ namespace DCL.CharacterMotion
         public static void Execute(in ICharacterControllerSettings settings,
             in CharacterRigidTransform rigidTransform,
             in JumpState jumpState,
-            in JumpInputComponent jump,
+            in JumpInputComponent jumpInput,
             ref GlideState glideState,
             int physicsTick,
             float dt)
         {
-            bool coolingDown = (physicsTick - glideState.CooldownStartedTick) * dt < settings.GlideCooldown;
-            bool canGlide = jumpState.JumpCount > jumpState.MaxAirJumpCount &&
-                            !rigidTransform.IsGrounded &&
-                            rigidTransform.GroundDistance > settings.GlideMinGroundDistance &&
-                            !coolingDown;
+            // When this flag is true we can press the button and keep holding it
+            // It's a form of input buffering that makes gameplay smoother
+            bool canTriggerGliding = jumpState.JumpCount > jumpState.MaxAirJumpCount &&
+                                     !rigidTransform.IsGrounded &&
+                                     rigidTransform.GroundDistance > settings.GlideMinGroundDistance;
 
-            // Allow pressing the glide button before the action is actually available
             // Player can hold the glide button to start gliding as soon as possible
-            // We added this to handle the specific case of trying to open the glider while it's still being closed
-            glideState.WantsToGlide |= canGlide && jump.Trigger.IsAvailable(physicsTick, 0);
+            // (hence the OR, because the button input will be in the available state only for 1 tick)
+            glideState.WantsToGlide |= canTriggerGliding && jumpInput.Trigger.IsAvailable(physicsTick, 0);
 
             // Obviously we still want to reset the flag if we release the button
-            glideState.WantsToGlide &= jump.IsPressed;
+            glideState.WantsToGlide &= jumpInput.IsPressed;
 
-            // Start gliding if can and want
-            if (glideState.Value == GlideStateValue.PROP_CLOSED && canGlide && glideState.WantsToGlide)
+            // Now that we decided whether or not the player wants to start gliding (and can do so) we need to consider animations and cooldowns
+            // Once the 'ready' flag becomes true the actual gliding sequence starts
+            bool enoughTimeSinceLastJump = (physicsTick - jumpInput.Trigger.TickWhenJumpWasConsumed) * dt >= settings.JumpToGlideTimeInterval;
+            bool coolingDown = (physicsTick - glideState.CooldownStartedTick) * dt < settings.GlideCooldown;
+            bool readyToStartGliding = enoughTimeSinceLastJump && !coolingDown && glideState.Value == GlideStateValue.PROP_CLOSED;
+
+            // Start gliding if want, can and ready
+            if (glideState.WantsToGlide && canTriggerGliding && readyToStartGliding)
             {
                 glideState.Value = GlideStateValue.OPENING_PROP;
                 return;
@@ -40,7 +45,7 @@ namespace DCL.CharacterMotion
 
             if (glideState.Value == GlideStateValue.GLIDING)
             {
-                if (!jump.IsPressed || !canGlide)
+                if (!jumpInput.IsPressed || !canTriggerGliding)
                     // Stop gliding if the jump button is released or any other condition prevents it
                     glideState.Value = GlideStateValue.CLOSING_PROP;
                 else
