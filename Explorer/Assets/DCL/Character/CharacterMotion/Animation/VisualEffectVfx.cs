c778efae660cc6018843513ac827e0721a11b477
Daniele Marotta <daniele.marotta@decentraland.org>
2026-03-02 16:38:59 +0100

    extracted const in visual effect vfx

--- Explorer/Assets/DCL/Character/CharacterMotion/Animation/VisualEffectVfx.cs
+++ Explorer/Assets/DCL/Character/CharacterMotion/Animation/VisualEffectVfx.cs
@@ -6,6 +6,8 @@ namespace DCL.CharacterMotion.Animation
 {
     public class VisualEffectVfx : Vfx<VisualEffect>
     {
+        private const int VFX_BURST_WAIT_MS = 1000;
+
         public VisualEffectVfx(VisualEffect target) : base(target)
         {
         }
@@ -13,7 +15,7 @@ namespace DCL.CharacterMotion.Animation
         public override async UniTask WaitForCompletionAsync(CancellationToken ct)
         {
             // Arbitrary 1-second delay, we assume by this time we emitted all particle bursts
-            await UniTask.Delay(1000, cancellationToken: ct);
+            await UniTask.Delay(VFX_BURST_WAIT_MS, cancellationToken: ct);
 
             while (target.aliveParticleCount > 0)
             {
