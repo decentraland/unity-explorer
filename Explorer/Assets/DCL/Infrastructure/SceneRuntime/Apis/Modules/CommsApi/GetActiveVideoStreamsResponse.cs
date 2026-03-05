d5f9552d07aa18c7a828b67679422bef358b98e3
Gabriel Diaz <gabrieldiaz31@gmail.com>
2026-03-05 16:51:55 -0300

    feat: enrich getActiveVideoStreams response and auto-resolve audio for livekit user streams
    Add participant metadata (name, speaking, trackName, width, height) to
    GetActiveVideoStreamsResponse JSON output. Enable automatic audio track
    resolution in LivekitPlayer when opening a user stream by pairing video
    source to the correct audio source (camera→microphone, screenshare→screenshare audio).

--- Explorer/Assets/DCL/Infrastructure/SceneRuntime/Apis/Modules/CommsApi/GetActiveVideoStreamsResponse.cs
+++ Explorer/Assets/DCL/Infrastructure/SceneRuntime/Apis/Modules/CommsApi/GetActiveVideoStreamsResponse.cs
@@ -1,6 +1,7 @@
 using DCL.Multiplayer.Connections.Rooms;
 using DCL.SDKComponents.MediaStream;
 using LiveKit.Proto;
+using LiveKit.Rooms.Participants;
 using LiveKit.Rooms.TrackPublications;
 using Newtonsoft.Json;
 using System;
@@ -20,7 +21,7 @@ namespace SceneRuntime.Apis.Modules.CommsApi
                 _ => throw new ArgumentOutOfRangeException()
             };
 
-        private static void WriteTo(JsonWriter writer, string identity, string trackSid, TrackPublication publication)
+        private static void WriteTo(JsonWriter writer, string identity, string trackSid, Participant participant, TrackPublication publication)
         {
             var sourceType = From(publication.Source);
 
@@ -31,19 +32,29 @@ namespace SceneRuntime.Apis.Modules.CommsApi
             writer.WriteValue(trackSid);
             writer.WritePropertyName("sourceType");
             writer.WriteValue(sourceType);
+            writer.WritePropertyName("name");
+            writer.WriteValue(participant.Name);
+            writer.WritePropertyName("speaking");
+            writer.WriteValue(participant.Speaking);
+            writer.WritePropertyName("trackName");
+            writer.WriteValue(publication.Name);
+            writer.WritePropertyName("width");
+            writer.WriteValue(publication.Width);
+            writer.WritePropertyName("height");
+            writer.WriteValue(publication.Height);
             writer.WriteEndObject();
         }
 
-        public static void WriteTo(JsonWriter writer, string identity, TrackPublication publication)
+        public static void WriteTo(JsonWriter writer, string identity, Participant participant, TrackPublication publication)
         {
             // like in unity-renderer version trackSid: `livekit-video://${sid}/${videoSid}`,
             // https://github.com/decentraland/unity-renderer/blob/ae68fec703f3c0ebd2251ce7cff2ad465f6f7f7d/browser-interface/packages/shared/apis/host/CommsAPI.ts#L19
             string address = identity.ToLivekitAddress(publication.Sid);
-            WriteTo(writer, identity, address, publication);
+            WriteTo(writer, identity, address, participant, publication);
         }
 
-        public static void WriteAsCurrentTo(JsonWriter writer, string identity, TrackPublication publication) =>
-            WriteTo(writer, identity, LiveKitMediaExtensions.LIVEKIT_CURRENT_STREAM, publication);
+        public static void WriteAsCurrentTo(JsonWriter writer, string identity, Participant participant, TrackPublication publication) =>
+            WriteTo(writer, identity, LiveKitMediaExtensions.LIVEKIT_CURRENT_STREAM, participant, publication);
 
         public enum VideoTrackSourceType
         {
