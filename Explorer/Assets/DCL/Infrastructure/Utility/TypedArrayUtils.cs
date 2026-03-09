b0f8e994420ab9c32c639d3b712eb47893a33d47
Fran Colarich <56565104+fcolarich@users.noreply.github.com>
2026-03-10 00:07:14 +0100

    Edited TypeArrayUtils to make very clear why we have diverging paths and to which platform they belong

--- Explorer/Assets/DCL/Infrastructure/Utility/TypedArrayUtils.cs
+++ Explorer/Assets/DCL/Infrastructure/Utility/TypedArrayUtils.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using Unity.Collections;
 
 namespace Utility
 {
     public static class TypedArrayUtils
     {
-        public static int Write<T>(this IDCLTypedArray<T> array,
+        public static void Write<T>(this IDCLTypedArray<T> array,
             NativeArray<byte>.ReadOnly source, ulong length, ulong index) where T : unmanaged
         {
             ulong totalLength = array.Length;
@@ -13,39 +13,13 @@ namespace Utility
             if (index >= totalLength) { throw new ArgumentOutOfRangeException(nameof(index)); }
 
 #if UNITY_WEBGL
-            ulong bytesPerElement = array.Size / totalLength;
-            ulong byteOffset = index * bytesPerElement;
-            int maxBytes = (int)Math.Min(length, totalLength - index) * (int)bytesPerElement;
-            ReadOnlySpan<byte> srcSpan = source.AsReadOnlySpan();
-            int bytesToCopy = Math.Min(maxBytes, srcSpan.Length);
-            byte[] buffer = srcSpan.Slice(0, bytesToCopy).ToArray();
-            array.WriteBytes(buffer, 0, (ulong)bytesToCopy, byteOffset);
-            return bytesToCopy;
+            WriteBytesManaged(array, source.AsReadOnlySpan(), length, index, totalLength);
 #else
-            return array.InvokeWithDirectAccess(pData => WriteByteArrayToUnmanagedMemory(source.AsReadOnlySpan(), (int)Math.Min(length, totalLength - index), array.GetPtrWithIndex(pData, index)));
+            array.InvokeWithDirectAccess(pData => WriteByteArrayToUnmanagedMemory(source.AsReadOnlySpan(), (int)Math.Min(length, totalLength - index), array.GetPtrWithIndex(pData, index)));
 #endif
         }
 
-        public static int Write<T>(this IDCLTypedArray<T> array, ReadOnlyMemory<byte> source,
-            ulong length, ulong index) where T : unmanaged
-        {
-            ulong totalLength = array.Length;
-
-            if (index >= totalLength) { throw new ArgumentOutOfRangeException(nameof(index)); }
-
-#if UNITY_WEBGL
-            ulong bytesPerElement = array.Size / totalLength;
-            ulong byteOffset = index * bytesPerElement;
-            int maxBytes = (int)Math.Min(length, totalLength - index) * (int)bytesPerElement;
-            ReadOnlySpan<byte> srcSpan = source.Span;
-            int bytesToCopy = Math.Min(maxBytes, srcSpan.Length);
-            byte[] buffer = srcSpan.Slice(0, bytesToCopy).ToArray();
-            array.WriteBytes(buffer, 0, (ulong)bytesToCopy, byteOffset);
-            return bytesToCopy;
-#else
-            return array.InvokeWithDirectAccess(pData => WriteByteArrayToUnmanagedMemory(source.Span, (int)Math.Min(length, totalLength - index), array.GetPtrWithIndex(pData, index)));
-#endif
-        }
+#if !UNITY_WEBGL
 
         private static IntPtr GetPtrWithIndex<T>(this IDCLTypedArray<T> array, IntPtr pData,
             ulong index) where T : unmanaged
@@ -54,15 +28,33 @@ namespace Utility
             return new IntPtr(unchecked((long)checked(baseAddress + (index * (array.Size / array.Length)))));
         }
 
-        private static unsafe int WriteByteArrayToUnmanagedMemory<T>(ReadOnlySpan<T> span, int length, IntPtr pDestination) where T: unmanaged
+        private static unsafe void WriteByteArrayToUnmanagedMemory<T>(ReadOnlySpan<T> span, int length, IntPtr pDestination) where T : unmanaged
         {
-            int sourceLength = span.Length;
-            length = Math.Min(length, sourceLength);
+            length = Math.Min(length, span.Length);
+            span.CopyTo(new Span<T>(pDestination.ToPointer(), length));
+        }
+#endif
 
-            var targetSpan = new Span<T>(pDestination.ToPointer(), length);
-            span.CopyTo(targetSpan);
-
-            return length;
+#if UNITY_WEBGL
+        /// <summary>
+        /// WebGL path for writing a span of bytes into a typed array at a given element index.
+        /// <para>
+        /// On WebGL, <see cref="IDCLTypedArray{T}.InvokeWithDirectAccess"/> cannot be used because the
+        /// runtime has no direct unmanaged memory access from the managed side. Instead, the target
+        /// byte offset is computed from <paramref name="index"/> and the array's bytes-per-element ratio,
+        /// and the data is handed off to <see cref="IDCLTypedArray{T}.WriteBytes"/> which marshals it
+        /// through managed memory into the underlying JS typed array.
+        /// </para>
+        /// </summary>
+        private static void WriteBytesManaged<T>(IDCLTypedArray<T> array, ReadOnlySpan<byte> srcSpan,
+            ulong length, ulong index, ulong totalLength) where T : unmanaged
+        {
+            ulong bytesPerElement = array.Size / totalLength;
+            ulong byteOffset = index * bytesPerElement;
+            int maxBytes = (int)Math.Min(length, totalLength - index) * (int)bytesPerElement;
+            int bytesToCopy = Math.Min(maxBytes, srcSpan.Length);
+            array.WriteBytes(srcSpan[..bytesToCopy].ToArray(), 0, (ulong)bytesToCopy, byteOffset);
         }
     }
+#endif
 }
