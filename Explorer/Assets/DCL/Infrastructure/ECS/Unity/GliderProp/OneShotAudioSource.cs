e5b5ee751f15d28929016e334a83330ca77c9eb8
Daniele Marotta <daniele.marotta@decentraland.org>
2026-03-09 10:55:01 +0100

    use pooled detached audio sources for glider open/close sounds

---
+++ Explorer/Assets/DCL/Infrastructure/ECS/Unity/GliderProp/OneShotAudioSource.cs
@@ -0,0 +1,35 @@
+using DCL.Optimization.Pools;
+using UnityEngine;
+
+namespace ECS.Unity.GliderProp
+{
+    public class OneShotAudioSource : MonoBehaviour
+    {
+        private AudioSource audioSource;
+        private GameObjectPool<OneShotAudioSource> pool;
+
+        public void Initialize(GameObjectPool<OneShotAudioSource> pool)
+        {
+            audioSource = GetComponent<AudioSource>();
+            this.pool = pool;
+        }
+
+        public void Play(AudioSource template, Vector3 position)
+        {
+            CancelInvoke();
+            transform.position = position;
+            audioSource.clip = template.clip;
+            audioSource.outputAudioMixerGroup = template.outputAudioMixerGroup;
+            audioSource.volume = template.volume;
+            audioSource.spatialBlend = template.spatialBlend;
+            audioSource.Play();
+            Invoke(nameof(ReturnToPool), template.clip.length);
+        }
+
+        private void ReturnToPool()
+        {
+            audioSource.Stop();
+            pool.Release(this);
+        }
+    }
+}
