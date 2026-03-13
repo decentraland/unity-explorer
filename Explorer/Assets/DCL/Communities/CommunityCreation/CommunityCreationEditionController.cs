81e4c425052a8f240f412dddf649a313cf582600
Santi Andrade <sandrade@decentraland.org>
2026-03-13 21:24:04 +0100

    Update CommunityCreationEditionController.cs

--- Explorer/Assets/DCL/Communities/CommunityCreation/CommunityCreationEditionController.cs
+++ Explorer/Assets/DCL/Communities/CommunityCreation/CommunityCreationEditionController.cs
@@ -283,39 +283,17 @@ namespace DCL.Communities.CommunityCreation
 
                 foreach (PlacesData.PlaceInfo placeInfo in placesResult.Value.Data)
                 {
-                    var placeText = $"{placeInfo.title} ({placeInfo.base_position})";
+                    bool isWorld = !string.IsNullOrEmpty(placeInfo.world_name);
+                    var placeText = $"{placeInfo.title} ({(isWorld ? placeInfo.world_name : placeInfo.base_position)})";
                     placesToAdd.Add(placeText);
 
                     currentCommunityPlaces.Add(
                         new CommunityPlace(
                             placeInfo.id,
-                            false,
+                            isWorld,
                             placeText,
                             placeInfo.owner,
                             string.Empty));
-                }
-
-                // Worlds
-                var worldsResult = await placesAPIService.GetWorldsByOwnerAsync(ownProfile.UserId, ct)
-                                                         .SuppressToResultAsync(ReportCategory.COMMUNITIES);
-
-                if (!worldsResult.Success)
-                {
-                    NotificationsBusController.Instance.AddNotification(new ServerErrorNotification(GET_WORLDS_ERROR_MESSAGE));
-                    return;
-                }
-
-                foreach (PlacesData.PlaceInfo worldInfo in worldsResult.Value.Data)
-                {
-                    var worldText = $"{worldInfo.title} ({worldInfo.world_name})";
-                    placesToAdd.Add(worldText);
-
-                    currentCommunityPlaces.Add(new CommunityPlace (
-                        worldInfo.id,
-                        true,
-                        worldText,
-                        worldInfo.owner,
-                        string.Empty));
                 }
 
                 // Owners' names
