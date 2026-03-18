175394822d0ddf0ac07c0aa5073452d739d83e54
Fran Colarich <56565104+fcolarich@users.noreply.github.com>
2026-03-19 03:36:00 +0900

    Cleanup ++

--- Explorer/Assets/DCL/Infrastructure/MVC/FSM/IndependentMVCState.cs
+++ Explorer/Assets/DCL/Infrastructure/MVC/FSM/IndependentMVCState.cs
@@ -1,45 +1,16 @@
 ﻿namespace MVC
 {
-    public abstract class IndependentMVCState : IndependentMVCState<ControllerNoData, ControllerNoData>
+    public abstract class IndependentMVCState
     {
-        protected IndependentMVCState() : base(new ControllerNoData()) { }
-
-        public bool TryActivate() =>
-            TryActivate(new ControllerNoData());
-
-        public void Reactivate() =>
-            ReActivate(new ControllerNoData());
-    }
-
-    public abstract class IndependentMVCState<TContext> : IndependentMVCState<TContext, ControllerNoData>
-    {
-        protected IndependentMVCState(TContext context) : base(context) { }
-
-        public bool TryActivate() =>
-            TryActivate(new ControllerNoData());
-
-        public void ReActivate() =>
-            ReActivate(new ControllerNoData());
-    }
-
-    public abstract class IndependentMVCState<TContext, TInput>
-    {
-        protected readonly TContext context;
-
-        protected IndependentMVCState(TContext context)
-        {
-            this.context = context;
-        }
-
         public bool IsActive { get; private set; }
 
-        public bool TryActivate(TInput input)
+        public bool TryActivate()
         {
             if (IsActive)
                 return false;
 
             IsActive = true;
-            Activate(input);
+            Activate();
             return true;
         }
 
@@ -53,16 +24,16 @@
             return true;
         }
 
-        public void ReActivate(TInput input)
+        public void ReActivate()
         {
             if (IsActive)
                 Deactivate();
 
             IsActive = true;
-            Activate(input);
+            Activate();
         }
 
-        protected abstract void Activate(TInput input);
+        protected abstract void Activate();
 
         protected abstract void Deactivate();
     }
