776495048788d42d1ee03b730ab658f3fcab3071
Fran Colarich <56565104+fcolarich@users.noreply.github.com>
2026-03-24 19:06:55 +0900

    Fixed issue with order of initialization of plugin

--- Explorer/Assets/DCL/PluginSystem/World/MaterialWorldPlugin.cs
+++ Explorer/Assets/DCL/PluginSystem/World/MaterialWorldPlugin.cs
@@ -1,14 +1,12 @@
 using Arch.SystemGroups;
 using Cysharp.Threading.Tasks;
 using DCL.Optimization.PerformanceBudgeting;
-using DCL.Optimization.Pools;
 using DCL.PluginSystem.World.Dependencies;
 using DCL.SDKComponents.MediaStream;
 using ECS.LifeCycle;
 using ECS.Unity.Materials;
 using ECS.Unity.Materials.Components;
 using ECS.Unity.Materials.Systems;
-using RenderHeads.Media.AVProVideo;
 using System;
 using System.Collections.Generic;
 using System.Threading;
@@ -40,7 +38,6 @@ namespace DCL.PluginSystem.World
         {
             memoryBudgetProvider = sharedDependencies.MemoryBudget;
             capFrameTimeBudget = sharedDependencies.FrameTimeBudget;
-            ConfigureSceneMaterial.forceBackfaceCullingEnabled = FeaturesRegistry.Instance.IsEnabled(FeatureId.FORCE_BACKFACE_CULLING);
 
             this.mediaFactory = mediaFactory;
         }
@@ -49,6 +46,8 @@ namespace DCL.PluginSystem.World
 
         public UniTask InitializeAsync(Settings settings, CancellationToken ct)
         {
+            ConfigureSceneMaterial.forceBackfaceCullingEnabled = FeaturesRegistry.Instance.IsEnabled(FeatureId.FORCE_BACKFACE_CULLING);
+
             basicMatPool = new ObjectPool<Material>(() => new Material(settings.basicMaterial), actionOnDestroy: UnityObjectUtils.SafeDestroy, defaultCapacity: settings.PoolInitialCapacity, maxSize: settings.PoolMaxSize);
             pbrMatPool = new ObjectPool<Material>(() => new Material(settings.pbrMaterial), actionOnDestroy: UnityObjectUtils.SafeDestroy, defaultCapacity: settings.PoolInitialCapacity, maxSize: settings.PoolMaxSize);
 
