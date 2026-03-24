86a439b58e3d4e9e4b898cd554a4b49d3e27bfa4
Pravus <pravusjif@gmail.com>
2026-03-25 00:48:43 +0900

    fixed position param ignored for world realms

--- Explorer/Assets/DCL/UserInAppInitializationFlow/StartupOperations/TeleportStartupOperation.cs
+++ Explorer/Assets/DCL/UserInAppInitializationFlow/StartupOperations/TeleportStartupOperation.cs
@@ -7,6 +7,7 @@ using DCL.Utilities;
 using DCL.Utility.Types;
 using ECS;
 using ECS.Prioritization.Components;
+using Global.AppArgs;
 using System.Threading;
 using UnityEngine;
 
@@ -15,28 +16,36 @@ namespace DCL.UserInAppInitializationFlow
     public class TeleportStartupOperation : TeleportToSpawnPointOperationBase<IStartupOperation.Params>, IStartupOperation
     {
         private readonly StartParcel startParcel;
+        private readonly IAppArgs appArgs;
 
         public TeleportStartupOperation(
             ILoadingStatus loadingStatus,
             IGlobalRealmController realmController,
             ObjectProxy<Entity> cameraEntity,
             ITeleportController teleportController,
-            CameraSamplingData cameraSamplingData, StartParcel startParcel, string reportCategory = ReportCategory.SCENE_LOADING)
+            CameraSamplingData cameraSamplingData,
+            StartParcel startParcel,
+            IAppArgs appArgs,
+            string reportCategory = ReportCategory.SCENE_LOADING)
             : base(loadingStatus, realmController, cameraEntity, teleportController, cameraSamplingData, reportCategory)
         {
             this.startParcel = startParcel;
+            this.appArgs = appArgs;
         }
 
         public override UniTask<EnumResult<TaskError>> ExecuteAsync(IStartupOperation.Params args, CancellationToken ct)
         {
-            // If the WorldManifest defines an explicit spawn coordinate, always use it
-            // (e.g. worlds with a fixed or curated spawn point)
-            WorldManifest manifest = realmController.RealmData.WorldManifest;
-            if (manifest is { IsEmpty: false, spawn_coordinate: { } spawn })
-                return InternalExecuteAsync(args, new Vector2Int(spawn.x, spawn.y), ct);
+            // If the user explicitly provided --position, it takes highest priority over any world manifest spawn coordinate
+            if (!appArgs.HasFlag(AppArgsFlags.POSITION))
+            {
+                // If the WorldManifest defines an explicit spawn coordinate, use it
+                // (e.g. worlds with a fixed or curated spawn point)
+                WorldManifest manifest = realmController.RealmData.WorldManifest;
+                if (manifest is { IsEmpty: false, spawn_coordinate: { } spawn })
+                    return InternalExecuteAsync(args, new Vector2Int(spawn.x, spawn.y), ct);
+            }
 
             return InternalExecuteAsync(args, startParcel.ConsumeByTeleportOperation(), ct);
-
         }
     }
 }
