ef26c84e2549d518254004d3c541964d9429dda8
Alejandro Jimenez <alejandro.jimenez@decentraland.org>
2026-03-24 11:57:48 +0100

    fix: instantiate the proper controller accordingly

--- Explorer/Assets/DCL/PluginSystem/Global/GiftingPlugin.cs
+++ Explorer/Assets/DCL/PluginSystem/Global/GiftingPlugin.cs
@@ -62,6 +62,7 @@ namespace DCL.PluginSystem.Global
         private GiftTransferController? giftTransferStatusController;
         private GiftTransferSuccessController? giftTransferSuccessController;
         private GiftReceivedPopupController? giftReceivedPopupController;
+        private GiftNotificationOpenerController? giftNotificationOpenerController;
 
         public GiftingPlugin(IAssetsProvisioner assetsProvisioner,
             IMVCManager mvcManager,
@@ -205,6 +206,8 @@ namespace DCL.PluginSystem.Global
             mvcManager.RegisterController(giftTransferStatusController);
             mvcManager.RegisterController(giftTransferSuccessController);
             mvcManager.RegisterController(giftReceivedPopupController);
+
+            giftNotificationOpenerController = new GiftNotificationOpenerController(mvcManager);
         }
 
         public void InjectToWorld(ref ArchSystemsWorldBuilder<Arch.Core.World> builder, in GlobalPluginArguments arguments) { }
