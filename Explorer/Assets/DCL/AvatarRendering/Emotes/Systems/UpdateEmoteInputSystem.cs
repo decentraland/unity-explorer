3f70b41a0095d1f95b45ba48b67a76b43d3c4acc
Nicolas Lorusso <nalorusso@gmail.com>
2026-04-10 13:25:55 -0300

    enable emotes on first person mode

--- Explorer/Assets/DCL/AvatarRendering/Emotes/Systems/UpdateEmoteInputSystem.cs
+++ Explorer/Assets/DCL/AvatarRendering/Emotes/Systems/UpdateEmoteInputSystem.cs
@@ -89,7 +89,7 @@ namespace DCL.AvatarRendering.Emotes
             in AvatarShapeComponent avatarShapeComponent,
             in GlideState glideState)
         {
-            if (inputModifier.DisableEmote || !avatarShapeComponent.IsVisible || glideState.Value != GlideStateValue.PROP_CLOSED) return;
+            if (inputModifier.DisableEmote /*|| !avatarShapeComponent.IsVisible*/ || glideState.Value != GlideStateValue.PROP_CLOSED) return;
 
             IReadOnlyList<URN> emotes = profile.Avatar.Emotes;
             if (emoteIndex < 0 || emoteIndex >= emotes.Count) return;
