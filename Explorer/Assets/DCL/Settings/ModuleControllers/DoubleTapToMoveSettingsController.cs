5db892e0c135dd91847eec20864672ec97c0b3ff
Alvaro Luque <olavra@live.com>
2026-04-10 18:02:20 +0200

    Added double tap setting, opt in by default

---
+++ Explorer/Assets/DCL/Settings/ModuleControllers/DoubleTapToMoveSettingsController.cs
@@ -0,0 +1,25 @@
+using DCL.Prefs;
+using DCL.Settings.ModuleViews;
+
+namespace DCL.Settings.ModuleControllers
+{
+    public class DoubleTapToMoveSettingsController : SettingsFeatureController
+    {
+        private readonly SettingsToggleModuleView view;
+
+        public DoubleTapToMoveSettingsController(SettingsToggleModuleView view)
+        {
+            this.view = view;
+
+            bool value = DCLPlayerPrefs.GetBool(DCLPrefKeys.SETTINGS_DOUBLE_TAP_TO_MOVE, false);
+            view.ToggleView.Toggle.SetIsOnWithoutNotify(value);
+            view.ToggleView.Toggle.onValueChanged.AddListener(OnToggleValueChanged);
+        }
+
+        public override void Dispose() =>
+            view.ToggleView.Toggle.onValueChanged.RemoveAllListeners();
+
+        private static void OnToggleValueChanged(bool isOn) =>
+            DCLPlayerPrefs.SetBool(DCLPrefKeys.SETTINGS_DOUBLE_TAP_TO_MOVE, isOn, true);
+    }
+}
