f318803b9eac32702745b7c85a8ffa9e945b0acf
Juanma Hidalgo <juanma06@gmail.com>
2026-05-05 18:11:36 +0200

    feat: expand auth funnel with is_new_account, profile_finalized, auth_completed and new_account_onboarding_started events

--- Explorer/Assets/DCL/PerformanceAndDiagnostics/Analytics/EventBased/AuthenticationScreenAnalytics.cs
+++ Explorer/Assets/DCL/PerformanceAndDiagnostics/Analytics/EventBased/AuthenticationScreenAnalytics.cs
@@ -19,6 +19,7 @@ namespace DCL.PerformanceAndDiagnostics.Analytics.EventBased
             controller.DiscordButtonClicked += OnDiscordButtonClicked;
             controller.OTPVerified += OnOTPVerified;
             controller.OTPResend += OnOTPResend;
+            controller.ProfileFinalized += OnProfileFinalized;
         }
 
         public void Dispose()
@@ -27,6 +28,7 @@ namespace DCL.PerformanceAndDiagnostics.Analytics.EventBased
             controller.DiscordButtonClicked -= OnDiscordButtonClicked;
             controller.OTPVerified -= OnOTPVerified;
             controller.OTPResend -= OnOTPResend;
+            controller.ProfileFinalized -= OnProfileFinalized;
         }
 
         private void OnAuthenticationScreenStateChanged(AuthStatus state)
@@ -56,24 +58,62 @@ namespace DCL.PerformanceAndDiagnostics.Analytics.EventBased
 
                 case AuthStatus.ProfileFetching:
                     analytics.Track(Authentication.PROFILE_FETCHING);
+                    // Auth flow finished and the engine is now resolving the profile. Pair this
+                    // with LOGIN_REQUESTED to measure the auth-attempt → auth-success conversion;
+                    // a LOGIN_REQUESTED with no AUTH_COMPLETED within a session = failed auth.
+                    analytics.Track(Authentication.AUTH_COMPLETED, new JObject
+                    {
+                        { "method", controller.CurrentLoginMethod.ToString() },
+                        { "is_cached", false },
+                    });
                     break;
                 case AuthStatus.LoggedIn: // Triggered WHEN the user gets in Lobby
                     analytics.Track(Authentication.LOGGED_IN, new JObject
                     {
                         { "method", controller.CurrentLoginMethod.ToString() },
+                        { "is_new_account", controller.IsCurrentlyNewAccount },
                     }, isInstant: true);
+                    if (controller.IsCurrentlyNewAccount)
+                    {
+                        analytics.Track(Authentication.NEW_ACCOUNT_ONBOARDING_STARTED, new JObject
+                        {
+                            { "method", controller.CurrentLoginMethod.ToString() },
+                        });
+                    }
                     break;
 
                 // CACHED FLOW - when the user is already logged in (has valid Identity)
                 case AuthStatus.ProfileFetchingCached:
                     analytics.Track(Authentication.PROFILE_FETCHING_CACHED);
+                    analytics.Track(Authentication.AUTH_COMPLETED, new JObject
+                    {
+                        { "method", controller.CurrentLoginMethod.ToString() },
+                        { "is_cached", true },
+                    });
                     break;
                 case AuthStatus.LoggedInCached: // Triggered WHEN the user gets in Lobby
-                    analytics.Track(Authentication.LOGGED_IN_CACHED, isInstant: true); break;
+                    analytics.Track(Authentication.LOGGED_IN_CACHED, new JObject
+                    {
+                        { "is_new_account", controller.IsCurrentlyNewAccount },
+                    }, isInstant: true);
+                    if (controller.IsCurrentlyNewAccount)
+                    {
+                        analytics.Track(Authentication.NEW_ACCOUNT_ONBOARDING_STARTED, new JObject
+                        {
+                            { "method", controller.CurrentLoginMethod.ToString() },
+                        });
+                    }
+                    break;
 
                 default: throw new ArgumentOutOfRangeException(nameof(state), state, null);
             }
         }
+
+        private void OnProfileFinalized() =>
+            analytics.Track(Authentication.PROFILE_FINALIZED, new JObject
+            {
+                { "method", controller.CurrentLoginMethod.ToString() },
+            });
 
         private void OnOTPVerified(string email, bool success)
         {
