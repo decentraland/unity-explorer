54ec518945e8faf6aa8361237f6144d3e5f8c785
nickkhalow <nickhalow@gmail.com>
2026-05-11 14:22:33 +0300

    disable locking for webgl

--- Explorer/Assets/DCL/Infrastructure/ECS/LifeCycle/Systems/LockECSSystem.cs
+++ Explorer/Assets/DCL/Infrastructure/ECS/LifeCycle/Systems/LockECSSystem.cs
@@ -7,6 +7,11 @@ using Utility.Multithreading;
 
 namespace ECS.LifeCycle.Systems
 {
+
+// MultiThreadSync is not required in WebGL because it's always the single threaded environment
+// Even if we use WebWorkers they still provide message passing to the main thread safely
+#if !UNITY_WEBGL
+
     /// <summary>
     ///     Locks ECS from modification while the whole cycle of the Unity systems is running
     /// </summary>
@@ -28,4 +33,6 @@ namespace ECS.LifeCycle.Systems
             boxedScope.Acquire(owner);
         }
     }
+#endif
+
 }
