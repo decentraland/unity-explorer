b9ea037c023f5a3bccdf067b603d0a3b19f3d549
nickkhalow <nickhalow@gmail.com>
2026-05-14 04:23:38 +0300

    stopwatch

--- Explorer/Assets/DCL/Infrastructure/Utility/ExitUtils.cs
+++ Explorer/Assets/DCL/Infrastructure/Utility/ExitUtils.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Diagnostics;
+using DCL.Diagnostics;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
@@ -7,17 +9,22 @@ namespace DCL.Utility
 {
     public static class ExitUtils
     {
-	    public static event Action BeforeApplicationQuitting;
+        public static event Action BeforeApplicationQuitting;
 
         public static void Exit()
         {
-	        BeforeApplicationQuitting?.Invoke();
+            var stopwatch = Stopwatch.StartNew();
+            ReportHub.LogProductionInfo($"[ExitUtils] Exit requested at {stopwatch.ElapsedMilliseconds}ms");
+
+            BeforeApplicationQuitting?.Invoke();
+            ReportHub.LogProductionInfo($"[ExitUtils] BeforeApplicationQuitting handlers finished at {stopwatch.ElapsedMilliseconds}ms");
+
 #if UNITY_EDITOR
             EditorApplication.isPlaying = false;
 #else
             UnityEngine.Application.Quit();
 #endif
+            ReportHub.LogProductionInfo($"[ExitUtils] Quit call dispatched at {stopwatch.ElapsedMilliseconds}ms");
         }
-
     }
 }
