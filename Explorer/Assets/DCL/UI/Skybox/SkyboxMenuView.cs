7c9c01955a81da9e0f05e93e5d11c335fb534f2d
Santi Andrade <sandrade@decentraland.org>
2026-05-18 13:36:16 +0200

    feat: add IPointerClickHandler to SkyboxMenuView for handling pointer click events

--- Explorer/Assets/DCL/UI/Skybox/SkyboxMenuView.cs
+++ Explorer/Assets/DCL/UI/Skybox/SkyboxMenuView.cs
@@ -1,11 +1,12 @@
 using MVC;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 namespace DCL.UI.Skybox
 {
-    public class SkyboxMenuView: ViewBaseWithAnimationElement, IView
+    public class SkyboxMenuView: ViewBaseWithAnimationElement, IView, IPointerClickHandler
     {
         [field: SerializeField] public Toggle TimeProgressionToggle { get; private set; } = null!;
         [field: SerializeField] public Slider TimeSlider { get; private set; } = null!;
@@ -13,5 +14,7 @@ namespace DCL.UI.Skybox
         [field: SerializeField] public CanvasGroup TimeProgressionGroup { get; private set; } = null!;
         [field: SerializeField] public CanvasGroup TopSliderGroup { get; private set; } = null!;
         [field: SerializeField] public CanvasGroup TextSliderGroup { get; private set; } = null!;
+
+        public void OnPointerClick(PointerEventData eventData) { }
     }
 }
