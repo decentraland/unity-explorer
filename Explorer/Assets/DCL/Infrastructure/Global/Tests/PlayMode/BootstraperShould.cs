2000a40a8462135fafcff3c53db043137dfb7b47
Pravus <pravusjif@gmail.com>
2026-05-19 14:36:57 +0200

    final tests correction

--- Explorer/Assets/DCL/Infrastructure/Global/Tests/PlayMode/BootstraperShould.cs
+++ Explorer/Assets/DCL/Infrastructure/Global/Tests/PlayMode/BootstraperShould.cs
@@ -6,7 +6,7 @@ using NUnit.Framework;
 using System.Threading;
 using System.Threading.Tasks;
 
-namespace Global.Tests.EditMode
+namespace Global.Tests.PlayMode
 {
     public class BootstraperShould
     {
