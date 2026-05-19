b42ac0fd460f6988783e1d3b77baf4c4d0f0aeb0
Nicolas Lorusso <nalorusso@gmail.com>
2026-05-19 12:28:07 -0300

    remove unnecessary comment

--- Explorer/Assets/DCL/Infrastructure/CrdtEcsBridge/JsModulesImplementation/Communications/SceneCommunicationPipe.cs
+++ Explorer/Assets/DCL/Infrastructure/CrdtEcsBridge/JsModulesImplementation/Communications/SceneCommunicationPipe.cs
@@ -89,7 +89,6 @@ namespace CrdtEcsBridge.JsModulesImplementation.Communications
 
             lock (sceneMessageHandlers)
             {
-                // Dont add but replace
                 // See: https://github.com/decentraland/unity-explorer/issues/8183
                 sceneMessageHandlers[key] = onSceneMessage;
             }
