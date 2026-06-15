dcfba8b92e743fb023de39db0a21d490ebf8f454
lorenzo-ranciaffi <41125365+lorenzo-ranciaffi@users.noreply.github.com>
2026-06-15 15:48:34 +0200

    fix: notifications panel empty state (#8991)

--- Explorer/Assets/DCL/Notifications/NotificationsMenu/NotificationsMenuView.cs
+++ Explorer/Assets/DCL/Notifications/NotificationsMenu/NotificationsMenuView.cs
@@ -1,9 +1,11 @@
 using DCL.Diagnostics;
 using MVC;
 using SuperScrollView;
+using System;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 namespace DCL.Notifications.NotificationsMenu
 {
@@ -24,11 +26,33 @@ namespace DCL.Notifications.NotificationsMenu
         [field: SerializeField]
         public GameObject ContentContainer { get; private set; } = null!;
 
+        [field: SerializeField]
+        public GameObject EmptyState { get; private set; } = null!;
+
+        [field: SerializeField]
+        private Button foundationCommunityButton { get; set; } = null!;
+
+        public event Action? FoundationCommunityButtonClicked;
+
+        private void Awake()
+        {
+            foundationCommunityButton.onClick.AddListener(OnFoundationCommunityButtonClick);
+        }
+
+        private void OnDestroy()
+        {
+            foundationCommunityButton.onClick.RemoveListener(OnFoundationCommunityButtonClick);
+        }
+
+        private void OnFoundationCommunityButtonClick() => FoundationCommunityButtonClicked?.Invoke();
+
         public void SetLoading(bool isLoading)
         {
             LoadingSpinner.SetActive(isLoading);
             ContentContainer.SetActive(!isLoading);
         }
+
+        public void ShowEmptyState(bool show) => EmptyState.SetActive(show);
 
         // Swallow the click event so it's not processed by the main sidebar button: retriggers -> cancel previous token -> panel stuck
         public void OnPointerClick(PointerEventData eventData)
