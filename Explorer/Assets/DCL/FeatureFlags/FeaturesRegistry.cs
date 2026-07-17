8584628c755893648a7677ff507d5731f917189e
davidejensen <davidejensen@live.it>
2026-07-17 09:50:54 +0200

    feat: allow buy from client with new credits shop (#9391)
    * feat: allow buy from client with new credits shop
    
    * Update Explorer/Assets/DCL/MarketplaceCredits/Purchase/Relay/CreditsManagerMetaTxRelayer.cs
    
    Co-authored-by: Muna <44584806+decentraland-bot@users.noreply.github.com>
    Signed-off-by: davidejensen <davidejensen@live.it>
    
    * Update Explorer/Assets/DCL/MarketplaceCredits/Purchase/Relay/PolygonSettlementPoller.cs
    
    Co-authored-by: Muna <44584806+decentraland-bot@users.noreply.github.com>
    Signed-off-by: davidejensen <davidejensen@live.it>
    
    * minor improvement
    
    * test fixes
    
    * Update Explorer/Assets/DCL/MarketplaceCredits/Purchase/CreditsPurchaseService.cs
    
    Co-authored-by: Muna <44584806+decentraland-bot@users.noreply.github.com>
    Signed-off-by: davidejensen <davidejensen@live.it>
    
    * Update Explorer/Assets/DCL/MarketplaceCredits/Purchase/CreditsPurchaseService.cs
    
    Co-authored-by: Muna <44584806+decentraland-bot@users.noreply.github.com>
    Signed-off-by: davidejensen <davidejensen@live.it>
    
    * Update Explorer/Assets/DCL/MarketplaceCredits/Purchase/Api/ShopListingDto.cs
    
    Co-authored-by: Muna <44584806+decentraland-bot@users.noreply.github.com>
    Signed-off-by: davidejensen <davidejensen@live.it>
    
    * Update Explorer/Assets/DCL/MarketplaceCredits/Purchase/Api/TradeDto.cs
    
    Co-authored-by: Muna <44584806+decentraland-bot@users.noreply.github.com>
    Signed-off-by: davidejensen <davidejensen@live.it>
    
    * Update Explorer/Assets/DCL/MarketplaceCredits/Purchase/CreditsPurchaseService.cs
    
    Co-authored-by: Muna <44584806+decentraland-bot@users.noreply.github.com>
    Signed-off-by: davidejensen <davidejensen@live.it>
    
    * Update Explorer/Assets/DCL/MarketplaceCredits/Purchase/UI/CreditPurchaseModalController.cs
    
    Co-authored-by: Muna <44584806+decentraland-bot@users.noreply.github.com>
    Signed-off-by: davidejensen <davidejensen@live.it>
    
    ---------
    
    Signed-off-by: davidejensen <davidejensen@live.it>
    Co-authored-by: Muna <44584806+decentraland-bot@users.noreply.github.com>

--- Explorer/Assets/DCL/FeatureFlags/FeaturesRegistry.cs
+++ Explorer/Assets/DCL/FeatureFlags/FeaturesRegistry.cs
@@ -41,6 +41,7 @@ namespace DCL.FeatureFlags
                 [FeatureId.CHAT_MESSAGE_BUFFER] = appArgs.ResolveFeatureFlagArg(AppArgsFlags.CHAT_MESSAGE_BUFFER, featureFlags.IsEnabled(FeatureFlagsStrings.CHAT_MESSAGE_BUFFER_CONFIG)),
                 [FeatureId.MARKETPLACE_CREDITS] = featureFlags.IsEnabled(FeatureFlagsStrings.MARKETPLACE_CREDITS),
                 [FeatureId.USER_CREDITS] = featureFlags.IsEnabled(FeatureFlagsStrings.USER_CREDITS),
+                [FeatureId.CREDITS_WEARABLE_PURCHASE] = featureFlags.IsEnabled(FeatureFlagsStrings.CREDITS_WEARABLE_PURCHASE),
                 [FeatureId.HEAD_SYNC] = appArgs.ResolveFeatureFlagArg(AppArgsFlags.HEAD_SYNC, featureFlags.IsEnabled(FeatureFlagsStrings.HEAD_SYNC) || isEditor),
                 [FeatureId.STOP_ON_DUPLICATE_IDENTITY] = appArgs.ResolveFeatureFlagArg(AppArgsFlags.STOP_ON_DUPLICATE_IDENTITY, featureFlags.IsEnabled(FeatureFlagsStrings.STOP_ON_DUPLICATE_IDENTITY)),
                 [FeatureId.PRIVATE_CHAT_REQUIRES_TOPIC] = appArgs.ResolveFeatureFlagArg(AppArgsFlags.PRIVATE_CHAT_REQUIRES_TOPIC, featureFlags.IsEnabled(FeatureFlagsStrings.PRIVATE_CHAT_REQUIRES_TOPIC)),
@@ -205,5 +206,6 @@ namespace DCL.FeatureFlags
         PULSE = 65,
         HARDWARE_FINGERPRINT = 66,
         USER_CREDITS = 67,
+        CREDITS_WEARABLE_PURCHASE = 68,
     }
 }
