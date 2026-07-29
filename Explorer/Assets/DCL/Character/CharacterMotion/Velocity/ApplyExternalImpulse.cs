5acd00c67d18867af3ddf275df0eb581ec0437fb
Vitaly Popuzin <popuzin@gmail.com>
2026-07-29 19:29:07 +0200

    removed reset on decending

--- Explorer/Assets/DCL/Character/CharacterMotion/Velocity/ApplyExternalImpulse.cs
+++ Explorer/Assets/DCL/Character/CharacterMotion/Velocity/ApplyExternalImpulse.cs
@@ -11,10 +11,6 @@ namespace DCL.Character.CharacterMotion
         // the scene can launch the character before our physics registers a grounded tick
         private const float JUMP_RESET_GROUND_DISTANCE = 1f;
 
-        // Minimum time between two jump-counter resets caused by a mid-air descending impulse,
-        // so a scene spamming impulses at a falling character can't restore jumps every tick
-        private const float DESCENDING_RESET_COOLDOWN = 1f;
-
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Execute(ICharacterControllerSettings settings, ref CharacterRigidTransform characterPhysics, ref JumpState jumpState, int physicsTick, float dt)
         {
@@ -29,17 +25,10 @@ namespace DCL.Character.CharacterMotion
 
             if (characterPhysics.ExternalImpulse.y > 0f)
             {
-                bool onOrNearGround = characterPhysics.IsGrounded || characterPhysics.GroundDistance <= JUMP_RESET_GROUND_DISTANCE;
-                bool descendingReset = (characterPhysics.GravityVelocity.y + characterPhysics.ExternalVelocity.y) < 0f &&
-                                       (physicsTick - jumpState.LastDescendingResetTick) * dt >= DESCENDING_RESET_COOLDOWN;
-
-                if (onOrNearGround || descendingReset)
+                if (characterPhysics.IsGrounded || characterPhysics.GroundDistance <= JUMP_RESET_GROUND_DISTANCE)
                 {
                     jumpState.JumpCount = 0;
                     jumpState.AirJumpDelay = float.MinValue;
-
-                    if (descendingReset)
-                        jumpState.LastDescendingResetTick = physicsTick;
                 }
 
                 characterPhysics.IsGrounded = false;
