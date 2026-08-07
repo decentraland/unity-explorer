8df6f62f90974f3dbb8f0db9b65e4b12f91f6dda
Esteban Ordano <esteban@decentraland.org>
2026-08-07 13:21:43 +0200

    opti(crdt-bridge): drop redundant lock around ArrayPool rent/return in SharedPoolsProvider
    ArrayPool<T> is already internally thread-safe; the extra per-call lock only added contention without protecting anything.

--- Explorer/Assets/DCL/Infrastructure/CrdtEcsBridge/PoolsProviders/SharedPoolsProvider.cs
+++ Explorer/Assets/DCL/Infrastructure/CrdtEcsBridge/PoolsProviders/SharedPoolsProvider.cs
@@ -7,12 +7,8 @@ namespace CrdtEcsBridge.PoolsProviders
 {
     public class SharedPoolsProvider : ISharedPoolsProvider
     {
-        // This is accessed rarely but the memory footprint is huge so share it between different scenes
-        // Must be synchronized
         private static readonly ArrayPool<ProcessedCRDTMessage> PROCESSED_CRDT_MESSAGES_POOL = ArrayPool<ProcessedCRDTMessage>.Create();
 
-        // This is accessed rarely but the memory footprint is huge so share it between different scenes
-        // Must be synchronized
         private static readonly ArrayPool<byte> SERIALIZED_STATE_BYTES_POOL = ArrayPool<byte>.Create();
 
         private static readonly ArrayPool<SDKObservableEvent> SDK_OBSERVABLE_EVENTS_POOL = ArrayPool<SDKObservableEvent>.Create();
@@ -26,34 +22,22 @@ namespace CrdtEcsBridge.PoolsProviders
             sdkObservableEventsPoolReleaseFuncCached = ReleaseSerializationSDKObservableEventsPool;
         }
 
-        public ProcessedCRDTMessage[] GetSerializationCrdtMessagesPool(int size)
-        {
-            lock (PROCESSED_CRDT_MESSAGES_POOL) { return PROCESSED_CRDT_MESSAGES_POOL.Rent(size); }
-        }
+        public ProcessedCRDTMessage[] GetSerializationCrdtMessagesPool(int size) =>
+            PROCESSED_CRDT_MESSAGES_POOL.Rent(size);
 
-        public void ReleaseSerializationCrdtMessagesPool(ProcessedCRDTMessage[] messages)
-        {
-            lock (PROCESSED_CRDT_MESSAGES_POOL) { PROCESSED_CRDT_MESSAGES_POOL.Return(messages); }
-        }
+        public void ReleaseSerializationCrdtMessagesPool(ProcessedCRDTMessage[] messages) =>
+            PROCESSED_CRDT_MESSAGES_POOL.Return(messages);
 
-        public PoolableByteArray GetSerializedStateBytesPool(int size)
-        {
-            lock (SERIALIZED_STATE_BYTES_POOL) { return new PoolableByteArray(SERIALIZED_STATE_BYTES_POOL.Rent(size), size, bytesPoolReleaseFuncCached); }
-        }
+        public PoolableByteArray GetSerializedStateBytesPool(int size) =>
+            new (SERIALIZED_STATE_BYTES_POOL.Rent(size), size, bytesPoolReleaseFuncCached);
 
-        public void ReleaseSerializedStateBytesPool(byte[] bytes)
-        {
-            lock (SERIALIZED_STATE_BYTES_POOL) { SERIALIZED_STATE_BYTES_POOL.Return(bytes, true); }
-        }
+        public void ReleaseSerializedStateBytesPool(byte[] bytes) =>
+            SERIALIZED_STATE_BYTES_POOL.Return(bytes, true);
 
-        public PoolableSDKObservableEventArray GetSerializationSDKObservableEventsPool(int size)
-        {
-            lock (SDK_OBSERVABLE_EVENTS_POOL) { return new PoolableSDKObservableEventArray(SDK_OBSERVABLE_EVENTS_POOL.Rent(size), size, sdkObservableEventsPoolReleaseFuncCached); }
-        }
+        public PoolableSDKObservableEventArray GetSerializationSDKObservableEventsPool(int size) =>
+            new (SDK_OBSERVABLE_EVENTS_POOL.Rent(size), size, sdkObservableEventsPoolReleaseFuncCached);
 
-        public void ReleaseSerializationSDKObservableEventsPool(SDKObservableEvent[] events)
-        {
-            lock (SDK_OBSERVABLE_EVENTS_POOL) { SDK_OBSERVABLE_EVENTS_POOL.Return(events, true); }
-        }
+        public void ReleaseSerializationSDKObservableEventsPool(SDKObservableEvent[] events) =>
+            SDK_OBSERVABLE_EVENTS_POOL.Return(events, true);
     }
 }
