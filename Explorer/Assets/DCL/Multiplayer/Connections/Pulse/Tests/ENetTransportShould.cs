9b2fb967488949a032cc0fd8fb4ffc18a6be5faa
lorenzo-ranciaffi <lorenzo.ranciaffi@decentraland.org>
2026-08-13 13:02:17 +0200

    fixes

--- Explorer/Assets/DCL/Multiplayer/Connections/Pulse/Tests/ENetTransportShould.cs
+++ Explorer/Assets/DCL/Multiplayer/Connections/Pulse/Tests/ENetTransportShould.cs
@@ -1,8 +1,5 @@
-using Cysharp.Threading.Tasks;
 using DCL.Multiplayer.Connections.Pulse.ENet;
 using NUnit.Framework;
-using System.Reflection;
-using System.Threading;
 
 namespace DCL.Multiplayer.Connections.Pulse.Tests
 {
@@ -12,28 +9,10 @@ namespace DCL.Multiplayer.Connections.Pulse.Tests
         [Test]
         public void NotThrowWhenTimeoutTeardownRunsAfterPeerWasCleared()
         {
+            // A fresh transport matches the state FinalizeHost() leaves behind: no peer, no host, loop inactive
             var transport = new ENetTransport(new ENetTransportOptions(), new MessagePipe());
 
-            // Simulate the listen loop having already run FinalizeHost() on a Disconnect/Timeout
-            // event: serverPeer and host are null while the connect timeout teardown fires.
-            SetField(transport, "lifeCycleCts", new CancellationTokenSource());
-            SetField(transport, "serverPeer", null!);
-            SetField(transport, "host", null!);
-            SetField(transport, "listenLoopIsActive", false);
-
-            MethodInfo forceDisconnect = typeof(ENetTransport)
-               .GetMethod("ForceDisconnectAsync", BindingFlags.Instance | BindingFlags.NonPublic);
-
-            Assert.DoesNotThrow(() =>
-            {
-                var task = (UniTask)forceDisconnect.Invoke(transport, null);
-                task.GetAwaiter().GetResult();
-            });
+            Assert.DoesNotThrow(() => transport.ForceDisconnectAsync().GetAwaiter().GetResult());
         }
-
-        private static void SetField(object target, string name, object value) =>
-            typeof(ENetTransport)
-               .GetField(name, BindingFlags.Instance | BindingFlags.NonPublic)
-               .SetValue(target, value);
     }
 }
