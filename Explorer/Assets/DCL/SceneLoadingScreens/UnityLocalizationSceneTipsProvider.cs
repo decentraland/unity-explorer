aef4d781b7cf13b91a360bc4e2fd4662a5756c05
lorenzo-ranciaffi <41125365+lorenzo-ranciaffi@users.noreply.github.com>
2026-08-21 15:11:52 +0200

    fix: bugsweep dataroom 3 — compounded fixes (24 PRs) (#9828)

--- Explorer/Assets/DCL/SceneLoadingScreens/UnityLocalizationSceneTipsProvider.cs
+++ Explorer/Assets/DCL/SceneLoadingScreens/UnityLocalizationSceneTipsProvider.cs
@@ -46,7 +46,7 @@ namespace DCL.SceneLoadingScreens
             fallbackTips = Get(tipsTable, imagesTable, ct);
         }
 
-        public async UniTask<SceneTips> GetAsync(CancellationToken ct) =>
+        public SceneTips Get() =>
 
             // TODO: we will need specific scene tips in the future, but its disabled at the moment
             /*StringTable tipsTable = await tipsDatabase.GetTableAsync($"LoadingSceneTips-{parcelCoord.x},{parcelCoord.y}").Task
