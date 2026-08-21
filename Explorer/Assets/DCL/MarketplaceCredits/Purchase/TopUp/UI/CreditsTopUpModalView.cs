ea62436a40e3384f2da53ec05f2fc692f25480fe
davidejensen <davidejensen@live.it>
2026-08-21 09:49:51 +0200

    Added animations for credits topup pack

--- Explorer/Assets/DCL/MarketplaceCredits/Purchase/TopUp/UI/CreditsTopUpModalView.cs
+++ Explorer/Assets/DCL/MarketplaceCredits/Purchase/TopUp/UI/CreditsTopUpModalView.cs
@@ -1,4 +1,7 @@
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
 using MVC;
+using System.Threading;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,6 +10,13 @@ namespace DCL.MarketplaceCredits.Purchase.TopUp.UI
 {
     public class CreditsTopUpModalView : ViewBase, IView
     {
+        private const float POPUP_SHOW_ANIMATION_TIME = 0.3f;
+        private const float POPUP_HIDE_ANIMATION_TIME = 0.2f;
+        private const float PACK_SHOW_ANIMATION_TIME = 0.25f;
+        private const float PACK_SHOW_ANIMATION_STAGGER = 0.08f;
+
+        [field: SerializeField] public RectTransform ContainerTransform { get; private set; } = null!;
+
         [field: Header("Chrome")]
         [field: SerializeField] public GameObject HeaderContainer { get; private set; } = null!;
 
@@ -32,5 +42,39 @@ namespace DCL.MarketplaceCredits.Purchase.TopUp.UI
         [field: SerializeField] public TMP_Text FailedReasonText { get; private set; } = null!;
         [field: SerializeField] public Button RetryButton { get; private set; } = null!;
         [field: SerializeField] public Button DoneButton { get; private set; } = null!;
+
+        public void AnimatePackItemsPopIn(int count)
+        {
+            for (var i = 0; i < PackItems.Length && i < count; i++)
+            {
+                CreditsTopUpPackItemView packItem = PackItems[i];
+                Transform packTransform = packItem.transform;
+
+                packTransform.DOKill();
+                packTransform.localScale = Vector3.zero;
+                packItem.gameObject.SetActive(false);
+
+                packTransform.DOScale(Vector3.one, PACK_SHOW_ANIMATION_TIME)
+                             .SetEase(Ease.OutBack)
+                             .SetDelay(i * PACK_SHOW_ANIMATION_STAGGER)
+                             .OnStart(() => packItem.gameObject.SetActive(true));
+            }
+        }
+
+        protected override UniTask PlayShowAnimationAsync(CancellationToken ct)
+        {
+            ContainerTransform.DOKill();
+            ContainerTransform.localScale = Vector3.zero;
+            return ContainerTransform.DOScale(Vector3.one, POPUP_SHOW_ANIMATION_TIME).SetEase(Ease.OutBack).ToUniTask(cancellationToken: ct);
+        }
+
+        protected override UniTask PlayHideAnimationAsync(CancellationToken ct)
+        {
+            foreach (CreditsTopUpPackItemView packItem in PackItems)
+                packItem.transform.DOKill();
+
+            ContainerTransform.DOKill();
+            return ContainerTransform.DOScale(Vector3.zero, POPUP_HIDE_ANIMATION_TIME).SetEase(Ease.InBack).ToUniTask(cancellationToken: ct);
+        }
     }
 }
