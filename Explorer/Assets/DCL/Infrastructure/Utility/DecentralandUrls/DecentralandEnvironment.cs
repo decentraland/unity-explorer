de7051c08fb7762c49f36dcb0aa9917bbbbf46db
Juan Ignacio Molteni <juanignaciomolteni@gmail.com>
2026-08-28 12:51:15 -0300

    Assign explicit values to DecentralandEnvironment enum
    Signed-off-by: Juan Ignacio Molteni <juanignaciomolteni@gmail.com>

--- Explorer/Assets/DCL/Infrastructure/Utility/DecentralandUrls/DecentralandEnvironment.cs
+++ Explorer/Assets/DCL/Infrastructure/Utility/DecentralandUrls/DecentralandEnvironment.cs
@@ -2,14 +2,14 @@ namespace DCL.Multiplayer.Connections.DecentralandUrls
 {
     public enum DecentralandEnvironment
     {
-        Org,
-        Zone,
+        Org = 0,
+        Zone = 1,
 
         /// <summary>
         ///     A deployment reachable under a base domain other than <c>decentraland.*</c>, selected with the
         ///     <c>--base-domain</c> app arg. New values must be appended: the enum is serialized by index on
         ///     <c>MainSceneLoader</c>.
         /// </summary>
-        Custom,
+        Custom = 2,
     }
 }
