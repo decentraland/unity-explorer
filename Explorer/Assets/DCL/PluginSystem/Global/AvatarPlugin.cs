8d84b960a8edb19ed8914c16ee81058d30217e84
Vitaly Popuzin <popuzin@gmail.com>
2026-09-01 15:29:27 +0200

    removed Debug system

--- Explorer/Assets/DCL/PluginSystem/Global/AvatarPlugin.cs
+++ Explorer/Assets/DCL/PluginSystem/Global/AvatarPlugin.cs
@@ -202,7 +202,6 @@ namespace DCL.PluginSystem.Global
 
             //Debug scripts
             InstantiateRandomAvatarsSystem.InjectToWorld(ref builder, debugContainerBuilder, realmData, transformPoolRegistry, avatarRandomizerAsset);
-            DebugSceneAvatarTagSystem.InjectToWorld(ref builder, debugContainerBuilder);
 #if UNITY_EDITOR
             PlayableDirectorUpdatingSystem.InjectToWorld(ref builder);
 #endif
