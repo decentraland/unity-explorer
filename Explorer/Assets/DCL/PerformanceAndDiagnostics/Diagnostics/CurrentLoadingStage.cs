8dc2bd39de7ea8afb75f0fafccbcc0e9f1e4f0bb
Nicolas Lorusso <nalorusso@gmail.com>
2026-09-04 12:24:59 -0300

    fix test

--- Explorer/Assets/DCL/PerformanceAndDiagnostics/Diagnostics/CurrentLoadingStage.cs
+++ Explorer/Assets/DCL/PerformanceAndDiagnostics/Diagnostics/CurrentLoadingStage.cs
@@ -1,18 +1,14 @@
-using System.Threading;
-
 namespace DCL.Diagnostics
 {
     public static class CurrentLoadingStage
     {
         public const string UNKNOWN = "Unknown";
 
-        private static string current = UNKNOWN;
-
-        public static string Value => Volatile.Read(ref current);
+        public static string Value { get; private set; } = UNKNOWN;
 
         public static void Set(string stage)
         {
-            Volatile.Write(ref current, stage);
+            Value = stage;
         }
     }
 }
