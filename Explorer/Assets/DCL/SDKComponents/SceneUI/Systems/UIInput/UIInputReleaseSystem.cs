8774df37f5c78b8785feeae6adc5baa1f155669e
Vitaly Popuzin <popuzin@gmail.com>
2026-09-23 22:51:45 +0200

    compilation fix - imported using

--- Explorer/Assets/DCL/SDKComponents/SceneUI/Systems/UIInput/UIInputReleaseSystem.cs
+++ Explorer/Assets/DCL/SDKComponents/SceneUI/Systems/UIInput/UIInputReleaseSystem.cs
@@ -7,6 +7,7 @@ using DCL.ECSComponents;
 using DCL.Optimization.Pools;
 using DCL.SDKComponents.SceneUI.Components;
 using DCL.SDKComponents.SceneUI.Groups;
+using DCL.SDKComponents.SceneUI.Utils;
 using ECS.Abstract;
 using ECS.Groups;
 using ECS.LifeCycle;
